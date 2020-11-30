12718970547ed56af4bbe570ce22a4e45efa3601
Shiyo <Shiyo@CELTY>
2020-11-30 04:50:46 -0500

    Doublet Set
    		Headgear
    				+1 Attk
    		Doublet
    				+3 Healing Magic Skill
    		Gloves
    				+1 Macc
    		Brais
    				+1 Attk
    		Gaiters
    				+5 MP

--- sql/item_mods.sql
+++ sql/item_mods.sql
@@ -8281,7 +8281,8 @@ INSERT INTO `item_mods` VALUES (12461,54,8);
 INSERT INTO `item_mods` VALUES (12461,242,3);
 INSERT INTO `item_mods` VALUES (12462,1,14);
 INSERT INTO `item_mods` VALUES (12463,1,3);
-INSERT INTO `item_mods` VALUES (12464,1,5);
+INSERT INTO `item_mods` VALUES (12464,1,5);		-- Headgear
+INSERT INTO `item_mods` VALUES (12464,23,1);	-- +1 Attk	
 INSERT INTO `item_mods` VALUES (12465,1,9);
 INSERT INTO `item_mods` VALUES (12466,1,12);
 INSERT INTO `item_mods` VALUES (12467,1,16);
@@ -8620,7 +8621,8 @@ INSERT INTO `item_mods` VALUES (12589,241,2);
 INSERT INTO `item_mods` VALUES (12590,1,12);
 INSERT INTO `item_mods` VALUES (12590,8,1);
 INSERT INTO `item_mods` VALUES (12591,1,11);
-INSERT INTO `item_mods` VALUES (12592,1,10);
+INSERT INTO `item_mods` VALUES (12592,1,10);	-- Doublet
+INSERT INTO `item_mods` VALUES (12592,112,3);	-- +3 Healing Magic Skill
 INSERT INTO `item_mods` VALUES (12593,1,17);
 INSERT INTO `item_mods` VALUES (12594,1,23);
 INSERT INTO `item_mods` VALUES (12595,1,31);
@@ -8954,7 +8956,8 @@ INSERT INTO `item_mods` VALUES (12717,54,6);
 INSERT INTO `item_mods` VALUES (12717,244,3);
 INSERT INTO `item_mods` VALUES (12718,1,9);
 INSERT INTO `item_mods` VALUES (12719,1,5);
-INSERT INTO `item_mods` VALUES (12720,1,3);
+INSERT INTO `item_mods` VALUES (12720,1,3);		-- Gloves
+INSERT INTO `item_mods` VALUES (12720,30,1);	-- +1 Magic Acc	
 INSERT INTO `item_mods` VALUES (12721,1,6);
 INSERT INTO `item_mods` VALUES (12722,1,8);
 INSERT INTO `item_mods` VALUES (12723,1,10);
@@ -9240,7 +9243,8 @@ INSERT INTO `item_mods` VALUES (12847,2,25);      -- HP +25
 INSERT INTO `item_mods` VALUES (12847,12,2);      -- INT +2
 INSERT INTO `item_mods` VALUES (12847,27,2);      -- Enmity +2
 INSERT INTO `item_mods` VALUES (12847,168,2);     -- Spell interruption rate down 2%
-INSERT INTO `item_mods` VALUES (12848,1,7);
+INSERT INTO `item_mods` VALUES (12848,1,7);		-- Brais
+INSERT INTO `item_mods` VALUES (12848,23,1);	-- +1 Attk	
 INSERT INTO `item_mods` VALUES (12849,1,12);
 INSERT INTO `item_mods` VALUES (12850,1,17);
 INSERT INTO `item_mods` VALUES (12851,1,22);
@@ -9529,7 +9533,8 @@ INSERT INTO `item_mods` VALUES (12973,13,2);
 INSERT INTO `item_mods` VALUES (12974,1,9);
 INSERT INTO `item_mods` VALUES (12975,1,4);
 INSERT INTO `item_mods` VALUES (12975,57,6);
-INSERT INTO `item_mods` VALUES (12976,1,3);
+INSERT INTO `item_mods` VALUES (12976,1,3);		-- Gaiters
+INSERT INTO `item_mods` VALUES (12976,5,5);		-- +5 MP	
 INSERT INTO `item_mods` VALUES (12977,1,5);
 INSERT INTO `item_mods` VALUES (12978,1,7);
 INSERT INTO `item_mods` VALUES (12979,1,9);
