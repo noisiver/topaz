25062aa53d8aaeff8fc02764587ed8625604baed
Shiyo <Shiyo@CELTY>
2020-11-30 17:19:34 -0500

    choke on enaero 2 experiment

--- scripts/globals/effects/enaero_ii.lua
+++ scripts/globals/effects/enaero_ii.lua
@@ -9,6 +9,8 @@ require("scripts/globals/status")
 function onEffectGain(target, effect)
     target:addMod(tpz.mod.ENSPELL, tpz.magic.element.WIND + 8) -- Tier IIs have higher "enspell IDs"
     target:addMod(tpz.mod.ENSPELL_DMG, effect:getPower())
+    target:addMod(tpz.mod.REGEN_DOWN, effect:getPower())        -- choke debuff
+    target:addMod(tpz.mod.VIT, -getElementalDebuffStatDownFromDOT(effect:getPower()))       -- choke debuff
 end
 
 function onEffectTick(target, effect)
@@ -17,4 +19,6 @@ end
 function onEffectLose(target, effect)
     target:setMod(tpz.mod.ENSPELL_DMG, 0)
     target:setMod(tpz.mod.ENSPELL, 0)
+    target:delMod(tpz.mod.REGEN_DOWN, effect:getPower())        --choke debuff
+    target:delMod(tpz.mod.VIT, -getElementalDebuffStatDownFromDOT(effect:getPower()))       --choke debuff
 end
