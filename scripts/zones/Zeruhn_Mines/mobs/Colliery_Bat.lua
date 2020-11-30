c76213ff4cad76839dd1ffa0cff6741f8e55e938
Shiyo <Shiyo@CELTY>
2020-11-29 22:15:19 -0500

    Update Colliery_Bat.lua

--- scripts/zones/Zeruhn_Mines/mobs/Colliery_Bat.lua
+++ scripts/zones/Zeruhn_Mines/mobs/Colliery_Bat.lua
@@ -6,7 +6,7 @@ require("scripts/globals/regimes")
 -----------------------------------
 
 function onMobDeath(mob, player, isKiller)
-    tpz.regime.checkRegime(player, mob, 628, 1, tpz.regime.type.GROUNDS)
-    tpz.regime.checkRegime(player, mob, 629, 1, tpz.regime.type.GROUNDS)
-    tpz.regime.checkRegime(player, mob, 630, 1, tpz.regime.type.GROUNDS)
+    tpz.regime.checkRegime(player, mob, 626, 1, tpz.regime.type.GROUNDS)
+    tpz.regime.checkRegime(player, mob, 626, 1, tpz.regime.type.GROUNDS)
+    tpz.regime.checkRegime(player, mob, 636, 1, tpz.regime.type.GROUNDS)
 end
