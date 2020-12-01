a9c71ee2675ae7a6ccb5819a1719c6c2d04de3b6
Shiyo <Shiyo@CELTY>
2020-12-01 18:07:28 -0500

    120s

--- scripts/globals/mobskills/sheep_song.lua
+++ scripts/globals/mobskills/sheep_song.lua
@@ -14,7 +14,7 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.SLEEP_I
 
-    skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, 1, 0, 40))
+    skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, 1, 0, 120))
 
     return typeEffect
 end
