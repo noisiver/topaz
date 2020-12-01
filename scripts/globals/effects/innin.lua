668e0271591edc19abae525d9833e71cccbbd028
Shiyo <Shiyo@CELTY>
2020-12-01 05:30:07 -0500

    innin yonin
    remove decay from both and remove acc penalty from yonin

--- scripts/globals/effects/innin.lua
+++ scripts/globals/effects/innin.lua
@@ -11,15 +11,15 @@ function onEffectGain(target, effect) --power=30 initially, subpower=20 for enmi
     target:addMod(tpz.mod.ENMITY, -effect:getSubPower())
 end
 
-function onEffectTick(target, effect)
+--function onEffectTick(target, effect)
     --tick down the effect and reduce the overall power
-    effect:setPower(effect:getPower()-1)
-    target:delMod(tpz.mod.EVA, -1)
-    if (effect:getPower() % 2 == 0) then -- enmity- decays from -20 to -10, so half as often as the rest.
-        effect:setSubPower(effect:getSubPower()-1)
-        target:delMod(tpz.mod.ENMITY, -1)
-    end
-end
+ --   effect:setPower(effect:getPower()-1)
+ --   target:delMod(tpz.mod.EVA, -1)
+  --  if (effect:getPower() % 2 == 0) then -- enmity- decays from -20 to -10, so half as often as the rest.
+  --      effect:setSubPower(effect:getSubPower()-1)
+   --     target:delMod(tpz.mod.ENMITY, -1)
+   -- end
+--end
 
 function onEffectLose(target, effect)
     --remove the remaining power
