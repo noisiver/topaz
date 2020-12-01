dc080b11f39e98c7e82001383c2a3ccad26a8b76
Shiyo <Shiyo@CELTY>
2020-12-01 05:35:33 -0500

    animated flourish - > provoke

--- scripts/globals/abilities/animated_flourish.lua
+++ scripts/globals/abilities/animated_flourish.lua
@@ -5,46 +5,62 @@
 -- Finishing Moves Used: 1-2
 -- Recast Time: 00:30
 -----------------------------------
-require("scripts/globals/settings")
-require("scripts/globals/status")
-require("scripts/globals/msg")
+--require("scripts/globals/settings")
+--require("scripts/globals/status")
+--require("scripts/globals/msg")
 -----------------------------------
 
-function onAbilityCheck(player, target, ability)
-    if
-        player:hasStatusEffect(tpz.effect.FINISHING_MOVE_1) or
-        player:hasStatusEffect(tpz.effect.FINISHING_MOVE_2) or
-        player:hasStatusEffect(tpz.effect.FINISHING_MOVE_3) or
-        player:hasStatusEffect(tpz.effect.FINISHING_MOVE_4) or
-        player:hasStatusEffect(tpz.effect.FINISHING_MOVE_5)
-    then
-        return 0, 0
-    else
-        return tpz.msg.basic.NO_FINISHINGMOVES, 0
-    end
-end
+--function onAbilityCheck(player, target, ability)
+  --  if
+   --     player:hasStatusEffect(tpz.effect.FINISHING_MOVE_1) or
+     --   player:hasStatusEffect(tpz.effect.FINISHING_MOVE_2) or
+       -- player:hasStatusEffect(tpz.effect.FINISHING_MOVE_3) or
+        --player:hasStatusEffect(tpz.effect.FINISHING_MOVE_4) or
+        --player:hasStatusEffect(tpz.effect.FINISHING_MOVE_5)
+   -- then
+      --  return 0, 0
+   -- else
+      --  return tpz.msg.basic.NO_FINISHINGMOVES, 0
+   -- end
+--end
 
-function onUseAbility(player, target, ability)
-    if (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_1)) then
-        player:delStatusEffect(tpz.effect.FINISHING_MOVE_1)
-
+--function onUseAbility(player, target, ability)
+ --   if (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_1)) then
+   --     player:delStatusEffect(tpz.effect.FINISHING_MOVE_1)
+--
     --Add extra enmity if 2 finishing moves are used
-    elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_2)) then
-        player:delStatusEffect(tpz.effect.FINISHING_MOVE_2)
-        target:addEnmity(player, 0, 500)
-    elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_3)) then
-        player:delStatusEffectSilent(tpz.effect.FINISHING_MOVE_3)
-        player:addStatusEffect(tpz.effect.FINISHING_MOVE_1, 1, 0, 7200)
-        target:addEnmity(player, 0, 500)
+  --  elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_2)) then
+  --      player:delStatusEffect(tpz.effect.FINISHING_MOVE_2)
+ --       target:addEnmity(player, 0, 500)
+ --   elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_3)) then
+--        player:delStatusEffectSilent(tpz.effect.FINISHING_MOVE_3)
+--        player:addStatusEffect(tpz.effect.FINISHING_MOVE_1, 1, 0, 7200)
+ --       target:addEnmity(player, 0, 500)
 
-    elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_4)) then
-        player:delStatusEffectSilent(tpz.effect.FINISHING_MOVE_4)
-        player:addStatusEffect(tpz.effect.FINISHING_MOVE_2, 1, 0, 7200)
-        target:addEnmity(player, 0, 500)
+  --  elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_4)) then
+ --       player:delStatusEffectSilent(tpz.effect.FINISHING_MOVE_4)
+ --       player:addStatusEffect(tpz.effect.FINISHING_MOVE_2, 1, 0, 7200)
+  --      target:addEnmity(player, 0, 500)
 
-    elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_5)) then
-        player:delStatusEffectSilent(tpz.effect.FINISHING_MOVE_5)
-        player:addStatusEffect(tpz.effect.FINISHING_MOVE_3, 1, 0, 7200)
-        target:addEnmity(player, 0, 500)
-    end
+  --  elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_5)) then
+  --      player:delStatusEffectSilent(tpz.effect.FINISHING_MOVE_5)
+  --      player:addStatusEffect(tpz.effect.FINISHING_MOVE_3, 1, 0, 7200)
+ --       target:addEnmity(player, 0, 500)
+ --   end
+--end
+
+-----------------------------------
+--
+--
+--
+-----------------------------------
+
+function onEffectGain(target, effect)
 end
+
+function onEffectTick(target, effect)
+end
+
+function onEffectLose(target, effect)
+end
+
