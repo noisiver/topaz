2c1c3e62f1e0b6a91056ef485d419cfdf09efc7d
Shiyo <Shiyo@CELTY>
2020-12-01 18:16:36 -0500

    Update sheep_song.lua

--- scripts/globals/mobskills/sheep_song.lua
+++ scripts/globals/mobskills/sheep_song.lua
@@ -6,18 +6,45 @@ require("scripts/globals/monstertpmoves")
 require("scripts/globals/settings")
 require("scripts/globals/status")
 require("scripts/globals/magic")
+require("scripts/globals/msg")
 ---------------------------------------------
 
 function onMobSkillCheck(target, mob, skill)
     return 0
 end
 
-function onMobWeaponSkill(target, mob, skill)
+--function onMobWeaponSkill(target, mob, skill)
+  --  local typeEffect = tpz.effect.SLEEP_I
+
+    --skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, 1, 0, duration))
+      -- local duration = calculateDuration(60, spell:getSkillType(), spell:getSpellGroup(), caster, target)
+
+
+--    return typeEffect
+--end
+
+
+function onMobWeaponSkillt(target, mob, skill)
     local typeEffect = tpz.effect.SLEEP_I
+    local dINT = (caster:getStat(tpz.mod.INT) - target:getStat(tpz.mod.INT))
+    local params = {}
+    params.diff = dINT
+    params.attribute = tpz.mod.INT
+    params.skillType = tpz.skill.BLUE_MAGIC
+    params.bonus = 0
+    params.effect = typeEffect
+    local resist = applyResistanceEffect(caster, target, spell, params)
+    local duration = 60 * resist
 
-    skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, 1, 0, duration))
-       local duration = calculateDuration(60, spell:getSkillType(), spell:getSpellGroup(), caster, target)
-
+    if (resist > 0.5) then -- Do it!
+        if (target:addStatusEffect(typeEffect, 1, 0, duration)) then
+            spell:setMsg(tpz.msg.basic.MAGIC_ENFEEB_IS)
+        else
+            spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT)
+        end
+    else
+        spell:setMsg(tpz.msg.basic.MAGIC_RESIST)
+    end
 
     return typeEffect
 end
