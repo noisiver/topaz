7577818938c77707007a66182045ac7e58a486c0
Shiyo <Shiyo@CELTY>
2020-12-01 14:58:03 -0500

    changed bokuto to para

--- scripts/globals/items/bokuto.lua
+++ scripts/globals/items/bokuto.lua
@@ -10,10 +10,10 @@ require("scripts/globals/msg")
 function onAdditionalEffect(player, target, damage)
     local chance = 10
 
-    if (math.random(0, 99) >= chance or applyResistanceAddEffect(player, target, tpz.magic.ele.DARK, 0) <= 0.5) then
+    if (math.random(0, 99) >= chance or applyResistanceAddEffect(player, target, tpz.magic.ele.ICE, 0) <= 0.5) then -- changed to ice element
         return 0, 0, 0
     else
-        target:addStatusEffect(tpz.effect.BLINDNESS, 10, 0, 30)
-        return tpz.subEffect.BLIND, tpz.msg.basic.ADD_EFFECT_STATUS, tpz.effect.BLINDNESS
+        target:addStatusEffect(tpz.effect.PARALYSIS, 10, 0, 10) -- added paralysis and changed potency to 10
+        return tpz.subEffect.BLIND, tpz.msg.basic.ADD_EFFECT_STATUS, tpz.effect.PARALYSIS -- added paealysis
     end
 end
--- scripts/globals/items/bokuto_+1.lua
+++ scripts/globals/items/bokuto_+1.lua
@@ -8,12 +8,12 @@ require("scripts/globals/magic")
 require("scripts/globals/msg")
 -----------------------------------
 function onAdditionalEffect(player, target, damage)
-    local chance = 15
+    local chance = 10
 
-    if (math.random(0, 99) >= chance or applyResistanceAddEffect(player, target, tpz.magic.ele.DARK, 0) <= 0.5) then
+    if (math.random(0, 99) >= chance or applyResistanceAddEffect(player, target, tpz.magic.ele.ICE, 0) <= 0.5) then -- changed to ice element
         return 0, 0, 0
     else
-        target:addStatusEffect(tpz.effect.BLINDNESS, 10, 0, 30)
-        return tpz.subEffect.BLIND, tpz.msg.basic.ADD_EFFECT_STATUS, tpz.effect.BLINDNESS
+        target:addStatusEffect(tpz.effect.PARALYSIS, 10, 0, 10) -- added paralysis and changed potency to 10
+        return tpz.subEffect.BLIND, tpz.msg.basic.ADD_EFFECT_STATUS, tpz.effect.PARALYSIS -- added paealysis
     end
 end
