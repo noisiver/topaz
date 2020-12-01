054770b3db71529d0016b233d459348f5346d837
Shiyo <Shiyo@CELTY>
2020-12-01 18:03:07 -0500

    reverted

--- scripts/globals/mobskills/sheep_song.lua
+++ scripts/globals/mobskills/sheep_song.lua
@@ -2,30 +2,7 @@
 -- Sheep Song
 -- 15' AoE sleep
 ---------------------------------------------
---require("scripts/globals/monstertpmoves")
---require("scripts/globals/settings")
---require("scripts/globals/status")
----------------------------------------------
-
---function onMobSkillCheck(target, mob, skill)
-   -- return 0
---end
-
---function onMobWeaponSkill(target, mob, skill)
-   -- local typeEffect = tpz.effect.SLEEP_I
-
-    --skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, 1, 0, 20))
-
-   -- return typeEffect
---end
-
-
----------------------------------------------
--- Sheep Song
--- 15' AoE sleep
----------------------------------------------
 require("scripts/globals/monstertpmoves")
-require("scripts/globals/magic")
 require("scripts/globals/settings")
 require("scripts/globals/status")
 ---------------------------------------------
@@ -34,30 +11,10 @@ function onMobSkillCheck(target, mob, skill)
     return 0
 end
 
-
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.SLEEP_I
-    local dINT = (caster:getStat(tpz.mod.INT) - target:getStat(tpz.mod.INT))
-    local params = {}
-    params.diff = dINT
-    params.attribute = tpz.mod.INT
-    params.skillType = tpz.skill.BLUE_MAGIC
-    params.bonus = 0
-    params.effect = typeEffect
-    local resist = applyResistanceEffect(caster, target, spell, params)
-    local duration = 180 * resist
 
-    if (resist > 0.5) then -- Do it!
-        if (target:addStatusEffect(typeEffect, 1, 0, duration)) then   --experimental
-            spell:setMsg(tpz.msg.basic.MAGIC_ENFEEB_IS)
-        else
-            spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT)
-        end
-    else
-        spell:setMsg(tpz.msg.basic.MAGIC_RESIST)
-    end
+    skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, 1, 0, 40))
 
     return typeEffect
 end
-
-
