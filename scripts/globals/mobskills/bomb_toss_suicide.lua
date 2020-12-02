6873d5b2ffc78ca21c8a252624fbbb980d617e4d
Shiyo <Shiyo@CELTY>
2020-12-01 20:26:47 -0500

    Update bomb_toss_suicide.lua

--- scripts/globals/mobskills/bomb_toss_suicide.lua
+++ scripts/globals/mobskills/bomb_toss_suicide.lua
@@ -18,12 +18,12 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local dmgmod = 1
-    local BOMB_TOSS_HPP = skill:getMobHPP() / 100
+    local BOMB_TOSS_HPP = skill:getMobHPP() / 200
 
     local job = mob:getMainJob()
     local power = math.random(12, 18)
 
-    local info = MobMagicalMove(mob, target, skill, BOMB_TOSS_HPP, tpz.magic.ele.FIRE, dmgmod, TP_MAB_BONUS, 1)
+    local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*power*BOMB_TOSS_HPP, tpz.magic.ele.FIRE, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.FIRE, MOBPARAM_IGNORE_SHADOWS)
 
     mob:setHP(0)
