a592ba134ca5192c612599a25caa3e211993f90b
Shiyo <Shiyo@CELTY>
2020-12-04 18:59:39 -0500

    Update paralyze.lua

--- scripts/globals/spells/paralyze.lua
+++ scripts/globals/spells/paralyze.lua
@@ -33,7 +33,7 @@ function onSpellCast(caster, target, spell)
     params.effect = tpz.effect.PARALYSIS
     local resist = applyResistanceEffect(caster, target, spell, params)
 
-    if resist >= 0.25 then -- effect taken  changed from 0.5
+    if resist >= 0.5 then -- effect taken  changed from 0.5
         if target:addStatusEffect(params.effect, power, 3, duration * resist) then
             spell:setMsg(tpz.msg.basic.MAGIC_ENFEEB_IS)
         else
