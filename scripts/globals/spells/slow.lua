ccb80c0624e24387d1ce2b762359326fe74102f1
Shiyo <Shiyo@CELTY>
2020-12-04 22:30:02 -0500

    added 1/4th resist slow para

--- scripts/globals/spells/slow.lua
+++ scripts/globals/spells/slow.lua
@@ -30,7 +30,7 @@ function onSpellCast(caster, target, spell)
     params.effect = tpz.effect.SLOW
     local resist = applyResistanceEffect(caster, target, spell, params)
 
-    if resist >= 0.5 then -- effect taken
+    if resist >= 0.25 then -- effect taken
         if target:addStatusEffect(params.effect, power, 3, duration * resist) then
             spell:setMsg(tpz.msg.basic.MAGIC_ENFEEB_IS)
         else
