29d68e8cf185b399d174d070cc119984b55db6a1
Shiyo <Shiyo@CELTY>
2020-12-04 19:32:03 -0500

    Update paralyze.lua

--- scripts/globals/spells/paralyze.lua
+++ scripts/globals/spells/paralyze.lua
@@ -14,11 +14,11 @@ end
 
 function onSpellCast(caster, target, spell)
     local dMND = caster:getStat(tpz.mod.MND) - target:getStat(tpz.mod.MND)
-    --local MND = caster:getStat(tpz.mod.MND) -- added
+    local MND = caster:getStat(tpz.mod.MND) -- added
 
     -- Calculate potency.
-    local potency = utils.clamp(math.floor(dMND / 4) + 15, 5, 25)
-    --local potency = utils.clamp(math.floor(2 * (MND+dMND)) / 10, 5, 25)
+    ---local potency = utils.clamp(math.floor(dMND / 4) + 15, 5, 25)
+    local potency = utils.clamp(math.floor(2 * (MND+dMND)) / 100, 5, 25)
     potency = calculatePotency(potency, spell:getSkillType(), caster, target)
 
     -- Calculate duration.
