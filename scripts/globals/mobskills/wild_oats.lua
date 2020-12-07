7093eee358e64352f8d892476567f1d91be94c68
Shiyo <Shiyo@CELTY>
2020-12-07 01:20:18 -0500

    Update wild_oats.lua

--- scripts/globals/mobskills/wild_oats.lua
+++ scripts/globals/mobskills/wild_oats.lua
@@ -17,8 +17,9 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.VIT_DOWN
+    local power = math.random(1, 10)    -- added
 
-    skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, 10, 3, 120))
+    skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, power, 3, 120))   -- changed from 10 to power
 
     return typeEffect
 end
