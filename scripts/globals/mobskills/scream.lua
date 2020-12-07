e40bba1fd975a3cbb6d9f96d1bfd54a4ec429b74
Shiyo <Shiyo@CELTY>
2020-12-07 02:31:27 -0500

    Update scream.lua

--- scripts/globals/mobskills/scream.lua
+++ scripts/globals/mobskills/scream.lua
@@ -13,7 +13,8 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.MND_DOWN
-    skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, 10, 3, 120))
+    local power = math.random(1, 10)  
+    skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, power, 3, 120))
 
     return typeEffect
 end
