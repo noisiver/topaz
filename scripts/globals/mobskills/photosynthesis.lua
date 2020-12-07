fce51056da7bc7770326a9eb87acc004dd8f8ec4
Shiyo <Shiyo@CELTY>
2020-12-07 01:01:54 -0500

    photosytnthesis nerf

--- scripts/globals/mobskills/photosynthesis.lua
+++ scripts/globals/mobskills/photosynthesis.lua
@@ -22,7 +22,7 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local power = mob:getMainLvl()/10 * 4 + 5
+    local power = mob:getMainLvl()/10 * 2 + 5 -- changed from  *4 
     local duration = 30
 
     local typeEffect = tpz.effect.REGEN
