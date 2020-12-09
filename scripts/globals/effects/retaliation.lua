8816b24651aaf3abe9bcd9c33afd2e8c89dc0e6e
Shiyo <Shiyo@CELTY>
2020-12-09 02:24:58 -0500

    retal change

--- scripts/globals/effects/retaliation.lua
+++ scripts/globals/effects/retaliation.lua
@@ -8,6 +8,9 @@ require("scripts/globals/status")
 
 function onEffectGain(target, effect)
     target:addMod(tpz.mod.MOVE, -8)
+    target:addMod(tpz.mod.ENMITY, +10)
+    target:delMod(tpz.mod.STORETP, -10)
+   
 end
 
 function onEffectTick(target, effect)
@@ -15,4 +18,8 @@ end
 
 function onEffectLose(target, effect)
     target:delMod(tpz.mod.MOVE, -8)
+    target:delMod(tpz.mod.ENMITY, +10)
+    target:delMod(tpz.mod.STORETP, -10)
+
+
 end
