f0247cf1b1e11eb003d9e2a92c9a64b6bd43caf0
Shiyo <Shiyo@CELTY>
2020-12-10 22:59:05 -0500

    magic shop

--- scripts/zones/Windurst_Waters/npcs/Hilkomu-Makimu.lua
+++ scripts/zones/Windurst_Waters/npcs/Hilkomu-Makimu.lua
@@ -4,7 +4,7 @@
 -- Standard Merchant NPC
 -- Confirmed shop stock, August 2013
 -----------------------------------
-require("scripts/globals/shop")
+require("scripts/globals/shop") 
 local ID = require("scripts/zones/Windurst_Waters/IDs")
 -----------------------------------
 
@@ -15,17 +15,17 @@ function onTrigger(player, npc)
     player:showText(npc, ID.text.HIKOMUMAKIMU_SHOP_DIALOG)
 
     stock = {
-        4829, 23184, 1,     --Scroll of Poison II
-        4839, 12880, 1,     --Scroll of Bio II
-        4833,  4747, 1,     --Scroll of Poisonga
+        4829, 23184, 3,     --Scroll of Poison II
+        4839, 12880, 3,     --Scroll of Bio II
+        4833,  4747, 3,     --Scroll of Poisonga
 
-        4797,  1191, 2,     --Scroll of Stonega
-        4807,  2143, 2,     --Scroll of Waterga
-        4792,  4239, 2,     --Scroll of Aeroga
-        4782,  7181, 2,     --Scroll of Firaga
-        4787, 10948, 2,     --Scroll of Blizzaga
-        4802, 15456, 2,     --Scroll of Thundaga
-        4859,  8280, 2,     --Scroll of Shock Spikes
+        4797,  1191, 3,     --Scroll of Stonega
+        4807,  2143, 3,     --Scroll of Waterga
+        4792,  4239, 3,     --Scroll of Aeroga
+        4782,  7181, 3,     --Scroll of Firaga
+        4787, 10948, 3,     --Scroll of Blizzaga
+        4802, 15456, 3,     --Scroll of Thundaga
+        4859,  8280, 3,     --Scroll of Shock Spikes
 
         4768,  5814, 3,     --Scroll of Stone II
         4778,  8100, 3,     --Scroll of Water II
