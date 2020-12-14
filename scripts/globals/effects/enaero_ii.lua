bdb875ff05f09a1c55e9e2b36662972da3a7ca4f
Shiyo <Shiyo@CELTY>
2020-12-13 21:23:46 -0500

    enaero2 experiement

--- scripts/globals/effects/enaero_ii.lua
+++ scripts/globals/effects/enaero_ii.lua
@@ -8,10 +8,10 @@ require("scripts/globals/magic")
 -----------------------------------
 
 function onEffectGain(target, effect)
-    target:addMod(tpz.mod.ENSPELL, tpz.magic.element.WIND + 8) -- Tier IIs have higher "enspell IDs"
+    target:addMod(tpz.mod.ENSPELL, tpz.magic.element.WIND + 8 + tpz.mod.REGEN_DOWN, effect:getPower()) -- Tier IIs have higher "enspell IDs"
     target:addMod(tpz.mod.ENSPELL_DMG, effect:getPower())
-    target:addMod(tpz.mod.REGEN_DOWN, effect:getPower())        -- choke debuff
-    target:addMod(tpz.mod.VIT, -getElementalDebuffStatDownFromDOT(effect:getPower()))       -- choke debuff
+    --target:addMod(tpz.mod.REGEN_DOWN, effect:getPower())        -- choke debuff
+    --target:addMod(tpz.mod.VIT, -getElementalDebuffStatDownFromDOT(effect:getPower()))       -- choke debuff
 end
 
 function onEffectTick(target, effect)
@@ -20,6 +20,6 @@ end
 function onEffectLose(target, effect)
     target:setMod(tpz.mod.ENSPELL_DMG, 0)
     target:setMod(tpz.mod.ENSPELL, 0)
-    target:delMod(tpz.mod.REGEN_DOWN, effect:getPower())        --choke debuff
-    target:delMod(tpz.mod.VIT, -getElementalDebuffStatDownFromDOT(effect:getPower()))       --choke debuff
+    --target:delMod(tpz.mod.REGEN_DOWN, effect:getPower())        --choke debuff
+   -- target:delMod(tpz.mod.VIT, -getElementalDebuffStatDownFromDOT(effect:getPower()))       --choke debuff
 end
