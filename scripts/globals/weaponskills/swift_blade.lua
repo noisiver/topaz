5178174667e89c8bbe86844ede6d26da7a4c3149
Shiyo <Shiyo@CELTY>
2020-12-14 16:13:37 -0500

    added str down to swift blade

--- scripts/globals/weaponskills/swift_blade.lua
+++ scripts/globals/weaponskills/swift_blade.lua
@@ -20,11 +20,11 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
 
     local params = {}
     params.numHits = 3
-    params.ftp100 = 1.5 params.ftp200 = 1.5 params.ftp300 = 1.5
-    params.str_wsc = 0.3 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.3 params.chr_wsc = 0.0
+    params.ftp100 = 2.0 params.ftp200 = 2.0 params.ftp300 = 2.0
+    params.str_wsc = 0.5 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.5 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
-    params.acc100 = 0.8 params.acc200= 0.9 params.acc300= 1
+    params.acc100 = 1.10 params.acc200= 1.15 params.acc300= 1.20
     params.atk100 = 1; params.atk200 = 1; params.atk300 = 1
 
     if (USE_ADOULIN_WEAPON_SKILL_CHANGES == true) then
@@ -32,10 +32,12 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     end
 
     local damage, criticalHit, tpHits, extraHits = doPhysicalWeaponskill(player, target, wsID, params, tp, action, primary, taChar)
-    if (damage > 0 and target:hasStatusEffect(tpz.effect.DEFENSE_DOWN) == false) then
-        local duration = (120 + (tp/1000 * 60)) * applyResistanceAddEffect(player, target, tpz.magic.ele.WIND, 0)
-        target:addStatusEffect(tpz.effect.DEFENSE_DOWN, 15, 0, duration)
+
+    if (damage > 0 and target:hasStatusEffect(tpz.effect.STR_DOWN) == false) then
+        local duration = (60 + (tp/1000 * 60)) * applyResistanceAddEffect(player, target, tpz.magic.ele.WATER, 0)
+        target:addStatusEffect(tpz.effect.STR_DOWN, 8, 0, duration)
     end
+
     return tpHits, extraHits, criticalHit, damage
 
 end
