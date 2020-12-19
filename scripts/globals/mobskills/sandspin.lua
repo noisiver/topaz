b7fa4425216918bcca9aa07f3eec2e542a32a930
Shiyo <Shiyo@CELTY>
2020-12-19 17:56:32 -0500

    worm TP moves

--- scripts/globals/mobskills/sandspin.lua
+++ scripts/globals/mobskills/sandspin.lua
@@ -19,12 +19,12 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.ACCURACY_DOWN
 
-    MobStatusEffectMove(mob, target, typeEffect, 50, 0, 120)
+    MobStatusEffectMove(mob, target, typeEffect, 20, 0, 120)
 
     local dmgmod = 1
-    local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*2.3, tpz.magic.ele.EARTH, dmgmod, TP_MAB_BONUS, 1)
-    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.EARTH, MOBPARAM_IGNORE_SHADOWS)
-    target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.EARTH)
+    local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*2.3, tpz.magic.ele.WATER, dmgmod, TP_MAB_BONUS, 1)
+    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.WATER, MOBPARAM_IGNORE_SHADOWS)
+    target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.WATER)
     return dmg
 
 end
