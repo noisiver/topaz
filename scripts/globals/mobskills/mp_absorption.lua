525233bc19cc41c3ffe326a2074fde549106a64c
Shiyo <Shiyo@CELTY>
2020-12-19 18:32:44 -0500

    Update mp_absorption.lua

--- scripts/globals/mobskills/mp_absorption.lua
+++ scripts/globals/mobskills/mp_absorption.lua
@@ -15,7 +15,7 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local dmgmod = 1
+    local dmgmod = 0.5
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*1.5, tpz.magic.ele.DARK, dmgmod, TP_MAB_BONUS, 1)
 
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.DARK, MOBPARAM_IGNORE_SHADOWS)
