46dbdd0831be4316c74a500b50732d959d6b393c
Shiyo <Shiyo@CELTY>
2020-12-25 01:36:25 -0500

    Update Ostalie.lua

--- scripts/zones/Southern_San_dOria/npcs/Ostalie.lua
+++ scripts/zones/Southern_San_dOria/npcs/Ostalie.lua
@@ -13,30 +13,30 @@ end
 function onTrigger(player, npc)
     local stock =
     {
-        4128,  4445, 1,    -- Ether
-        4112,   837, 1,    -- Potion
-        4151,   736, 2,    -- Echo Drops
-        4148,   290, 3,    -- Antidote
-        12472,  144, 3,    -- Circlet
-        12728,  118, 3,    -- Cuffs
-        4150,  2387, 3,    -- Eye Drops
-        1021,   450, 3,    -- Hatchet
-        13192,  382, 3,    -- Leather Belt
-        13193, 2430, 3,    -- Lizard Belt
-        605,    180, 3,    -- Pickaxe
-        12600,  216, 3,    -- Robe
-        12856,  172, 3,    -- Slops
+        4128,  5025, 1,    -- Ether
+        4112,   946, 1,    -- Potion
+        4151,   832, 2,    -- Echo Drops
+        4148,   328, 3,    -- Antidote
+        12472,  166, 3,    -- Circlet
+        12728,  137, 3,    -- Cuffs
+        4150,  2698, 3,    -- Eye Drops
+        1021,   525, 3,    -- Hatchet
+        13192,  442, 3,    -- Leather Belt
+        13193, 2808, 3,    -- Lizard Belt
+        605,    208, 3,    -- Pickaxe
+        12600,  249, 3,    -- Robe
+        12856,  199, 3,    -- Slops
     }
 
     local rank = getNationRank(tpz.nation.SANDORIA)
 
     if rank ~= 1 then
         table.insert(stock, 1022)    -- Thief's Tools
-        table.insert(stock, 3643)
+        table.insert(stock, 4118)
         table.insert(stock, 3)
     elseif rank == 3 then
         table.insert(stock, 1023)    -- Living Key
-        table.insert(stock, 5520)
+        table.insert(stock, 6240)
         table.insert(stock, 3)
     end
 
