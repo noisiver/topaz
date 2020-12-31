f6806458db2ab4c716bd22d80ee39fc1f3a87879
Shiyo <Shiyo@CELTY>
2020-12-31 00:24:21 -0500

    Update innin.lua

--- scripts/globals/abilities/innin.lua
+++ scripts/globals/abilities/innin.lua
@@ -17,5 +17,5 @@ function onUseAbility(player, target, ability)
     target:delStatusEffect(tpz.effect.INNIN)
     target:delStatusEffect(tpz.effect.YONIN)
    -- target:addStatusEffect(tpz.effect.INNIN, 30, 15, 300, 0, 20)
-    target:addStatusEffect(tpz.effect.INNIN, math.floor(player:getMainLvl() / 2) + 3, 15, 300, 0, 20) -- 30(get power), 15(MAB), 300(duration), 0, 20(get sub power)
+    target:addStatusEffect(tpz.effect.INNIN, math.floor(player:getMainLvl() / 4) + 2, 15, 300, 0, math.floor(player:getMainLvl() / 2) + 3) -- 30(get power), 15(MAB), 300(duration), 0, 20(get sub power)
 end
