ea64f18cd504563c95c93241ddaa5b47490e42e1
Shiyo <Shiyo@CELTY>
2021-01-03 06:22:20 -0500

    Removed item from ghemi.
    Ghemi removed Aquilaria Log

--- scripts/zones/Kazham/npcs/Ghemi_Sinterilo.lua
+++ scripts/zones/Kazham/npcs/Ghemi_Sinterilo.lua
@@ -21,7 +21,7 @@ function onTrigger(player, npc)
         5187,  156,    -- Elshimo Coconut
         5604,  154,    -- Elshimo Pachira Fruit
         2869, 9100,    -- Kazham Waystone
-        731,  2877,    -- Aquilaria Log
+       -- 731,  2877,    -- Aquilaria Log
     }
 
     player:showText(npc, ID.text.GHEMISENTERILO_SHOP_DIALOG)
