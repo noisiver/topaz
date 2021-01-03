31c80659d34f6d468b5aa2ec6f6dbf81f10707cd
Shiyo <Shiyo@CELTY>
2021-01-03 03:47:33 -0500

    Update spirited_etude.lua

--- scripts/globals/spells/spirited_etude.lua
+++ scripts/globals/spells/spirited_etude.lua
@@ -15,21 +15,21 @@ function onSpellCast(caster, target, spell)
     local sLvl = caster:getSkillLevel(tpz.skill.SINGING) -- Gets skill level of Singing
     local iLvl = caster:getWeaponSkillLevel(tpz.slot.RANGED)
 
-    local power = 3
+    local power = 0
 
     if (sLvl+iLvl <= 75) then
         power = 3
-    elseif ((sLvl+iLvl >= 203) and (sLvl+iLvl <= 129)) then
+    elseif ((sLvl+iLvl >= 103) and (sLvl+iLvl <= 129)) then
         power = 4
-    elseif ((sLvl+iLvl >= 231) and (sLvl+iLvl <= 169)) then
+    elseif ((sLvl+iLvl >= 131) and (sLvl+iLvl <= 169)) then
         power = 5
-    elseif ((sLvl+iLvl >= 270) and (sLvl+iLvl <= 202)) then
+    elseif ((sLvl+iLvl >= 170) and (sLvl+iLvl <= 202)) then
         power = 6
-    elseif ((sLvl+iLvl >= 303) and (sLvl+iLvl <= 229)) then
+    elseif ((sLvl+iLvl >= 203) and (sLvl+iLvl <= 229)) then
         power = 7
-    elseif ((sLvl+iLvl >= 330) and (sLvl+iLvl <= 249)) then
+    elseif ((sLvl+iLvl >= 230) and (sLvl+iLvl <= 249)) then
         power = 8
-    elseif (sLvl+iLvl >= 350) then
+    elseif (sLvl+iLvl >= 250) then
         power = 9
     end
 
