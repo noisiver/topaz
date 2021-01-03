d6185004ef8d84d35bf4113b97c0003c2fa51a24
Shiyo <Shiyo@CELTY>
2021-01-03 18:56:09 -0500

    Buffed numerous consumables. numerous mob TP moves nerfs
    MP Potions
    
    Ether		50
    Hi-Ether		100
    Super ether	200
    Hyper Ether	400
    Pro-Ether		800
    
    Stat Boosting Potions
    
    All: Duration 1 hour

--- scripts/globals/mobskills/power_attack.lua
+++ scripts/globals/mobskills/power_attack.lua
@@ -20,7 +20,7 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local numhits = 1
     local accmod = 1
-    local dmgmod = 2.8
+    local dmgmod = 1.5
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_DMG_VARIES, 1, 2, 3)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.H2H, info.hitslanded)
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.H2H)
