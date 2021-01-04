38a3f13d83c163da84f9d44c5a210d21ac2e5214
Shiyo <Shiyo@CELTY>
2021-01-03 20:53:13 -0500

    Update Brave_Ox.lua

--- scripts/zones/Rabao/npcs/Brave_Ox.lua
+++ scripts/zones/Rabao/npcs/Brave_Ox.lua
@@ -31,7 +31,7 @@ function onTrigger(player, npc)
     }
 
     player:showText(npc, ID.text.BRAVEOX_SHOP_DIALOG)
-    tpz.shop.general(player, stock)
+    tpz.shop.general(player, stock, RABAO)
 end
 
 function onEventUpdate(player, csid, option)
