ff5a1f3980c45c4f4c32138a703343c0cddcb5eb
Shiyo <Shiyo@CELTY>
2021-01-08 02:47:12 -0500

    added regain

--- scripts/zones/Horlais_Peak/mobs/Compound_Eyes.lua
+++ scripts/zones/Horlais_Peak/mobs/Compound_Eyes.lua
@@ -9,7 +9,7 @@ require("scripts/globals/status")
 function onMobInitialize(mob)
     mob:setMod(tpz.mod.SLEEPRES, 100)
     mob:setMod(tpz.mod.SILENCERES, 75)
-    mob:addStatusEffect(tpz.effect.REGAIN, 1000, 3, 0)
+    mob:setMod(tpz.mod.REGAIN, 1000)
 end
 
 function onMobDeath(mob, player, isKiller)
