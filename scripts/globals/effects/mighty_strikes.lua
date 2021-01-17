a15287972997bee98830a9ea301ac59f76dcb905
Shiyo <Shiyo@CELTY>
2021-01-17 04:24:24 -0500

    Mighty strikes buff
    +30% WSD

--- scripts/globals/effects/mighty_strikes.lua
+++ scripts/globals/effects/mighty_strikes.lua
@@ -8,6 +8,7 @@ require("scripts/globals/status")
 
 function onEffectGain(target, effect)
 target:addMod(tpz.mod.CRITHITRATE, 100)
+target:addMod(tpz.mod.ALL_WSDMG_ALL_HITS , 30)
 end
 
 function onEffectTick(target, effect)
