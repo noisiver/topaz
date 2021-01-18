f5c5260a82d9c3be0d2c5364c8257a22988a38e6
Shiyo <Shiyo@CELTY>
2021-01-18 03:25:07 -0500

    Update Valley_Manticore.lua

--- scripts/zones/Valley_of_Sorrows/mobs/Valley_Manticore.lua
+++ scripts/zones/Valley_of_Sorrows/mobs/Valley_Manticore.lua
@@ -13,28 +13,13 @@ require("scripts/globals/regimes")
 --end
 
 
---function onMobDeath(mob, player, isKiller)
-   -- local ID = zones[player:getZoneID()]
-   -- tpz.regime.checkRegime(player, mob, 140, 2, tpz.regime.type.FIELDS)
-   -- tpz.regime.checkRegime(player, mob, 141, 2, tpz.regime.type.FIELDS)
-   -- if math.random(1,100) <= 99 then -- 24% chance
-       -- player:addItem(13120,1,514,0)
-     --   player:messageSpecial( ID.text.ITEM_OBTAINED, 13120)
-   -- end
---end
-
 function onMobDeath(mob, player, isKiller)
+    local ID = zones[player:getZoneID()]
     tpz.regime.checkRegime(player, mob, 140, 2, tpz.regime.type.FIELDS)
     tpz.regime.checkRegime(player, mob, 141, 2, tpz.regime.type.FIELDS)
     if isKiller then
-        if math.random(1,100) <= 99-- 24% chance
-            local party = player:getParty()
-            local partySize = #party -- Not sure if this is right
-            local randomIndex = math.random(1, partySize)
-            local pickedMember = party[randomIndex]
-
-            pickedMember:addItem(13120,1,514,0)
-            player:messageSpecial( ID.text.ITEM_OBTAINED, 13120)
-        end
+    if math.random(1,100) <= 99 then -- 24% chance
+        player:addItem(13120,1,514,0)
+        player:messageSpecial( ID.text.ITEM_OBTAINED, 13120)
     end
 end
