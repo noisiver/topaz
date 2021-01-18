4a89f57b1f8d7ec5c3dae23cd43ccec6deefa86b
Shiyo <Shiyo@CELTY>
2021-01-18 03:18:11 -0500

    test

--- scripts/zones/Valley_of_Sorrows/mobs/Valley_Manticore.lua
+++ scripts/zones/Valley_of_Sorrows/mobs/Valley_Manticore.lua
@@ -17,8 +17,8 @@ function onMobDeath(mob, player, isKiller)
     local ID = zones[player:getZoneID()]
     tpz.regime.checkRegime(player, mob, 140, 2, tpz.regime.type.FIELDS)
     tpz.regime.checkRegime(player, mob, 141, 2, tpz.regime.type.FIELDS)
-    if math.random(1,100) <= 99 then -- 24% chance
-        player:addItem(13120,1,514,0)
+    if math.random(1,100) <= 24 then -- 24% chance
+        player:addTreasure(13120,1,514,0)
         player:messageSpecial( ID.text.ITEM_OBTAINED, 13120)
     end
 end
