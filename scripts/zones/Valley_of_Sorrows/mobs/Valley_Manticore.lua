b47a9584c50cd66f672896f166684c91b9c8994a
Shiyo <Shiyo@CELTY>
2021-01-18 02:26:18 -0500

    Update Valley_Manticore.lua

--- scripts/zones/Valley_of_Sorrows/mobs/Valley_Manticore.lua
+++ scripts/zones/Valley_of_Sorrows/mobs/Valley_Manticore.lua
@@ -9,4 +9,5 @@ function onMobDeath(mob, player, isKiller)
     tpz.regime.checkRegime(player, mob, 140, 2, tpz.regime.type.FIELDS)
     tpz.regime.checkRegime(player, mob, 141, 2, tpz.regime.type.FIELDS)
     player:addItem(13120,1,514,0)
+    player:messageSpecial( ID.text.ITEM_OBTAINED, 13120)
 end
