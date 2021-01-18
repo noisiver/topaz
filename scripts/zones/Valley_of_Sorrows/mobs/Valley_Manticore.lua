7e10b430635a15d7b6cac29e4d5bbf86cd000922
Shiyo <Shiyo@CELTY>
2021-01-18 02:33:44 -0500

    Update Valley_Manticore.lua

--- scripts/zones/Valley_of_Sorrows/mobs/Valley_Manticore.lua
+++ scripts/zones/Valley_of_Sorrows/mobs/Valley_Manticore.lua
@@ -5,9 +5,19 @@
 require("scripts/globals/regimes")
 -----------------------------------
 
+--function onMobDeath(mob, player, isKiller)
+  --  tpz.regime.checkRegime(player, mob, 140, 2, tpz.regime.type.FIELDS)
+   -- tpz.regime.checkRegime(player, mob, 141, 2, tpz.regime.type.FIELDS)
+   -- player:addItem(13120,1,514,0)
+   -- player:messageSpecial( ID.text.ITEM_OBTAINED, 13120)
+--end
+
+
 function onMobDeath(mob, player, isKiller)
     tpz.regime.checkRegime(player, mob, 140, 2, tpz.regime.type.FIELDS)
     tpz.regime.checkRegime(player, mob, 141, 2, tpz.regime.type.FIELDS)
-    player:addItem(13120,1,514,0)
-    player:messageSpecial( ID.text.ITEM_OBTAINED, 13120)
+    if math.random(1,100) <= 24
+        player:addItem(13120,1,514,0)
+        player:messageSpecial( ID.text.ITEM_OBTAINED, 13120)
+    end
 end
