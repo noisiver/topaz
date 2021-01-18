85b79db222f77c9959ce218d8e979338d8017a88
Shiyo <Shiyo@CELTY>
2021-01-18 02:57:46 -0500

    Update Valley_Manticore.lua

--- scripts/zones/Valley_of_Sorrows/mobs/Valley_Manticore.lua
+++ scripts/zones/Valley_of_Sorrows/mobs/Valley_Manticore.lua
@@ -16,7 +16,7 @@ require("scripts/globals/regimes")
 function onMobDeath(mob, player, isKiller)
     tpz.regime.checkRegime(player, mob, 140, 2, tpz.regime.type.FIELDS)
     tpz.regime.checkRegime(player, mob, 141, 2, tpz.regime.type.FIELDS)
-    if math.random(1,100) <= 99 -- 24% chance
+    if math.random(1,100) <= 99 then -- 24% chance
         player:addItem(13120,1,514,0)
         player:messageSpecial( ID.text.ITEM_OBTAINED, 13120)
     end
