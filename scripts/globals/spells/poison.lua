18e5d3b67176b5b70766f500fc1b3f6830d1b212
Shiyo <Shiyo@CELTY>
2021-01-18 23:31:57 -0500

    Poison fix

--- scripts/globals/spells/poison.lua
+++ scripts/globals/spells/poison.lua
@@ -20,7 +20,7 @@ function onSpellCast(caster, target, spell)
     end
     power = calculatePotency(power, spell:getSkillType(), caster, target)
 
-    local duration = calculateDuration(90, spell:getSkillType(), spell:getSpellGroup(), caster, target)
+    local duration = 90
 
     local params = {}
     params.diff = dINT
