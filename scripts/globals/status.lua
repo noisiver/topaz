019eeb12ec6546cfc6479d94236ce285d4d07c02
Shiyo <Shiyo@CELTY>
2021-01-21 23:02:30 -0500

    status fixes

--- scripts/globals/status.lua
+++ scripts/globals/status.lua
@@ -1647,7 +1647,7 @@ tpz.mod =
 
     -- The spares take care of finding the next ID to use so long as we don't forget to list IDs that have been freed up by refactoring.
     -- 570 - 825 used by WS DMG mods these are not spares.
-   TP_BOOST_WHEN_DMGD               = 986    -- Bonus 30-100 TP gained when taking damage. Modifier = percent chance of proccing
+   TP_BOOST_WHEN_DMGD               = 1239    -- Bonus 30-100 TP gained when taking damage. Modifier = percent chance of proccing
     -- SPARE = 986, -- stuff
     -- SPARE = 987, -- stuff
     -- SPARE = 988, -- stuff
