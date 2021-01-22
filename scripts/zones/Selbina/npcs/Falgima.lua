24b4e606db9e5025552538ac47cc72bcef1dc6b0
Shiyo <Shiyo@CELTY>
2021-01-21 23:53:54 -0500

    Update Falgima.lua

--- scripts/zones/Selbina/npcs/Falgima.lua
+++ scripts/zones/Selbina/npcs/Falgima.lua
@@ -16,7 +16,7 @@ function onTrigger(player, npc)
         --4744,  5351,    -- Scroll of Invisible
         --4745,  2325,    -- Scroll of Sneak
         --4746,  1204,    -- Scroll of Deodorize
-        5104, 30360,    -- Scroll of Flurry
+        5104, 200569,    -- Scroll of Flurry
     }
 
     player:showText(npc, ID.text.FALGIMA_SHOP_DIALOG)
