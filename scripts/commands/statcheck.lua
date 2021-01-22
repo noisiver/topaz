39b41a0364b8c8ea43f161f033d4f5a0084c6321
Shiyo <Shiyo@CELTY>
2021-01-21 22:39:57 -0500

    Update statcheck.lua

--- scripts/commands/statcheck.lua
+++ scripts/commands/statcheck.lua
@@ -17,12 +17,12 @@ function onTrigger(player)
         local macc = targ:getMod(tpz.mod.MACC)
         local mdef = targ:getMod(tpz.mod.MDEF)
 
-        local fire = targ:getMod(tpz.mod.FIREEVA)
-        local ice = targ:getMod(tpz.mod.ICEEVA)
-        local wind = targ:getMod(tpz.mod.WINDEVA)
-        local earth = targ:getMod(tpz.mod.EARTHEVA)
-        local water = targ:getMod(tpz.mod.WATEREVA)
-        local thunder = targ:getMod(tpz.mod.THUNDEREVA)
+        local fire = targ:getMod(tpz.mod.SDT_FIRE)
+        local ice = targ:getMod(tpz.mod.SDT_ICE)
+        local wind = targ:getMod(tpz.mod.SDT_WIND)
+        local earth = targ:getMod(tpz.mod.SDT_EARTH)
+        local water = targ:getMod(tpz.mod.SDT_WATER)
+        local thunder = targ:getMod(tpz.mod.SDT_THUNDER)
         
         local hthres = target:getMod(tpz.mod.HTHRES)
         local pierceres = target:getMod(tpz.mod.PIERCERES)
@@ -34,7 +34,7 @@ function onTrigger(player)
         player:PrintToPlayer(string.format("%s Stats 1: LVL: %u, SLVL: %u, ACC: %u, EVA: %u, ATT: %u, DEF: %u, MATT: %u, MACC: %u, MDEF: %u ", targ:getName(), targ:getMainLvl(), targ:getSubLvl(), acc,eva,att,def,matt,macc,mdef))
         player:PrintToPlayer(string.format("%s Stats 2: STR: %u, DEX: %u, VIT: %u, AGI: %u, INT: %u, MND: %u, CHR: %u", targ:getName(), str,dex,vit,agi,int,mnd,chr))
         player:PrintToPlayer(string.format("%s Stats 3: Fire: %f, Ice: %f, Wind: %f, Earth: %f, Water: %f, Thunder: %f", targ:getName(), fire, ice, wind, earth, water, thunder))
-        player:PrintToPlayer(string.format("%s Stats 4: H2hthres: %h, pierceres: %p, impactres: %i, slashres: %s, spdefdown: %sp", targ:getName(), h2h, pierce, impact, slash, spdef))
+        player:PrintToPlayer(string.format("%s Stats 4: H2hthres: %f, pierceres: %f, impactres: %f, slashres: %f, spdefdown: %f", targ:getName(), h2h, pierce, impact, slash, spdef))
     else
         player:PrintToPlayer("Must select a target using in game cursor first.");
     end
