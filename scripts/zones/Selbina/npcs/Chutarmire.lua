b89f5e8f2cc464d19f40d90be6199183783315b2
Shiyo <Shiyo@CELTY>
2021-01-21 23:52:02 -0500

    selbina scroll NPC's fixed

--- scripts/zones/Selbina/npcs/Chutarmire.lua
+++ scripts/zones/Selbina/npcs/Chutarmire.lua
@@ -13,22 +13,22 @@ end
 function onTrigger(player, npc)
     local stock =
     {
-        4768,  6645,    -- Scroll of Stone II
-        4778,  9360,    -- Scroll of Water II
-        4763, 13832,    -- Scroll of Aero II
-        4753, 19136,    -- Scroll of Fire II
-        4758, 25272,    -- Scroll of Blizzard II
-        4773, 32240,    -- Scroll of Thunder II
-        4797,  1346,    -- Scroll of Stonega
-        4807,  2423,    -- Scroll of Waterga
-        4792,  4792,    -- Scroll of Aeroga
-        4782,  8118,    -- Scroll of Firaga
-        4787, 12376,    -- Scroll of Blizzaga
-        4802, 17472,    -- Scroll of Thundaga
-        4829, 26208,    -- Scroll of Poison II
-        4839, 14560,    -- Scroll of Bio II
-        4833,  5366,    -- Scroll of Poisonga
-        4859,  9360,    -- Scroll of Shock Spikes
+        4768,  5751 ,    -- Scroll of Stone II
+        4778,  8100 ,    -- Scroll of Water II
+        4763, 11970 ,    -- Scroll of Aero II
+        4753, 16560 ,    -- Scroll of Fire II
+        4758, 21870 ,    -- Scroll of Blizzard II
+        4773, 27900 ,    -- Scroll of Thunder II
+        4797,  1165 ,    -- Scroll of Stonega
+        4807,  2097 ,    -- Scroll of Waterga
+        4792,  4147 ,    -- Scroll of Aeroga
+        4782,  7025 ,    -- Scroll of Firaga
+        4787, 10710 ,    -- Scroll of Blizzaga
+        4802, 15120 ,    -- Scroll of Thundaga
+        4829, 22680 ,    -- Scroll of Poison II
+        4839, 12600 ,    -- Scroll of Bio II
+        4833,  4644 ,    -- Scroll of Poisonga
+        4859,  8100 ,    -- Scroll of Shock Spikes
     }
 
     player:showText(npc, ID.text.CHUTARMIRE_SHOP_DIALOG)
--- scripts/zones/Selbina/npcs/Quelpia.lua
+++ scripts/zones/Selbina/npcs/Quelpia.lua
@@ -13,22 +13,22 @@ end
 function onTrigger(player, npc)
     local stock =
     {
-        4610,   676,    -- Scroll of Cure II
-        4611,  3768,    -- Scroll of Cure III
-        4616, 11648,    -- Scroll of Curaga II
-        4620,  5984,    -- Scroll of Raise
-        4629, 36400,    -- Scroll of Holy
-        4632, 11648,    -- Scroll of Dia II
-        4637,  9360,    -- Scroll of Banish II
-        4652,  7356,    -- Scroll of Protect II
-        4657, 18304,    -- Scroll of Shell II
-        4665, 20800,    -- Scroll of Haste
-        4708,  5366,    -- Scroll of Enfire
-        4709,  4261,    -- Scroll of Enblizzard
-        4710,  2600,    -- Scroll of Enaero
-        4711,  2111,    -- Scroll of Enstone
-        4712,  1575,    -- Scroll of Enthunder
-        4713,  7356,    -- Scroll of Enwater
+        4610,   585 ,    -- Scroll of Cure II
+        4611,  3261 ,    -- Scroll of Cure III
+        4616, 10080 ,    -- Scroll of Curaga II
+        4620,  5178 ,    -- Scroll of Raise
+        4629, 31500 ,    -- Scroll of Holy
+        4632, 10080 ,    -- Scroll of Dia II
+        4637,  8100 ,    -- Scroll of Banish II
+        4652,  6366 ,    -- Scroll of Protect II
+        4657, 15840 ,    -- Scroll of Shell II
+        4665, 18000 ,    -- Scroll of Haste
+        4708,  4644 ,    -- Scroll of Enfire
+        4709,  3688 ,    -- Scroll of Enblizzard
+        4710,  2250 ,    -- Scroll of Enaero
+        4711,  1827 ,    -- Scroll of Enstone
+        4712,  1363 ,    -- Scroll of Enthunder
+        4713,  6366 ,    -- Scroll of Enwater
     }
 
     player:showText(npc, ID.text.QUELPIA_SHOP_DIALOG)
