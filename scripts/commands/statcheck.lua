489da27a39742d98d03b9e1e0b6cd5f4c440387d
Shiyo <Shiyo@CELTY>
2021-01-21 22:36:45 -0500

    Update statcheck.lua

--- scripts/commands/statcheck.lua
+++ scripts/commands/statcheck.lua
@@ -23,11 +23,18 @@ function onTrigger(player)
         local earth = targ:getMod(tpz.mod.EARTHEVA)
         local water = targ:getMod(tpz.mod.WATEREVA)
         local thunder = targ:getMod(tpz.mod.THUNDEREVA)
-
+        
+        local hthres = target:getMod(tpz.mod.HTHRES)
+        local pierceres = target:getMod(tpz.mod.PIERCERES)
+        local impactres = target:getMod(tpz.mod.IMPACTRES)
+        local slashres = target:getMod(tpz.mod.SLASHRES)
+        local spdefdown = target:getMod(tpz.mod.SPDEF_DOWN)
+       
         player:PrintToPlayer(string.format("%s Stats 0: MJOB: %u, SJOB: %u, HP: %u, MP: %u", targ:getName(), targ:getMainJob(), targ:getSubJob(), targ:getMaxHP(), targ:getMaxMP()))
         player:PrintToPlayer(string.format("%s Stats 1: LVL: %u, SLVL: %u, ACC: %u, EVA: %u, ATT: %u, DEF: %u, MATT: %u, MACC: %u, MDEF: %u ", targ:getName(), targ:getMainLvl(), targ:getSubLvl(), acc,eva,att,def,matt,macc,mdef))
         player:PrintToPlayer(string.format("%s Stats 2: STR: %u, DEX: %u, VIT: %u, AGI: %u, INT: %u, MND: %u, CHR: %u", targ:getName(), str,dex,vit,agi,int,mnd,chr))
         player:PrintToPlayer(string.format("%s Stats 3: Fire: %f, Ice: %f, Wind: %f, Earth: %f, Water: %f, Thunder: %f", targ:getName(), fire, ice, wind, earth, water, thunder))
+        player:PrintToPlayer(string.format("%s Stats 4: H2hthres: %h, pierceres: %p, impactres: %i, slashres: %s, spdefdown: %sp", targ:getName(), h2h, pierce, impact, slash, spdef))
     else
         player:PrintToPlayer("Must select a target using in game cursor first.");
     end
