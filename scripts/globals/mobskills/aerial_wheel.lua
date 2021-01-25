e7f5490d61f843438d34adc62803908fc2883135
Shiyo <Shiyo@CELTY>
2021-01-25 01:44:46 -0500

    Moar nerfs

--- scripts/globals/mobskills/aerial_wheel.lua
+++ scripts/globals/mobskills/aerial_wheel.lua
@@ -15,8 +15,8 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local numhits = 1
-    local accmod = 0.8
-    local dmgmod = 2.3
+    local accmod = 1
+    local dmgmod = 1.5
     local info = MobRangedMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.RANGED, tpz.damageType.PIERCING, info.hitslanded)
 
--- scripts/globals/mobskills/blitzstrahl.lua
+++ scripts/globals/mobskills/blitzstrahl.lua
@@ -21,7 +21,7 @@ function onMobWeaponSkill(target, mob, skill)
 
     local dmgmod = 1
     local accmod = 1
-    local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*6, tpz.magic.ele.THUNDER, dmgmod, TP_NO_EFFECT)
+    local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*1.5, tpz.magic.ele.THUNDER, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.LIGHTNING, MOBPARAM_IGNORE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.LIGHTNING)
     return dmg
