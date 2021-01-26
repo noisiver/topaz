04a5ee809f067f26a509b80cf8eb10b939699abc
Shiyo <Shiyo@CELTY>
2021-01-26 03:20:30 -0500

    Shadowstitch nerf, some oldton spawn fixes

--- scripts/zones/Oldton_Movalpolos/mobs/Bugbear_Bondman.lua
+++ scripts/zones/Oldton_Movalpolos/mobs/Bugbear_Bondman.lua
@@ -11,5 +11,5 @@ function onMobDeath(mob, player, isKiller)
 end
 
 function onMobDespawn(mob)
-    tpz.mob.phOnDespawn(mob, ID.mob.BUGBEAR_STRONGMAN_PH, 10, 1) -- no cooldown
+    tpz.mob.phOnDespawn(mob, ID.mob.BUGBEAR_STRONGMAN_PH, 30, 1) -- no cooldown
 end
