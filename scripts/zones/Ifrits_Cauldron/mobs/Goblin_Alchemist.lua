581b9dd7a1adf073e11a3f21d5a108969a07580d
Shiyo <Shiyo@CELTY>
2021-01-26 05:12:52 -0500

    Ifrit's cauldron PH/NM/Spawns fixed

--- scripts/zones/Ifrits_Cauldron/mobs/Goblin_Alchemist.lua
+++ scripts/zones/Ifrits_Cauldron/mobs/Goblin_Alchemist.lua
@@ -8,3 +8,7 @@ require("scripts/globals/regimes")
 function onMobDeath(mob, player, isKiller)
     tpz.regime.checkRegime(player, mob, 757, 1, tpz.regime.type.GROUNDS)
 end
+
+function onMobDespawn(mob)
+    tpz.mob.phOnDespawn(mob, ID.mob.FORESEER_ORAMIX_PH, 30, 3600) -- 1 hour
+end
