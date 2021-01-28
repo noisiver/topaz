d915272c316eb5b544b2c0199f8237264bc3aaa0
Shiyo <Shiyo@CELTY>
2021-01-27 21:50:34 -0500

    100 fist change

--- scripts/globals/effects/hundred_fists.lua
+++ scripts/globals/effects/hundred_fists.lua
@@ -4,11 +4,27 @@
 --
 -----------------------------------
 
+--function onEffectGain(target, effect)
+--end
+
+--function onEffectTick(target, effect)
+--end
+
+--function onEffectLose(target, effect)
+--end
+
+
 function onEffectGain(target, effect)
+    if target:isMob() then
+        target:SetMobAbilityEnabled(false) 
+    end
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
+    if target:isMob() then
+        target:SetMobAbilityEnabled(true) 
+    end
 end
--- scripts/globals/mobskills/hundred_fists.lua
+++ scripts/globals/mobskills/hundred_fists.lua
@@ -12,7 +12,6 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     MobBuffMove(mob, tpz.effect.HUNDRED_FISTS, 1, 0, 30)
-
     skill:setMsg(tpz.msg.basic.USES)
 
     return tpz.effect.HUNDRED_FISTS
