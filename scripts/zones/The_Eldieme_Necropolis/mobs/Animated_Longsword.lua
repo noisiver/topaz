6767c69d7d9b1f613bbee2e306f86e3dd2afe2e0
Shiyo <Shiyo@CELTY>
2021-01-28 21:44:33 -0500

    Update Animated_Longsword.lua

--- scripts/zones/The_Eldieme_Necropolis/mobs/Animated_Longsword.lua
+++ scripts/zones/The_Eldieme_Necropolis/mobs/Animated_Longsword.lua
@@ -1,17 +1,20 @@
 ------------------------------
 -- Area: The Eldieme Necropolis
---   NM: Skull of Gluttony
+--   Animated Longsword(RDM)
 ------------------------------
 require("scripts/globals/hunts")
+require("scripts/globals/titles")
+require("scripts/globals/mobs")
+require("scripts/globals/status")
 mixins = {require("scripts/mixins/job_special")}
 ------------------------------
 
 function onMobSpawn(mob)
-    mob:addStatusEffect(id, power, 3, duration, subid, subPower)) 
-    mob:addStatusEffect(id, power, 3, duration, subid, subPower))
-    mob:addStatusEffect(id, power, 3, duration, subid, subPower))
-    mob:addStatusEffect(id, power, 3, duration, subid, subPower))
-    mob:addStatusEffect(id, power, 3, duration, subid, subPower)) 
+    mob:addStatusEffect(protect_V, 1, 0, 3600)
+    mob:addStatusEffect(shell_V, 1, 0, 3600)
+    mob:addStatusEffect(phalanx, 1, 0, 180)
+    mob:addStatusEffect(temper, 1, 0, 180)
+    mob:addStatusEffect(haste_II, 1, 0, 180)
 end
 
 function onMobDeath(mob, player, isKiller)
