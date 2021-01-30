7886b1fb6b14f3e41373ee5e3f30c7edae98a7b4
Shiyo <Shiyo@CELTY>
2021-01-30 00:18:40 -0500

    Focus changed
    5m CD 5m duration

--- scripts/globals/abilities/focus.lua
+++ scripts/globals/abilities/focus.lua
@@ -14,5 +14,5 @@ end
 
 function onUseAbility(player, target, ability)
     local power = 20 + player:getMod(tpz.mod.FOCUS_EFFECT)
-    player:addStatusEffect(tpz.effect.FOCUS, power, 0, 120)
+    player:addStatusEffect(tpz.effect.FOCUS, power, 0, 300)
 end
