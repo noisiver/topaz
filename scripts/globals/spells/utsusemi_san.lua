94dd6dc6e8cd7fc8bee6816a1d2813d10b326d89
Shiyo <Shiyo@CELTY>
2021-01-31 00:12:33 -0500

    TE and utsu overwriting eachother fixes

--- scripts/globals/spells/utsusemi_san.lua
+++ scripts/globals/spells/utsusemi_san.lua
@@ -4,7 +4,7 @@
 require("scripts/globals/status")
 require("scripts/globals/msg")
 -----------------------------------------
-
+--[[
 function onMagicCastingCheck(caster, target, spell)
     return 0
 end
@@ -24,3 +24,29 @@ function onSpellCast(caster, target, spell)
 
     return tpz.effect.COPY_IMAGE
 end
+--]]
+
+
+
+function onMagicCastingCheck(caster, target, spell)
+    return 0
+end
+
+function onSpellCast(caster, target, spell)
+    local effect = target:getStatusEffect(tpz.effect.COPY_IMAGE)
+    and target:delStatusEffect(67) -- Third Eye and Utsusemi don't stack.  Utsusemi removes Third Eye.
+
+    -- Get extras shadows
+    local numShadows = 5 + target:getMod(tpz.mod.UTSUSEMI_BONUS)
+
+    if (effect == nil or effect:getPower() <= 3) then
+        target:addStatusEffectEx(tpz.effect.COPY_IMAGE, tpz.effect.COPY_IMAGE_4, numShadows, 0, 900, 0, numShadows)
+        spell:setMsg(tpz.msg.basic.MAGIC_GAIN_EFFECT)
+    else
+        spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT)
+    end
+
+    return tpz.effect.COPY_IMAGE
+end
+
+
