cfac64af7ea6445853b159bdf466d00f435d8c3a
Shiyo <Shiyo@CELTY>
2021-01-31 06:46:48 -0500

    Update Animated_Longsword.lua

--- scripts/zones/The_Eldieme_Necropolis/mobs/Animated_Longsword.lua
+++ scripts/zones/The_Eldieme_Necropolis/mobs/Animated_Longsword.lua
@@ -10,8 +10,8 @@ mixins = {require("scripts/mixins/job_special")}
 ------------------------------
 
 function onMobSpawn(mob)
-    mob:addStatusEffect(tpz.effect.PROTECT, 175, 0, 3600)
-    mob:addStatusEffect(tpz.effect.SHELL, 24, 0, 3600)
+    mob:addStatusEffect(tpz.effect.PROTECT, 200, 0, 3600)
+    mob:addStatusEffect(tpz.effect.SHELL, 50, 0, 3600)
     mob:addStatusEffect(tpz.effect.PHALANX, 30, 0, 180)
     mob:addStatusEffect(tpz.effect.TEMPER, 50, 0, 180)
     mob:addStatusEffect(tpz.effect.HASTE, 30, 0, 180)
