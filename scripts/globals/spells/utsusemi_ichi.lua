67043d37a2fe715c112cd531b7c2ed340bc8d47a
Shiyo <Shiyo@CELTY>
2021-02-01 01:04:23 -0500

    Revert "TE and utsu overwriting eachother fixes"
    This reverts commit 94dd6dc6e8cd7fc8bee6816a1d2813d10b326d89.

--- scripts/globals/spells/utsusemi_ichi.lua
+++ scripts/globals/spells/utsusemi_ichi.lua
@@ -4,32 +4,6 @@
 require("scripts/globals/status")
 require("scripts/globals/msg")
 -----------------------------------------
---[[
-function onMagicCastingCheck(caster, target, spell)
-    return 0
-end
-
-function onSpellCast(caster, target, spell)
-    local effect = target:getStatusEffect(tpz.effect.COPY_IMAGE)
-
-    -- Get extras shadows
-    local numShadows = 3 + target:getMod(tpz.mod.UTSUSEMI_BONUS)
-    local icon = tpz.effect.COPY_IMAGE_3
-    if (numShadows > 3) then
-        icon = tpz.effect.COPY_IMAGE_4
-    end
-
-    if (effect == nil or effect:getPower() <= 1) then
-        target:addStatusEffectEx(tpz.effect.COPY_IMAGE, icon, 1, 0, 900, 0, numShadows)
-        spell:setMsg(tpz.msg.basic.MAGIC_GAIN_EFFECT)
-    else
-        spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT)
-    end
-
-    return tpz.effect.COPY_IMAGE
-end
---]]
-
 
 function onMagicCastingCheck(caster, target, spell)
     return 0
@@ -37,7 +11,6 @@ end
 
 function onSpellCast(caster, target, spell)
     local effect = target:getStatusEffect(tpz.effect.COPY_IMAGE)
-	and target:delStatusEffect(67) -- Third Eye and Utsusemi don't stack.  Utsusemi removes Third Eye.
 
     -- Get extras shadows
     local numShadows = 3 + target:getMod(tpz.mod.UTSUSEMI_BONUS)
