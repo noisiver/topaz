cca319e567b7f219f5e66e426f245064c2b06cb4
Shiyo <Shiyo@CELTY>
2021-02-01 21:43:10 -0500

    Death scissors buff, cockatrice hp nerfed a bit.

--- scripts/globals/mobskills/death_scissors.lua
+++ scripts/globals/mobskills/death_scissors.lua
@@ -17,14 +17,14 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local numhits = 1
     local accmod = 1
-    local dmgmod = 1.5
+    local dmgmod = 2
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_DMG_VARIES, 1, 2, 3)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, info.hitslanded)
 
     -- keep damage around 700
-    if (dmg > 700) then
-        dmg = dmg * 0.7
-    end
+  --  if (dmg > 700) then
+    --    dmg = dmg * 0.7
+    --end
 
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
     return dmg
