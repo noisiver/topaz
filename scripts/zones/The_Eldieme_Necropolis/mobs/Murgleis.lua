f1e94833c5e46ff2c0bda95d6b329ba3f31fd019
Shiyo <Shiyo@CELTY>
2021-02-02 07:13:45 -0500

    Update Murgleis.lua

--- scripts/zones/The_Eldieme_Necropolis/mobs/Murgleis.lua
+++ scripts/zones/The_Eldieme_Necropolis/mobs/Murgleis.lua
@@ -1,6 +1,6 @@
 ------------------------------
 -- Area: The Eldieme Necropolis
---   Animated Longsword(RDM)
+--   Murgleis
 ------------------------------
 require("scripts/globals/hunts")
 require("scripts/globals/titles")
@@ -10,10 +10,10 @@ mixins = {require("scripts/mixins/job_special")}
 ------------------------------
 
 function onMobSpawn(mob)
-    mob:addStatusEffect(tpz.effect.PROTECT, 200, 0, 3600)
-    mob:addStatusEffect(tpz.effect.SHELL, 50, 0, 3600)
+    mob:addStatusEffect(tpz.effect.PROTECT, 175, 0, 3600)
+    mob:addStatusEffect(tpz.effect.SHELL, 25, 0, 3600)
     mob:addStatusEffect(tpz.effect.PHALANX, 30, 0, 180)
-    mob:addStatusEffect(tpz.effect.TEMPER, 50, 0, 180)
+    mob:addStatusEffect(tpz.effect.TEMPER, 15, 0, 180)
     mob:addStatusEffect(tpz.effect.HASTE, 30, 0, 180)
 end
 
