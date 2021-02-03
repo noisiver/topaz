e31b30a951dd8a327d121d7d53f2cf75ea78264d
Shiyo <Shiyo@CELTY>
2021-02-03 08:33:58 -0500

    Update bind.lua

--- scripts/globals/effects/bind.lua
+++ scripts/globals/effects/bind.lua
@@ -7,7 +7,6 @@
 function onEffectGain(target, effect)
     effect:setPower(target:speed())
     target:speed(0)
-    print("why am I here lord? " ..  target:speed())
 end
 
 function onEffectTick(target, effect)
