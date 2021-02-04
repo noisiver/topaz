e3104f51c5d4fa906d897ddd3247c80cc388e26e
Shiyo <Shiyo@CELTY>
2021-02-04 04:44:23 -0500

    TP move fixes

--- scripts/globals/mobskills/deadly_hold.lua
+++ scripts/globals/mobskills/deadly_hold.lua
@@ -20,7 +20,7 @@ function onMobWeaponSkill(target, mob, skill)
     local numhits = 1
     local accmod = 1
     local dmgmod = 1.25
-    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_CRIT_VARIES, 1, 2, 3)
+    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_CRIT_VARIES, 1.25, 1.5, 2)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, info.hitslanded)
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
     return dmg
