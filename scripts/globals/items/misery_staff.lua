8c729a27178d74b63fab1947536782f0cc56a480
Shiyo <Shiyo@CELTY>
2021-02-03 23:35:09 -0500

    Drop fixes

--- scripts/globals/items/misery_staff.lua
+++ scripts/globals/items/misery_staff.lua
@@ -9,7 +9,7 @@ require("scripts/globals/msg")
 -----------------------------------
 
 function onAdditionalEffect(player, target, damage)
-    local chance = 10
+    local chance = 30
 
     if (math.random(0, 99) >= chance or applyResistanceAddEffect(player, target, tpz.magic.ele.DARK, 0) <= 0.5) then
         return 0, 0, 0
