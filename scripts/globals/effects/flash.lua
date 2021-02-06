49db9a23c9ef20795fab42ec88e48d442f9f09ab
Shiyo <Shiyo@CELTY>
2021-02-06 06:33:20 -0500

    Flash testing

--- scripts/globals/effects/flash.lua
+++ scripts/globals/effects/flash.lua
@@ -11,7 +11,7 @@ function onEffectGain(target, effect)
 end
 
 function onEffectTick(target, effect)
-effect:setPower(effect:getPower()-1)
+effect:setPower(effect:getPower()-30)
 target:delMod(tpz.mod.ACC, -1)
 end
 
