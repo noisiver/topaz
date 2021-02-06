ecdcb46735e7e3a2dba676ae169e1db5bf7490f0
Shiyo <Shiyo@CELTY>
2021-02-06 05:58:07 -0500

    Update tachi_koki.lua

--- scripts/globals/weaponskills/tachi_koki.lua
+++ scripts/globals/weaponskills/tachi_koki.lua
@@ -20,7 +20,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
 
     local params = {}
     params.numHits = 1
-    params.ftp100 = 1.4 params.ftp200 = 1.8 params.ftp300 = 2
+    params.ftp100 = 1.7 params.ftp200 = 1.8 params.ftp300 = 2
     params.str_wsc = 0.0 params.dex_wsc = 0.0 params.vit_wsc = 0.5 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
