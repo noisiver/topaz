f37cee172ff1308f9ac10d50b7fe1dd2e49e6352
Shiyo <Shiyo@CELTY>
2021-02-07 23:16:26 -0500

    Removed magic WS from being effected by mighty strikes

--- scripts/globals/effects/mighty_strikes.lua
+++ scripts/globals/effects/mighty_strikes.lua
@@ -9,6 +9,7 @@ require("scripts/globals/status")
 function onEffectGain(target, effect)
 target:addMod(tpz.mod.CRITHITRATE, 100)
 target:addMod(tpz.mod.ALL_WSDMG_ALL_HITS , 50)
+target:addMod(tpz.mod.MATT , -50)
 end
 
 function onEffectTick(target, effect)
@@ -17,4 +18,5 @@ end
 function onEffectLose(target, effect)
 target:addMod(tpz.mod.CRITHITRATE, -100)
 target:addMod(tpz.mod.ALL_WSDMG_ALL_HITS , -50)
+target:addMod(tpz.mod.MATT , 50)
 end
