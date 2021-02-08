caa0c8feb2f2648bf295d50c418da5ba604e7fd7
Shiyo <Shiyo@CELTY>
2021-02-07 21:26:33 -0500

    Added -mab to TA/SA

--- scripts/globals/effects/sneak_attack.lua
+++ scripts/globals/effects/sneak_attack.lua
@@ -6,6 +6,7 @@
 
 function onEffectGain(target, effect)
 target:addMod(tpz.mod.ALL_WSDMG_FIRST_HIT , 50)
+target:addMod(tpz.mod.MATT , 50)
 end
 
 function onEffectTick(target, effect)
@@ -13,4 +14,5 @@ end
 
 function onEffectLose(target, effect)
 target:addMod(tpz.mod.ALL_WSDMG_FIRST_HIT , -50)
+target:addMod(tpz.mod.MATT , -50)
 end
