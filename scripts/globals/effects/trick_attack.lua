3b689f721635818e7df745aad345f67580cb902c
Shiyo <Shiyo@CELTY>
2021-02-07 23:32:15 -0500

    TA damage bonus assassin(thief main) only.

--- scripts/globals/effects/trick_attack.lua
+++ scripts/globals/effects/trick_attack.lua
@@ -5,14 +5,18 @@
 -----------------------------------
 
 function onEffectGain(target, effect)
-target:addMod(tpz.mod.ALL_WSDMG_FIRST_HIT , 50)
-target:addMod(tpz.mod.MATT , -50)
+    if (player:getMainJob() == tpz.job.THF) then
+        target:addMod(tpz.mod.MATT , -50)
+        target:addMod(tpz.mod.ALL_WSDMG_FIRST_HIT , 50)
+    end
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-target:addMod(tpz.mod.ALL_WSDMG_FIRST_HIT , -50)
-target:addMod(tpz.mod.MATT , 50)
+    if (player:getMainJob() == tpz.job.THF) then
+        target:addMod(tpz.mod.MATT , 50)
+        target:addMod(tpz.mod.ALL_WSDMG_FIRST_HIT , -50)
+    end
 end
