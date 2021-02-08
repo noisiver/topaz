9281e511010c60cf116b33d185bd310f05ad0d70
Shiyo <Shiyo@CELTY>
2021-02-08 03:51:25 -0500

    Update Murgleis.lua

--- scripts/zones/The_Eldieme_Necropolis/mobs/Murgleis.lua
+++ scripts/zones/The_Eldieme_Necropolis/mobs/Murgleis.lua
@@ -42,12 +42,12 @@ function onMobFight(mob, target)
     local hitTrigger = mob:getLocalVar("TriggerHit")
     if mob:getHPP() <= 50 and hitTrigger == 0 then
         mob:setMod(tpz.mod.UFASTCAST, 50)
-        mob:setMobMod(tpz.mod.SPELL_LIST, 446)
+        mob:setMobMod(tpz.mobmod.SPELL_LIST, 446)
         mob:setLocalVar("TriggerHit", 1)
     end
     if mob:getHPP() <= 25 and hitTrigger == 1 then
         mob:setMod(tpz.mod.UFASTCAST, 75)
-        mob:setMobMod(tpz.mod.SPELL_LIST, 445)
+        mob:setMobMod(tpz.mobmod.SPELL_LIST, 445)
         mob:setLocalVar("TriggerHit", 2)
     end
 end
