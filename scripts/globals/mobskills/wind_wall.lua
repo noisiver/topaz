7c58df5ff13793412cbbd3e77be99ff0b16a1bce
Shiyo <Shiyo@CELTY>
2021-02-07 21:26:56 -0500

    Update wind_wall.lua

--- scripts/globals/mobskills/wind_wall.lua
+++ scripts/globals/mobskills/wind_wall.lua
@@ -18,7 +18,7 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.EVASION_BOOST
-     local power = 10 + (mob:getMainLvl() / 1)
+     local power = 25 + (mob:getMainLvl() / 1)
     skill:setMsg(MobBuffMove(mob, typeEffect, power, 0, 60))
     return typeEffect
 end
