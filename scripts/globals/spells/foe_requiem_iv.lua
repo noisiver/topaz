f2d511ba57527b7d782de2b3694a51bb368f3a29
Shiyo <Shiyo@CELTY>
2021-02-08 21:49:48 -0500

    Update foe_requiem_iv.lua

--- scripts/globals/spells/foe_requiem_iv.lua
+++ scripts/globals/spells/foe_requiem_iv.lua
@@ -13,7 +13,7 @@ end
 function onSpellCast(caster, target, spell)
     local effect = tpz.effect.REQUIEM
     local duration = 143
-    local power = 6
+    local power = 30
 
     local pCHR = caster:getStat(tpz.mod.CHR)
     local mCHR = target:getStat(tpz.mod.CHR)
