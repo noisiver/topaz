c5f780908aeccf508e85953b55aa51af2f2266d5
Shiyo <Shiyo@CELTY>
2021-02-08 21:39:18 -0500

    Add Pet RACC bonus to Drachen Roll

--- scripts/globals/effects/drachen_roll.lua
+++ scripts/globals/effects/drachen_roll.lua
@@ -8,6 +8,8 @@ require("scripts/globals/status")
 
 function onEffectGain(target, effect)
     target:addPetMod(tpz.mod.ACC, effect:getPower())
+    target:addPetMod(tpz.mod.RACC, effect:getPower())
+
 end
 
 function onEffectTick(target, effect)
@@ -15,4 +17,5 @@ end
 
 function onEffectLose(target, effect)
     target:delPetMod(tpz.mod.ACC, effect:getPower())
+     target:delPetMod(tpz.mod.RACC, effect:getPower())
 end
