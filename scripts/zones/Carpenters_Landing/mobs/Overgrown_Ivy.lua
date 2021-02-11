bb9994a26599428e51ca1b8f9de6e2eba9ba53b8
Shiyo <Shiyo@CELTY>
2021-02-11 05:47:04 -0500

    Wings overgrown ivy

--- scripts/zones/Carpenters_Landing/mobs/Overgrown_Ivy.lua
+++ scripts/zones/Carpenters_Landing/mobs/Overgrown_Ivy.lua
@@ -2,6 +2,10 @@
 -- Area: Carpenters_Landing
 --  Mob: Overgrown Ivy
 -----------------------------------
+function onMobInitialize(mob)
+    mob:setMobMod(tpz.mobMod.MULTI_HIT, 3)
+    mob:addMod(tpz.mod.REGAIN, 50)
+end
 
 function onMobDeath(mob, player, isKiller)
     if (player:getCurrentMission(COP) == tpz.mission.id.cop.THE_ROAD_FORKS and player:getCharVar("EMERALD_WATERS_Status") == 4) then
