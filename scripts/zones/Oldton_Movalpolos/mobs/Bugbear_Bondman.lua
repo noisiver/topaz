c7dbc6bf6cd2761c55b846ed4e35e14fdc96a5fa
Shiyo <Shiyo@CELTY>
2021-02-11 04:43:42 -0500

    Update Bugbear_Bondman.lua

--- scripts/zones/Oldton_Movalpolos/mobs/Bugbear_Bondman.lua
+++ scripts/zones/Oldton_Movalpolos/mobs/Bugbear_Bondman.lua
@@ -6,14 +6,9 @@
 local ID = require("scripts/zones/Oldton_Movalpolos/IDs")
 require("scripts/globals/mobs")
 -----------------------------------
-
-function onMobSpawn(mob)
-    mob:setMobMod(tpz.mobMod.ROAM_DISTANCE, 0)
-end
-
 function onMobDeath(mob, player, isKiller)
 end
 
 function onMobDespawn(mob)
-    tpz.mob.phOnDespawn(mob, ID.mob.BUGBEAR_STRONGMAN_PH, 30, 1) -- no cooldown
+    tpz.mob.phOnDespawn(mob, ID.mob.BUGBEAR_STRONGMAN_PH, 100, 1) -- no cooldown
 end
