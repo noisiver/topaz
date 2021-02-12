dd2635f4bdcb771a4fba7d5c79d95acea715bc19
Shiyo <Shiyo@CELTY>
2021-02-12 00:23:41 -0500

    Old footwork 1.0

--- scripts/globals/effects/footwork.lua
+++ scripts/globals/effects/footwork.lua
@@ -7,7 +7,9 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onEffectGain(target, effect)
-    target:addMod(tpz.mod.KICK_ATTACK_RATE, 20)
+    target:addMod(tpz.mod.ATTP, 10)
+    target:addMod(tpz.mod.DELAY, 150)
+    target:addMod(tpz.mod.STORETP, 20)
     target:addMod(tpz.mod.KICK_DMG, effect:getPower())
 end
 
@@ -15,6 +17,8 @@ function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-    target:delMod(tpz.mod.KICK_ATTACK_RATE, 20)
+    target:delMod(tpz.mod.ATTP, -10)
+    target:delMod(tpz.mod.DELAY, -150)
+    target:addMod(tpz.mod.STORETP, -20)
     target:delMod(tpz.mod.KICK_DMG, effect:getPower())
 end
