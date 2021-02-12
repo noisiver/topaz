023e5f96db00b5c14bd9cb63779e9baf9b5da6ab
Shiyo <Shiyo@CELTY>
2021-02-11 21:32:03 -0500

    No roam of bugbears

--- scripts/zones/Oldton_Movalpolos/mobs/Bugbear_Muscleman.lua
+++ scripts/zones/Oldton_Movalpolos/mobs/Bugbear_Muscleman.lua
@@ -4,6 +4,9 @@
 ------------------------------
 require("scripts/globals/hunts")
 ------------------------------
+function onMobSpawn(mob)
+    mob:setMobMod(tpz.mobMod.ROAM_DISTANCE, 0)
+end
 
 function onMobDeath(mob, player, isKiller)
     tpz.hunts.checkHunt(mob, player, 246)
