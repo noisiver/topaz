ee5cbb6569e0b640539dc2c215c75044ac695d8e
Shiyo <Shiyo@CELTY>
2021-02-13 06:12:12 -0500

    Overgrown ivy fix

--- scripts/zones/Carpenters_Landing/mobs/Overgrown_Ivy.lua
+++ scripts/zones/Carpenters_Landing/mobs/Overgrown_Ivy.lua
@@ -4,13 +4,21 @@
 -----------------------------------
 function onMobInitialize(mob)
     mob:addMod(tpz.mod.DELAY, -360)
-    mob:addMod(tpz.mod.REGAIN, 50)
+    mob:setMod(tpz.mod.REGAIN, 50)
     mob:addMod(tpz.mod.DEFP, 20) 
     mob:addMod(tpz.mod.ATTP, 10)
     mob:addMod(tpz.mod.ACC, 30) 
     mob:addMod(tpz.mod.EVA, 30)
     mob:setMod(tpz.mod.REFRESH, 40)
 end
+
+function onMobFight( mob, target )
+    if mob:getHPP() >= 15 then
+         mob:setMod(tpz.mod.REGAIN, 3000)
+         mob:setMobMod(tpz.mobMod.SKILL_LIST, 1154)
+    end
+end
+
 
 function onMobDeath(mob, player, isKiller)
     if (player:getCurrentMission(COP) == tpz.mission.id.cop.THE_ROAD_FORKS and player:getCharVar("EMERALD_WATERS_Status") == 4) then
--- sql/mob_skill_lists.sql
+++ sql/mob_skill_lists.sql
@@ -3709,6 +3709,7 @@ INSERT INTO `mob_skill_lists` VALUES ('Doll-NM',1152,538);
 INSERT INTO `mob_skill_lists` VALUES ('Doll-NM',1152,539);
 INSERT INTO `mob_skill_lists` VALUES ('Doll-NM',1152,541);
 INSERT INTO `mob_skill_lists` VALUES ('DarkSpark',1153,4262); -- 50% Berserk
+INSERT INTO `mob_skill_lists` VALUES ('BadBreath',1154,319); -- Bad Breath Only
 
 
 -- Custom Content
