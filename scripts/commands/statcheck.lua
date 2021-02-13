01d2f1cbb6670bf2acfb17ce07110fb6fbc500d5
Shiyo <Shiyo@CELTY>
2021-02-12 21:51:14 -0500

    Update statcheck.lua

--- scripts/commands/statcheck.lua
+++ scripts/commands/statcheck.lua
@@ -35,7 +35,7 @@ function onTrigger(player)
        
         player:PrintToPlayer(string.format("%s Stats 0: MJOB: %u, SJOB: %u, HP: %u, MP: %u", targ:getName(), targ:getMainJob(), targ:getSubJob(), targ:getMaxHP(), targ:getMaxMP()))
         player:PrintToPlayer(string.format("%s Stats 1: LVL: %u, SLVL: %u, ACC: %u, EVA: %u, ATT: %u, DEF: %u, MATT: %u, MACC: %u, MDEF: %u ", targ:getName(), targ:getMainLvl(), targ:getSubLvl(), acc,eva,att,def,matt,macc,mdef))
-        player:PrintToPlayer(string.format("%s Stats 2: Delay: %u, STR: %u, DEX: %u, VIT: %u, AGI: %u, INT: %u, MND: %u, CHR: %u", targ:getName(), delay, str,dex,vit,agi,int,mnd,chr))
+        player:PrintToPlayer(string.format("%s Stats 2: DELAY: %u, STR: %u, DEX: %u, VIT: %u, AGI: %u, INT: %u, MND: %u, CHR: %u", targ:getName(), delay,str,dex,vit,agi,int,mnd,chr))
         player:PrintToPlayer(string.format("%s Stats 3: Fire: %f, Ice: %f, Wind: %f, Earth: %f, Water: %f, Thunder: %f, Dark: %f, Light: %f", targ:getName(), fire, ice, wind, earth, water, thunder, dark, light))
         player:PrintToPlayer(string.format("%s Stats 4: hthres: %f, pierceres: %f, impactres: %f, slashres: %f, spdefdown: %f", targ:getName(), hthres, pierceres, impactres, slashres, spdefdown))
     else
