d420b9ae23b3818f579f152b744cded546f3fe65
Shiyo <Shiyo@CELTY>
2021-02-12 20:32:33 -0500

    Update Dark_Spark.lua

--- scripts/zones/Castle_Zvahl_Baileys/mobs/Dark_Spark.lua
+++ scripts/zones/Castle_Zvahl_Baileys/mobs/Dark_Spark.lua
@@ -8,10 +8,7 @@ require("scripts/globals/keyitems")
 -----------------------------------
 
 function onMobSpawn(mob)
-     mob:addMod(tpz.mod.DEFP, 20) 
-     mob:addMod(tpz.mod.ATTP, 10)
-     mob:addMod(tpz.mod.ACC, 30) 
-     mob:addMod(tpz.mod.EVA, 30)
+
 end
 
 function onMobDeath(mob, player, isKiller)
