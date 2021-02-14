52d407d5f618b8f1f93d46dacfc8d782d58adc8e
Shiyo <Shiyo@CELTY>
2021-02-13 21:43:58 -0500

    Fire in the sky 1.2

--- scripts/zones/Monarch_Linn/mobs/Razon.lua
+++ scripts/zones/Monarch_Linn/mobs/Razon.lua
@@ -28,13 +28,17 @@ end
 function onMobFight(mob, target)
     if os.time() > mob:getLocalVar("selfDestruct1") and mob:getHPP() > 66  then
         mob:useMobAbility(4263) -- 500 damage
+        mob:AnimationSub(1)
     elseif os.time() > mob:getLocalVar("selfDestruct1") and mob:getHPP() < 66  then
         mob:useMobAbility(4264) -- 0 damage
+        mob:AnimationSub(1)
     end
     if os.time() > mob:getLocalVar("selfDestruct2") and mob:getHPP() > 33  then
         mob:useMobAbility(4263) -- 500 damage
+        mob:AnimationSub(2)
     elseif os.time() > mob:getLocalVar("selfDestruct2") and mob:getHPP() < 33  then
         mob:useMobAbility(4264) -- 0 damage
+        mob:AnimationSub(2)
     end
     if os.time() > mob:getLocalVar("selfDestruct3") and mob:getHPP() >= 1  then
         mob:useMobAbility(256) -- instant KO + fail
@@ -49,6 +53,7 @@ function onMagicHit(caster, target, spell)
         target:useMobAbility(624) -- 2 hour "cloud" animation
     elseif (ELEM == tpz.magic.dayElement[DAY] and (caster:isPC() or caster:isPet())) and if (tp >= 1000) then
         target:useMobAbility(4263)  -- 500 damage
+        mob:AnimationSub(1)
     end
     end
     return 1
