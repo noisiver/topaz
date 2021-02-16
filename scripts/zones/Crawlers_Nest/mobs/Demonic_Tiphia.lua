d5491a7d52f938edcad9910118f9814c55140512
Shiyo <Shiyo@CELTY>
2021-02-15 20:16:48 -0500

    Update Demonic_Tiphia.lua

--- scripts/zones/Crawlers_Nest/mobs/Demonic_Tiphia.lua
+++ scripts/zones/Crawlers_Nest/mobs/Demonic_Tiphia.lua
@@ -4,6 +4,13 @@
 -----------------------------------
 require("scripts/globals/hunts")
 
+function onMobSpawn(mob)
+     mob:addMod(tpz.mod.DEFP, 20) 
+     mob:addMod(tpz.mod.ATTP, 10)
+     mob:addMod(tpz.mod.ACC, 30) 
+     mob:addMod(tpz.mod.EVA, 30)
+end
+
 function onMobDeath(mob, player, isKiller)
     tpz.hunts.checkHunt(mob, player, 236)
 end
