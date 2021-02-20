36f6da793e94afd73d61a007548e28b019fb95d9
Shiyo <Shiyo@CELTY>
2021-02-19 21:29:22 -0500

    Added  2 hours to simurgh/roc and changed rocs job to WAR

--- scripts/zones/Rolanberry_Fields/mobs/Simurgh.lua
+++ scripts/zones/Rolanberry_Fields/mobs/Simurgh.lua
@@ -8,27 +8,29 @@ mixins = {require("scripts/mixins/job_special")}
 -----------------------------------
 
 function onMobSpawn(mob)
-tpz.mix.jobSpecial.config(mob, {
-    specials =
-    {
-        {
-            id = tpz.jsa.MIGHTY_STRIKES,
-            hpp = math.random(25,50),
-            begCode = function(mob)
-                mob:messageText(mob, ID.text.HOW_CAN_YOU_EXPECT_TO_KILL_ME)
-            end,
-            endCode = function(mob)
-                mob:messageText(mob, ID.text.WHEN_YOU_CANT_EVEN_HIT_ME)
-            end,
-        },
-    },
-})
      mob:addMod(tpz.mod.DEFP, 50) 
      mob:setMod(tpz.mod.ATT, 800)
      mob:setMod(tpz.mod.ACC, 430) 
      mob:addMod(tpz.mod.EVA, 50)
      mob:setMod(tpz.mod.MACC, 1000) 
      mob:setMobMod(tpz.mobMod.GIL_MIN, 20000)
+    tpz.mix.jobSpecial.config(mob, {
+        specials =
+        {
+            {
+                id = tpz.jsa.MIGHTY_STRIKES,
+                hpp = 25,
+                begCode = function(mob)
+                  
+                 -- mob:messageText(mob, ID.text.HOW_CAN_YOU_EXPECT_TO_KILL_ME)
+                 -- mob:PrintToArea("My power is too great for you!",0,"Murgleis")
+                end,
+                endCode = function(mob)
+               --mob:messageText(mob, ID.text.WHEN_YOU_CANT_EVEN_HIT_ME)
+                end,
+            },
+        },
+    })
 end
 
 function onMobDeath(mob, player, isKiller)
