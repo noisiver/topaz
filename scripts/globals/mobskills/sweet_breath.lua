8e965d87ca0ffdfd8a7026e180532192eb676133
Shiyo <Shiyo@CELTY>
2021-02-22 02:18:56 -0500

    Numerous TP move changes

--- scripts/globals/mobskills/sweet_breath.lua
+++ scripts/globals/mobskills/sweet_breath.lua
@@ -16,12 +16,11 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.SLEEP_I
-    
-	 skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, 1, 0, 30))
 
     local dmgmod = MobBreathMove(mob, target, 0.10, 1, tpz.magic.ele.WATER, 500)
     local dmg = MobFinalAdjustments(dmgmod, mob, skill, target, tpz.attackType.BREATH, tpz.damageType.WATER, MOBPARAM_IGNORE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.BREATH, tpz.damageType.WATER)
+    target:addStatusEffect(mob, target, typeEffect, 1, 0, 30)
 
     return dmg
 end
