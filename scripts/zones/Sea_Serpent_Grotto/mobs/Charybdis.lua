ca3f2a80c517afd0d224ed3f242b2a237e9545c3
Shiyo <Shiyo@CELTY>
2021-02-22 09:04:22 -0500

    Update Charybdis.lua

--- scripts/zones/Sea_Serpent_Grotto/mobs/Charybdis.lua
+++ scripts/zones/Sea_Serpent_Grotto/mobs/Charybdis.lua
@@ -9,7 +9,6 @@ require("scripts/globals/status")
 function onMobSpawn(mob)
      mob:addMod(tpz.mod.DEFP, 20) 
      mob:addMod(tpz.mod.ATTP, 10)
-     mob:addMod(tpz.mod.ACC, 30) 
      mob:addMod(tpz.mod.EVA, 30)
      mob:setMobMod(tpz.mobMod.GIL_MIN, 20000)
 end
