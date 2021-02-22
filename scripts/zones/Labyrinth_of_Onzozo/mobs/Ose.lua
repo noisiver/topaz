1d377aff4c3c6ca0030c8b253bad2b5be5adcdba
Shiyo <Shiyo@CELTY>
2021-02-21 21:49:04 -0500

    Update Ose.lua

--- scripts/zones/Labyrinth_of_Onzozo/mobs/Ose.lua
+++ scripts/zones/Labyrinth_of_Onzozo/mobs/Ose.lua
@@ -5,6 +5,13 @@
 require("scripts/globals/mobs")
 -----------------------------------
 
+function onMobSpawn(mob)
+    mob:addMod(tpz.mod.DEFP, 20) 
+    mob:addMod(tpz.mod.ATTP, 10)
+    mob:addMod(tpz.mod.ACC, 30) 
+    mob:addMod(tpz.mod.EVA, 30)
+end
+
 function onMobInitialize(mob)
     mob:setMobMod(tpz.mobMod.ADD_EFFECT, 1)
 end
