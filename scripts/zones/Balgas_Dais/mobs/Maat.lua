070877239a0b6e36e6af789fa966f1ccb192e072
Shiyo <Shiyo@CELTY>
2021-02-22 05:03:47 -0500

    RDM / SAM / WHM / BRD  Maat 1.0

--- scripts/zones/Balgas_Dais/mobs/Maat.lua
+++ scripts/zones/Balgas_Dais/mobs/Maat.lua
@@ -6,5 +6,13 @@
 mixins = {require("scripts/mixins/families/maat")}
 -----------------------------------
 
+function onMobSpawn(mob)
+    mob:addMod(tpz.mod.DEFP, 20) 
+    mob:addMod(tpz.mod.ATTP, 10)
+    mob:addMod(tpz.mod.ACC, 30) 
+    mob:addMod(tpz.mod.EVA, 30)
+    mob:setMod(tpz.mod.REFRESH, 40)
+end
+
 function onMobDeath(mob, player, isKiller)
 end
--- scripts/zones/Waughroon_Shrine/mobs/Maat.lua
+++ scripts/zones/Waughroon_Shrine/mobs/Maat.lua
@@ -6,5 +6,18 @@
 mixins = {require("scripts/mixins/families/maat")}
 -----------------------------------
 
+function onMobSpawn(mob)
+    mob:addMod(tpz.mod.DEFP, 20) 
+    mob:addMod(tpz.mod.ATTP, 10)
+    mob:addMod(tpz.mod.ACC, 30) 
+    mob:addMod(tpz.mod.EVA, 30)
+    mob:setMod(tpz.mod.REFRESH, 40)
+    mob:addStatusEffect(tpz.effect.PROTECT, 55, 0, 3600)
+    mob:addStatusEffect(tpz.effect.SHELL, 22, 0, 3600)
+    mob:addStatusEffect(tpz.effect.AQUAVEIL, 1, 0, 300)
+    mob:addStatusEffect(tpz.effect.HASTE, 15, 0, 180)
+    mob:addStatusEffect(tpz.effect.BLAZE_SPIKES, 15, 0, 300)
+end
+
 function onMobDeath(mob, player, isKiller)
 end
