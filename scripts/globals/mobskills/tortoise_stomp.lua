42b4a30c6445cea44a9c7fe74b40804951ab5219
Shiyo <Shiyo@CELTY>
2021-02-23 08:11:31 -0500

    Adamantoise TP move tuning

--- scripts/globals/mobskills/tortoise_stomp.lua
+++ scripts/globals/mobskills/tortoise_stomp.lua
@@ -19,12 +19,12 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local numhits = 1
     local accmod = 1
-    local dmgmod = 1.5
+    local dmgmod = 3
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT, info.hitslanded)
 
     local typeEffect = tpz.effect.DEFENSE_DOWN
-    MobPhysicalStatusEffectMove(mob, target, skill, typeEffect, 30, 0, 180)
+    MobPhysicalStatusEffectMove(mob, target, skill, typeEffect, 50, 0, 180)
 
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT)
     return dmg
