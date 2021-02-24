305bccb33ff208fc7e9168773555efb102c6391f
Shiyo <Shiyo@CELTY>
2021-02-23 23:31:29 -0500

    Added stats to mysticmaker

--- scripts/zones/Labyrinth_of_Onzozo/mobs/Mysticmaker_Profblix.lua
+++ scripts/zones/Labyrinth_of_Onzozo/mobs/Mysticmaker_Profblix.lua
@@ -6,6 +6,15 @@ mixins = {require("scripts/mixins/job_special")}
 require("scripts/globals/regimes")
 -----------------------------------
 
+function onMobSpawn(mob)
+    mob:addMod(tpz.mod.DEFP, 20) 
+    mob:addMod(tpz.mod.ATTP, 10)
+    mob:addMod(tpz.mod.ACC, 30) 
+    mob:addMod(tpz.mod.EVA, 30)
+    mob:setMod(tpz.mod.REFRESH, 40)
+    mob:setMobMod(tpz.mobMod.GIL_MIN, 6300)
+end
+
 function onMobDeath(mob, player, isKiller)
     tpz.regime.checkRegime(player, mob, 771, 2, tpz.regime.type.GROUNDS)
     tpz.regime.checkRegime(player, mob, 772, 2, tpz.regime.type.GROUNDS)
