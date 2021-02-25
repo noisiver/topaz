e5ba1615fa2c79c405e13bbe37d79e07661e7b26
Shiyo <Shiyo@CELTY>
2021-02-24 20:52:01 -0500

    Niddhogg debugging 1.71

--- scripts/zones/Dragons_Aery/mobs/Nidhogg.lua
+++ scripts/zones/Dragons_Aery/mobs/Nidhogg.lua
@@ -37,7 +37,7 @@ function onMobFight(mob, target)
         mob:setMod(tpz.mod.UDMGPHYS, -50)
         mob:setMod(tpz.mod.UDMGMAGIC, -5)
         mob:SetMobAbilityEnabled(false)
-        mob:AnimationSub(1)
+        mob:AnimationSub(2)
         mob:useMobAbility(1053) -- Legitimately captured super_buff ID
         mob:setLocalVar("wingsTime", battletime + math.random(20, 30))
         mob:setLocalVar("wingsDown", 1)
