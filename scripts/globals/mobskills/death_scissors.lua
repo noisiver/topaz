2fb65e2fa3c77717571042784748ae2fae385983
Shiyo <Shiyo@CELTY>
2021-02-25 04:57:57 -0500

    Critical bite/death scissors

--- scripts/globals/mobskills/death_scissors.lua
+++ scripts/globals/mobskills/death_scissors.lua
@@ -17,8 +17,8 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local numhits = 1
     local accmod = 1
-    local dmgmod = 2
-    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_DMG_VARIES, 1, 2, 3)
+    local dmgmod = 1.5
+    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_CRIT_VARIES, 1, 1.5, 2)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, info.hitslanded)
 
     -- keep damage around 700
