d74473fc271968380a30448fd9df0e9d1c8cc4d4
Shiyo <Shiyo@CELTY>
2021-02-25 04:07:45 -0500

    Razon debugging 1.0

--- scripts/zones/Monarch_Linn/mobs/Razon.lua
+++ scripts/zones/Monarch_Linn/mobs/Razon.lua
@@ -26,22 +26,27 @@ function onMobFight(mob, target)
     local boom = mob:getLocalVar("boom")
 
     if mob:getBattleTime() >= 30 and mob:getHPP() > 66 and boom == 0  then
+        printf("Boom 2");
         mob:useMobAbility(571) -- 650 damage
         mob:AnimationSub(1)
         mob:setLocalVar("boom", 1)
     elseif mob:getBattleTime() >= 30 and mob:getHPP() < 66 and boom == 0  then
+        printf("Boom 1 - 0 Dmg");
         mob:useMobAbility(574) -- 0 damage
         mob:AnimationSub(1)
         mob:setLocalVar("boom", 1)
     elseif mob:getBattleTime() >= 60 and mob:getHPP() > 33 and boom == 1  then
+        printf("Boom 2");
         mob:useMobAbility(572) -- 750 damage
         mob:AnimationSub(2)
         mob:setLocalVar("boom", 2)
     elseif mob:getBattleTime() >= 60 and mob:getHPP() < 33 and boom == 1  then
+        printf("Boom 2 - 0 DMG");
         mob:useMobAbility(574) -- 0 damage
         mob:AnimationSub(2)
         mob:setLocalVar("boom", 2)
     elseif mob:getBattleTime() >= 90 and mob:getHPP() >= 1 and boom == 2  then
+        printf("Boom 3 - Fail");
         mob:useMobAbility(573) -- 900 damage
         mob:setLocalVar("boom", 3)
     end
@@ -54,16 +59,20 @@ function onMagicHit(caster, target, spell)
     local elementboom = mob:getLocalVar("elementboom")
 
     if (ELEM == tpz.magic.dayElement[DAY] and (caster:isPC() or caster:isPet())) and elementboom == 0 then
+        printf("2 Hour Cloud");
         target:useMobAbility(624) -- 2 hour "cloud" animation
         mob:setLocalVar("elementboom", 1)
     elseif (ELEM == tpz.magic.dayElement[DAY] and (caster:isPC() or caster:isPet())) and elementboom == 1 then
+        printf("Cast Boom 1");
         target:useMobAbility(571)  -- 650 damage
         mob:AnimationSub(1)
         mob:setLocalVar("elementboom", 2)
     elseif (ELEM == tpz.magic.dayElement[DAY] and (caster:isPC() or caster:isPet())) and elementboom == 2 then
+        printf("Cast Boom 2");
         target:useMobAbility(572)  -- 650 damage
         mob:AnimationSub(1)
         mob:setLocalVar("elementboom", 3)
+        printf("Cast Boom 3");
     elseif (ELEM == tpz.magic.dayElement[DAY] and (caster:isPC() or caster:isPet())) and elementboom == 3 then
         target:useMobAbility(573)  -- 900 damage
         mob:AnimationSub(1)
