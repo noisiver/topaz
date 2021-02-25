589eddbc94e1d901e89d53dfb83c04fd166160f9
Shiyo <Shiyo@CELTY>
2021-02-25 04:12:02 -0500

    Hamadryad bugtesting 1.0

--- scripts/zones/Monarch_Linn/mobs/Hamadryad.lua
+++ scripts/zones/Monarch_Linn/mobs/Hamadryad.lua
@@ -12,14 +12,14 @@ local buffone = tpz.effect.PHALANX
 local bufftwo = tpz.effect.BLAZE_SPIKES
 
 function onMobSpawn(mob)
-     mob:addMod(tpz.mod.DEFP, 20) 
-     mob:addMod(tpz.mod.ATTP, 10)
-     mob:addMod(tpz.mod.ACC, 30) 
-     mob:addMod(tpz.mod.EVA, 100)
-     mob:addStatusEffect(tpz.effect.REGEN, 25, 3, 0)
-     mob:addStatusEffect(tpz.effect.PHALANX, 50, 0, 0)
-     local startingbuff = mob:getStatusEffect(buffone)
-     startingbuff:unsetFlag(tpz.effectFlag.DISPELABLE)
+    local startingbuff = mob:getStatusEffect(buffone)
+    mob:addMod(tpz.mod.DEFP, 20) 
+    mob:addMod(tpz.mod.ATTP, 10)
+    mob:addMod(tpz.mod.ACC, 30) 
+    mob:addMod(tpz.mod.EVA, 100)
+    mob:addStatusEffect(tpz.effect.REGEN, 25, 3, 0)
+    mob:addStatusEffect(tpz.effect.PHALANX, 50, 0, 0)
+    startingbuff:unsetFlag(tpz.effectFlag.DISPELABLE)
 end
 
 function onMobEngaged(mob, target)
@@ -35,23 +35,26 @@ function onMobFight(mob, target)
 
     if phalanxTime == 0 then
         mob:setLocalVar("Phalanx", math.random(2, 3)*15)
+        printf("Set Phalanx Time");
         return
-    end
-    if mob:getBattleTime() >= phalanxTime then
-        mob:addStatusEffect(tpz.effect.PHALANX, 50, 0, 0)
+    elseif mob:getBattleTime() >= phalanxTime then
+        mob:addStatusEffect(tpz.effect.PHALANX, 50, 0, 3600)
         local buff1 = mob:getStatusEffect(buffone)
         mob:setLocalVar("Phalanx", mob:getBattleTime() + math.random(2, 3)*15)
         buff1:unsetFlag(tpz.effectFlag.DISPELABLE)
+        printf("Add Phalanx Buff");
     end
     if spikesTime == 0 then
-        mob:setLocalVar("Phalanx", math.random(2, 3)*30)
+        mob:setLocalVar("Blazespikes", math.random(2, 3)*30)
+        printf("Set Spikes Time");
         return
     end
-    if mob:getBattleTime() >= spikesTime then
-        mob:addStatusEffect(tpz.effect.BLAZE_SPIKES, 25, 0, 0)
+    elseif mob:getBattleTime() >= spikesTime then
+        mob:addStatusEffect(tpz.effect.BLAZE_SPIKES, 25, 0, 3600)
         local buff2 = mob:getStatusEffect(bufftwo)
         mob:setLocalVar("Blazespikes", mob:getBattleTime() + math.random(2, 3)*30)
         buff2:unsetFlag(tpz.effectFlag.DISPELABLE)
+        printf("Add Blaze Spikes Buff");
     end
 end
 
@@ -59,8 +62,10 @@ function onMagicHit(caster, target, spell)
     local DAY = VanadielDayOfTheWeek()
     local ELEM = spell:getElement()
     if (ELEM == tpz.magic.dayElement[DAY] and (caster:isPC() or caster:isPet())) then
-        target:delStatusEffect(106)
-        target:delStatusEffect(249)  
+        target:delStatusEffect(116) -- Phalanx
+        target:delStatusEffect(34) -- Blaze spikes
+        printf("Delete Buffs");
+    --eald_narche:delStatusEffect(tpz.effect.PHYSICAL_SHIELD, 0, 1, 0, 0)
     end
     return 1
 end
