ab99732e91f9d4eb3efb4e15fd2b7368717ce9e5
Shiyo <Shiyo@CELTY>
2021-02-27 21:28:27 -0500

    Added stat boosts to enspells

--- scripts/globals/effects/enblizzard_ii.lua
+++ scripts/globals/effects/enblizzard_ii.lua
@@ -9,6 +9,7 @@ require("scripts/globals/status")
 function onEffectGain(target, effect)
     target:addMod(tpz.mod.ENSPELL, tpz.magic.element.ICE + 8) -- Tier IIs have higher "enspell IDs"
     target:addMod(tpz.mod.ENSPELL_DMG, 14 + effect:getPower())
+    target:addMod(tpz.mod.STORETP, 5)
 end
 
 function onEffectTick(target, effect)
@@ -17,4 +18,5 @@ end
 function onEffectLose(target, effect)
     target:setMod(tpz.mod.ENSPELL_DMG, 0)
     target:setMod(tpz.mod.ENSPELL, 0)
+    target:delMod(tpz.mod.STORETP, 5)
 end
