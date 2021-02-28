17d7f9c1f33e44a8991ecbd202c960322e954948
Shiyo <Shiyo@CELTY>
2021-02-28 07:02:50 -0500

    Update self-destruct_3.lua

--- scripts/globals/mobskills/self-destruct_3.lua
+++ scripts/globals/mobskills/self-destruct_3.lua
@@ -11,8 +11,12 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
+    local HP = mob:getHP()
     local amount = 650 * skill:getTotalTargets()
     local dmg = MobFinalAdjustments(amount, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.FIRE, MOBPARAM_WIPE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.FIRE)
+    if HP < 2625 then
+       dmg = 0
+    end
     return dmg
 end
