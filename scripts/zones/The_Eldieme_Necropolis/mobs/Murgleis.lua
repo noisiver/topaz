c79f2818f7e77280d32842e614a0abfdb6bfa38e
Shiyo <Shiyo@CELTY>
2021-03-01 05:16:57 -0500

    Murg debugging 1.4

--- scripts/zones/The_Eldieme_Necropolis/mobs/Murgleis.lua
+++ scripts/zones/The_Eldieme_Necropolis/mobs/Murgleis.lua
@@ -10,6 +10,7 @@ require("scripts/globals/status")
 ------------------------------
 
 function onMobSpawn(mob)
+    mob:setMod(tpz.mod.DOUBLE_ATTACK, 30)
     mob:addMod(tpz.mod.DEFP, 20) 
     mob:addMod(tpz.mod.ATTP, 10)
     mob:addMod(tpz.mod.ACC, 30) 
@@ -28,19 +29,11 @@ function onMobFight(mob, target)
         mob:setMod(tpz.mod.DOUBLE_ATTACK, 0)
         mob:addMod(tpz.mod.ATT, -400)
         mob:setMobMod(tpz.mobMod.MAGIC_COOL, 0)
-        mob:setMobMod(tpz.mobMod.SPELL_LIST, 446)
+        mob:setMod(tpz.mod.UFASTCAST, 50)
         mob:addMod(tpz.mod.MATT, 30)
         mob:setLocalVar("TriggerHit", 1)
     end
     if mob:getHPP() <= 80 and hitTrigger == 1 then
-        printf("Phys");
-        mob:SetMagicCastingEnabled(false)
-        mob:addMod(tpz.mod.MATT, -30)
-        mob:addMod(tpz.mod.ATT, 400)
-        mob:setMod(tpz.mod.DOUBLE_ATTACK, 30)
-        mob:setLocalVar("TriggerHit", 2)
-    end
-    if mob:getHPP() <= 70 and hitTrigger == 2 then
         printf("Regenning");
         mob:setMod(tpz.mod.REGEN, 300)
         mob:addStatusEffect(tpz.effect.BLAZE_SPIKES, 200, 0, 3600)
@@ -49,13 +42,22 @@ function onMobFight(mob, target)
         mob:SetMobAbilityEnabled(false)
         mob:setLocalVar("TriggerHit", 3)
     end
+    if mob:getHPP() <= 70 and hitTrigger == 2 then
+        printf("Phys");
+        mob:SetMagicCastingEnabled(false)
+        mob:addMod(tpz.mod.MATT, -30)
+        mob:addMod(tpz.mod.ATT, 400)
+        mob:setMod(tpz.mod.DOUBLE_ATTACK, 30)
+        mob:setLocalVar("TriggerHit", 2)
+        mob:setLocalVar("TriggerHit", 3)
+    end
     if mob:getHPP() <= 60 and hitTrigger == 3 then
         mob:SetMagicCastingEnabled(true)
         printf("Magic");
         mob:setMod(tpz.mod.DOUBLE_ATTACK, 0)
         mob:addMod(tpz.mod.ATT, -400)
         mob:setMobMod(tpz.mobMod.MAGIC_COOL, 0)
-        mob:setMobMod(tpz.mobMod.SPELL_LIST, 446)
+        mob:setMod(tpz.mod.UFASTCAST, 50)
         mob:addMod(tpz.mod.MATT, 30)
         mob:setLocalVar("TriggerHit", 4)
     end
@@ -83,7 +85,7 @@ function onMobFight(mob, target)
         mob:setMod(tpz.mod.DOUBLE_ATTACK, 0)
         mob:addMod(tpz.mod.ATT, -400)
         mob:setMobMod(tpz.mobMod.MAGIC_COOL, 0)
-        mob:setMobMod(tpz.mobMod.SPELL_LIST, 446)
+        mob:setMod(tpz.mod.UFASTCAST, 50)
         mob:addMod(tpz.mod.MATT, 30)
         mob:setLocalVar("TriggerHit", 7)
     end
