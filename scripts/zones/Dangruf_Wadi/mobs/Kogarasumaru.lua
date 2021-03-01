3bcc79ef24b159534ba84f8bd4744219ff74fd7f
Shiyo <Shiyo@CELTY>
2021-03-01 03:10:58 -0500

    Koga debugging 1.0

--- scripts/zones/Dangruf_Wadi/mobs/Kogarasumaru.lua
+++ scripts/zones/Dangruf_Wadi/mobs/Kogarasumaru.lua
@@ -11,12 +11,11 @@ mixins = {require("scripts/mixins/job_special")}
 ------------------------------
 
 function onMobSpawn(mob)
-    mob:setMod(tpz.mod.STORETP, 50)
     mob:addMod(tpz.mod.DEFP, 20) 
     mob:addMod(tpz.mod.ATTP, 10)
     mob:addMod(tpz.mod.ACC, 30) 
     mob:addMod(tpz.mod.EVA, 30)
-    mob:setMod(tpz.mod.COUNTER, 10)
+    mob:AnimationSub(0)
     tpz.mix.jobSpecial.config(mob, {
         specials =
         {
@@ -24,12 +23,10 @@ function onMobSpawn(mob)
                 id = tpz.jsa.MEIKYO_SHISUI,
                 hpp = 25,
                 begCode = function(mob)
-                mob:setMobMod(tpz.mobMod.SKILL_LIST, 2002)
                 mob:messageText(mob, ID.text.HOW_CAN_YOU_EXPECT_TO_KILL_ME)
                -- mob:PrintToArea("My power is too great for you!",0,"Murgleis")
                end,
                endCode = function(mob)
-               mob:setMobMod(tpz.mobMod.SKILL_LIST, 2002)
                mob:messageText(mob, ID.text.WHEN_YOU_CANT_EVEN_HIT_ME)
                end,
             },
@@ -38,16 +35,35 @@ function onMobSpawn(mob)
 end
 
 function onMobFight(mob, target)
-    local hitTrigger = mob:getLocalVar("TriggerHit")
-    if mob:getHPP() <= 50 and hitTrigger == 0 then
-        mob:setMod(tpz.mod.STORETP, 75)
-        mob:setMod(tpz.mod.HASTE_GEAR, 2500)
-        mob:setLocalVar("TriggerHit", 1)
+    local battletime = mob:getBattleTime()
+    local twohourTime = mob:getLocalVar("twohourTime")
+    local STANCEdps = mob:getLocalVar("STANCEdps")
+    local STANCEtank = mob:getLocalVar("STANCEtank")
+
+    if twohourTime == 0 then
+        printf("Setting two hour time");
+        mob:setLocalVar("twohourTime", math.random(10, 15))
+    elseif battletime >= twohourTime and STANCEtank == 0 then
+        printf("DPS Stance");
+        mob:setMod(tpz.mod.COUNTER, 0)
+        mob:setMod(tpz.mod.HASTE_MAGIC, mob:getMod(tpz.mod.HASTE_MAGIC) + 0)
+        mob:setMod(tpz.mod.DOUBLE_ATTACK, 50)
+        mob:addMod(tpz.mod.ATT, 300)
+        player:PrintToPlayer("You cannot withstand my might!",0,"Kogarasumaru")
+        mob:setLocalVar("STANCEdps", battletime + math.random(15, 16))
+        mob:setLocalVar("STANCEtank", 1)
     end
-    if mob:getHPP() <= 25 and hitTrigger == 1 then
-        mob:setMod(tpz.mod.STORETP, 100)
-        mob:setMod(tpz.mod.HASTE_GEAR, 5000)
-        mob:setLocalVar("TriggerHit", 2)
+
+    if battletime >= STANCEdps and STANCEtank == 1 then
+        printf("Tank Stance");
+        mob:setMod(tpz.mod.DOUBLE_ATTACK, 0)
+        mob:addMod(tpz.mod.ATT, -300)
+        mob:setMod(tpz.mod.COUNTER, 50)
+        mob:setMod(tpz.mod.HASTE_MAGIC, mob:getMod(tpz.mod.HASTE_MAGIC) - 7500)
+        player:PrintToPlayer("Go ahead, try and hit me.",0,"Kogarasumaru")
+        mob:setLocalVar("twohourTime", battletime + math.random(15, 16))
+        mob:setLocalVar("STANCEdps", 0)
+        mob:setLocalVar("STANCEtank", 0)
     end
 end
 
