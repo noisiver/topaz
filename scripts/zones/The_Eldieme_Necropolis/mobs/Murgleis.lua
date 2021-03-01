53d7e8b45fddced9d03f166fdc6aa396c9a21c6f
Shiyo <Shiyo@CELTY>
2021-03-01 04:35:10 -0500

    Murgleis debugging 1.0

--- scripts/zones/The_Eldieme_Necropolis/mobs/Murgleis.lua
+++ scripts/zones/The_Eldieme_Necropolis/mobs/Murgleis.lua
@@ -16,41 +16,92 @@ function onMobSpawn(mob)
     mob:addMod(tpz.mod.ACC, 30) 
     mob:addMod(tpz.mod.EVA, 30)
     mob:setMod(tpz.mod.REFRESH, 40)
-    mob:addStatusEffect(tpz.effect.PROTECT, 175, 0, 3600)
-    mob:addStatusEffect(tpz.effect.SHELL, 25, 0, 3600)
-    mob:addStatusEffect(tpz.effect.PHALANX, 30, 0, 180)
-    mob:addStatusEffect(tpz.effect.TEMPER, 15, 0, 180)
-    mob:addStatusEffect(tpz.effect.HASTE, 30, 0, 180)
-    mob:addStatusEffect(tpz.effect.BLAZE_SPIKES, 30, 0, 180)
     mob:AnimationSub(3)
-    tpz.mix.jobSpecial.config(mob, {
-        specials =
-        {
-            {
-                id = tpz.jsa.MIGHTY_STRIKES,
-                hpp = 25,
-                begCode = function(mob)
-                  
-                 -- mob:messageText(mob, ID.text.HOW_CAN_YOU_EXPECT_TO_KILL_ME)
-                 -- mob:PrintToArea("My power is too great for you!",0,"Murgleis")
-                end,
-                endCode = function(mob)
-               --mob:messageText(mob, ID.text.WHEN_YOU_CANT_EVEN_HIT_ME)
-                end,
-            },
-        },
-    })
 end
 
 function onMobFight(mob, target)
     local hitTrigger = mob:getLocalVar("TriggerHit")
-    if mob:getHPP() <= 50 and hitTrigger == 0 then
-        mob:setMod(tpz.mod.UFASTCAST, 50)
-         mob:setLocalVar("TriggerHit", 1)
+
+    if mob:getHPP() <= 90 and hitTrigger == 0 then
+        printf("Magic");
+        mob:setMod(tpz.mod.DOUBLE_ATTACK, 0)
+        mob:addMod(tpz.mod.ATT, -400)
+        mob:setMobMod(tpz.mobMod.MAGIC_COOL, 0)
+        mob:setMobMod(tpz.mobMod.SPELL_LIST, 446)
+        mob:addMod(tpz.mod.MATT, 30)
+        mob:setLocalVar("TriggerHit", 1)
     end
-    if mob:getHPP() <= 25 and hitTrigger == 1 then
-        mob:setMod(tpz.mod.UFASTCAST, 75)
+    if mob:getHPP() <= 80 and hitTrigger == 1 then
+        printf("Phys");
+        mob:setMobMod(tpz.mobMod.MAGIC_COOL, 13)
+        mob:setMobMod(tpz.mobMod.SPELL_LIST, 445)
+        mob:addMod(tpz.mod.MATT, -30)
+        mob:addMod(tpz.mod.ATT, 400)
+        mob:setMod(tpz.mod.DOUBLE_ATTACK, 30)
         mob:setLocalVar("TriggerHit", 2)
+    end
+    if mob:getHPP() <= 70 and hitTrigger == 2 then
+        printf("Regenning");
+        mob:setMod(tpz.mod.REGEN, 300)
+        mob:addStatusEffect(tpz.effect.BLAZE_SPIKES, 200, 0, 3600)
+        mob:setMobMod(tpz.mobMod.NO_MOVE, 1)
+        mob:SetAutoAttackEnabled(false)
+        mob:SetMobAbilityEnabled(false)
+        mob:setLocalVar("TriggerHit", 3)
+    end
+    if mob:getHPP() <= 60 and hitTrigger == 3 then
+        printf("Magic");
+        mob:setMod(tpz.mod.DOUBLE_ATTACK, 0)
+        mob:addMod(tpz.mod.ATT, -400)
+        mob:setMobMod(tpz.mobMod.MAGIC_COOL, 0)
+        mob:setMobMod(tpz.mobMod.SPELL_LIST, 446)
+        mob:addMod(tpz.mod.MATT, 30)
+        mob:setLocalVar("TriggerHit", 4)
+    end
+    if mob:getHPP() <= 50 and hitTrigger == 4 then
+        printf("Regenning");
+        mob:setMod(tpz.mod.REGEN, 300)
+        mob:addStatusEffect(tpz.effect.BLAZE_SPIKES, 200, 0, 3600)
+        mob:setMobMod(tpz.mobMod.NO_MOVE, 1)
+        mob:SetAutoAttackEnabled(false)
+        mob:SetMobAbilityEnabled(false)
+        mob:setLocalVar("TriggerHit", 5)
+    end
+    if mob:getHPP() <= 40 and hitTrigger == 5 then
+        printf("Phys");
+        mob:setMobMod(tpz.mobMod.MAGIC_COOL, 13)
+        mob:setMobMod(tpz.mobMod.SPELL_LIST, 445)
+        mob:addMod(tpz.mod.MATT, -30)
+        mob:addMod(tpz.mod.ATT, 400)
+        mob:setMod(tpz.mod.DOUBLE_ATTACK, 30)
+        mob:setLocalVar("TriggerHit", 6)
+    end
+    if mob:getHPP() <= 30 and hitTrigger == 6 then
+        printf("Magic");
+        mob:setMod(tpz.mod.DOUBLE_ATTACK, 0)
+        mob:addMod(tpz.mod.ATT, -400)
+        mob:setMobMod(tpz.mobMod.MAGIC_COOL, 0)
+        mob:setMobMod(tpz.mobMod.SPELL_LIST, 446)
+        mob:addMod(tpz.mod.MATT, 30)
+        mob:setLocalVar("TriggerHit", 7)
+    end
+    if mob:getHPP() <= 20 and hitTrigger == 7 then
+        printf("Regenning");
+        mob:setMod(tpz.mod.REGEN, 300)
+        mob:addStatusEffect(tpz.effect.BLAZE_SPIKES, 200, 0, 3600)
+        mob:setMobMod(tpz.mobMod.NO_MOVE, 1)
+        mob:SetAutoAttackEnabled(false)
+        mob:SetMobAbilityEnabled(false)
+        mob:setLocalVar("TriggerHit", 8)
+    end
+    if mob:getHPP() <= 10 and hitTrigger == 8 then
+        printf("Phys");
+        mob:setMobMod(tpz.mobMod.MAGIC_COOL, 13)
+        mob:setMobMod(tpz.mobMod.SPELL_LIST, 445)
+        mob:addMod(tpz.mod.MATT, -30)
+        mob:addMod(tpz.mod.ATT, 400)
+        mob:setMod(tpz.mod.DOUBLE_ATTACK, 30)
+        mob:setLocalVar("TriggerHit", 0)
     end
 end
 
@@ -66,6 +117,23 @@ function onMonsterMagicPrepare(mob, target)
     else
         return 493 -- Temper
     end
+end
+
+function onMagicHit(caster, target, spell)
+    local DAY = target:getLocalVar("RNGelement")
+    local ELEM = spell:getElement()
+    if DAY == 0 then
+         target:setLocalVar("RNGelement", math.random(1,8))
+    elseif (ELEM == tpz.magic.dayElement[DAY] and (caster:isPC() or caster:isPet())) then
+        target:delStatusEffect(34) -- Blaze spikes
+        target:setMod(tpz.mod.REGEN, 0)
+        target:setMobMod(tpz.mobMod.NO_MOVE, 0)
+        target:SetAutoAttackEnabled(true)
+        target:SetMobAbilityEnabled(true)
+        printf("Delete Buffs, Reset Element");
+        target:setLocalVar("RNGelement", math.random(1,8))
+    end
+    return 1
 end
 
 function onMobDeath(mob, player, isKiller)
