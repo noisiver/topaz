4594d4ca73127469499481f0b6e4f20cfc306253
Shiyo <Shiyo@CELTY>
2021-03-01 00:53:36 -0500

    Beloved of the Atlantes debugging 1.21

--- scripts/zones/Monarch_Linn/mobs/Guard_Hippogryph.lua
+++ scripts/zones/Monarch_Linn/mobs/Guard_Hippogryph.lua
@@ -13,6 +13,7 @@ function onMobSpawn(mob)
      mob:addMod(tpz.mod.ATTP, 10)
      mob:addMod(tpz.mod.ACC, 30) 
      mob:addMod(tpz.mod.EVA, 30)
+     mob:setMobMod(tpz.mobMod.NO_DROPS)
 end
 
 function onMobInitialize(mob)
@@ -21,4 +22,7 @@ end
 
 function onAdditionalEffect(mob, target, damage)
     return tpz.mob.onAddEffect(mob, target, damage, tpz.mob.ae.HP_DRAIN, {chance = 25, power = math.random(80, 120)})
+end
+
+function onMobDeath(mob, player, isKiller)
 end
--- scripts/zones/Monarch_Linn/mobs/Watch_Hippogryph.lua
+++ scripts/zones/Monarch_Linn/mobs/Watch_Hippogryph.lua
@@ -13,11 +13,14 @@ function onMobSpawn(mob)
      mob:addMod(tpz.mod.ATTP, 10)
      mob:addMod(tpz.mod.ACC, 30) 
      mob:addMod(tpz.mod.EVA, 30)
+     mob:setMobMod(tpz.mobMod.NO_DROPS)
 end
 
 function onMobFight(mob, target)
     local hitTrigger = mob:getLocalVar("TriggerHit")
     local Guard = GetMobByID(mob:getID()+1)
+    local Guardtwo = GetMobByID(mob:getID()+3)
+    local Guardthree = GetMobByID(mob:getID()+5)
 
     if mob:getHPP() <= 75 and hitTrigger == 0 then
         Guard:spawn()
@@ -26,16 +29,16 @@ function onMobFight(mob, target)
         printf("Spawning Guard Hippo #1");
     end
     if mob:getHPP() <= 50 and hitTrigger == 1 then
-        Guard:spawn()
+        Guardtwo:spawn()
         Guard:updateEnmity(target)
         mob:setLocalVar("TriggerHit", 2)
-        printf("Spawning Guard Hippo #1");
+        printf("Spawning Guard Hippo #2");
     end
     if mob:getHPP() <= 25 and hitTrigger == 2 then
-        Guard:spawn()
+        Guardthree:spawn()
         Guard:updateEnmity(target)
         mob:setLocalVar("TriggerHit", 3)
-        printf("Spawning Guard Hippo #1");
+        printf("Spawning Guard Hippo #3");
     end
 end
 
