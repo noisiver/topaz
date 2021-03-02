43b6a7039b3ac53911763b2121f6aee0bcdbdac8
Shiyo <Shiyo@CELTY>
2021-03-01 22:37:33 -0500

    Removed dmg bonus off SA/WS

--- scripts/globals/effects/sneak_attack.lua
+++ scripts/globals/effects/sneak_attack.lua
@@ -5,14 +5,10 @@
 -----------------------------------
 
 function onEffectGain(target, effect)
-target:addMod(tpz.mod.ALL_WSDMG_FIRST_HIT , 50)
-target:addMod(tpz.mod.MATT , -50)
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-target:addMod(tpz.mod.ALL_WSDMG_FIRST_HIT , -50)
-target:addMod(tpz.mod.MATT , 50)
 end
--- scripts/globals/effects/trick_attack.lua
+++ scripts/globals/effects/trick_attack.lua
@@ -5,18 +5,10 @@
 -----------------------------------
 
 function onEffectGain(target, effect)
-    if (player:getMainJob() == tpz.job.THF) then
-        target:addMod(tpz.mod.MATT , -50)
-        target:addMod(tpz.mod.ALL_WSDMG_FIRST_HIT , 50)
-    end
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-    if (player:getMainJob() == tpz.job.THF) then
-        target:addMod(tpz.mod.MATT , 50)
-        target:addMod(tpz.mod.ALL_WSDMG_FIRST_HIT , -50)
-    end
 end
