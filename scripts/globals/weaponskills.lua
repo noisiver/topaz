e400d6e0b9f427630f1a42282d29b9cf0a5877ea
Shiyo <Shiyo@CELTY>
2021-03-01 21:28:02 -0500

    Crit dmg added to SA/ms WS

--- scripts/globals/weaponskills.lua
+++ scripts/globals/weaponskills.lua
@@ -32,7 +32,7 @@ function getSingleHitDamage(attacker, target, dmg, wsParams, calcParams)
             criticalHit = (wsParams.canCrit and critChance <= calcParams.critRate) or
                           calcParams.forcedFirstCrit or calcParams.mightyStrikesApplicable
             if criticalHit then
-                pdif = 1  + getMod(tpz.mod.CRIT_DMG_INCREASE) 
+                pdif = 1  + (getMod(tpz.mod.CRIT_DMG_INCREASE) / 1000 )
                 calcParams.criticalHit = true
                 calcParams.pdif = generatePdif (calcParams.ccritratio[1], calcParams.ccritratio[2], true)
             else
