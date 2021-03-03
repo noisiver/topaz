dd7835e272359fe0a3523168ffd85449cdc75fa1
Shiyo <Shiyo@CELTY>
2021-03-03 00:04:11 -0500

    (Promy) Empty drop testing based on element

--- scripts/mixins/families/empty.lua
+++ scripts/mixins/families/empty.lua
@@ -8,26 +8,26 @@ g_mixins.families.empty = function(mob)
 
     mob:addListener("ITEM_DROPS", "EMPTY_ITEM_DROPS", function(mob)
         local element = mob:getLocalVar("element")
-        if element == 1 and math.random(1,100) <= 24 then
+        if element == 1 and math.random(1,100) <= 100 then
             mob:addTreasure(1613) -- Malevolent Memory (darkness skillchain)
-        elseif element == 2 and math.random(1,100) <= 24 then
+        elseif element == 2 and math.random(1,100) <= 100 then
             mob:addTreasure(1611) -- Somber Memory (water)
             mob:addTreasure(1613) -- Malevolent Memory (darkness skillchain)
-        elseif element == 3 and math.random(1,100) <= 24 then
+        elseif element == 3 and math.random(1,100) <= 100 then
             mob:addTreasure(1610) -- Startling Memory (lightning)
             mob:addTreasure(1612) -- Radiant Memory (light skillchain)
-        elseif element == 4 and math.random(1,100) <= 24 then
+        elseif element == 4 and math.random(1,100) <= 100 then
             mob:addTreasure(1609) -- Profane Memory (earth)
             mob:addTreasure(1613) -- Malevolent Memory (darkness skillchain)
-        elseif element == 5 and math.random(1,100) <= 24 then
+        elseif element == 5 and math.random(1,100) <= 100 then
             mob:addTreasure(1612) -- Radiant Memory (light skillchain)
-        elseif element == 6 and math.random(1,100) <= 24 then
+        elseif element == 6 and math.random(1,100) <= 100 then
             mob:addTreasure(1606) -- Burning Memory (fire)
             mob:addTreasure(1612) -- Radiant Memory (light skillchain)
-        elseif element == 7 and math.random(1,100) <= 24 then
+        elseif element == 7 and math.random(1,100) <= 100 then
             mob:addTreasure(1607) -- Bitter Memory (ice)
             mob:addTreasure(1613) -- Malevolent Memory (darkness skillchain)
-        elseif element == 8 and math.random(1,100) <= 24 then
+        elseif element == 8 and math.random(1,100) <= 100 then
             mob:addTreasure(1608) -- Fleeting Memory (wind)
             mob:addTreasure(1612) -- Radiant Memory (light skillchain)
         end
