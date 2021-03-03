222b0d581f206b0ec062660bcf0ffd13d9066cbd
Shiyo <Shiyo@CELTY>
2021-03-03 01:01:31 -0500

    (Promy) element drop 1.2 debugging

--- scripts/mixins/families/empty.lua
+++ scripts/mixins/families/empty.lua
@@ -6,38 +6,38 @@ g_mixins.families = g_mixins.families or {}
 
 g_mixins.families.empty = function(mob)
 
-    mob:addListener("ITEM_DROPS", "EMPTY_ITEM_DROPS", function(mob, player)
+    mob:addListener("DEATH", "DYNAMIS_ITEM_DISTRIBUTION", function(mob, killer)
         local element = mob:getLocalVar("element")
         if element == 1  then
             printf("Dark");
-            player:addItem(1613) -- Malevolent Memory (darkness skillchain)
+            killer:addItem(1613) -- Malevolent Memory (darkness skillchain)
         elseif element == 2 then
             printf("Water");
-            player:addItem(1611) -- Somber Memory (water)
-            player:addItem(1613) -- Malevolent Memory (darkness skillchain)
+            killer:addItem(1611) -- Somber Memory (water)
+            killer:addItem(1613) -- Malevolent Memory (darkness skillchain)
         elseif element == 3 then
             printf("Thunder");
-            player:addItem(1610) -- Startling Memory (lightning)
-            player:addItem(1612) -- Radiant Memory (light skillchain)
+            killer:addItem(1610) -- Startling Memory (lightning)
+            killer:addItem(1612) -- Radiant Memory (light skillchain)
         elseif element == 4  then
             printf("Earth");
-            player:addItem(1609) -- Profane Memory (earth)
-            player:addItem(1613) -- Malevolent Memory (darkness skillchain)
+            killer:addItem(1609) -- Profane Memory (earth)
+            killer:addItem(1613) -- Malevolent Memory (darkness skillchain)
         elseif element == 5 then
             printf("Light");
-            player:addItem(1612) -- Radiant Memory (light skillchain)
+            killer:addItem(1612) -- Radiant Memory (light skillchain)
         elseif element == 6  then
             printf("Fire");
-            player:addItem(1606) -- Burning Memory (fire)
-            player:addItem(1612) -- Radiant Memory (light skillchain)
+            killer:addItem(1606) -- Burning Memory (fire)
+            killer:addItem(1612) -- Radiant Memory (light skillchain)
         elseif element == 7 then
             printf("Ice");
-            player:addItem(1607) -- Bitter Memory (ice)
-            player:addItem(1613) -- Malevolent Memory (darkness skillchain)
+            killer:addItem(1607) -- Bitter Memory (ice)
+            killer:addItem(1613) -- Malevolent Memory (darkness skillchain)
         elseif element == 8 then
             printf("Wind");
-            player:addItem(1608) -- Fleeting Memory (wind)
-            player:addItem(1612) -- Radiant Memory (light skillchain)
+            killer:addItem(1608) -- Fleeting Memory (wind)
+            killer:addItem(1612) -- Radiant Memory (light skillchain)
         end
     end)
 end
