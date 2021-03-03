76c4d9e36d6e9da105485678f6a2dc79904d3a2c
Shiyo <Shiyo@CELTY>
2021-03-03 00:22:49 -0500

    (Promy) Element drops 1.14

--- scripts/mixins/families/empty.lua
+++ scripts/mixins/families/empty.lua
@@ -8,26 +8,26 @@ g_mixins.families.empty = function(mob)
 
     mob:addListener("ITEM_DROPS", "EMPTY_ITEM_DROPS", function(mob, killer)
         local element = mob:getLocalVar("element")
-        if element == 1 and math.random(1,100) <= 99 then
+        if element == 1 and killer and math.random(1,100) <= 99 then
             killer:addTreasure(1613) -- Malevolent Memory (darkness skillchain)
-        elseif element == 2 and math.random(1,100) <= 99 then
+        elseif element == 2 and killer and math.random(1,100) <= 99 then
             killer:addTreasure(1611) -- Somber Memory (water)
             killer:addTreasure(1613) -- Malevolent Memory (darkness skillchain)
-        elseif element == 3 and math.random(1,100) <= 99 then
+        elseif element == 3 and killer and math.random(1,100) <= 99 then
             killer:addTreasure(1610) -- Startling Memory (lightning)
             killer:addTreasure(1612) -- Radiant Memory (light skillchain)
-        elseif element == 4 and math.random(1,100) <= 99 then
+        elseif element == 4 and killer and math.random(1,100) <= 99 then
             killer:addTreasure(1609) -- Profane Memory (earth)
             killer:addTreasure(1613) -- Malevolent Memory (darkness skillchain)
-        elseif element == 5 and math.random(1,100) <= 99 then
+        elseif element == 5 and killer and  math.random(1,100) <= 99 then
             killer:addTreasure(1612) -- Radiant Memory (light skillchain)
-        elseif element == 6 and math.random(1,100) <= 99 then
+        elseif element == 6 and killer and  math.random(1,100) <= 99 then
             killer:addTreasure(1606) -- Burning Memory (fire)
             killer:addTreasure(1612) -- Radiant Memory (light skillchain)
-        elseif element == 7 and math.random(1,100) <= 99 then
+        elseif element == 7 and killer and math.random(1,100) <= 99 then
             killer:addTreasure(1607) -- Bitter Memory (ice)
             killer:addTreasure(1613) -- Malevolent Memory (darkness skillchain)
-        elseif element == 8 and math.random(1,100) <= 99 then
+        elseif element == 8 and killer and math.random(1,100) <= 99 then
             killer:addTreasure(1608) -- Fleeting Memory (wind)
             killer:addTreasure(1612) -- Radiant Memory (light skillchain)
         end
