eb6564b6706026b750361916360e817ae67474f5
Shiyo <Shiyo@CELTY>
2021-03-03 21:37:19 -0500

    Golem / Magic Pot TP move adjustments

--- scripts/globals/mobskills/crystal_rain.lua
+++ scripts/globals/mobskills/crystal_rain.lua
@@ -16,7 +16,7 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local dmgmod = 1
-    local accmod = 1
+    local accmod = 2
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.LIGHT, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.LIGHT, MOBPARAM_WIPE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.LIGHT)
--- scripts/globals/mobskills/double_ray.lua
+++ scripts/globals/mobskills/double_ray.lua
@@ -18,7 +18,7 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local dmgmod = 1
-    local accmod = 1.5
+    local accmod = 3
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.THUNDER, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.LIGHTNING, MOBPARAM_IGNORE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.LIGHTNING)
