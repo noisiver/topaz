2bded8036c529749d8c7fa00bf235cac95c514ea
Shiyo <Shiyo@CELTY>
2021-03-05 06:44:52 -0500

    Added stats to familiar for mobs

--- scripts/globals/mobskills/familiar.lua
+++ scripts/globals/mobskills/familiar.lua
@@ -10,6 +10,18 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
+    local pet = mob:getPet()
+        pet:addMod(tpz.mod.HASTE_MAGIC, 2500)
+        pet:addMod(tpz.mod.MAIN_DMG_RATING, 30)
+        pet:addMod(tpz.mod.RANGED_DMG_RATING, 30)
+        pet:addMod(tpz.mod.ATTP, 100)
+        pet:addMod(tpz.mod.RATTP, 100)
+        pet:addMod(tpz.mod.ACC, 100)
+        pet:addMod(tpz.mod.RACC, 100)
+        pet:addMod(tpz.mod.EVA, 100)
+        pet:addMod(tpz.mod.MEVA, 100)
+        pet:addMod(tpz.mod.REVA, 100)
+        pet:addMod(tpz.mod.DMG, -50)
     mob:familiar()
 
     skill:setMsg(tpz.msg.basic.FAMILIAR_MOB)
