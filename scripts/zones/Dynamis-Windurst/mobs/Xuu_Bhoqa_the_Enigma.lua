0933409192a5c7f95c3d033e5270a6b22362fe07
Shiyo <Shiyo@CELTY>
2021-03-05 06:01:05 -0500

    Update Xuu_Bhoqa_the_Enigma.lua

--- scripts/zones/Dynamis-Windurst/mobs/Xuu_Bhoqa_the_Enigma.lua
+++ scripts/zones/Dynamis-Windurst/mobs/Xuu_Bhoqa_the_Enigma.lua
@@ -10,13 +10,10 @@ mixins =
 }
 -----------------------------------
 function onMobSpawn(mob)
-     local Pet = GetMobByID(mob:getID()+1)
      mob:addMod(tpz.mod.DEFP, 30) 
      mob:addMod(tpz.mod.ATTP, 20)
      mob:addMod(tpz.mod.ACC, 50) 
      mob:addMod(tpz.mod.EVA, 30)
-     Pet:spawn()
-     Pet:updateEnmity(target)
 end
 
 function onMobFight(mob, target)
@@ -31,5 +28,4 @@ function onMobFight(mob, target)
 end
 
 function onMobDeath(mob, player, isKiller)
-    DespawnMob(mob:getID()+1)
 end
