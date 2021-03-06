b1ffc10ec98c8a2c512447d61912aea89576a4d6
Shiyo <Shiyo@CELTY>
2021-03-06 02:20:34 -0500

    Soulvoice +macc

--- scripts/globals/effects/soul_voice.lua
+++ scripts/globals/effects/soul_voice.lua
@@ -5,10 +5,12 @@
 -----------------------------------
 
 function onEffectGain(target, effect)
+    target:addMod(tpz.mod.MACC, 100) 
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
+    target:delMod(tpz.mod.MACC, 100) 
 end
