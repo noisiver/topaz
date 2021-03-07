e50b7fc316a09790df93baba1ea8f84e83cac1e7
Shiyo <Shiyo@CELTY>
2021-03-07 05:37:20 -0500

    Savage & Kasha WSNM NM's

--- scripts/zones/Kuftal_Tunnel/mobs/Kettenkaefer.lua
+++ scripts/zones/Kuftal_Tunnel/mobs/Kettenkaefer.lua
@@ -9,6 +9,12 @@ require("scripts/globals/wsquest")
 -----------------------------------
 
 function onMobInitialize(mob)
+    mob:addMod(tpz.mod.DEFP, 200)
+    mob:addMod(tpz.mod.MDEF, 100) 
+    mob:addMod(tpz.mod.ATTP, 50)
+    mob:addMod(tpz.mod.ACC, 50) 
+    mob:addMod(tpz.mod.EVA, 30)
+    mob:setMod(tpz.mod.REFRESH, 300)
     mob:setMobMod(tpz.mobMod.EXP_BONUS, -100)
     mob:setMobMod(tpz.mobMod.IDLE_DESPAWN, 180)
 end
