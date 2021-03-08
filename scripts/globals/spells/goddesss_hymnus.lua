a8d1980a1929202b0cadb2449c03eab2d94198a8
Shiyo <Shiyo@CELTY>
2021-03-08 04:09:39 -0500

    (Spell)Goddess Hymnus duration fix v1.1

--- scripts/globals/spells/goddesss_hymnus.lua
+++ scripts/globals/spells/goddesss_hymnus.lua
@@ -15,7 +15,7 @@ function onSpellCast(caster, target, spell)
 
         duration = duration * (caster:getMod(tpz.mod.SONG_DURATION_BONUS)/100)
 
-        target:addBardSong(caster, tpz.effect.HYMNUS, 1, 0, duration, caster:getID(), 0, 2)
+        target:addBardSong(caster, tpz.effect.HYMNUS, 1, 0, 120, caster:getID(), 0, 1)
 
     return tpz.effect.HYMNUS
 end
