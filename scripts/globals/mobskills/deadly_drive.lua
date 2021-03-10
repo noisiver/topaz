3932c6c3fb991da821a9c931978879724d577bd7
Shiyo <Shiyo@CELTY>
2021-03-10 08:37:57 -0500

    (Mobs) Bune and Wyvern TP move adjustments

--- scripts/globals/mobskills/deadly_drive.lua
+++ scripts/globals/mobskills/deadly_drive.lua
@@ -11,7 +11,7 @@ function onMobWeaponSkill(target, mob, skill)
     local numhits = 1
     local accmod = 1
     local dmgmod = 4
-    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_DMG_VARIES, 1, 2, 3)
+    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_DMG_VARIES, 1, 1.25, 1.5)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, info.hitslanded)
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
     return dmg
--- scripts/zones/Gustav_Tunnel/mobs/Bune.lua
+++ scripts/zones/Gustav_Tunnel/mobs/Bune.lua
@@ -1,7 +1,34 @@
 -----------------------------------
 -- Area: Gustav Tunnel
 --   NM: Bune
+mixins = {require("scripts/mixins/job_special")}
+require("scripts/globals/status")
 -----------------------------------
+
+function onMobSpawn(mob)
+    tpz.mix.jobSpecial.config(mob, {
+        specials =
+        {
+            {
+                id = tpz.jsa.INVINCIBLE,
+                hpp = math.random(25, 50),
+                begCode = function(mob)
+                  
+                 -- mob:messageText(mob, ID.text.HOW_CAN_YOU_EXPECT_TO_KILL_ME)
+                 -- mob:PrintToArea("My power is too great for you!",0,"Murgleis")
+                end,
+                endCode = function(mob)
+               --mob:messageText(mob, ID.text.WHEN_YOU_CANT_EVEN_HIT_ME)
+                end,
+            },
+        },
+    })
+    mob:addMod(tpz.mod.DEFP, 50) 
+    mob:addMod(tpz.mod.ATTP, 100)
+    mob:addMod(tpz.mod.ACC, 100) 
+    mob:addMod(tpz.mod.EVA, 100)
+    mob:setMobMod(tpz.mobMod.GIL_MIN, 15000)
+end
 
 function onMobDeath(mob, player, isKiller)
 end
