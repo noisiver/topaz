4114e01ac519fee0388a2fb5e6d4233363169d33
Shiyo <Shiyo@CELTY>
2021-03-11 08:18:16 -0500

    (Abilities) Warcry enmity fix 2.0

--- scripts/globals/abilities/warcry.lua
+++ scripts/globals/abilities/warcry.lua
@@ -23,25 +23,22 @@ function onUseAbility(player, target, ability)
         power = math.floor((player:getSubLvl()/4)+4.75)/256
     end
 
-    local mob = player:getTarget()
-    if mob then
-        local enmityList = mob:getEnmityList()
-        if enmityList and #enmityList > 0 then
-            if #enmityList == 2 then
-                target:addEnmity(mob, 1, 600)
-            elseif #enmityList == 3 then
-                target:addEnmity(mob, 1, 900)
-            elseif #enmityList == 4 then
-                target:addEnmity(mob, 1, 1200)
-            elseif #enmityList == 5 then
-                target:addEnmity(mob, 1, 1500)
-            elseif #enmityList == 6 then
-                target:addEnmity(mob, 1, 1800)
-            else
-                target:addEnmity(mob, 1, 300)
-            end
-        end
+    local PT = getPartySize()
+    
+    if PT  == 2 then
+         target:addEnmity(player, 1, 600)
+    elseif PT  == 3 then
+         target:addEnmity(player, 1, 900)
+    elseif PT  == 4 then
+         target:addEnmity(player, 1, 1200)
+    elseif PT  == 5 then
+         target:addEnmity(player, 1, 1500)
+    elseif PT  == 6 then
+         target:addEnmity(player, 1, 1800)
+    else
+         target:addEnmity(player, 1, 300)
     end
+
 
 
     power = power * 100
