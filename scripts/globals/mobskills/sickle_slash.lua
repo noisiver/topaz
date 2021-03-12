8e6a6dfa11f64510c4856b581730b0acacb2d053
Shiyo <Shiyo@CELTY>
2021-03-11 23:52:08 -0500

    (Mobs) Buffed sickle slash

--- scripts/globals/mobskills/sickle_slash.lua
+++ scripts/globals/mobskills/sickle_slash.lua
@@ -23,8 +23,8 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local numhits = 1
     local accmod = 1
-    local dmgmod = 1.25
-    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_CRIT_VARIES, 1, 1.5, 2)
+    local dmgmod = 3
+    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_CRIT_VARIES, 1.5, 1.75, 2)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, info.hitslanded)
 
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
