e97b5bbc01901eb74e5d4fb3677da3f6989d00ef
Shiyo <Shiyo@CELTY>
2021-03-12 22:43:52 -0500

    (Mobs)Throat stab fix?

--- scripts/globals/mobskills/throat_stab.lua
+++ scripts/globals/mobskills/throat_stab.lua
@@ -31,8 +31,8 @@ function onMobWeaponSkill(target, mob, skill)
         damage = currentHP
     end
     local dmg = MobFinalAdjustments(damage, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.NONE, MOBPARAM_IGNORE_SHADOWS)
-    target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.NONE)
+    target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.PIERCING)
     mob:resetEnmity(target)
-
     return dmg
 end
+
