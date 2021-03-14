e27c6e7283c6ed35c74c13fed61f495aaf75c0ae
Shiyo <Shiyo@CELTY>
2021-03-14 05:37:27 -0400

    (Effects) Added racc and rattk down to blind and attack down

--- scripts/globals/effects/blindness.lua
+++ scripts/globals/effects/blindness.lua
@@ -8,6 +8,7 @@ require("scripts/globals/status")
 
 function onEffectGain(target, effect)
     target:addMod(tpz.mod.ACC, -effect:getPower())
+    target:addMod(tpz.mod.RACC, -effect:getPower())
 end
 
 function onEffectTick(target, effect)
@@ -15,4 +16,5 @@ end
 
 function onEffectLose(target, effect)
     target:delMod(tpz.mod.ACC, -effect:getPower())
+    target:delMod(tpz.mod.RACC, -effect:getPower())
 end
