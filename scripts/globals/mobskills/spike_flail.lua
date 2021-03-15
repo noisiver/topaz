52e71f40847eef8b85c417e8972614849389f91c
Shiyo <Shiyo@CELTY>
2021-03-15 07:32:49 -0400

    (Mob - TP Moves) Fafnir / Nidhogg moves buffed

--- scripts/globals/mobskills/spike_flail.lua
+++ scripts/globals/mobskills/spike_flail.lua
@@ -29,7 +29,7 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local numhits = 3
     local accmod = 1
-    local dmgmod = 0.7
+    local dmgmod = 1
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_DMG_VARIES, 2, 3, 4)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, MOBPARAM_3_SHADOW)
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
