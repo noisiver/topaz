b6ff9563eea602d0163f5ecff45e93448a3ce2f0
Shiyo <Shiyo@CELTY>
2021-03-16 03:40:52 -0400

    (Weapon Skills) Savage blade tuning

--- scripts/globals/weaponskills/savage_blade.lua
+++ scripts/globals/weaponskills/savage_blade.lua
@@ -21,7 +21,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
 
     local params = {}
     params.numHits = 2
-    params.ftp100 = 3.0 params.ftp200 = 8.0 params.ftp300 = 9.0
+    params.ftp100 = 2.5 params.ftp200 = 2.7 params.ftp300 = 3
     params.str_wsc = 0.4 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.4 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
