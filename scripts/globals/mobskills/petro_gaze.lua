6155af55259a7accf76988cc21514a81d15383a4
Shiyo <Shiyo@CELTY>
2021-03-16 03:24:37 -0400

    (Mobs) Petro eyes duration buffed

--- scripts/globals/mobskills/petro_gaze.lua
+++ scripts/globals/mobskills/petro_gaze.lua
@@ -17,7 +17,7 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.PETRIFICATION
 
-    skill:setMsg(MobGazeMove(mob, target, typeEffect, 1, 0, 25))
+    skill:setMsg(MobGazeMove(mob, target, typeEffect, 1, 0, 60))
 
     return typeEffect
 end
