2d00675c4a453b2490dd73255f3e176c34f558b3
Shiyo <Shiyo@CELTY>
2021-03-16 05:43:30 -0400

    (Mobs - TP Moves) Adamantoise breath move dmg reduced to 700

--- scripts/globals/mobskills/earth_breath.lua
+++ scripts/globals/mobskills/earth_breath.lua
@@ -17,7 +17,7 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local dmgmod = MobBreathMove(mob, target, 0.25, 1, tpz.magic.ele.EARTH, 1000)
+    local dmgmod = MobBreathMove(mob, target, 0.25, 1, tpz.magic.ele.EARTH, 700)
 
     local dmg = MobFinalAdjustments(dmgmod, mob, skill, target, tpz.attackType.BREATH, tpz.damageType.EARTH, MOBPARAM_IGNORE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.BREATH, tpz.damageType.EARTH)
