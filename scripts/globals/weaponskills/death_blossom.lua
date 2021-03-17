93a90d5a586d4e522b16906ce82956f585eb5bf3
Shiyo <Shiyo@CELTY>
2021-03-16 22:03:11 -0400

    (WS) Death Blossom buff

--- scripts/globals/weaponskills/death_blossom.lua
+++ scripts/globals/weaponskills/death_blossom.lua
@@ -21,7 +21,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local params = {}
     params.numHits = 3
     -- ftp damage mods (for Damage Varies with TP lines are calculated in the function
-    params.ftp100 = 1.125 params.ftp200 = 1.125 params.ftp300 = 1.125
+    params.ftp100 = 2 params.ftp200 = 2 params.ftp300 = 2
     -- wscs are in % so 0.2=20%
     params.str_wsc = 0.3 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0
     params.mnd_wsc = 0.5 params.chr_wsc = 0.0
