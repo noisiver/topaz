d7dd9c620649bd375c7a43f93ace21d694d56019
Shiyo <Shiyo@CELTY>
2021-03-18 02:09:43 -0400

    (Custom - Mythic WS) Koga changes, needs debugging

--- scripts/zones/Dangruf_Wadi/mobs/Kogarasumaru.lua
+++ scripts/zones/Dangruf_Wadi/mobs/Kogarasumaru.lua
@@ -21,6 +21,7 @@ function onMobFight(mob, target)
     local twohourTime = mob:getLocalVar("twohourTime")
     local STANCEdps = mob:getLocalVar("STANCEdps")
     local STANCEtank = mob:getLocalVar("STANCEtank")
+    local DMGtaken = mob:getLocalVar("DMGtaken")
 
     if twohourTime == 0 then
         printf("Setting two hour time");
@@ -29,17 +30,24 @@ function onMobFight(mob, target)
         printf("DPS Stance");
         mob:useMobAbility(624) -- 2 hour "cloud" animation
         mob:setMod(tpz.mod.HASTE_MAGIC, mob:getMod(tpz.mod.HASTE_MAGIC) + 2500)
+        mob:setMod(tpz.mod.COUNTER, 0)
         mob:setMod(tpz.mod.DOUBLE_ATTACK, 50)
         mob:setMod(tpz.mod.ATT, 550)
         mob:setMod(tpz.mod.UDMGPHYS, 75)
         mob:setMod(tpz.mod.UDMGMAGIC, 75)
         mob:setLocalVar("STANCEdps", battletime + math.random(60, 90))
         mob:setLocalVar("STANCEtank", 1)
+        mob:addListener("TAKE_DAMAGE", "URAGNITE_TAKE_DAMAGE", function(mob, amount, attacker, attackType, damageType)
+        if amount >= 1  then
+        DMGtaken = DMGtaken +1
+        end
+    end)
     end
 
-    if battletime >= STANCEdps and STANCEtank == 1 then
+    if battletime >= STANCEdps and STANCEtank == 1 or DMGtaken == 3000 then
         printf("Tank Stance");
         mob:useMobAbility(624) -- 2 hour "cloud" animation
+        mob:setMod(tpz.mod.COUNTER, 100)
         mob:setMod(tpz.mod.DOUBLE_ATTACK, 0)
         mob:setMod(tpz.mod.ATT, 200)
         mob:setMod(tpz.mod.HASTE_MAGIC, mob:getMod(tpz.mod.HASTE_MAGIC) - 2500)
@@ -48,6 +56,7 @@ function onMobFight(mob, target)
         mob:setLocalVar("twohourTime", battletime + math.random(60, 90))
         mob:setLocalVar("STANCEdps", 0)
         mob:setLocalVar("STANCEtank", 0)
+        mob:setLocalVar("DMGtaken", 0)
     end
 end
 
