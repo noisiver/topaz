cae340878d29128cb5c9a6a62776cc59856a1b12
Shiyo <Shiyo@CELTY>
2021-03-18 03:26:48 -0400

    (Mobs - TP moves) Crystal rain dmg buff

--- scripts/globals/mobskills/crystal_rain.lua
+++ scripts/globals/mobskills/crystal_rain.lua
@@ -15,8 +15,8 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local dmgmod = 1
-    local accmod = 2
+    local dmgmod = 2
+    local accmod = 1
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.LIGHT, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.LIGHT, MOBPARAM_WIPE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.LIGHT)
