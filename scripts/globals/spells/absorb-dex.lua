edb98757b0648b9fa155a2d4ad8983de445f7c50
Shiyo <Shiyo@CELTY>
2021-03-20 08:54:41 -0400

    Revert "Revert "Revert "(Debugging) Absorb dex"""
    This reverts commit 5b8c9f96a33522b17f37827f4cbd4b016430093e.

--- scripts/globals/spells/absorb-dex.lua
+++ scripts/globals/spells/absorb-dex.lua
@@ -13,8 +13,6 @@ function onMagicCastingCheck(caster, target, spell)
 end
 
 function onSpellCast(caster, target, spell)
-    local level = player:getMainLvl()
-    local power = math.floor(3 + (level / 5))
 
     if (target:hasStatusEffect(tpz.effect.DEX_DOWN) or caster:hasStatusEffect(tpz.effect.DEX_BOOST)) then
         spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT) -- no effect
@@ -27,12 +25,12 @@ function onSpellCast(caster, target, spell)
         params.bonus = 0
         params.effect = nil
         local resist = applyResistance(caster, target, spell, params)
-        if (resist <= 0.5) then
+        if (resist <= 0.125) then
             spell:setMsg(tpz.msg.basic.MAGIC_RESIST)
         else
             spell:setMsg(tpz.msg.basic.MAGIC_ABSORB_DEX)
-            caster:addStatusEffect(tpz.effect.DEX_BOOST, power*resist*((100+(caster:getMod(tpz.mod.AUGMENTS_ABSORB)))/100), ABSORB_SPELL_TICK, power*ABSORB_SPELL_TICK) -- caster gains DEX
-            target:addStatusEffect(tpz.effect.DEX_DOWN, power*resist*((100+(caster:getMod(tpz.mod.AUGMENTS_ABSORB)))/100), ABSORB_SPELL_TICK, power*ABSORB_SPELL_TICK)    -- target loses DEX
+            caster:addStatusEffect(tpz.effect.DEX_BOOST, ABSORB_SPELL_AMOUNT*resist*((100+(caster:getMod(tpz.mod.AUGMENTS_ABSORB)))/100), ABSORB_SPELL_TICK, ABSORB_SPELL_AMOUNT*ABSORB_SPELL_TICK) -- caster gains DEX
+            target:addStatusEffect(tpz.effect.DEX_DOWN, ABSORB_SPELL_AMOUNT*resist*((100+(caster:getMod(tpz.mod.AUGMENTS_ABSORB)))/100), ABSORB_SPELL_TICK, ABSORB_SPELL_AMOUNT*ABSORB_SPELL_TICK)    -- target loses DEX
         end
     end
     return tpz.effect.DEX_DOWN
