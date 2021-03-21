51a51b583cca5cc5da2d9964fba482c92f1dd2aa
Shiyo <Shiyo@CELTY>
2021-03-21 05:41:17 -0400

    (Items) Attk bonus adjusted to be accurate

--- scripts/globals/items/amanomurakumo.lua
+++ scripts/globals/items/amanomurakumo.lua
@@ -10,9 +10,17 @@ require("scripts/globals/status")
 function onAdditionalEffect(player, target, damage)
     local chance = 10
 
+    local SDT = target:getMod(tpz.mod.WATER)
+    if SDT <= 5 then
+        chance = 0
+    else 
+        chance = chance * (SDT / 100)
+        chance = utils.clamp(chance, 5, 95)
+    end
+
     if math.random(100) <= chance and applyResistanceAddEffect(player, target, tpz.magic.ele.WATER, 0) > 0.5 then
         target:delStatusEffect(tpz.effect.ATTACK_BOOST)
-        target:addStatusEffect(tpz.effect.ATTACK_DOWN, 10, 0, 60) -- Power needs verification/correction
+        target:addStatusEffect(tpz.effect.ATTACK_DOWN, 20, 0, 60) -- Power needs verification/correction
         return tpz.subEffect.ATTACK_DOWN, tpz.msg.basic.ADD_EFFECT_STATUS, tpz.effect.ATTACK_DOWN
     end
 
