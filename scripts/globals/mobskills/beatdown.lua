b1afb9033924bb48a66e03bc5d97bcd2567315b6
Shiyo <Shiyo@CELTY>
2021-03-21 00:40:45 -0400

    (Mobs - TP Moves) Beatdown nerfed

--- scripts/globals/mobskills/beatdown.lua
+++ scripts/globals/mobskills/beatdown.lua
@@ -17,7 +17,7 @@ function onMobWeaponSkill(target, mob, skill)
 
     local numhits = 1
     local accmod = 1
-    local dmgmod = 4
+    local dmgmod = 3
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT, info.hitslanded)
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT)
