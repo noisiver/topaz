2841e4d54892933fa8fc36b2b8844b5b8a8cb5a9
Shiyo <Shiyo@CELTY>
2021-03-22 03:27:18 -0400

    (Spells) Corrected all status resist songs/spells to be % chance instead of mevasion

--- scripts/globals/effects/barparalyze.lua
+++ scripts/globals/effects/barparalyze.lua
@@ -7,12 +7,12 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onEffectGain(target, effect)
-    target:addMod(tpz.mod.PARALYZERES, effect:getPower())
+    target:addMod(tpz.mod.PARALYZERESTRAIT, effect:getPower())
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-    target:delMod(tpz.mod.PARALYZERES, effect:getPower())
+    target:delMod(tpz.mod.PARALYZERESTRAIT, effect:getPower())
 end
--- scripts/globals/effects/barvirus.lua
+++ scripts/globals/effects/barvirus.lua
@@ -7,12 +7,12 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onEffectGain(target, effect)
-    target:addMod(tpz.mod.VIRUSRES, effect:getPower())
+    target:addMod(tpz.mod.VIRUSRESTRAIT, effect:getPower())
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-    target:delMod(tpz.mod.VIRUSRES, effect:getPower())
+    target:delMod(tpz.mod.VIRUSRESTRAIT, effect:getPower())
 end
