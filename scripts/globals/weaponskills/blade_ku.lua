d950fe0af0d074baba68ca6b2bbb045ef63156cf
Shiyo <Shiyo@CELTY>
2021-03-22 05:58:50 -0400

    (WS) 1h WS tuning pass 1

--- scripts/globals/weaponskills/blade_ku.lua
+++ scripts/globals/weaponskills/blade_ku.lua
@@ -25,8 +25,8 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
 
     local params = {}
     params.numHits = 5
-    params.ftp100 = 1.5 params.ftp200 = 1.5 params.ftp300 = 1.5
-    params.str_wsc = 0.0 params.dex_wsc = 0.5 params.vit_wsc = 0.0 params.agi_wsc = 0.5 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
+    params.ftp100 = 1 params.ftp200 = 1 params.ftp300 = 1
+    params.str_wsc = 0.0 params.dex_wsc = 0.4 params.vit_wsc = 0.0 params.agi_wsc = 0.4 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
     params.acc100 = 0.1 params.acc200= 0.15 params.acc300= 0.20
--- scripts/globals/weaponskills/dancing_edge.lua
+++ scripts/globals/weaponskills/dancing_edge.lua
@@ -22,7 +22,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local params = {}
     params.numHits = 5
     params.ftp100 = 1.1875 params.ftp200 = 1.1875 params.ftp300 = 1.1875
-    params.str_wsc = 0.0 params.dex_wsc = 0.3 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.4
+    params.str_wsc = 0.0 params.dex_wsc = 0.75 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
     params.acc100 = 1 params.acc200= 1.05 params.acc300= 1.10
