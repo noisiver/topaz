41c889b038efaaa5ac79ae4e1fa8de18103bbfe3
Shiyo <Shiyo@CELTY>
2021-03-22 06:00:34 -0400

    (WS) Decimation acc fixed

--- scripts/globals/weaponskills/decimation.lua
+++ scripts/globals/weaponskills/decimation.lua
@@ -25,7 +25,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     params.str_wsc = 0.0 params.dex_wsc = 0.0 params.vit_wsc = 0.75 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
-    params.acc100 = 1.10 params.acc200= 1.15 params.acc300= 1.20
+    params.acc100 = 1.05 params.acc200= 1.10 params.acc300= 1.15
     params.atk100 = 1; params.atk200 = 1; params.atk300 = 1
     params.multiHitfTP = true
 
