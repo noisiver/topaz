e657b5934d75e18688d19f7ac2d86a13b4886b6d
Shiyo <Shiyo@CELTY>
2021-03-22 05:59:07 -0400

    (Jobs - DNC) 25% WSD added to building flourish

--- scripts/globals/effects/building_flourish.lua
+++ scripts/globals/effects/building_flourish.lua
@@ -5,10 +5,12 @@
 -----------------------------------
 
 function onEffectGain(target, effect)
+target:addMod(tpz.mod.ALL_WSDMG_ALL_HITS, 25)
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
+target:delMod(tpz.mod.ALL_WSDMG_ALL_HITS, 25)
 end
