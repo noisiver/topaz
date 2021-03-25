b0bef94e600404026cd1c479cb71f6192ba174a5
Shiyo <Shiyo@CELTY>
2021-03-25 02:43:23 -0400

    (WS) wasp sting overwrite weaker poison and cant land past 50% resist

--- scripts/globals/weaponskills/wasp_sting.lua
+++ scripts/globals/weaponskills/wasp_sting.lua
@@ -32,12 +32,15 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     end
 
     local damage, criticalHit, tpHits, extraHits = doPhysicalWeaponskill(player, target, wsID, params, tp, action, primary, taChar)
+    local effect = tpz.effect.POISON
+    local resist = applyResistanceAddEffect(player, target, tpz.magic.ele.WATER, 0)
 
-    if (damage > 0 and target:hasStatusEffect(tpz.effect.POISON) == false) then
+    if damage > 0 and (canOverwrite(target, effect, power)) and resist >= 0.5 then
         --local duration = (0 + (tp/2000 * 30)) * applyResistanceAddEffect(player, target, tpz.magic.ele.WATER, 0)
-        local duration = 60 * applyResistanceAddEffect(player, target, tpz.magic.ele.WATER, 0)
+        local duration = 60 * resist
         local power = math.floor(player:getMainLvl() * (tp / 2000) + 20)
-        target:addStatusEffect(tpz.effect.POISON, power, 0, duration)
+        target:delStatusEffect(effect)
+        target:addStatusEffect(effect, power, 3, duration)
     end
     return tpHits, extraHits, criticalHit, damage
 
