c351d1d93bcac5af6c92bb293f52cae5025c2339
Shiyo <Shiyo@CELTY>
2021-03-29 07:56:58 -0400

    (Jobs - DRG - Pet) Wyvern stats

--- scripts/globals/pets/wyvern.lua
+++ scripts/globals/pets/wyvern.lua
@@ -79,8 +79,7 @@ function onMobSpawn(mob)
     local master = mob:getMaster()
     mob:addMod(tpz.mod.DMG, -40)
     mob:addMod(tpz.mod.DEFP, 50)
-    mob:addMod(tpz.mod.ATT, 10)
-    mob:addMod(tpz.mod.HPP, 10)
+    mob:addMod(tpz.mod.HPP, 15)
     if mob:getMainLvl() >= 70 then
 		    mob:addMod(tpz.mod.ACC, 40)
 	elseif mob:getMainLvl() >= 50 then
