ba3ade6b3c19cf81b18746d4e77b69ed3625632c
Shiyo <Shiyo@CELTY>
2021-03-30 02:48:19 -0400

    (Items - Food) Nerfed Marinara pizza to 10/11% attk down from 20/22%

--- scripts/globals/items/marinara_pizza.lua
+++ scripts/globals/items/marinara_pizza.lua
@@ -26,7 +26,7 @@ end
 
 function onEffectGain(target, effect)
     target:addMod(tpz.mod.HP, 20)
-    target:addMod(tpz.mod.FOOD_ATTP, 20)
+    target:addMod(tpz.mod.FOOD_ATTP, 10)
     target:addMod(tpz.mod.FOOD_ATT_CAP, 50)
     target:addMod(tpz.mod.FOOD_ACCP, 10)
     target:addMod(tpz.mod.FOOD_ACC_CAP, 54)
@@ -35,7 +35,7 @@ end
 
 function onEffectLose(target, effect)
     target:delMod(tpz.mod.HP, 20)
-    target:delMod(tpz.mod.FOOD_ATTP, 20)
+    target:delMod(tpz.mod.FOOD_ATTP, 10)
     target:delMod(tpz.mod.FOOD_ATT_CAP, 50)
     target:delMod(tpz.mod.FOOD_ACCP, 10)
     target:delMod(tpz.mod.FOOD_ACC_CAP, 54)
--- scripts/globals/items/marinara_pizza_+1.lua
+++ scripts/globals/items/marinara_pizza_+1.lua
@@ -26,7 +26,7 @@ end
 
 function onEffectGain(target, effect)
     target:addMod(tpz.mod.HP, 25)
-    target:addMod(tpz.mod.FOOD_ATTP, 21)
+    target:addMod(tpz.mod.FOOD_ATTP, 11)
     target:addMod(tpz.mod.FOOD_ATT_CAP, 55)
     target:addMod(tpz.mod.FOOD_ACCP, 11)
     target:addMod(tpz.mod.FOOD_ACC_CAP, 58)
@@ -35,7 +35,7 @@ end
 
 function onEffectLose(target, effect)
     target:delMod(tpz.mod.HP, 25)
-    target:delMod(tpz.mod.FOOD_ATTP, 21)
+    target:delMod(tpz.mod.FOOD_ATTP, 11)
     target:delMod(tpz.mod.FOOD_ATT_CAP, 55)
     target:delMod(tpz.mod.FOOD_ACCP, 11)
     target:delMod(tpz.mod.FOOD_ACC_CAP, 58)
