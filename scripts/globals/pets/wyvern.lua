dc03bbf06231afe62a272aae4c0c551f9475c20b
Shiyo <Shiyo@CELTY>
2021-04-05 12:09:14 -0400

    Revert "(Jobs - DRG - Pet) Adjust wyvern stats"
    This reverts commit 8d03bfed132b97dd7c1db3d5bd3cf2b22798f8bb.

--- scripts/globals/pets/wyvern.lua
+++ scripts/globals/pets/wyvern.lua
@@ -79,8 +79,9 @@ function onMobSpawn(mob)
     local master = mob:getMaster()
     
 	mob:addMod(tpz.mod.DMG, -40)
-
-    	if mob:getMainLvl() >= 70 then
+    mob:addMod(tpz.mod.DEFP, 150)
+    
+	if mob:getMainLvl() >= 70 then
 		    mob:addMod(tpz.mod.ACC, 48)
 	elseif mob:getMainLvl() >= 50 then
 	    mob:addMod(tpz.mod.ACC, 35)
