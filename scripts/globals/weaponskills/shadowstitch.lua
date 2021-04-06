9ae0f50bb3b375ed9e1d5e61e8b69623615eabe3
Shiyo <Shiyo@CELTY>
2021-04-06 07:05:24 -0400

    (WS) Revamped shadowstitch, halved ftp and made it crit mod

--- scripts/globals/weaponskills/shadowstitch.lua
+++ scripts/globals/weaponskills/shadowstitch.lua
@@ -20,10 +20,10 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
 
     local params = {}
     params.numHits = 1
-    params.ftp100 = 2.0 params.ftp200 = 2.2 params.ftp300 = 2.5
+    params.ftp100 = 1 params.ftp200 = 1.1 params.ftp300 = 1.2
     params.str_wsc = 0.0 params.dex_wsc = 0.35 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
-    params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
-    params.canCrit = false
+    params.crit100 = 0.75 params.crit200 = 0.85 params.crit300 = 1.0
+    params.canCrit = true
     params.acc100 = 0.0 params.acc200= 0.0 params.acc300= 0.0
     params.atk100 = 1; params.atk200 = 1; params.atk300 = 1
 
