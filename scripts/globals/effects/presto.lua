39c80399c3589e4651bbcb60120042cfb795997d
Shiyo <Shiyo@CELTY>
2021-04-06 10:39:56 -0400

    (Jobs - DNC) Added step acc bonus to presto

--- scripts/globals/effects/presto.lua
+++ scripts/globals/effects/presto.lua
@@ -4,10 +4,12 @@
 --     
 -----------------------------------
 function onEffectGain(target, effect)
+    player:addMod(tpz.mod.STEP_ACCURACY, 50)
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
+    player:delMod(tpz.mod.STEP_ACCURACY, 50)
 end
