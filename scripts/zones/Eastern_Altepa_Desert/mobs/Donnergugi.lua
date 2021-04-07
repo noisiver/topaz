95c3056bc31764b4881917ecd0a8c8891b6cb736
Shiyo <Shiyo@CELTY>
2021-04-07 03:30:15 -0400

    (Mobs) Coded donnergugi

--- scripts/zones/Eastern_Altepa_Desert/mobs/Donnergugi.lua
+++ scripts/zones/Eastern_Altepa_Desert/mobs/Donnergugi.lua
@@ -5,6 +5,24 @@
 require("scripts/globals/hunts")
 ------------------------------
 
+function onMobInitialize(mob)
+    mob:setMobMod(tpz.mobMod.HP_STANDBACK, -1)
+end
+
+function onMobSpawn(mob)
+    mob:addMod(tpz.mod.DEFP, 25)
+    mob:addMod(tpz.mod.MDEF, 100) 
+    mob:addMod(tpz.mod.ATTP, 25)
+    mob:addMod(tpz.mod.ACC, 25) 
+    mob:addMod(tpz.mod.EVA, 25)
+    mob:setMod(tpz.mod.REFRESH, 300)
+    mob:addMod(tpz.mod.SPELLINTERRUPT, 300)
+    mob:addMod(tpz.mod.SLEEPRESTRAIT, 100)
+    mob:addMod(tpz.mod.LULLABYRESTRAIT, 100)
+    mob:addMod(tpz.mod.GRAVITYRESTRAIT, 100)
+    mob:addMod(tpz.mod.BINDRESTRAIT, 100)
+end
+
 function onMobDeath(mob, player, isKiller)
     tpz.hunts.checkHunt(mob, player, 410)
 end
