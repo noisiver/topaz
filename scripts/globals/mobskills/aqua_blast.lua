6b4f6cfed23d00e75ea592e1ecec22648e6341c0
Shiyo <Shiyo@CELTY>
2021-04-09 01:52:48 -0400

    (TP Moves) Added aqua blast

---
+++ scripts/globals/mobskills/aqua_blast.lua
@@ -0,0 +1,26 @@
+---------------------------------------------
+--  Aqua Blast
+--
+--  Description: Fires a blast of Water, dealing damage in a fan-shaped area. Additional effect: knockback
+--  Type: Magical
+--  Utsusemi/Blink absorb: Ignores shadows
+--  Range: Melee
+--  Notes:
+---------------------------------------------
+require("scripts/globals/settings")
+require("scripts/globals/status")
+require("scripts/globals/monstertpmoves")
+
+---------------------------------------------
+function onMobSkillCheck(target, mob, skill)
+    return 0
+end
+
+function onMobWeaponSkill(target, mob, skill)
+    local dmgmod = 3
+    local accmod = 1
+    local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.WATER, dmgmod, TP_NO_EFFECT)
+    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.WATER, MOBPARAM_IGNORE_SHADOWS)
+    target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.WATER)
+    return dmg
+end
