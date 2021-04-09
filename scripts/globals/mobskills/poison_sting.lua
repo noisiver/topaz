6df5341ee5eeb324c88a7ce5905e2e5f3d3cc57b
Shiyo <Shiyo@CELTY>
2021-04-09 04:31:23 -0400

    (TP Moves) Buffed poison sting damage

--- scripts/globals/mobskills/poison_sting.lua
+++ scripts/globals/mobskills/poison_sting.lua
@@ -21,7 +21,7 @@ function onMobWeaponSkill(target, mob, skill)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, info.hitslanded)
 
     local typeEffect = tpz.effect.POISON
-    local power = mob:getMainLvl()/5 + 3
+    local power = mob:getMainLvl()/2 + 5
     MobPhysicalStatusEffectMove(mob, target, skill, typeEffect, power, 3, 60)
 
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
