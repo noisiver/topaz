f5084efe6ab84a68fbb619472315ca612e28447d
Shiyo <Shiyo@CELTY>
2021-04-10 06:10:57 -0400

    (WS) Updated scythe WS

--- scripts/globals/weaponskills/cross_reaper.lua
+++ scripts/globals/weaponskills/cross_reaper.lua
@@ -15,9 +15,9 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local params = {}
     params.numHits = 2
     -- ftp damage mods (for Damage Varies with TP lines are calculated in the function
-    params.ftp100 = 2.0 params.ftp200 = 2.25 params.ftp300 = 2.5
+    params.ftp100 = 1.5 params.ftp200 = 3.0 params.ftp300 = 4.0
     -- wscs are in % so 0.2=20%
-    params.str_wsc = 0.3 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.3 params.chr_wsc = 0.0
+    params.str_wsc = 0.75 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.3 params.chr_wsc = 0.0
     -- critical mods, again in % (ONLY USE FOR CRITICAL HIT VARIES WITH TP)
     params.crit100 = 0.0 params.crit200=0.0 params.crit300=0.0
     params.canCrit = false
--- scripts/globals/weaponskills/spiral_hell.lua
+++ scripts/globals/weaponskills/spiral_hell.lua
@@ -16,9 +16,9 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local params = {}
     params.numHits = 1
     -- ftp damage mods (for Damage Varies with TP lines are calculated in the function
-    params.ftp100 = 1.375 params.ftp200 = 1.875 params.ftp300 = 3.625
+    params.ftp100 = 3 params.ftp200 = 3.5 params.ftp300 = 4.0
     -- wscs are in % so 0.2=20%
-    params.str_wsc = 0.5 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.5 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
+    params.str_wsc = 0.4 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.4 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     -- critical mods, again in % (ONLY USE FOR critICAL HIT VARIES WITH TP)
     params.crit100 = 0.0 params.crit200=0.0 params.crit300=0.0
     params.canCrit = false
