5297e5d4b8b27fe88d9c91cb77c154f294005af4
Shiyo <Shiyo@CELTY>
2021-04-10 08:42:03 -0400

    (Shops) Fixed NPC to sell rank1/2 conquest items at all times

--- scripts/zones/Northern_San_dOria/npcs/Arlenne.lua
+++ scripts/zones/Northern_San_dOria/npcs/Arlenne.lua
@@ -13,11 +13,11 @@ end
 function onTrigger(player, npc)
     local stock =
     {
-        17051,  1409, 1,    -- Yew Wand
-        17090,  3245, 1,    -- Elm Staff
-        17097, 16416, 1,    -- Elm Pole
-        16770, 11286, 1,    -- Zaghnal
-        17096,  4568, 2,    -- Holly Pole
+        17051,  1409, 3,    -- Yew Wand
+        17090,  3245, 3,    -- Elm Staff
+        17097, 16416, 3,    -- Elm Pole
+        16770, 11286, 3,    -- Zaghnal
+        17096,  4568, 3,    -- Holly Pole
         17024,    66, 3,    -- Ash Club
         17049,    46, 3,    -- Maple Wand
         17050,   333, 3,    -- Willow Wand
@@ -28,7 +28,7 @@ function onTrigger(player, npc)
         16391,   828, 3,    -- Brass Knuckles
         16407,  1554, 3,    -- Brass Baghnakhs
         16768,   309, 3,    -- Bronze Zaghnal
-        16769,  2542, 2,    -- Brass Zaghnal
+        16769,  2542, 3,    -- Brass Zaghnal
     }
 
     player:showText(npc, ID.text.ARLENNE_SHOP_DIALOG)
