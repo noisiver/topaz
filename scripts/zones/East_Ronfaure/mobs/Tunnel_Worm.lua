01ba061e021dd6ac5b823d3af621186deeb75da3
Shiyo <Shiyo@CELTY>
2021-04-11 01:37:21 -0400

    (Spawns / Worms) Fixed some spawns and south gusta/east ronfaure tunnel worms missing mixin

---
+++ scripts/zones/East_Ronfaure/mobs/Tunnel_Worm.lua
@@ -0,0 +1,9 @@
+-----------------------------------
+-- Area: East Ronfaure
+--  Mob: Tunnel Worm
+-----------------------------------
+mixins = {require("scripts/mixins/families/worm")}
+-----------------------------------
+
+function onMobDeath(mob, player, isKiller)
+end
---
+++ scripts/zones/South_Gustaberg/mobs/Tunnel_Worm.lua
@@ -0,0 +1,9 @@
+-----------------------------------
+-- Area: South Gustaberg
+--  Mob: Tunnel Worm
+-----------------------------------
+mixins = {require("scripts/mixins/families/worm")}
+-----------------------------------
+
+function onMobDeath(mob, player, isKiller)
+end
