6092ca677bdfe64ee2657daa08cee141a7fb4b67
Shiyo <Shiyo@CELTY>
2021-04-12 12:45:35 -0400

    Revert "(Abilities) Fix gaining TP during meikyo shisui"
    This reverts commit 275fa81363f95311201201f6e1509a3cf9248c67.

--- scripts/globals/effects/meikyo_shisui.lua
+++ scripts/globals/effects/meikyo_shisui.lua
@@ -5,12 +5,10 @@
 -----------------------------------
 
 function onEffectGain(target, effect)
-    target:addMod(tpz.mod.STORETP, -1000)
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-    target:delMod(tpz.mod.STORETP, -1000)
 end
