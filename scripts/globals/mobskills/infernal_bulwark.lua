bacfcbe01638229067b0f055545f94858842c89b
Shiyo <Shiyo@CELTY>
2021-04-12 09:09:22 -0400

    (TP Moves) Updated Infernal Bulwark

--- scripts/globals/mobskills/infernal_bulwark.lua
+++ scripts/globals/mobskills/infernal_bulwark.lua
@@ -1,7 +1,6 @@
 ---------------------------------------------
--- Magma_Hoplon
--- Covers the user in fiery spikes and absorbs damage. Enemies that hit it take fire damage.
--- Stoneskin portion cannot be removed with dispel.
+-- Infernal Bulwark
+-- 	Grants the enemy Haste, Stoneskin, Attack Boost, Defense Boost, Magic Attack Bonus, and Magic Defense Bonus.
 ---------------------------------------------
 require("scripts/globals/monstertpmoves")
 require("scripts/globals/settings")
@@ -14,12 +13,12 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local typeEffectOne = tpz.effect.STONESKIN
-    skill:setMsg(MobBuffMove(mob, typeEffectOne, 1000, 0, 300))
-    MobBuffMove(mob, tpaz.effect.HASTE, 60, 0, 180)
-    MobBuffMove(mob, tpaz.effect.MAGIC_ATTACK_BOOST, 60, 0, 180)
-    MobBuffMove(mob, tpaz.effect.MAGIC_DEFENSE, 60, 0, 180)
-    MobBuffMove(mob, tpaz.effect.ATTACK_BOOST, 60, 0, 180)
-    MobBuffMove(mob, tpaz.effect.DEFENSE_BOOST, 60, 0, 180)
+    skill:setMsg(MobBuffMove(mob, typeEffectOne, 350, 0, 60))
+    MobBuffMove(mob, tpz.effect.HASTE, 3520, 0, 60)
+    MobBuffMove(mob, tpz.effect.MAGIC_ATK_BOOST, 24, 0, 60)
+    MobBuffMove(mob, tpz.effect.MAGIC_DEF_BOOST, 24, 0, 60)
+    MobBuffMove(mob, tpz.effect.ATTACK_BOOST, 33, 0, 60)
+    MobBuffMove(mob, tpz.effect.DEFENSE_BOOST, 33, 0, 60)
 
     return typeEffectOne
 end
