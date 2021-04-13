40233f7991f58478ec226d48907f7f2c60a9683d
Shiyo <Shiyo@CELTY>
2021-04-13 11:24:45 -0400

    (2 Hours) Buffed eagle eye shot

--- scripts/globals/abilities/eagle_eye_shot.lua
+++ scripts/globals/abilities/eagle_eye_shot.lua
@@ -31,31 +31,25 @@ function onUseAbility(player, target, ability, action)
     if (player:getWeaponSkillType(tpz.slot.RANGED) == tpz.skill.MARKSMANSHIP) then
         action:animation(target:getID(), action:animation(target:getID()) + 1)
     end
-
-    local hit = 4
-    --get fstr
-    local  fstr = fSTR2(attacker:getStat(tpz.mod.STR), target:getStat(tpz.mod.VIT), attacker:getRangedDmgRank())
-
     local params = {}
+    params.numHits = 1
+    local ftp = 5
+    params.ftp100 = ftp params.ftp200 = ftp params.ftp300 = ftp
+    params.str_wsc = 0.16 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.25 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
+    params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
+    params.canCrit = true
+    params.acc100 = 5.0 params.acc200= 5.0 params.acc300= 5.0
     params.atk100 = 1 params.atk200 = 1 params.atk300 = 1
+    params.enmityMult = 0.5
 
-    --apply WSC
-    local  weaponDamage = player:getRangedDmg()
-
-    local base = weaponDamage + fstr
-    local cratio, ccritratio = cRangedRatio(player, target, params, 0, 0)
-    local pdif = generatePdif (cratio[1], cratio[2], true)
-    local hitrate = getRangedHitRate(player, target, true, +100)
-
-    if (math.random() <= hitrate) then
-        hit = 5
-        damage = (base * pdif) * 5
+    local damage, criticalHit, tpHits, extraHits = doRangedWeaponskill(player, target, 0, params, 0, action, true)
+	
+	damage = damage * 5
 
     -- Set the message id ourselves
-    target:takeDamage(damage, player, tpz.attackType.PHYSICAL, tpz.damageType.PIERCING)
-    target:updateEnmityFromDamage(player, damage)
+    if (tpHits + extraHits > 0) then
         action:messageID(target:getID(), tpz.msg.basic.JA_DAMAGE)
-		action:speceffect(target:getID(), hit)
+        action:speceffect(target:getID(), 32)
     else
         action:messageID(target:getID(), tpz.msg.basic.JA_MISS_2)
         action:speceffect(target:getID(), 0)
