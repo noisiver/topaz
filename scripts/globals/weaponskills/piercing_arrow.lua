b1394889ec0b3af4d779e02e9c0b0f3677242e84
Shiyo <Shiyo@CELTY>
2021-04-13 03:59:31 -0400

    (WS) Piercing arrow given bonus acc

--- scripts/globals/weaponskills/piercing_arrow.lua
+++ scripts/globals/weaponskills/piercing_arrow.lua
@@ -25,7 +25,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     params.str_wsc = 0.0 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.35 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.50 params.crit200 = 0.75 params.crit300 = 1.0
     params.canCrit = true
-    params.acc100 = 0.0 params.acc200= 0.0 params.acc300= 0.0
+    params.acc100 = 2.0 params.acc200= 2.0 params.acc300= 2.0
     params.atk100 = 1; params.atk200 = 1; params.atk300 = 1
     -- Defense ignored is 0%, 35%, 50% as per wiki.bluegartr.com
     params.ignoresDef = false
