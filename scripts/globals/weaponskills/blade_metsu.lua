5894db6d44f4e8afc65759813e8de9d75df34bfd
Shiyo <Shiyo@CELTY>
2021-04-13 12:52:06 -0400

    (WS) Few relic WS tuning

--- scripts/globals/weaponskills/blade_metsu.lua
+++ scripts/globals/weaponskills/blade_metsu.lua
@@ -23,7 +23,7 @@ require("scripts/globals/weaponskills")
 function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local params = {}
     params.numHits = 1
-    params.ftp100 = 3 params.ftp200 = 3 params.ftp300 = 3
+    params.ftp100 = 4 params.ftp200 = 4 params.ftp300 = 4
     params.str_wsc = 0.0 params.dex_wsc = 0.6 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
