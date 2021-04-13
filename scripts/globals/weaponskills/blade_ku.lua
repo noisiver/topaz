ff5381b926a092c88bc568ed8f74bd5ecadad35b
Shiyo <Shiyo@CELTY>
2021-04-13 12:32:29 -0400

    (WS) Tuning Dagger/Katana WS

--- scripts/globals/weaponskills/blade_ku.lua
+++ scripts/globals/weaponskills/blade_ku.lua
@@ -29,7 +29,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     params.str_wsc = 0.0 params.dex_wsc = 0.4 params.vit_wsc = 0.0 params.agi_wsc = 0.4 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
-    params.acc100 = 0.1 params.acc200= 0.15 params.acc300= 0.20
+    params.acc100 = 1 params.acc200= 1.05 params.acc300= 1.10
     params.atk100 = 1; params.atk200 = 1; params.atk300 = 1
     params.multiHitfTP = true
 
