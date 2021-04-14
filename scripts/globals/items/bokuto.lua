a1d9a870c988bd2858f262a79bc02415f666c012
Shiyo <Shiyo@CELTY>
2021-04-14 01:36:13 -0400

    (Items) Added SDT and immunity to Bokuto para proc and buffed its proc rate and potency.

--- scripts/globals/items/bokuto.lua
+++ scripts/globals/items/bokuto.lua
@@ -1,16 +1,21 @@
 -----------------------------------------
 -- ID: 16905
 -- Item: Bokuto
--- Additional Effect: Blindness
+-- Additional Effect: Paralyze
 -----------------------------------------
 require("scripts/globals/status")
 require("scripts/globals/magic")
 require("scripts/globals/msg")
 -----------------------------------
 function onAdditionalEffect(player, target, damage)
-    local chance = 10
-
-    if (math.random(0, 99) >= chance or applyResistanceAddEffect(player, target, tpz.magic.ele.ICE, 0) <= 0.5) then -- changed to ice element
+    local chance = 25
+    local SDT = target:getMod(tpz.mod.SDT_ICE)
+    if SDT <= 5 then
+        chance = 0
+    end
+    if (target:hasImmunity(32)) then
+        spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT)
+    elseif (math.random(0, 99) >= chance or applyResistanceAddEffect(player, target, tpz.magic.ele.ICE, 0) <= 0.5) then -- changed to ice element
         return 0, 0, 0
     else
         target:addStatusEffect(tpz.effect.PARALYSIS, 10, 0, 10) -- added paralysis and changed potency to 10
