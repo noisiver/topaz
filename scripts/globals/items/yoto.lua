58d366fea49150b35347e0f4b648a4d3875dfdc8
Shiyo <Shiyo@CELTY>
2021-04-14 02:12:35 -0400

    (Items) Updated Yoto def down and added immunity.

--- scripts/globals/items/yoto.lua
+++ scripts/globals/items/yoto.lua
@@ -9,13 +9,17 @@ require("scripts/globals/msg")
 -----------------------------------
 
 function onAdditionalEffect(player, target, damage)
-    local chance = 5
+    local chance = 25
+    local SDT = target:getMod(tpz.mod.SDT_WIND)
+    if SDT <= 5 then
+        chance = 0
+    end
 
     if (math.random(0, 99) >= chance or applyResistanceAddEffect(player, target, tpz.magic.ele.WIND, 0) <= 0.5) then
         return 0, 0, 0
     else
         target:delStatusEffect(tpz.effect.DEFENSE_BOOST)
-        target:addStatusEffect(tpz.effect.DEFENSE_DOWN, 12, 0, 60)
+        target:addStatusEffect(tpz.effect.DEFENSE_DOWN, 18.75, 0, 60)
         return tpz.subEffect.DEFENSE_DOWN, tpz.msg.basic.ADD_EFFECT_STATUS, tpz.effect.DEFENSE_DOWN
     end
 end
