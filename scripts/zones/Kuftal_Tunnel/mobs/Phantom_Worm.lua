0a03197b60e012295315a486e481d0fa256c2b3c
Shiyo <Shiyo@CELTY>
2021-04-25 20:48:51 -0400

    Phantom/Morion worm ??? repop fixes and stats

--- scripts/zones/Kuftal_Tunnel/mobs/Phantom_Worm.lua
+++ scripts/zones/Kuftal_Tunnel/mobs/Phantom_Worm.lua
@@ -11,17 +11,18 @@ function onMobInitialize(mob)
 end
 
 function onMobSpawn(mob)
-    mob:addMod(tpz.mod.DEFP, 20) 
-    mob:addMod(tpz.mod.ATTP, 10)
-    mob:addMod(tpz.mod.ACC, 30) 
+    mob:addMod(tpz.mod.DEFP, 50) 
+    mob:addMod(tpz.mod.ATTP, 20)
+    mob:addMod(tpz.mod.ACC, 50) 
     mob:addMod(tpz.mod.EVA, 30)
     mob:setMod(tpz.mod.REFRESH, 40)
     mob:setMod(tpz.mod.UFASTCAST, 50) 
-    mob:addStatusEffect(tpz.effect.REGEN, 10, 3, 0)
+    mob:addStatusEffect(tpz.effect.REGEN, 50, 3, 0)
     mob:addMod(tpz.mod.SPELLINTERRUPT, 50)
     mob:addMod(tpz.mod.SLEEPRESTRAIT, 75)
+	mob:addMod(tpz.mod.SILENCERESTRAIT, 100)
 end
 
 function onMobDeath(mob, player, isKiller)
-    GetNPCByID(ID.npc.MORION_WORM_QM):updateNPCHideTime(900)
+    GetNPCByID(ID.npc.PHANTOM_WORM_QM):updateNPCHideTime(900)
 end
