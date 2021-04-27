e50ab248be26b2d3a66eb0ddc9dda7d163e47cbc
Shiyo <Shiyo@CELTY>
2021-04-26 21:25:28 -0400

    (Mobs - TP Moves) added can interrupt to numerous TP moves

--- scripts/globals/mobskills/damnation_dive.lua
+++ scripts/globals/mobskills/damnation_dive.lua
@@ -28,7 +28,7 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local numhits = 3
     local accmod = 1
-    local dmgmod = .5
+    local dmgmod = 1
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, info.hitslanded)
     local typeEffect = tpz.effect.STUN
@@ -36,5 +36,6 @@ function onMobWeaponSkill(target, mob, skill)
     MobPhysicalStatusEffectMove(mob, target, skill, typeEffect, 1, 0, 4)
 
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
+	if dmg > 0 and skill:getMsg() ~= 31 then target:tryInterruptSpell(mob, info.hitslanded) end
     return dmg
 end
