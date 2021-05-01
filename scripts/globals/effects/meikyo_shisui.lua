e4cef0b30dc0ea23c7ceaf48830975dba51aa9f3
Shiyo <Shiyo@CELTY>
2021-05-01 09:12:25 -0400

    Revert "(Debugging) Meikyo Shisui"
    This reverts commit 6e8be3dda762efbbdb8036e901811a030e37a943.

--- scripts/globals/effects/meikyo_shisui.lua
+++ scripts/globals/effects/meikyo_shisui.lua
@@ -8,12 +8,6 @@ function onEffectGain(target, effect)
 end
 
 function onEffectTick(target, effect)
-	local tp = target:getTP()
-	if tp >= 2001 and <= 2999 then
-		target:setTP(2000)
-	elseif tp >= 1001 and <= 1999 then
-		target:setTP(1000)
-	end
 end
 
 function onEffectLose(target, effect)
