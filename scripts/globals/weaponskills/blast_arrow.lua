d56583c186f3f51e08adabb2a22d8f362c8b9ff0
Shiyo <Shiyo@CELTY>
2021-05-02 10:58:05 -0400

    (WS) Buffed arching/blast arrow

--- scripts/globals/weaponskills/blast_arrow.lua
+++ scripts/globals/weaponskills/blast_arrow.lua
@@ -18,7 +18,7 @@ require("scripts/globals/weaponskills")
 function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
 
     local params = {}
-    params.ftp100 = 3.0 params.ftp200 = 3.5 params.ftp300 = 4.0
+    params.ftp100 = 4.0 params.ftp200 = 4.25 params.ftp300 = 4.75
     params.str_wsc = 0.0 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 1.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.ele = tpz.magic.ele.LIGHTNING
     params.skill = tpz.skill.ARCHERY
