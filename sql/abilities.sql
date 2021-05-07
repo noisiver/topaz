afb7b3012b4078ce568afde0d01f749cb6b5d0cd
Shiyo <Shiyo@CELTY>
2021-05-07 09:58:40 -0400

    (Abilities) Fixed recast ID and made 2m CD

--- sql/abilities.sql
+++ sql/abilities.sql
@@ -63,7 +63,7 @@ INSERT INTO `abilities` VALUES (37,'dodge',2,15,1,60,14,121,0,5,2000,0,6,20.0,0,
 INSERT INTO `abilities` VALUES (38,'chakra',2,20,1,30,15,102,0,6,2000,0,6,20.0,0,1,300,452,0,NULL);
 INSERT INTO `abilities` VALUES (39,'boost',2,5,1,15,16,116,0,7,2000,0,6,20.0,0,1,300,0,0,NULL);
 INSERT INTO `abilities` VALUES (40,'counterstance',2,45,1,300,17,0,0,8,2000,0,6,20.0,0,1,900,0,0,NULL);
-INSERT INTO `abilities` VALUES (41,'steal',6,5,4,60,120,125,0,181,2000,0,3,5.4,0,1,300,0,0,NULL);		-- made 60s
+INSERT INTO `abilities` VALUES (41,'steal',6,5,4,120,60,125,0,181,2000,0,3,5.4,0,1,300,0,0,NULL);		-- made 120s
 INSERT INTO `abilities` VALUES (42,'flee',6,25,1,300,62,126,0,9,2000,0,6,20.0,0,1,80,704,0,NULL);	
 INSERT INTO `abilities` VALUES (43,'hide',6,45,1,300,63,0,0,10,2000,0,6,20.0,0,0,0,706,0,NULL);		
 INSERT INTO `abilities` VALUES (44,'sneak_attack',6,15,1,60,64,0,0,17,2000,0,6,20.0,0,1,0,708,0,NULL);
