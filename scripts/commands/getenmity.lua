b8bc6f7f06ed0bc119405230d714918f165cea68
Shiyo <Shiyo@CELTY>
2021-05-10 15:48:49 -0400

    (Commands) New command for seeing mob enmity

---
+++ scripts/commands/getenmity.lua
@@ -0,0 +1,28 @@
+---------------------------------------------------------------------------------------------------
+-- func: getenmity
+-- desc: prints the target mob's current CE and VE towards you
+---------------------------------------------------------------------------------------------------
+require("scripts/globals/status")
+
+cmdprops =
+{
+    permission = 1,
+    parameters = ""
+}
+
+function error(player, msg)
+    player:PrintToPlayer(msg)
+    player:PrintToPlayer("!getenmity")
+end
+
+function onTrigger(player)
+    
+    local targ = player:getCursorTarget()
+    
+    if targ == nil or targ:isMob() == false then
+        error(player, "you must select a target monster with the cursor first")
+    end
+    
+    player:PrintToPlayer(string.format("your enmity against %s is ... CE = %u ... VE = %u", targ:getName(), targ:getCE(player), targ:getVE(player)))
+    
+end
