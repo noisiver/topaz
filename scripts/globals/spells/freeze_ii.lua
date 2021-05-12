225eeb959dd798ce49f066845f48769e57842125
Shiyo <Shiyo@CELTY>
2021-05-12 11:04:57 -0400

    (Debugging) Freeze

--- scripts/globals/spells/freeze_ii.lua
+++ scripts/globals/spells/freeze_ii.lua
@@ -30,18 +30,13 @@ function onSpellCast(caster, target, spell)
 
 
     -- no point in making a separate function for this if the only thing they won't have in common is the name
+
+    target:addStatusEffect(tpz.effect.DEFENSE_DOWN, 18.75, 0, 60)
+	target:addStatusEffect(tpz.effect.EVASION_DOWN, 20, 0, 60)
     handleNinjutsuDebuff(caster, target, spell, 30, 60, tpz.mod.FIRERES)
 	
 
-        local duration = 60
-        local resist = applyResistanceEffect(player, target, tpz.magic.ele.WIND, 0)
-        if (target:hasStatusEffect(tpz.effect.DEFENSE_DOWN) == false) and resist >= 0.5  then
-            target:addStatusEffect(tpz.effect.DEFENSE_DOWN, 18.75, 0, duration * resist)
-        end
-		local resist = applyResistanceEffect(player, target, tpz.magic.ele.ICE, 0)
-        if (target:hasStatusEffect(tpz.effect.EVASION_DOWN) == false) and resist >= 0.5  then
-            target:addStatusEffect(tpz.effect.EVASION_DOWN, 20, 0, duration * resist)
-        end
+
 
 
     return doElementalNuke(caster, spell, target, spellParams)
