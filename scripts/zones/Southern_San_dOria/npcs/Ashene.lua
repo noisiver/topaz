d7aa404674f02948371020543ef47af7926fdadb
Shiyo <Shiyo@CELTY>
2021-05-13 11:43:43 -0400

    (NPC) Fixed conquest requirements

--- scripts/zones/Southern_San_dOria/npcs/Ashene.lua
+++ scripts/zones/Southern_San_dOria/npcs/Ashene.lua
@@ -14,14 +14,14 @@ end
 function onTrigger(player, npc)
     local stock =
     {
-        16455,  4309, 1,    -- Baselard
-        16532, 16934, 1,    -- Gladius
-        16545, 21067, 1,    -- Broadsword
-        16576, 35769, 1,    -- Hunting Sword
-        16524, 13406, 1,    -- Fleuret
-        16450,  1827, 2,    -- Dagger
-        16536,  7128, 2,    -- Iron Sword
-        16566,  8294, 2,    -- Longsword
+        16455,  4309, 3,    -- Baselard
+        16532, 16934, 3,    -- Gladius
+        16545, 21067, 3,    -- Broadsword
+        16576, 35769, 3,    -- Hunting Sword
+        16524, 13406, 3,    -- Fleuret
+        16450,  1827, 3,    -- Dagger
+        16536,  7128, 3,    -- Iron Sword
+        16566,  8294, 3,    -- Longsword
         16448,   140, 3,    -- Bronze Dagger
         16449,   837, 3,    -- Brass Dagger
         16531,  3523, 3,    -- Brass Xiphos
