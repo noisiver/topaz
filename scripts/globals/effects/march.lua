1b3e8839e6b8edbd38cb7f06f236b4c8696a07a6
Shiyo <Shiyo@CELTY>
2021-05-15 16:55:50 -0400

    (debugging) honor march

--- scripts/globals/effects/march.lua
+++ scripts/globals/effects/march.lua
@@ -10,6 +10,7 @@ require("scripts/globals/status")
 
 function onEffectGain(target, effect)
     target:addMod(tpz.mod.HASTE_MAGIC, effect:getPower())
+    target:addMod(tpz.mod.ATT, effect:getSubPower())
 end
 
 function onEffectTick(target, effect)
@@ -17,4 +18,5 @@ end
 
 function onEffectLose(target, effect)
     target:delMod(tpz.mod.HASTE_MAGIC, effect:getPower())
+    target:delMod(tpz.mod.ATT, effect:getSubPower())
 end
