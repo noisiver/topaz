ceea16d68e628e95af2db9271ccee0d2b446ff02
Shiyo <Shiyo@CELTY>
2021-05-15 16:45:16 -0400

    (debugging) honor march

--- scripts/globals/spells/honor_march.lua
+++ scripts/globals/spells/honor_march.lua
@@ -11,45 +11,10 @@ function onMagicCastingCheck(caster, target, spell)
 end
 
 function onSpellCast(caster, target, spell)
-    local sLvl = caster:getSkillLevel(tpz.skill.SINGING) -- Gets skill level of Singing
-    local iLvl = caster:getWeaponSkillLevel(tpz.slot.RANGED)
-
     --local power = 43
-     local power = 25
-     local subpower = 300
+    local power = 25
+    local subpower = 300
 
-   -- if (sLvl+iLvl > 300) then
-     --   power = power + math.floor((sLvl+iLvl-300) / 7) -- cap is 600 skill
-    --end
-
-    --if (power >= 163) then
-      --  power = 163
-        
-
-    local iBoost = caster:getMod(tpz.mod.MARCH_EFFECT) + caster:getMod(tpz.mod.ALL_SONGS_EFFECT)
-    power = power + iBoost*16
-    
-
-    if (caster:hasStatusEffect(tpz.effect.SOUL_VOICE)) then
-        power = power * 2
-    elseif (caster:hasStatusEffect(tpz.effect.MARCATO)) then
-        power = power * 1.5
-    end
-    caster:delStatusEffect(tpz.effect.MARCATO)
-
-    -- convert to new haste system
-    power = (power / 1024) * 10000
-
-    local duration = 30
-    duration = duration * ((iBoost * 0.1) + (caster:getMod(tpz.mod.SONG_DURATION_BONUS)/100) + 1)
-
-    if (caster:hasStatusEffect(tpz.effect.TROUBADOUR)) then
-        duration = duration * 2
-    end
-
-    if not (target:addBardSong(caster, tpz.effect.MARCH, power, 0, duration, caster:getID(), 0, 2)) then
-        spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT)
-    end
 
     return tpz.effect.HONOR_MARCH
 end
