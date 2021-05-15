d08ca41674e7202101a797d32820d0f78bf2b83c
Shiyo <Shiyo@CELTY>
2021-05-15 12:20:14 -0400

    Msg fixes

--- scripts/zones/The_Eldieme_Necropolis/npcs/_5fu.lua
+++ scripts/zones/The_Eldieme_Necropolis/npcs/_5fu.lua
@@ -12,12 +12,12 @@ require("scripts/globals/npc_util")
 function onTrade(player, npc, trade)
     if npcUtil.tradeHasExactly(trade, 768) then -- Flint Stone
         if npcUtil.popFromQM(player, npc, ID.mob.SKULL_OF_GLUTTONY) then -- items and mob id here under mob = in IDs.lua
-            player:messageSpecial(npc, ID.text.THE_BRAZIER_IS_LIT)
+            player:messageSpecial(ID.text.THE_BRAZIER_IS_LIT)
             player:confirmTrade()
         end
     end
 end
 
 function onTrigger(player, npc)
-    player:messageSpecial(npc, ID.text.SENSE_OF_FOREBODING)
+    player:messageSpecial(ID.text.SENSE_OF_FOREBODING)
 end
