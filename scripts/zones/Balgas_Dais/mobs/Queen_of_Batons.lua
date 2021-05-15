06967efc880f405bd10eed6abc23fd5486cb1e32
Shiyo <Shiyo@CELTY>
2021-05-15 11:28:19 -0400

    fixes

--- scripts/zones/Balgas_Dais/mobs/Queen_of_Batons.lua
+++ scripts/zones/Balgas_Dais/mobs/Queen_of_Batons.lua
@@ -4,7 +4,6 @@
 -- Royale Ramble
 -- !addkeyitem 1178
 -----------------------------------
-local ID = require("scripts/zones/Balgias_Dais/IDs")
 require("scripts/globals/titles")
 require("scripts/globals/status")
 require("scripts/globals/magic")
