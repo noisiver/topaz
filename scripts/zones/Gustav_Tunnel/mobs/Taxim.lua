17144b6ac5653a47401b38ffa04689cdf5a91f47
Shiyo <Shiyo@CELTY>
2021-05-16 10:39:10 -0400

    (Mobs - NMS) Added stats/gil for Taxim.

--- scripts/zones/Gustav_Tunnel/mobs/Taxim.lua
+++ scripts/zones/Gustav_Tunnel/mobs/Taxim.lua
@@ -4,6 +4,15 @@
 -----------------------------------
 require("scripts/globals/hunts")
 
+function onMobSpawn(mob)
+    mob:addMod(tpz.mod.ATTP, 25)
+    mob:addMod(tpz.mod.DEFP, 35)
+    mob:addMod(tpz.mod.ACC, 50) 
+    mob:addMod(tpz.mod.EVA, 20)
+    mob:setMod(tpz.mod.REFRESH, 400)
+    mob:setMobMod(tpz.mobMod.GIL_MIN, 3000)
+end
+
 function onMobDeath(mob, player, isKiller)
     tpz.hunts.checkHunt(mob, player, 424)
 end
