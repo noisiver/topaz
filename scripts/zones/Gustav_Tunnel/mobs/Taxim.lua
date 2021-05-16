a3e87e44b84b6cf90a9c9c10b4205b838a2389f1
Shiyo <Shiyo@CELTY>
2021-05-16 10:41:24 -0400

    Update Taxim.lua

--- scripts/zones/Gustav_Tunnel/mobs/Taxim.lua
+++ scripts/zones/Gustav_Tunnel/mobs/Taxim.lua
@@ -10,6 +10,8 @@ function onMobSpawn(mob)
     mob:addMod(tpz.mod.ACC, 50) 
     mob:addMod(tpz.mod.EVA, 20)
     mob:setMod(tpz.mod.REFRESH, 400)
+    mob:addMod(tpz.mod.SLEEPRESTRAIT, 100)
+    mob:addMod(tpz.mod.LULLABYRESTRAIT, 100)
     mob:setMobMod(tpz.mobMod.GIL_MIN, 3000)
 end
 
