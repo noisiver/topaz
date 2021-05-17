1c576f841b31222977cced677f6c65ced90fb757
Shiyo <Shiyo@CELTY>
2021-05-16 20:17:13 -0400

    Revert "(C++) Fix gil drops and gilfinder from mobs"
    This reverts commit b312331e5006e6f268359f4a38ddcdf667886ffd.

--- src/map/utils/charutils.cpp
+++ src/map/utils/charutils.cpp
@@ -3236,13 +3236,12 @@ namespace charutils
             {
                 // distribute gil
                 int32 gilPerPerson = static_cast<int32>(gil / members.size());
-                for (auto* PMember : members)
+                for (auto PMember : members)
                 {
                     // Check for gilfinder
-                    int32 gilForThisPerson = gilPerPerson + (gilPerPerson * PMember->getMod(Mod::GILFINDER) / 100);
-                    gilForThisPerson = std::clamp<int32>(gilForThisPerson, 1, gilForThisPerson);
-                    UpdateItem(PMember, LOC_INVENTORY, 0, gilForThisPerson);
-                    PMember->pushPacket(new CMessageBasicPacket(PMember, PMember, gilForThisPerson, 0, 565));
+                    gilPerPerson += gilPerPerson * PMember->getMod(Mod::GILFINDER) / 100;
+                    UpdateItem(PMember, LOC_INVENTORY, 0, gilPerPerson);
+                    PMember->pushPacket(new CMessageBasicPacket(PMember, PMember, gilPerPerson, 0, 565));
                 }
             }
         }
