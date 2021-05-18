17e3753f5de67a702024e69ec55bb82cfca6437b
Shiyo <Shiyo@CELTY>
2021-05-18 19:20:52 -0400

    (Abilities) Added fealty to mijin and buff its damage

--- scripts/globals/mobskills/mijin_gakure.lua
+++ scripts/globals/mobskills/mijin_gakure.lua
@@ -10,13 +10,18 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local dmgmod = 1
+    local dmgmod = 2
     local hpmod = skill:getMobHPP() / 100
     local basePower = (mob:getFamily() == 335) and 4 or 6 -- Maat has a weaker (4) Mijin than usual (6)
     local power = hpmod * 1.3 + basePower -- changed from *10
     local baseDmg = mob:getWeaponDmg() * power
     local info = MobMagicalMove(mob, target, skill, baseDmg, tpz.magic.ele.NONE, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.ELEMENTAL, MOBPARAM_IGNORE_SHADOWS)
+	
+    if target:hasStatusEffect(tpz.effect.FEALTY) then
+        dmg = math.floor(dmg / 16)
+    end
+
 
 --    if mob:isInDynamis() then -- dynamis mobs will kill themselves, other mobs might not
 --        mob:setHP(0)
