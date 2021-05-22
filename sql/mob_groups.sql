f6fd07feda68044671206d67663d5fbe3018ac1c
Shiyo <Shiyo@CELTY>
2021-05-22 10:10:16 -0400

    (Mobs - Spawns / Levels) Cape terrigan spawns and level fixes

--- sql/mob_groups.sql
+++ sql/mob_groups.sql
@@ -7696,7 +7696,7 @@ INSERT INTO `mob_groups` VALUES (18,1235,113,'Enna-enna',330,8,569,0,0,65,69,0);
 INSERT INTO `mob_groups` VALUES (19,3451,113,'Sand_Cockatrice',330,0,2147,0,0,71,74,0);
 INSERT INTO `mob_groups` VALUES (20,5889,113,'Killer_Jonny',28800,0,3088,0,0,85,85,0);
 INSERT INTO `mob_groups` VALUES (21,3882,113,'Terror_Pugil',330,0,1229,0,0,66,70,0);
-INSERT INTO `mob_groups` VALUES (22,71,113,'Air_Elemental',330,4,38,0,0,52,54,0);
+INSERT INTO `mob_groups` VALUES (22,71,113,'Air_Elemental',330,4,38,0,0,67,69,0);
 INSERT INTO `mob_groups` VALUES (23,5425,113,'Tegmine',7200,0,2945,10500,5000,70,73,0);
 INSERT INTO `mob_groups` VALUES (24,1806,113,'Greater_Manticore',330,0,1228,0,0,76,79,0);
 INSERT INTO `mob_groups` VALUES (25,1429,113,'Frostmane',0,32,912,0,0,80,80,0);
