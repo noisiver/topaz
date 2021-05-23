497a662d8679866dbd9f9577ece1d3ab0e7b3d3b
Shiyo <Shiyo@CELTY>
2021-05-22 23:04:34 -0400

    (WS) Buffed swift blade to era accurate 1.5 FTP

--- scripts/globals/weaponskills/swift_blade.lua
+++ scripts/globals/weaponskills/swift_blade.lua
@@ -20,7 +20,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
 
     local params = {}
     params.numHits = 3
-    params.ftp100 = 1.33 params.ftp200 = 1.33 params.ftp300 = 1.33
+    params.ftp100 = 1.5 params.ftp200 = 1.5 params.ftp300 = 1.5
     params.str_wsc = 0.4 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.4 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
