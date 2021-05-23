df01e796013488c2d134b24c8289b382663ecf2f
Shiyo <Shiyo@CELTY>
2021-05-23 19:35:38 -0400

    Update rampart.lua

--- scripts/globals/abilities/rampart.lua
+++ scripts/globals/abilities/rampart.lua
@@ -18,7 +18,4 @@ end
 function onUseAbility(player, target, ability)
     local duration = 30 + player:getMod(tpz.mod.RAMPART_DURATION)
     target:addStatusEffect(tpz.effect.RAMPART, 25, 0, duration)
-   
-   local mob = player:getTarget()
-        mob:addEnmity(player, 1920, 1920)
 end
