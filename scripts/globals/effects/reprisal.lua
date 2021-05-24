cde3a0f4d648b8009ee6a4377b5f4dbce0447381
Shiyo <Shiyo@CELTY>
2021-05-24 18:22:09 -0400

    Update reprisal.lua

--- scripts/globals/effects/reprisal.lua
+++ scripts/globals/effects/reprisal.lua
@@ -20,6 +20,7 @@ function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
+	local skill = target:getSkillLevel(tpz.skill.SHIELD) * 15
     target:delMod(tpz.mod.SPIKES, 6)
     target:setMod(tpz.mod.SPIKES_DMG, 0)
 	target:delMod(tpz.mod.SHIELD, skill skill)
