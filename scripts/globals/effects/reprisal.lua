8b61397b385389cf35338ef639f0153416f3749a
Shiyo <Shiyo@CELTY>
2021-05-24 18:16:43 -0400

    Update reprisal.lua

--- scripts/globals/effects/reprisal.lua
+++ scripts/globals/effects/reprisal.lua
@@ -7,12 +7,12 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onEffectGain(target, effect)
-	local skill = target:getSkillLevel(tpz.skill.SHIELD)
+	local skill = target:getSkillLevel(tpz.skill.SHIELD) * 15
     
 	target:addMod(tpz.mod.SPIKES, 6)
      -- Spike damage is calculated on hit in battleutils::TakePhysicalDamage
     target:setMod(tpz.mod.SPIKES_DMG, 0)
-	target:addMod(tpz.mod.SHIELD, skill * 0.15)
+	target:addMod(tpz.mod.SHIELD, skill)
     target:addMod(tpz.mod.SHIELDBLOCKRATE, 50)
 end
 
@@ -22,6 +22,6 @@ end
 function onEffectLose(target, effect)
     target:delMod(tpz.mod.SPIKES, 6)
     target:setMod(tpz.mod.SPIKES_DMG, 0)
-	target:delMod(tpz.mod.SHIELD, skill * 0.15)
+	target:delMod(tpz.mod.SHIELD, skill skill)
     target:delMod(tpz.mod.SHIELDBLOCKRATE, 50)
 end
