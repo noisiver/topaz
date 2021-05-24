c1639a96897823063af1d5e501f906c4f1654054
Shiyo <Shiyo@CELTY>
2021-05-24 11:16:42 -0400

    (Items) Set anni ring to 50k

--- scripts/globals/items/anniversary_ring.lua
+++ scripts/globals/items/anniversary_ring.lua
@@ -19,5 +19,5 @@ function onItemCheck(target)
 end
 
 function onItemUse(target)
-    target:addStatusEffect(tpz.effect.DEDICATION, 100, 0, 43200, 0, 20000)
+    target:addStatusEffect(tpz.effect.DEDICATION, 100, 0, 43200, 0, 50000)
 end
