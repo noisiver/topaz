50336867ee4ec1d786909ea553558b47d4f7a74c
Shiyo <Shiyo@CELTY>
2021-05-26 10:17:28 -0400

    (Mobs) Spawn fixes and goblin wolfman mechanics

--- scripts/zones/Oldton_Movalpolos/mobs/Goblin_Wolfman.lua
+++ scripts/zones/Oldton_Movalpolos/mobs/Goblin_Wolfman.lua
@@ -11,6 +11,21 @@ function onMobInitialize(mob)
     mob:setMobMod(tpz.mobMod.IDLE_DESPAWN, 180)
 end
 
+function onMobSpawn(mob)
+     mob:addMod(tpz.mod.DEFP, 30) 
+     mob:addMod(tpz.mod.ATTP, 50)
+     mob:addMod(tpz.mod.EVA, 30)
+     mob:setMod(tpz.mod.REFRESH, 300)
+end
+
+function onMobFight(mob, target)
+    if (mob:hasStatusEffect(tpz.effect.BLOOD_WEAPON) == true) then
+        mob:setMod(tpz.mod.ATTP, 300)
+    else
+	        mob:setMod(tpz.mod.ATTP, 50)
+    end
+end
+
 function onMobDeath(mob, player, isKiller)
     tpz.hunts.checkHunt(mob, player, 245)
 end
