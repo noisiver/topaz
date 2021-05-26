02532b2f68190634f9b66a0512ecf02d5f1bcb93
Shiyo <Shiyo@CELTY>
2021-05-26 14:31:03 -0400

    (WS) Made moonlight/starlight accurate

--- scripts/globals/weaponskills/moonlight.lua
+++ scripts/globals/weaponskills/moonlight.lua
@@ -8,8 +8,8 @@ require("scripts/globals/weaponskills")
 
 function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local lvl = player:getSkillLevel(11) -- get club skill
-    local damage = lvl/5
-    local damagemod = damage * (1 + (tp-1000)/1000*1.25)
+    local damage = lvl * 0.11
+    local damagemod = damage * (tp + 1000)
 
 
     return 1, 0, false, damagemod
