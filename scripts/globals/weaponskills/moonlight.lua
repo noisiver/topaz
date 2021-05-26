a81ba4a38bfe565ce4f25e958c57582080341e34
Shiyo <Shiyo@CELTY>
2021-05-26 14:49:49 -0400

    Revert "Update moonlight.lua"
    This reverts commit 2b6ea29fc07f4e4b297b1f420193d1865ef27823.

--- scripts/globals/weaponskills/moonlight.lua
+++ scripts/globals/weaponskills/moonlight.lua
@@ -10,6 +10,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local lvl = player:getSkillLevel(11) -- get club skill
     local damage = lvl * 0.11
     local damagemod = damage * (tp + 1000)
-    damagemod = damagemod * WEAPON_SKILL_POWER
-	return 1, 0, false, damagemod
+
+
+    return 1, 0, false, damagemod
 end
