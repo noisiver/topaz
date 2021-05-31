1e5cce5ce1e90baf3d614188ce704ac74554a3bf
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-05-31 17:44:44 -0400

    Update venom_shower.lua

--- scripts/globals/mobskills/venom_shower.lua
+++ scripts/globals/mobskills/venom_shower.lua
@@ -1,12 +1,13 @@
----------------------------------------------
---
--- Venom Storm
---
----------------------------------------------
-require("scripts/globals/monstertpmoves")
+---------------------------------------------------
+-- Venom Shower
+-- Deals Water damage to enemies within an area of effect. Additional effect: Poison
+---------------------------------------------------
+
 require("scripts/globals/settings")
 require("scripts/globals/status")
----------------------------------------------
+require("scripts/globals/monstertpmoves")
+
+---------------------------------------------------
 
 function onMobSkillCheck(target, mob, skill)
     return 0
@@ -14,7 +15,14 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.POISON
+    local power = 8
+    local duration = 30
 
-    skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, 5, 3, 60))
-    return typeEffect
+    MobStatusEffectMove(mob, target, typeEffect, power, 0, duration)
+
+    local dmgmod = 0.5
+    local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg() * 3, tpz.magic.ele.WATER, dmgmod, TP_MAB_BONUS, 1)
+    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.WATER, MOBPARAM_WIPE_SHADOWS)
+    target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.WATER)
+    return dmg
 end
