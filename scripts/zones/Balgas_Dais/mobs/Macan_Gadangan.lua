717285504c7a1ef84598b04c5e448552ccabe3a8
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-06-01 21:59:30 -0400

    (BCNM) WWW Bacan add no standback and no drops and stats

--- scripts/zones/Balgas_Dais/mobs/Macan_Gadangan.lua
+++ scripts/zones/Balgas_Dais/mobs/Macan_Gadangan.lua
@@ -6,6 +6,17 @@
 require("scripts/globals/magic")
 require("scripts/globals/status")
 -----------------------------------
+function onMobInitialize(mob)
+    mob:setMobMod(tpz.mobMod.HP_STANDBACK, -1)
+end
+
+function onMobSpawn(mob)
+     mob:addMod(tpz.mod.DEFP, 40) 
+     mob:addMod(tpz.mod.ATTP, 40)
+     mob:addMod(tpz.mod.ACC, 40) 
+     mob:addMod(tpz.mod.EVA, 30)
+     mob:setMobMod(tpz.mobMod.NO_DROPS, 0)
+end
 
 function onMobFight(mob, target)
     if (mob:hasStatusEffect(tpz.effect.SILENCE) == true) then
