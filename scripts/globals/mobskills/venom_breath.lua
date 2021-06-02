899909dd76859993e4cfafd40c1ded204ab6e5a3
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-06-02 11:44:06 -0400

    (HNM) Serket TP move Tuning

--- scripts/globals/mobskills/venom_breath.lua
+++ scripts/globals/mobskills/venom_breath.lua
@@ -14,10 +14,7 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.POISON
-    MobStatusEffectMove(mob, target, typeEffect, 100, 3, 60)
+    skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, 30, 3, 120))
 
-    local dmgmod = MobBreathMove(mob, target, 0.25, 1, tpz.magic.ele.WATER, 100)
-    local dmg = MobFinalAdjustments(dmgmod, mob, skill, target, tpz.attackType.BREATH, tpz.damageType.ICE, MOBPARAM_IGNORE_SHADOWS)
-    target:takeDamage(dmg, mob, tpz.attackType.BREATH, tpz.damageType.ICE)
-    return dmg
+    return effect
 end
