7229a90e9ab2b339e5beab1457d2b1286d46b496
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-06-06 11:30:41 -0400

    (TP Moves) Fix T4 avatar nukes wiping shadows

--- scripts/globals/mobskills/water_iv.lua
+++ scripts/globals/mobskills/water_iv.lua
@@ -17,7 +17,7 @@ function onMobWeaponSkill(target, mob, skill)
 
     dmgmod = 2
     info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg() * 4, tpz.magic.ele.WATER, dmgmod, TP_MAB_BONUS, 1)
-    dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.WATER, MOBPARAM_WIPE_SHADOWS)
+    dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.WATER, MOBPARAM_IGNORE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.WATER)
     return dmg
 
