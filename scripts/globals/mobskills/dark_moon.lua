61127c8ca87dd9ceaabb4f273a6f911307036dbb
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-06-06 13:29:02 -0400

    (Custom) Fealty added to dark moons weakness

--- scripts/globals/mobskills/dark_moon.lua
+++ scripts/globals/mobskills/dark_moon.lua
@@ -14,7 +14,12 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.WEAKNESS
 
-    MobStatusEffectMove(mob, target, typeEffect, 1, 0, 30)
+    if target:hasStatusEffect(tpz.effect.FEALTY) then
+        skill:setMsg(tpz.msg.basic.SKILL_NO_EFFECT)
+    else
+        skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, 1, 0, 30))
+    end
+	
     local dmgmod = 1.5
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg() * 3, tpz.magic.ele.DARK, dmgmod, TP_NO_EFFECT, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.DARK, MOBPARAM_WIPE_SHADOWS)
