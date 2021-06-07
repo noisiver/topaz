df6f0a30fa1f7cfc3ffd0a18a0c16cbbe75749a0
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-06-07 15:11:02 -0400

    (NMs) Updated sozu to drop gil

--- scripts/zones/Temple_of_Uggalepih/mobs/Sozu_Rogberry.lua
+++ scripts/zones/Temple_of_Uggalepih/mobs/Sozu_Rogberry.lua
@@ -9,6 +9,14 @@ mixins =
     require("scripts/mixins/job_special")
 }
 -----------------------------------
+function onMobSpawn(mob)
+    mob:addMod(tpz.mod.DEFP, 50) 
+    mob:addMod(tpz.mod.ATTP, 35)
+    mob:addMod(tpz.mod.ACC, 30) 
+    mob:addMod(tpz.mod.EVA, 30)
+    mob:setMobMod(tpz.mobMod.GIL_MIN, 4000)
+end
+
 
 function onMobDeath(mob, player, isKiller)
     tpz.hunts.checkHunt(mob, player, 390)
