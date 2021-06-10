23652d4dd50837ffd23871dab1f46279445017e3
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-06-10 17:42:25 -0400

    (Dynamis) Add BDT to statues

--- sql/mob_family_mods.sql
+++ sql/mob_family_mods.sql
@@ -356,6 +356,7 @@ INSERT INTO `mob_family_mods` VALUES (92,63,50,0);	-- Statue-Goblin +50% DEF
 INSERT INTO `mob_family_mods` VALUES (92,25,50,0);	-- Statue-Goblin +50 ACC
 INSERT INTO `mob_family_mods` VALUES (92,68,50,0);	-- Statue-Goblin +50 EVAS
 INSERT INTO `mob_family_mods` VALUES (92,29,112,0);	-- Statue-Goblin +112 MDB
+INSERT INTO `mob_family_mods` VALUES (92,388,-50,0); -- Statue-Goblin -50% BDT
 INSERT INTO `mob_family_mods` VALUES (92,73,100,0);	-- Statue-Goblin +100 Store TP
 INSERT INTO `mob_family_mods` VALUES (92,1,5000,0); -- Statue-Goblin +5000 Defense Bonus
 INSERT INTO `mob_family_mods` VALUES (92,28,-100,1); -- Statue-Goblin -100% exp
@@ -365,6 +366,7 @@ INSERT INTO `mob_family_mods` VALUES (93,63,50,0);	-- Statue-Orc +50% DEF
 INSERT INTO `mob_family_mods` VALUES (93,25,50,0);	-- Statue-Orc +50 ACC
 INSERT INTO `mob_family_mods` VALUES (93,68,50,0);	-- Statue-Orc +50 EVAS
 INSERT INTO `mob_family_mods` VALUES (93,29,62,0);	-- Statue-Orc +62 MDB
+INSERT INTO `mob_family_mods` VALUES (93,388,-50,0); -- Statue-Orc -50% BDT
 INSERT INTO `mob_family_mods` VALUES (93,73,100,0);	-- Statue-Orc +100 Store TP
 INSERT INTO `mob_family_mods` VALUES (93,1,5000,0); -- Statue-Orc +5000 Defense Bonus
 INSERT INTO `mob_family_mods` VALUES (93,28,-100,1); -- Statue-Orc -100% exp
@@ -374,6 +376,7 @@ INSERT INTO `mob_family_mods` VALUES (94,63,50,0);	-- Statue-Quadav +50% DEF
 INSERT INTO `mob_family_mods` VALUES (94,25,50,0);	-- Statue-Quadav +50 ACC
 INSERT INTO `mob_family_mods` VALUES (94,68,50,0);	-- Statue-Quadav +50 EVAS
 INSERT INTO `mob_family_mods` VALUES (94,29,112,0);	-- Statue-Quadav +112 MDB
+INSERT INTO `mob_family_mods` VALUES (94,388,-50,0); -- Statue-Quadav -50% BDT
 INSERT INTO `mob_family_mods` VALUES (94,73,100,0);	-- Statue-Quadav +100 Store TP
 INSERT INTO `mob_family_mods` VALUES (94,1,5000,0); -- Statue-Quadav +5000 Defense Bonus
 INSERT INTO `mob_family_mods` VALUES (94,28,-100,1); -- Statue-Quadav -100% exp
@@ -383,6 +386,7 @@ INSERT INTO `mob_family_mods` VALUES (95,63,50,0);	-- Statue-Yagud +50% DEF
 INSERT INTO `mob_family_mods` VALUES (95,25,50,0);	-- Statue-Yagud +50 ACC
 INSERT INTO `mob_family_mods` VALUES (95,68,50,0);	-- Statue-Yagud +50 EVAS
 INSERT INTO `mob_family_mods` VALUES (95,29,162,0);	-- Statue-Yagudo +162 MDB
+INSERT INTO `mob_family_mods` VALUES (95,388,-50,0); -- Statue-Yagudo -50% BDT
 INSERT INTO `mob_family_mods` VALUES (95,73,100,0);	-- Statue-Yagudo +100 Store TP
 INSERT INTO `mob_family_mods` VALUES (95,1,5000,0); -- Statue-Yagudo +5000 Defense Bonus
 INSERT INTO `mob_family_mods` VALUES (95,28,-100,1); -- Statue-Yagudo -100% exp
