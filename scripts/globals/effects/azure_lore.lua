cf881a2cfe0328e3497626a01e386c61df51d0fe
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-06-11 13:48:28 -0400

    (BLU) Give azure lore acc and macc bonuses

--- scripts/globals/effects/azure_lore.lua
+++ scripts/globals/effects/azure_lore.lua
@@ -5,10 +5,14 @@
 -----------------------------------
 
 function onEffectGain(target, effect)
+    target:addMod(tpz.mod.ACC, 100) 
+    target:addMod(tpz.mod.MACC, 100)
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
+    target:delMod(tpz.mod.ACC, 100) 
+    target:delMod(tpz.mod.MACC, 100)
 end
