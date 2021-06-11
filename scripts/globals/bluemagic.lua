170cfb014248e6084b84cfab690d2ccf4fcbc0c2
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-06-11 14:50:37 -0400

    Revert "(debugging) blu"
    This reverts commit b6237ed6bd5397619ba0efb07f63e63f44aef822.

--- scripts/globals/bluemagic.lua
+++ scripts/globals/bluemagic.lua
@@ -424,13 +424,7 @@ function BlueGetHitRate(attacker, target, capHitRate)
 end
 
 -- Function to stagger duration of effects by using the resistance to change the value
-function getBlueEffectDuration(caster, resist, effect, params, tp)
-    if (params.dura150 == nil) then
-		params.dura150 = 0
-	end
-    if (params.dura300 == nil) then
-		params.dura300 = 0
-	end
+function getBlueEffectDuration(caster, resist, effect)
     local duration = 0
     local DurationTPBonus = 1
 
