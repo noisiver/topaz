a58e3a0480b84a677ed012ae56afa97d45c71e3b
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-06-10 21:43:37 -0400

    (blu) debugging

--- scripts/globals/bluemagic.lua
+++ scripts/globals/bluemagic.lua
@@ -125,7 +125,7 @@ function BluePhysicalSpell(caster, target, spell, params, tp)
 		AtkTPBonus = params.atk300
 	end
 	
-	local bluphysattk = (((caster:getSkillLevel(tpz.skill.BLUE_MAGIC) + 8 + (caster:getStat(tpz.mod.STR) / 2)) * params.attkbonus) * atkmulti)
+	local bluphysattk = (((caster:getSkillLevel(tpz.skill.BLUE_MAGIC) + 8 + (caster:getStat(tpz.mod.STR) / 2)) * params.attkbonus) * AtkTPBonus)
     if (params.offcratiomod == nil) then -- default to attack. Pretty much every physical spell will use this, Cannonball being the exception.
         params.offcratiomod = bluphysattk
     end
