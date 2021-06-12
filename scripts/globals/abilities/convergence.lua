468bf659eb30614650294ab45956c6ccd86c1f14
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-06-12 11:01:19 -0400

    (BLU) Add convergence

---
+++ scripts/globals/abilities/convergence.lua
@@ -0,0 +1,21 @@
+-----------------------------------
+-- Ability: Convergence
+-- Increases the power of your next magical Blue Magic spell. Limits area of effect to single target.
+-- Obtained: Blue Mage(Merit)
+-- Recast Time: 10:00
+-- Duration: 1 Spell or 60 seconds, whichever occurs first.
+-- Maximum of +25 Magic Accuracy and +25% Magic damage.
+-- +35% Magic damage with the Relic Armor augment
+-----------------------------------
+require("scripts/globals/settings")
+require("scripts/globals/status")
+-----------------------------------
+
+function onAbilityCheck(player, target, ability)
+    return 0, 0
+end
+
+function onUseAbility(player, target, ability)
+local power =  caster:getMerit(tpz.merit.CONVERGENCE) * 5 
+    player:addStatusEffect(tpz.effect.CONVERGENCE, 1, 0, 60)
+end
--- scripts/globals/effects/convergence.lua
+++ scripts/globals/effects/convergence.lua
@@ -1,14 +1,17 @@
 -----------------------------------
 --
---
+--tpz.effect.CONVERGENCE
 --
 -----------------------------------
-
 function onEffectGain(target, effect)
+   target:addMod(tpz.mod.MATTK, effect:getPower())
+   target:addMod(tpz.mod.MACC, effect:getPower()))
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
+    target:delMod(tpz.mod.MATTK, effect:getPower())
+    target:delMod(tpz.mod.MACC, effect:getPower())
 end
