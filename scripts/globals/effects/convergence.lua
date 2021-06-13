f838466fb010eb29acfbbc3826abb204f5a29eb4
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-06-12 22:06:24 -0400

    (debugging) convergence

--- scripts/globals/effects/convergence.lua
+++ scripts/globals/effects/convergence.lua
@@ -4,7 +4,7 @@
 --
 -----------------------------------
 function onEffectGain(target, effect)
-   target:addMod(tpz.mod.MATTK, effect:getPower())
+   target:addMod(tpz.mod.MATT, effect:getPower())
    target:addMod(tpz.mod.MACC, effect:getPower())
 end
 
@@ -12,6 +12,6 @@ function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-    target:delMod(tpz.mod.MATTK, effect:getPower())
+    target:delMod(tpz.mod.MATT, effect:getPower())
     target:delMod(tpz.mod.MACC, effect:getPower())
 end
