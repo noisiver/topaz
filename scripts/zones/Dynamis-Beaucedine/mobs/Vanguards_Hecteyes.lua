d650b8cd499efbbd5c1e5aa41883611674d87a0f
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-08-22 15:56:24 -0400

    (Dynamis) Add hecteyes pet dark SDT(5)

---
+++ scripts/zones/Dynamis-Beaucedine/mobs/Vanguards_Hecteyes.lua
@@ -0,0 +1,17 @@
+-----------------------------------
+-- Area: Dynamis - Beaucedine
+--  Mob: Vanguards Hecteyes
+-----------------------------------
+require("scripts/globals/mobs")
+require("scripts/globals/status")
+-----------------------------------
+function onMobInitialize(mob)
+    mob:setMobMod(tpz.mobMod.HP_STANDBACK, -1)
+end
+
+function onMobSpawn(mob)
+    mob:setMod(tpz.mod.SDT_DARK, 5)
+
+end
+function onMobDeath(mob, player, isKiller)
+end
