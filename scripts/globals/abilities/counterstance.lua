1a19a9382f71e740f694693fe9ab6acf6f23db95
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-08-25 14:26:36 -0400

    (Custom) Monk tank redesign
    Chakra - strong heal + status removal(from gear) 30s CD
    Pefect counter - 30s CD level 75
    Counter stance - +100% UDT(sentinel) 30s on 5m CD level 30
    Dodge - 100% guard rate and +25 evasion for 60s, 3m cd

--- scripts/globals/abilities/counterstance.lua
+++ scripts/globals/abilities/counterstance.lua
@@ -3,7 +3,7 @@
 -- Increases chance to counter but lowers defense.
 -- Obtained: Monk Level 45
 -- Recast Time: 5:00
--- Duration: 5:00
+-- Duration: 0:30
 -----------------------------------
 require("scripts/globals/settings")
 require("scripts/globals/status")
@@ -14,8 +14,9 @@ function onAbilityCheck(player, target, ability)
 end
 
 function onUseAbility(player, target, ability)
-    local power = 45 + player:getMod(tpz.mod.COUNTERSTANCE_EFFECT)
+    local power = 90 
 
     target:delStatusEffect(tpz.effect.COUNTERSTANCE) --if not found this will do nothing
-    target:addStatusEffect(tpz.effect.COUNTERSTANCE, power, 0, 300)
+   player:addStatusEffect(tpz.effect.SENTINEL, power, 3, 30)
+   -- target:addStatusEffect(tpz.effect.COUNTERSTANCE, power, 0, 300)
 end
