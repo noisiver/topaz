ad609a468165939db8c4722e5084a2958b435657
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-08-25 16:01:52 -0400

    (Custom) MNK ascetics fury buff

--- scripts/globals/weaponskills/ascetics_fury.lua
+++ scripts/globals/weaponskills/ascetics_fury.lua
@@ -20,13 +20,13 @@ require("scripts/globals/weaponskills")
 function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local params = {}
     params.numHits = 1
-    params.ftp100 = 1 params.ftp200 = 1 params.ftp300 = 1
-    params.str_wsc = 0.5 params.dex_wsc = 0.0 params.vit_wsc = 0.5 params.agi_wsc = 0.0 params.int_wsc = 0.0
+    params.ftp100 = 2.0 params.ftp200 = 2.0 params.ftp300 = 2.0
+    params.str_wsc = 0.75 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0
     params.mnd_wsc = 0.0 params.chr_wsc = 0.0
-    params.crit100 = 0.1 params.crit200 = 0.3 params.crit300 = 0.5
+    params.crit100 = 0.5 params.crit200 = 0.7 params.crit300 = 1.0
     params.canCrit = true
     params.acc100 = 0.0 params.acc200 = 0.0 params.acc300 = 0.0
-    params.atk100 = 1; params.atk200 = 1; params.atk300 = 1
+    params.atk100 = 2; params.atk200 = 2; params.atk300 = 2
 
     if USE_ADOULIN_WEAPON_SKILL_CHANGES then
         params.crit100 = 0.2 params.crit200 = 0.3 params.crit300 = 0.5
