65117a1e47de765a9ce8992711c2650e7d82dcd5
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-08-29 23:57:59 -0400

    saline coat decay

--- scripts/globals/effects/magic_def_boost.lua
+++ scripts/globals/effects/magic_def_boost.lua
@@ -11,26 +11,11 @@ function onEffectGain(target, effect)
 end
 
 function onEffectTick(target, effect)
-   local power = effect:getPower()
-   local decayby = 0
-   -- MDB reduction decays until 8 then stops
-   if (power > 8) then
-      if (power == 50) then
-         decayby = 13
-      elseif (power == 37) then
-         decayby = 10
-      elseif (power == 27) then
-         decayby = 7
-      elseif (power == 20) then
-         decayby = 5
-      elseif (power == 15) then
-         decayby = 4
-      elseif (power == 11) then
-         decayby = 3
-      end
-      effect:setPower(power-decayby)
-      target:delMod(tpz.mod.MDEF, decayby)
-   end
+effect:setPower(effect:getPower()-7)
+target:delMod(tpz.mod.MDEF, -7)
+	if effect:getPower() <= 8 then
+		effect:getPower() = 8
+	end
 end
 
 function onEffectLose(target, effect)
