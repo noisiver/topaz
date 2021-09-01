9c7089bcc7805542ed7af69d53671ac1099b627d
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-01 18:24:38 -0400

    (BLU) debugging..

--- scripts/globals/bluemagic.lua
+++ scripts/globals/bluemagic.lua
@@ -137,11 +137,11 @@ function BluePhysicalSpell(caster, target, spell, params, tp)
     if chainAffinity ~= nil then
 		if params.AttkTPModifier == true then
 			AttkTPModifier =  getAttkTPModifier(caster:getTP())
-			print(string.format("Attk TP Modifier: %u",AttkTPModifier))
+			GetPlayerByID(6):PrintToPlayer(string.format("Attack TP Modifier: %u",AttkTPModifier))
 		end
 		if params.CritTPModifier == true then
 			CritTPBonus = getTPModifier(caster:getTP()) * 45
-			print(string.format("Crit TP Modifier: %u",CritTPBonus))
+			GetPlayerByID(6):PrintToPlayer(string.format("Crit TP Modifier: %u",CritTPBonus))
 		end
 	end
 
@@ -463,7 +463,7 @@ function BlueGetHitRate(attacker, target, capHitRate)
     if chainAffinity ~= nil then
 		if params.AccTPModifier == true then
 			AccTPBonus =  getTPModifier(caster:getTP()) * 10
-			print(string.format("Acc TP Modifier: %u",AccTPBonus))
+			GetPlayerByID(6):PrintToPlayer(string.format("Acc TP Modifier: %u",AccTPBonus))
 		end
 	end
     local acc = attacker:getACC() + 35 + AccTPBonus 
