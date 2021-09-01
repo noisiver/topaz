0d501a4d6407b15d9bebeffc21c6a71f5db062b7
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-01 17:57:19 -0400

    (AF) RNG AF3 boss stats

--- scripts/zones/Xarcabard/mobs/Koenigstiger.lua
+++ scripts/zones/Xarcabard/mobs/Koenigstiger.lua
@@ -3,6 +3,15 @@
 --  Mob: Koenigstiger
 -- Involved in Quests: Unbridled Passion (RNG AF3)
 -----------------------------------
+require("scripts/globals/status")
+require("scripts/globals/mobs")
+----------------------------------
+function onMobSpawn(mob)
+    mob:addMod(tpz.mod.ATTP, 200)
+    mob:addMod(tpz.mod.DEFP, 35) 
+    mob:addMod(tpz.mod.ACC, 30) 
+    mob:addMod(tpz.mod.EVA, 30)
+end
 
 function onMobDeath(mob, player, isKiller)
     if player:getCharVar("unbridledPassion") == 4 then
