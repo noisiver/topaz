fc17c5831f82298a28ac5bc191c254ac2cdab587
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-01 21:29:37 -0400

    (BLU) remove debug

--- scripts/globals/bluemagic.lua
+++ scripts/globals/bluemagic.lua
@@ -191,7 +191,6 @@ function BluePhysicalSpell(caster, target, spell, params, tp)
 			local subtleblow = (caster:getMod(tpz.mod.SUBTLE_BLOW) / 100)
 			local TP =  (hitslanded  + 100) - hitslanded * (1 - subtleblow)
             target:addTP(TP)
-			GetPlayerByID(6):PrintToPlayer(string.format("TP Given %u",TP))
         end
 
         hitsdone = hitsdone + 1
