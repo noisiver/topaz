66e9ef8b7f5af57c3aac214f5a80d1588b5b22a9
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-01 21:09:48 -0400

    (Debugging) BLU TP feed

--- scripts/globals/bluemagic.lua
+++ scripts/globals/bluemagic.lua
@@ -190,7 +190,7 @@ function BluePhysicalSpell(caster, target, spell, params, tp)
 
             -- increment target's TP (100TP per hit landed)
 			local subtleblow = (caster:getMod(tpz.mod.SUBTLE_BLOW) / 100)
-			local TP =  (hitslanded * 100) * (1 - subtleblow)
+			local TP =  ((hitslanded / 2) * 100) * (1 - subtleblow)
             target:addTP(TP)
 			GetPlayerByID(6):PrintToPlayer(string.format("TP Given %u",TP))
         end
