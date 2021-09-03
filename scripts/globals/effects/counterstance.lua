19cd0167cde3e7c7c404441c540016ca52a6788f
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-02 21:31:50 -0400

    (Custom - MNK) Added +50 enmity to counterstance

--- scripts/globals/effects/counterstance.lua
+++ scripts/globals/effects/counterstance.lua
@@ -8,6 +8,7 @@ require("scripts/globals/status")
 function onEffectGain(target, effect)
     local power = effect:getPower()
     target:addMod(tpz.mod.COUNTER, effect:getPower())
+    target:addMod(tpz.mod.ENMITY, effect:getPower())
     target:addMod(tpz.mod.ATTP, -power)
     target:addMod(tpz.mod.RATTP, -power)
 end
@@ -18,6 +19,7 @@ end
 function onEffectLose(target, effect)
     local power = effect:getPower()
     target:delMod(tpz.mod.COUNTER, effect:getPower())
+    target:delMod(tpz.mod.ENMITY, effect:getPower())
     target:delMod(tpz.mod.ATTP, -power)
     target:delMod(tpz.mod.RATTP, -power)
 end
