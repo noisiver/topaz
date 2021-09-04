de19e9c2966495707680813234ebd3e45023d751
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-03 23:13:06 -0400

    debuggimg

--- scripts/globals/weaponskills.lua
+++ scripts/globals/weaponskills.lua
@@ -438,7 +438,7 @@ end
         fencerBonus = fencerBonus(attacker),
         bonusTP = wsParams.bonusTP or 0,
         bonusfTP = gorgetBeltFTP or 0,
-        bonusAcc = (gorgetBeltAcc or 0) + attacker:getMod(tpz.mod.WSACC) + 100,
+        bonusAcc = (gorgetBeltAcc or 0) + attacker:getMod(tpz.mod.WSACC),
         bonusWSmods = wsParams.bonusWSmods or 0
     }
 
