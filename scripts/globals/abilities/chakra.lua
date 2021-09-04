efe7f0cd804db51a77e1bb9f8308f448d1666bdd
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-04 00:18:56 -0400

    Update chakra.lua

--- scripts/globals/abilities/chakra.lua
+++ scripts/globals/abilities/chakra.lua
@@ -38,7 +38,7 @@ function onUseAbility(player, target, ability)
     end
     player:setHP(player:getHP() + recover)
 	ability:setMsg(tpz.msg.basic.JA_RECOVERS_HP)
-	ability:setMsg(tpz.msg.basic.JA_REMOVE_EFFECT_2)
+	ability:setMsg(tpz.msg.basic.JA_REMOVE_EFFECT)
 
     local merits = player:getMerit(tpz.merit.INVIGORATE)
     if merits > 0 then
