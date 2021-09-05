a06076ee059fe3eb7b193266679ad81afa356f97
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-05 16:23:07 -0400

    (MNK) Footwork tuning

--- scripts/globals/effects/footwork.lua
+++ scripts/globals/effects/footwork.lua
@@ -9,7 +9,7 @@ require("scripts/globals/status")
 function onEffectGain(target, effect)
     target:addMod(tpz.mod.ATTP, 10)
 	target:addMod(tpz.mod.MAIN_DMG_RATING, 50)
-	target:addMod(tpz.mod.HASTE_MAGIC, -3000)
+	target:addMod(tpz.mod.HASTE_MAGIC, -5000)
     target:addMod(tpz.mod.STORETP, 20)
     target:addMod(tpz.mod.KICK_DMG, effect:getPower())
 end
@@ -20,7 +20,7 @@ end
 function onEffectLose(target, effect)
     target:delMod(tpz.mod.ATTP, 10)
     target:delMod(tpz.mod.MAIN_DMG_RATING, 50)
-	target:delMod(tpz.mod.HASTE_MAGIC, -3000)
+	target:delMod(tpz.mod.HASTE_MAGIC, -5000)
     target:delMod(tpz.mod.STORETP, 20)
     target:delMod(tpz.mod.KICK_DMG, effect:getPower())
 end
