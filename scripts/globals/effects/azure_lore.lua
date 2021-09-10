173063eb8c8d8345c6b84573979b65efeb08d587
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-09 21:39:43 -0400

    (BLU) Azure Lore now gives +20 all stats, free spellcasting and +100 racc

--- scripts/globals/effects/azure_lore.lua
+++ scripts/globals/effects/azure_lore.lua
@@ -1,18 +1,36 @@
 -----------------------------------
 --
---
+-- tpz.effect.AZURE_LORE
 --
 -----------------------------------
 
 function onEffectGain(target, effect)
-    target:addMod(tpz.mod.ACC, 100) 
-    target:addMod(tpz.mod.MACC, 100)
+	target:addMod(tpz.mod.STR, 20)
+	target:addMod(tpz.mod.DEX, 20)
+	target:addMod(tpz.mod.VIT, 20)
+	target:addMod(tpz.mod.AGI, 20)
+	target:addMod(tpz.mod.INT, 20)
+	target:addMod(tpz.mod.MND, 20)
+	target:addMod(tpz.mod.CHR, 20)
+	target:addMod(tpz.mod.ACC, 100) 
+	target:addMod(tpz.mod.RACC, 100) 
+	target:addMod(tpz.mod.MACC, 100)
+	target:addMod(tpz.mod.NO_SPELL_MP_DEPLETION, 100)
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
+	target:delMod(tpz.mod.STR, 20)
+	target:delMod(tpz.mod.DEX, 20)
+	target:delMod(tpz.mod.VIT, 20)
+	target:delMod(tpz.mod.AGI, 20)
+	target:delMod(tpz.mod.INT, 20)
+	target:delMod(tpz.mod.MND, 20)
+	target:delMod(tpz.mod.CHR, 20)
     target:delMod(tpz.mod.ACC, 100) 
+    target:delMod(tpz.mod.RACC, 100) 
     target:delMod(tpz.mod.MACC, 100)
+	target:delMod(tpz.mod.NO_SPELL_MP_DEPLETION, 100)
 end
