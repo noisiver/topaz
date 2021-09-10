a39db3b4adcbca0c654fc684e8620537f383e9d3
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-10 19:30:45 -0400

    (TOAU) Part3: Mob levels

--- scripts/zones/Alzadaal_Undersea_Ruins/mobs/Nepionic_Soulflayer.lua
+++ scripts/zones/Alzadaal_Undersea_Ruins/mobs/Nepionic_Soulflayer.lua
@@ -7,6 +7,12 @@ mixins = {require("scripts/mixins/job_special")}
 
 --TODO: Immortal Shield - Magic Shield (Only prevents direct damage from spells)
 --TODO: Immortal Mind - Magic Atk Boost
+function onMobSpawn(mob)
+     mob:addMod(tpz.mod.DEFP, 20) 
+     mob:addMod(tpz.mod.ATTP, 10)
+     mob:addMod(tpz.mod.ACC, 30) 
+     mob:addMod(tpz.mod.EVA, 30)
+end
 
 function onMobInitialize(mob)
     mob:setMobMod(tpz.mobMod.IDLE_DESPAWN, 180)
