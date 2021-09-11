134e0efaacdbb6743d5c375bf094692893267bde
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-11 19:08:38 -0400

    (TP Moves)

--- scripts/globals/mobskills/incinerate.lua
+++ scripts/globals/mobskills/incinerate.lua
@@ -12,8 +12,8 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local dmgmod = MobBreathMove(mob, target, 0.25, 0.75, tpz.magic.ele.FIRE)
-    local dmg = MobFinalAdjustments(dmgmod, mob, skill, target, tpz.attackType.BREATH, tpz.damageType.FIRE, MOBPARAM_IGNORE_SHADOWS)
+	local dmgmod = MobBreathMove(mob, target, 0.10, 1, tpz.magic.ele.FIRE, 800)
+	local dmg = MobFinalAdjustments(dmgmod, mob, skill, target, tpz.attackType.BREATH, tpz.damageType.FIRE, MOBPARAM_IGNORE_SHADOWS)
 
     target:takeDamage(dmg, mob, tpz.attackType.BREATH, tpz.damageType.FIRE)
     return dmg
