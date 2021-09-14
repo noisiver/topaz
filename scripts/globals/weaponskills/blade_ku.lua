117254a3dabc49050848481332620254ab1ee12d
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-14 00:05:06 -0400

    (WS) Katana WS balancing

--- scripts/globals/weaponskills/blade_ku.lua
+++ scripts/globals/weaponskills/blade_ku.lua
@@ -25,8 +25,8 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
 
     local params = {}
     params.numHits = 5
-    params.ftp100 = 0.7 params.ftp200 = 0.7 params.ftp300 = 0.7
-    params.str_wsc = 0.0 params.dex_wsc = 0.4 params.vit_wsc = 0.0 params.agi_wsc = 0.4 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
+    params.ftp100 = 1 params.ftp200 = 1 params.ftp300 = 1
+    params.str_wsc = 0.0 params.dex_wsc = 0.1 params.vit_wsc = 0.0 params.agi_wsc = 0.1 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
     params.acc100 = 1 params.acc200= 1.05 params.acc300= 1.10
