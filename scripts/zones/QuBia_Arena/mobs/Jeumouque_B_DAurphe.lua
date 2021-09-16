e29c5a052445c937b74c4cc7414a5cc7059de416
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-16 17:47:38 -0400

    (BCNM) Brothers D'Aurphe

--- scripts/zones/QuBia_Arena/mobs/Jeumouque_B_DAurphe.lua
+++ scripts/zones/QuBia_Arena/mobs/Jeumouque_B_DAurphe.lua
@@ -6,6 +6,7 @@
 mixins = {require("scripts/mixins/job_special")}
 -----------------------------------
 function onMobSpawn(mob)
+     mob:setMod(tpz.mod.LULLABYRESTRAIT, 100)
      mob:setMod(tpz.mod.REFRESH, 400)
      mob:setMobMod(tpz.mobMod.NO_DROPS, 0)
 end
