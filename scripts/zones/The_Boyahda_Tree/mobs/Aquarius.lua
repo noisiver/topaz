a04061ae0c15bc352883f75011be533228143148
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-16 15:03:47 -0400

    Update Aquarius.lua

--- scripts/zones/The_Boyahda_Tree/mobs/Aquarius.lua
+++ scripts/zones/The_Boyahda_Tree/mobs/Aquarius.lua
@@ -10,6 +10,16 @@ function onMobInitialize(mob)
     mob:setMobMod(tpz.mobMod.ADD_EFFECT, 1)
 end
 
+function onMobSpawn(mob)
+    mob:setMod(tpz.mod.SLEEPRESTRAIT, 100)
+    mob:setMod(tpz.mod.LULLABYRESTRAIT, 100)
+    mob:setMod(tpz.mod.BINDRESTRAIT, 100)
+    mob:addMod(tpz.mod.ATTP, 100)
+    mob:addMod(tpz.mod.DEFP, 100)
+        mob:setMod(tpz.mod.HASTE_MAGIC, mob:getMod(tpz.mod.HASTE_MAGIC) + 5000)
+end
+
+
 function onAdditionalEffect(mob, target, damage)
     return tpz.mob.onAddEffect(mob, target, damage, tpz.mob.ae.ENWATER)
 end
