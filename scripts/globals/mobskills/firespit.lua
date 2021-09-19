3dbc99892e800bbc4c356ad771ef2ffbdb444e4e
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-18 23:46:33 -0400

    (ToAU) Fix Firespit

--- scripts/globals/mobskills/firespit.lua
+++ scripts/globals/mobskills/firespit.lua
@@ -21,11 +21,7 @@ function onMobSkillCheck(target, mob, skill)
         return 1
     end
   end
-	if (mob:getFamily() == 176 or mob:getFamily() == 177 or or mob:getFamily() == 285) and mob:getPool() = 2526 or mob:getPool() = 2533 or mob:getPool() = 2534
-	or mob:getPool() = 2516 or mob:getPool() = 2507 or mob:getPool() = 2528 then
-		return 1
-	end
-	return 0
+	return 1
 end
 
 function onMobWeaponSkill(target, mob, skill)
