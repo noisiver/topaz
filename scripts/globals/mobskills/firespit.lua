9a7644eab596e4a4e889290d5f9eb4e3cb83310b
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-18 23:57:38 -0400

    debugging

--- scripts/globals/mobskills/firespit.lua
+++ scripts/globals/mobskills/firespit.lua
@@ -12,14 +12,14 @@ require("scripts/globals/monstertpmoves")
 ---------------------------------------------
 
 function onMobSkillCheck(target, mob, skill)
-	if mob:getFamily() == 176 then
-		local mobSkin = mob:getModelId()
-		if (mobSkin == 1618  then
-			return 0
-		else
-			return 1
-		end
-	end
+    if mob:getMainJob() == tpz.job.BLU or mob:getMainJob() == tpz.job.BST or mob:getMainJob() == tpz.job.DRG  or mob:getMainJob() == tpz.job.NIN  or mob:getMainJob() == tpz.job.THF  then
+        -- If animationSub is 1, the mob has already lost his weapeon and cant do this TP attack.
+        if mob:AnimationSub() == 0 then
+            return 0
+        end
+    end
+
+    return 1
 end
 
 function onMobWeaponSkill(target, mob, skill)
