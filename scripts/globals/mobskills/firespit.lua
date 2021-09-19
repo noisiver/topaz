c36a62e93e8dc350cae465a983ec363f55998649
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-18 23:48:53 -0400

    firespit debugging

--- scripts/globals/mobskills/firespit.lua
+++ scripts/globals/mobskills/firespit.lua
@@ -12,15 +12,14 @@ require("scripts/globals/monstertpmoves")
 ---------------------------------------------
 
 function onMobSkillCheck(target, mob, skill)
-  if(mob:getFamily() == 91) then
-    local mobSkin = mob:getModelId()
 
-    if (mobSkin == 1639) then
+    local mobSkin = mob:getModelId()
+	if (mobSkin == 1639) then
         return 0
     else
         return 1
     end
-  end
+
 	return 1
 end
 
