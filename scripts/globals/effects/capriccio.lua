6ddf04f9373968ef613232905a8899409a4d00da
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-19 15:41:04 -0400

    (Spells) Bar-Status and Bard Status Resist songs changed to resist trait instead of mevasion

--- scripts/globals/effects/capriccio.lua
+++ scripts/globals/effects/capriccio.lua
@@ -7,12 +7,12 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onEffectGain(target, effect)
-    target:addMod(tpz.mod.PETRIFYRES, effect:getPower())
+    target:addMod(tpz.mod.PETRIFYRESTRAIT, effect:getPower())
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-    target:delMod(tpz.mod.PETRIFYRES, effect:getPower())
+    target:delMod(tpz.mod.PETRIFYRESTRAIT, effect:getPower())
 end
--- scripts/globals/effects/round.lua
+++ scripts/globals/effects/round.lua
@@ -7,12 +7,12 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onEffectGain(target, effect)
-    target:addMod(tpz.mod.CURSERES, effect:getPower())
+    target:addMod(tpz.mod.CURSERESTRAIT, effect:getPower())
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-    target:delMod(tpz.mod.CURSERES, effect:getPower())
+    target:delMod(tpz.mod.CURSERESTRAIT, effect:getPower())
 end
