735356fde0d1fc97316737c93bb551f4f5ed06e7
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-21 01:12:07 -0400

    (WAR) Fixes to parry skill coding

--- scripts/globals/effects/retaliation.lua
+++ scripts/globals/effects/retaliation.lua
@@ -9,7 +9,7 @@ require("scripts/globals/status")
 
 function onEffectGain(target, effect)
     target:addMod(tpz.mod.MOVE, -8)
-    target:addMod(tpz.skill.PARRY, 50)
+    target:addMod(tpz.mod.PARRY, 50)
 end
 
 function onEffectTick(target, effect)
@@ -17,5 +17,5 @@ end
 
 function onEffectLose(target, effect)
     target:delMod(tpz.mod.MOVE, -8)
-    target:delMod(tpz.skill.PARRY, 50)
+    target:delMod(tpz.mod.PARRY, 50)
 end
