4caae10d370c3cea5a9b189c025c6b7c551b3d0b
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-21 22:40:51 -0400

    debugging

--- scripts/globals/utils.lua
+++ scripts/globals/utils.lua
@@ -97,7 +97,7 @@ function utils.takeShadows(target, dmg, shadowbehav)
                         effect:setIcon(tpz.effect.COPY_IMAGE_3)
                     end
                 end
-					return 0
+					return  shadowbehav
             end
             -- remove icon
             if (shadowsLeft <= 0) then
