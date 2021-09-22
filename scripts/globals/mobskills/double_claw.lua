e04a1ef9864a9c9ed436c08bd48db3314beb7613
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-22 13:44:11 -0400

    (TP Moves) Tune Diremite TP moves

--- scripts/globals/mobskills/double_claw.lua
+++ scripts/globals/mobskills/double_claw.lua
@@ -14,9 +14,9 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local numhits = 1
+    local numhits = 2
     local accmod = 1
-    local dmgmod = 4
+    local dmgmod = 1
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, info.hitslanded)
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
