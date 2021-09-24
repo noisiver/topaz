f79df9c33f23ba8a6c2c3d789718319a5d70111f
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-23 22:15:39 -0400

    (ToAU) Cluster fixes

--- scripts/globals/mobskills/self-destruct_2.lua
+++ scripts/globals/mobskills/self-destruct_2.lua
@@ -21,7 +21,7 @@ function onMobWeaponSkill(target, mob, skill)
 		local amount = 750 * skill:getTotalTargets()
 		local dmg = MobFinalAdjustments(amount, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.FIRE, MOBPARAM_WIPE_SHADOWS)
 		target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.FIRE)
-		if HP < 1300 then
+		if (mob:getPool() == 3333)  and HP < 1300 then
 		   dmg = 0
 		end
 			return dmg
---
+++ scripts/zones/Mount_Zhayolm/mobs/Sweeping_Cluster.lua
@@ -0,0 +1,11 @@
+-----------------------------------
+-- Area: Mount Zhayolm
+--  Mob: Sweeping Cluster
+-----------------------------------
+-----------------------------------
+function onMobSpawn(mob)
+	mob:AnimationSub(0)
+end
+
+function onMobDeath(mob)
+end
