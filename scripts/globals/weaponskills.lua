922f40269265b086a1385db1b75e9b7d95fe27b2
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-24 15:17:52 -0400

    Revert "(WS) Add  "Acc Varies with TP" properly to ranged WS formula"
    This reverts commit 062a6d2462fb6358b06ab9e63b9d0a3ebb49b653.

--- scripts/globals/weaponskills.lua
+++ scripts/globals/weaponskills.lua
@@ -75,8 +75,6 @@ function calculateRawWSDmg(attacker, target, wsID, tp, action, wsParams, calcPar
     -- Recalculate accuracy if it varies with TP, applied to all hits
     if wsParams.acc100 ~= 0 then
         calcParams.hitRate = accVariesWithTP(calcParams.hitRate, calcParams.accStat, tp, wsParams.acc100, wsParams.acc200, wsParams.acc300)
-		calcParams.slugwinder = accVariesWithTP(calcParams.hitRate, calcParams.accStat, tp, wsParams.acc100, wsParams.acc200, wsParams.acc300)
-		calcParams.multihitRate = accVariesWithTP(calcParams.hitRate, calcParams.accStat, tp, wsParams.acc100, wsParams.acc200, wsParams.acc300)
     end
 
     -- Calculate alpha, WSC, and our modifiers for our base per-hit damage
