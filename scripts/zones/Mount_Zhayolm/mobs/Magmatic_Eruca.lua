29d9359ca99c19b6137be07766cffb4a371e1707
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-24 15:26:51 -0400

    (ToAU) Remove alex off xp mob  erucas

--- scripts/zones/Mount_Zhayolm/mobs/Magmatic_Eruca.lua
+++ scripts/zones/Mount_Zhayolm/mobs/Magmatic_Eruca.lua
@@ -12,9 +12,6 @@ function onMobSpawn(mob)
 end
 
 function onMobDeath(mob, player, isKiller)
-	if isKiller and math.random(1,100) <= 24 then 
-		player:addTreasure(2488, mob)--Alexandrite 
-	end
 end
 
 function onMobDespawn(mob)
