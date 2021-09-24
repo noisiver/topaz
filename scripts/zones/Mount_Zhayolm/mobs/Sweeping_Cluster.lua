6e62ac74a2a2955af4318938817d98be9401560b
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-23 22:26:23 -0400

    (ToAU) More cluster fixes

--- scripts/zones/Mount_Zhayolm/mobs/Sweeping_Cluster.lua
+++ scripts/zones/Mount_Zhayolm/mobs/Sweeping_Cluster.lua
@@ -7,17 +7,11 @@ function onMobSpawn(mob)
 	mob:AnimationSub(0)
 end
 
-function onMobFight(mob, target)
-	if mob:AnimationSub() > 2 then
-		mob:AnimationSub(0)
-		mob:AnimationSub(2)
-	end
-end
-
 function onMobWeaponSkill(target, mob, skill)
     if skill:getID() == 571 then --3 Clusters Self-Destruct
 		mob:AnimationSub(1)
 	elseif skill:getID() == 572 then --2 Clusters Self-Destruct
+		mob:AnimationSub(0)
 		mob:AnimationSub(2)
 	end
 end
