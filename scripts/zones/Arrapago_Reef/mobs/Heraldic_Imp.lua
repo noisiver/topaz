f9fe284b0b3f8b8ef5270dfddbd105547b7f33b9
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-23 22:04:49 -0400

    (ToAU) Fix Imp AI

--- scripts/zones/Arrapago_Reef/mobs/Heraldic_Imp.lua
+++ scripts/zones/Arrapago_Reef/mobs/Heraldic_Imp.lua
@@ -4,6 +4,10 @@
 -----------------------------------
 mixins = {require("scripts/mixins/families/imp")}
 -----------------------------------
+function onMobSpawn(mob)
+	mob:setMobMod(tpz.mobMod.MAGIC_COOL, 45)
+    mob:setMobMod(tpz.mobMod.HP_STANDBACK, -1)
+end
 
 function onMobDeath(mob, player, isKiller)
 	if mob:AnimationSub() >= 1 then
