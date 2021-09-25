12ae5e1c871aee7eb5f9ca1e8ac13a5380c47203
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-25 01:40:03 -0400

    formatting

--- scripts/globals/mobskills/Turbulence.lua
+++ scripts/globals/mobskills/Turbulence.lua
@@ -19,9 +19,9 @@ function onMobWeaponSkill(target, mob, skill)
 	
 	if (mob:getFamily() == 72) then 
 		dmgmod = 2
-	
 	end
-    local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.WIND, dmgmod, TP_MAB_BONUS, 1)
+    
+	local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.WIND, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.WIND, MOBPARAM_WIPE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.WIND)
    
