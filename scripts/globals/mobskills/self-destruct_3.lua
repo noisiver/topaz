85d54ed60ff5380bcba24baa1208c9821ebc3287
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-26 16:55:29 -0400

    (ToAU) Fix clusters self destruct

--- scripts/globals/mobskills/self-destruct_3.lua
+++ scripts/globals/mobskills/self-destruct_3.lua
@@ -16,21 +16,21 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
+local dmgmod = 2       -- changed from 1
+	BOMB_TOSS_HPP = skill:getMobHPP() / 100
+
+local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*12*BOMB_TOSS_HPP, tpz.magic.ele.FIRE, dmgmod, TP_MAB_BONUS, 1)
+local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.FIRE, MOBPARAM_IGNORE_SHADOWS)
+	if (mob:getPool() == 3333) then-- Razon 
     local HP = mob:getHP()
     local amount = 650 * skill:getTotalTargets()
     local dmg = MobFinalAdjustments(amount, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.FIRE, MOBPARAM_WIPE_SHADOWS)
-    target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.FIRE)
-    if (mob:getPool() == 3333)  and  HP < 2625 then
-       dmg = 0
-			return dmg
-    else
-		local dmgmod = 2       -- changed from 1
-			BOMB_TOSS_HPP = skill:getMobHPP() / 100
-
-		local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*12*BOMB_TOSS_HPP, tpz.magic.ele.FIRE, dmgmod, TP_MAB_BONUS, 1)
-		local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.FIRE, MOBPARAM_IGNORE_SHADOWS)
-		mob:AnimationSub(1)
-		target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.FIRE)
-			return dmg
+		if mob:getPool() == 3333  and  HP < 2625 then
+		   dmg = 0
+		end
 	end
+mob:AnimationSub(1)
+target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.FIRE)
+
+		return dmg
 end
