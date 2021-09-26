b7309fc279372eefaa5e4c78d47e31980a600d1b
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-26 13:38:55 -0400

    Proper formula racc penalty on slugwinder

--- scripts/globals/weaponskills.lua
+++ scripts/globals/weaponskills.lua
@@ -800,7 +800,7 @@ function getRangedHitRate(attacker, target, capHitRate, bonus)
     else 
         hitdiff = hitrate + math.floor((((acc - eva)) / 2) - 2 * (target:getMainLvl() - attacker:getMainLvl()))
         firsthit = hitrate + math.floor((((firstacc - eva)) / 2) - 2 * (target:getMainLvl() - attacker:getMainLvl()))
-		slugwinder = hitrate + math.floor((((acc - eva)) / 2) - 2 * (target:getMainLvl() - attacker:getMainLvl()) * 0.85)
+		slugwinder = hitrate + math.floor(((acc - eva) / 2) - (2 * (target:getMainLvl() - attacker:getMainLvl()))) - 15
     end
 
     hitrate = hitdiff / 100
