a00cc5212872469e05569c9ba3d3ce8e4588b7e3
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-27 21:33:15 -0400

    (ToAU) Mob TP tuning

--- scripts/globals/mobskills/overthrow.lua
+++ scripts/globals/mobskills/overthrow.lua
@@ -25,7 +25,7 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local numhits = 1
     local accmod = 1
-    local dmgmod = (1 - (mob:getHP() / mob:getMaxHP())) * 3.5 
+    local dmgmod = (1 - (mob:getHP() / mob:getMaxHP())) * 3
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.H2H, MOBPARAM_IGNORE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.H2H)
