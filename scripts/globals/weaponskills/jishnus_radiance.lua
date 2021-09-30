ca9b3760cfc76ed4befe59c42b3f97d98483c605
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-09-30 16:01:11 -0400

    (WS) Crit WS Rebalancing
    Changed all to 15% / 30% / 50% crit chance at 1k/2k/3k TP

--- scripts/globals/weaponskills/jishnus_radiance.lua
+++ scripts/globals/weaponskills/jishnus_radiance.lua
@@ -24,7 +24,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     params.str_wsc = 0.0 params.dex_wsc = 0.60 params.vit_wsc = 0.0
     params.agi_wsc = 0.00 params.int_wsc = 0.0 params.mnd_wsc = 0.0
     params.chr_wsc = 0.0
-    params.crit100 = 0.15 params.crit200 = 0.2 params.crit300 = 0.25
+    params.crit100 = 0.15 params.crit200 = 0.3 params.crit300 = 0.5
     params.canCrit = true
     params.acc100 = 0.0 params.acc200 = 0.0 params.acc300 = 0.0
     params.atk100 = 1; params.atk200 = 1; params.atk300 = 1
