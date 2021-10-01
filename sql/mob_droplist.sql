2e50c0d6f32215670a6258d5d41f689a00373c68
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-01 14:52:31 -0400

    (ToAU) Treant / Diremite / Spider drop fixes

--- sql/mob_droplist.sql
+++ sql/mob_droplist.sql
@@ -212,7 +212,7 @@ INSERT INTO `mob_droplist` VALUES (33,0,0,1000,535,1000);	-- Ahtu engraved key
 INSERT INTO `mob_droplist` VALUES (33,0,0,1000,918,330);
 INSERT INTO `mob_droplist` VALUES (33,0,0,1000,923,240);
 INSERT INTO `mob_droplist` VALUES (34,0,0,1000,838,10); -- (Aht Urghan Attercop) Spider's Web
-INSERT INTO `mob_droplist` VALUES (34,0,0,1000,2462,100);
+INSERT INTO `mob_droplist` VALUES (34,0,0,1000,2462,100); -- Frayed Arrow
 INSERT INTO `mob_droplist` VALUES (35,0,0,1000,1122,240); -- (Aiatar) Wyvern Skin 
 INSERT INTO `mob_droplist` VALUES (35,0,0,1000,1691,240); -- Giant scale
 INSERT INTO `mob_droplist` VALUES (35,0,0,1000,15367,240); -- Falconer's Hose
@@ -1281,10 +1281,10 @@ INSERT INTO `mob_droplist` VALUES (203,0,0,1000,881,10);
 INSERT INTO `mob_droplist` VALUES (203,0,0,1000,936,110);
 INSERT INTO `mob_droplist` VALUES (203,0,0,1000,4400,100);
 INSERT INTO `mob_droplist` VALUES (203,0,0,1000,4400,50);
-INSERT INTO `mob_droplist` VALUES (204,2,0,1000,924,0);
-INSERT INTO `mob_droplist` VALUES (204,0,0,1000,1626,50);
-INSERT INTO `mob_droplist` VALUES (204,0,0,1000,1700,80);
-INSERT INTO `mob_droplist` VALUES (204,0,0,1000,2463,400);
+INSERT INTO `mob_droplist` VALUES (204,2,0,1000,924,0); -- (Aydeewa Diremite) Vial Of Fiend Blood (Steal)
+INSERT INTO `mob_droplist` VALUES (204,0,0,1000,1626,50); -- Bottle of avatar blood
+INSERT INTO `mob_droplist` VALUES (204,0,0,1000,1700,100); -- Spool of bloodthread
+INSERT INTO `mob_droplist` VALUES (204,0,0,1000,2463,100); -- Tuft of colorful hair
 INSERT INTO `mob_droplist` VALUES (205,0,0,1000,823,50);
 INSERT INTO `mob_droplist` VALUES (205,0,0,1000,915,100);
 INSERT INTO `mob_droplist` VALUES (205,0,0,1000,931,100);
@@ -4023,13 +4023,19 @@ INSERT INTO `mob_droplist` VALUES (752,4,0,1000,4552,0);
 INSERT INTO `mob_droplist` VALUES (753,2,0,1000,749,0);
 INSERT INTO `mob_droplist` VALUES (753,0,0,1000,1034,50);
 INSERT INTO `mob_droplist` VALUES (754,2,0,1000,749,0);
-INSERT INTO `mob_droplist` VALUES (755,0,0,1000,700,10);
-INSERT INTO `mob_droplist` VALUES (755,0,0,1000,701,10);
-INSERT INTO `mob_droplist` VALUES (755,0,0,1000,702,10);
-INSERT INTO `mob_droplist` VALUES (755,0,0,1000,703,10);
-INSERT INTO `mob_droplist` VALUES (755,0,0,1000,918,50);
-INSERT INTO `mob_droplist` VALUES (755,0,0,1000,923,320);
-INSERT INTO `mob_droplist` VALUES (755,0,0,1000,4448,50);
+INSERT INTO `mob_droplist` VALUES (755,0,0,1000,700,50); -- (ToAU Treant) Mahogany log
+INSERT INTO `mob_droplist` VALUES (755,0,0,1000,701,50); -- Rosewood log
+INSERT INTO `mob_droplist` VALUES (755,0,0,1000,702,50); -- Ebony Log
+INSERT INTO `mob_droplist` VALUES (755,0,0,1000,703,50); -- Petrified Log
+INSERT INTO `mob_droplist` VALUES (755,0,0,1000,918,240); -- Mistletoe
+INSERT INTO `mob_droplist` VALUES (755,0,0,1000,918,150); -- Mistletoe
+INSERT INTO `mob_droplist` VALUES (755,0,0,1000,918,100); -- Mistletoe
+INSERT INTO `mob_droplist` VALUES (755,0,0,1000,923,1000); -- Dryad Root
+INSERT INTO `mob_droplist` VALUES (755,0,0,1000,923,240); -- Dryad Root
+INSERT INTO `mob_droplist` VALUES (755,0,0,1000,923,150); -- Dryad Root
+INSERT INTO `mob_droplist` VALUES (755,0,0,1000,923,100); -- Dryad Root
+INSERT INTO `mob_droplist` VALUES (755,0,0,1000,4448,240); -- Puffball
+INSERT INTO `mob_droplist` VALUES (755,0,0,1000,4448,150); -- Puffball
 INSERT INTO `mob_droplist` VALUES (756,2,0,1000,4373,0);
 INSERT INTO `mob_droplist` VALUES (756,0,0,1000,5680,40);
 INSERT INTO `mob_droplist` VALUES (757,0,0,1000,1614,1000); -- (Elel) Corse bracelet
