e10b1cf6c26c7c4956b400955948b341001e78ea
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-01 16:51:31 -0400

    (ToAU) TP Move fixes

--- scripts/globals/mobskills/granite_hide.lua
+++ scripts/globals/mobskills/granite_hide.lua
@@ -1,13 +1,15 @@
 ---------------------------------------------
--- Demoralizing Roar
+-- Granite Hide
 --
--- Description: Weakens attack of enemies within range.
--- Type: Magical (Water)
--- 50% 
+-- Description: Immunity to physical attacks in front.
+-- Type: Enhancing
+--
+-- Range: Self
 ---------------------------------------------
 require("scripts/globals/monstertpmoves")
 require("scripts/globals/settings")
 require("scripts/globals/status")
+require("scripts/globals/msg")
 ---------------------------------------------
 
 function onMobSkillCheck(target, mob, skill)
@@ -15,7 +17,10 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local typeEffect = tpz.effect.ATTACK_DOWN
-    skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, 50, 0, 180))
-    return typeEffect
+
+    -- addEx to pervent dispel
+    mob:addStatusEffectEx(tpz.effect.PHYSICAL_SHIELD, 0, 1, 0, 45)
+    skill:setMsg(tpz.msg.basic.SKILL_GAIN_EFFECT)
+
+    return tpz.effect.PHYSICAL_SHIELD
 end
