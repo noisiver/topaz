47add90b71895a9f93f32a7ee21c933356e35fb2
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-03 01:13:36 -0400

    (Mobs) BLU AF3 fight and Empyreal Arrow WSNM

--- scripts/zones/Cape_Teriggan/mobs/Stolas.lua
+++ scripts/zones/Cape_Teriggan/mobs/Stolas.lua
@@ -7,6 +7,19 @@
 -----------------------------------
 require("scripts/globals/wsquest")
 -----------------------------------
+function onMobSpawn(mob)
+    mob:addMod(tpz.mod.DEFP, 100)
+    mob:addMod(tpz.mod.MDEF, 100) 
+    mob:addMod(tpz.mod.ATTP, 50)
+    mob:addMod(tpz.mod.ACC, 50) 
+    mob:addMod(tpz.mod.EVA, 30)
+    mob:setMod(tpz.mod.REFRESH, 300)
+    mob:setMod(tpz.mod.DOUBLE_ATTACK, 15)
+    mob:setMod(tpz.mod.TRIPLE_ATTACK, 10)
+	mob:setMobMod(tpz.mobMod.NO_DROPS, 0)
+	mob:setMobMod(tpz.mobMod.GIL_MAX, -1)
+end
+
 
 function onMobInitialize(mob)
     mob:setMobMod(tpz.mobMod.EXP_BONUS, -100)
