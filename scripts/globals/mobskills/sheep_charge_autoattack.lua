e65c3da60dc40d03c5965953639f95d7e2ba0c77
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-04 14:40:35 -0400

    (Mobs) WS as autoattack fixes

--- scripts/globals/mobskills/sheep_charge_autoattack.lua
+++ scripts/globals/mobskills/sheep_charge_autoattack.lua
@@ -1,5 +1,5 @@
 ---------------------------------------------------
--- Sheep Charge
+-- Sheep Charge Autoattack
 -- Deals damage to a single target. Additional effect: Knockback
 ---------------------------------------------------
 
@@ -16,9 +16,15 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local numhits = 1
     local accmod = 1
-    local dmgmod = 1.5
+    local dmgmod = 1
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT, info.hitslanded)
+	
+	 if (dmg > 0) then
+       target:addTP(20)
+       mob:addTP(750)
+    end
+	
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT)
 	if ((skill:getMsg() ~= tpz.msg.basic.SHADOW_ABSORB) and (dmg > 0)) then   target:tryInterruptSpell(mob, info.hitslanded) end
     return dmg
