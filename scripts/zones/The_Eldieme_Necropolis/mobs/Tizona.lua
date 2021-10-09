b9109f2a5f8008db9edae8616e319d5a2f1f1348
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-09 14:57:56 -0400

    Revert "debugging"
    This reverts commit 17a19efd638d15912cc07a689ea379463015f0e9.

--- scripts/zones/The_Eldieme_Necropolis/mobs/Tizona.lua
+++ scripts/zones/The_Eldieme_Necropolis/mobs/Tizona.lua
@@ -1,7 +1,7 @@
 ------------------------------
--- Area: Outer Horutoto Ruins
---   Lilisette
---   !additem 476 
+-- Area: The Eldieme Necropolis
+--   Raubahn
+--  !additem 475 
 ------------------------------
 require("scripts/globals/hunts")
 require("scripts/globals/titles")
@@ -10,45 +10,55 @@ require("scripts/globals/status")
 ------------------------------
 
 function onMobSpawn(mob)
-    mob:addMod(tpz.mod.DEFP, 25) 
-    mob:addMod(tpz.mod.ATTP, 25)
+    mob:addMod(tpz.mod.ATTP, 10)
+    mob:addMod(tpz.mod.DEFP, 10) 
+    mob:setMod(tpz.mod.REFRESH, 400)
+	mob:setMod(tpz.mod.UFASTCAST, 50)
+	mob:setModelId(2234) -- Raubahn
 	mob:setMobMod(tpz.mobMod.EXP_BONUS, -100)
 	mob:setMobMod(tpz.mobMod.GIL_MAX, -1)
+    mob:setUnkillable(true)
 end
 
 function onMobFight(mob, target)
-    local battletime = mob:getBattleTime()
-    local Dance = mob:getLocalVar("Dance")
-    local DanceChangeTime = mob:getLocalVar("DanceChangeTime")
-
-    if Dance == 0 then
-        printf("Setting Dance");
-        mob:setLocalVar("DanceChangeTime", 3)
-        mob:setLocalVar("Dance", math.random(1, 2))
-    elseif battletime >= DanceChangeTime and Dance == 1 then
-        printf("Thorned Dance");
-        target:PrintToPlayer("Now I'm really hyped!",0,"Lilisette")
-        mob:useMobAbility(3308) -- Thorned Dance
-		mob:useMobAbility(25) -- evisceration
-		mob:useMobAbility(2313) -- Reverse Flourish
-		mob:useMobAbility(29)  -- pyrrhic_kleos
-        mob:setMobMod(tpz.mobMod.SKILL_LIST, 6038)
-        mob:setLocalVar("DanceChangeTime", battletime + 60)
-        mob:setLocalVar("Dance", math.random(1, 2))
-    end
-
-    if battletime >= DanceChangeTime and Dance == 2 then
-        printf("Sensual Dance");
-        target:PrintToPlayer("The funs just getting started!",0,"Lilisette")
-        mob:useMobAbility(3309) -- Sensual Dance
-        mob:setMobMod(tpz.mobMod.SKILL_LIST, 6039)
-        mob:setLocalVar("DanceChangeTime", battletime + 60)
-        mob:setLocalVar("Dance", math.random(1, 2))
-    end
+    local Phase = mob:getLocalVar("Phase")
+	local HP = mob:getHPP()
+    if HP < 25 and Phase == 0 then
+		printf("Phase 1");
+		mob:addMod(tpz.mod.ATTP, 20)
+		mob:addMod(tpz.mod.DEFP, 20) 
+		mob:useMobAbility(689) -- Benediction
+        mob:setModelId(2234) -- Yellow Flan
+        mob:setMobMod(tpz.mobMod.SKILL_LIST, 112)
+        target:PrintToPlayer("This power...I cannot contain it much longer...",0,"Raubahn")
+		Phase = 1
+	end
+    if HP < 25 and Phase == 1 then
+		printf("Phase 2");
+		mob:addMod(tpz.mod.ATTP, 20)
+		mob:addMod(tpz.mod.DEFP, 20) 
+		mob:useMobAbility(689) -- Benediction
+		mob:setModelId(1774) -- Qutrub
+        mob:setMobMod(tpz.mobMod.SKILL_LIST, 203)
+		target:PrintToPlayer("It's...too....powerful.....",0,"Raubahn")
+		Phase = 2
+	end
+    if HP < 25 and Phase == 2 then
+		printf("Phase 3");
+		mob:addMod(tpz.mod.ATTP, 20)
+		mob:addMod(tpz.mod.DEFP, 20) 
+		mob:useMobAbility(689) -- Benediction
+		mob:setModelId(1775) -- Soulflayer
+        mob:setMobMod(tpz.mobMod.SKILL_LIST, 1963)
+		target:PrintToPlayer("...All will succumb to the void....",0,"Soulflayer")
+		Phase = 3
+		mob:setUnkillable(false)
+	end
 end
 
+
 function onMobDeath(mob, player, isKiller)
-player:PrintToPlayer("Let's dance again some other time!",0,"Lilisette")
+	player:PrintToPlayer("...and you too shall succumb.",0,"Soulflayer")
 end
 
 
