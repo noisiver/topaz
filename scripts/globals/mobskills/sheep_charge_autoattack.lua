260d99ae74dea9775c1a32dc9a6ba05e2006b585
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-09 01:26:42 -0400

    debugging

--- scripts/globals/mobskills/sheep_charge_autoattack.lua
+++ scripts/globals/mobskills/sheep_charge_autoattack.lua
@@ -10,7 +10,9 @@ require("scripts/globals/monstertpmoves")
 ---------------------------------------------------
 
 function onMobSkillCheck(target, mob, skill)
-    return 0
+	local CurrentTP = mob:getTP()
+	mob:setLocalVar("TP", CurrentTP)
+		return 0
 end
 
 function onMobWeaponSkill(target, mob, skill)
@@ -20,8 +22,6 @@ function onMobWeaponSkill(target, mob, skill)
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT, info.hitslanded)
 	
-	local TP = mob:getTP()
-	mob:setLocalVar("TP", TP)
 	local CurrentTP = mob:getLocalVar("TP")
 	local AddTP = CurrentTP + 100
 	 if (dmg > 0) then
