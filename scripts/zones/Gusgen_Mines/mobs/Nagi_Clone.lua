9793cc5b405b6cb3bf17fd1fead7ced44a23efcd
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-10 00:20:15 -0400

    (Custom) More Nagi tuning

---
+++ scripts/zones/Gusgen_Mines/mobs/Nagi_Clone.lua
@@ -0,0 +1,16 @@
+------------------------------
+-- Area: Gusgen Mines
+--   Nagi Clone
+------------------------------
+require("scripts/globals/mobs")
+require("scripts/globals/status")
+------------------------------
+function onMobSpawn(mob)
+	mob:setMobMod(tpz.mobMod.EXP_BONUS, -100)
+	mob:setMobMod(tpz.mobMod.GIL_MAX, -1)
+    mob:setMobMod(tpz.mobMod.HP_STANDBACK, -1)
+	mob:SetMagicCastingEnabled(false)
+end
+
+function onMobDeath(mob, player, isKiller)
+  end
