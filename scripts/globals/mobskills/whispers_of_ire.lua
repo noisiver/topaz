76a63f78e8cbbd533f22402b281f3d90bd6d68ef
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-14 14:12:05 -0400

    Whispers of ire fixes

--- scripts/globals/mobskills/whispers_of_ire.lua
+++ scripts/globals/mobskills/whispers_of_ire.lua
@@ -12,15 +12,7 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local numhits = 1
-    local accmod = 1
-    local dmgmod = 0.01
-    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod,TP_NO_EFFECT)
-    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, MOBPARAM_2_SHADOW)
-    target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
-	if (skill:getMsg() ~= tpz.msg.basic.SHADOW_ABSORB) then 
-		skill:setMsg(DrainMultipleAttributes(mob, target, 10, 3, math.random(1, 7), 120))
-	end
+	skill:setMsg(DrainMultipleAttributes(mob, target, 10, 3, math.random(1, 7), 120))
 
-    return dmg
+    return 1
 end
