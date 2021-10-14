283864186b73eb488cdfd262cd74b29abd30b091
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-13 23:24:25 -0400

    (Missions - ZM8 and ZM16) Tuning

--- scripts/zones/The_Celestial_Nexus/mobs/Ealdnarche_2.lua
+++ scripts/zones/The_Celestial_Nexus/mobs/Ealdnarche_2.lua
@@ -12,8 +12,11 @@ function onMobInitialize(mob)
     -- 60% fast cast, -75% physical damage taken, 10tp/tick regain, no standback
     mob:addMod(tpz.mod.UFASTCAST, 60)
     mob:addMod(tpz.mod.UDMGPHYS, -75)
-    mob:addMod(tpz.mod.REGAIN, 100)
     mob:setMobMod(tpz.mobMod.HP_STANDBACK, -1)
+end
+
+function onMobFight(mob, target)
+	mob:setMod(tpz.mod.REGAIN, 100)
 end
 
 function onMobSpawn(mob)
@@ -21,6 +24,7 @@ function onMobSpawn(mob)
     mob:addMod(tpz.mod.ATTP, 50)
     mob:addMod(tpz.mod.ACC, 30) 
     mob:addMod(tpz.mod.EVA, 30)
+    mob:setMod(tpz.mod.REFRESH, 400)
     mob:setMobMod(tpz.mobMod.GA_CHANCE, 25)
     if GetMobByID(mob:getID() - 1):isDead() and GetMobByID(mob:getID() - 2):isDead() then
         mob:getBattlefield():setLocalVar("phaseChange", 0)
