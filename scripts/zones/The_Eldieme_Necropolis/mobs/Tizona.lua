10959eb7b4334ae1f2ad3500385d2b5ed1cb3e9e
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-15 01:56:35 -0400

    Debugging

--- scripts/zones/The_Eldieme_Necropolis/mobs/Tizona.lua
+++ scripts/zones/The_Eldieme_Necropolis/mobs/Tizona.lua
@@ -15,7 +15,6 @@ function onMobSpawn(mob)
 	mob:setMod(tpz.mod.UFASTCAST, 50)
 	mob:setMobMod(tpz.mobMod.EXP_BONUS, -100)
 	mob:setMobMod(tpz.mobMod.GIL_MAX, -1)
-	mob:setModelId(32) -- Human Male
     mob:setUnkillable(true)
 end
 
