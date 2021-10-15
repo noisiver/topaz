937e3e1b74f3cff6a1f590f443f2174f35f2b2be
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-15 01:35:02 -0400

    (Mythic - Tizona) Fixes

--- scripts/zones/The_Eldieme_Necropolis/mobs/Tizona.lua
+++ scripts/zones/The_Eldieme_Necropolis/mobs/Tizona.lua
@@ -23,33 +23,33 @@ function onMobFight(mob, target)
 	local HP = mob:getHPP()
     if HP < 25 and Phase == 0 then
 		printf("Phase 1");
+		mob:setHP(35000)
+		mob:setLocalVar("Phase", 1)
 		mob:addMod(tpz.mod.ATTP, 20)
 		mob:addMod(tpz.mod.DEFP, 20) 
-		mob:useMobAbility(689) -- Benediction
         mob:setModelId(1799) -- Yellow Flan
         mob:setMobMod(tpz.mobMod.SKILL_LIST, 112)
         target:PrintToPlayer("This power...I cannot contain it much longer...",0,"Raubahn")
-		mob:setLocalVar("Phase", 1)
 	end
     if HP < 25 and Phase == 1 then
 		printf("Phase 2");
+		mob:setHP(35000)
+		mob:setLocalVar("Phase", 2)
 		mob:addMod(tpz.mod.ATTP, 20)
 		mob:addMod(tpz.mod.DEFP, 20) 
-		mob:useMobAbility(689) -- Benediction
 		mob:setModelId(1774) -- Qutrub
         mob:setMobMod(tpz.mobMod.SKILL_LIST, 203)
 		target:PrintToPlayer("It's...too....powerful.....",0,"Raubahn")
-		mob:setLocalVar("Phase", 2)
 	end
     if HP < 25 and Phase == 2 then
 		printf("Phase 3");
+		mob:setHP(35000)
+		mob:setLocalVar("Phase", 3)
 		mob:addMod(tpz.mod.ATTP, 20)
 		mob:addMod(tpz.mod.DEFP, 20) 
-		mob:useMobAbility(689) -- Benediction
 		mob:setModelId(1775) -- Soulflayer
         mob:setMobMod(tpz.mobMod.SKILL_LIST, 1963)
 		target:PrintToPlayer("...All will succumb to the void....",0,"Soulflayer")
-		mob:setLocalVar("Phase", 3)
 		mob:setUnkillable(false)
 	end
 end
