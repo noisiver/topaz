9ea868a0692509c1c051ce6dd3a483b22292a93b
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-19 16:42:36 -0400

    Revert "(WS) Add more acc bonus to first hit of ranged WS"
    This reverts commit 73982bf8a1a501ca85006d2c199c9a61c9d09da8.

--- scripts/globals/weaponskills.lua
+++ scripts/globals/weaponskills.lua
@@ -799,8 +799,8 @@ function getRangedHitRate(attacker, target, capHitRate, bonus)
         firsthit = hitrate + math.floor((firstacc - eva) / 2)
 		slugwinder = hitrate + math.floor(((acc - eva) / 2)/ 2) 
     else 
-        hitdiff = hitrate + math.floor((((acc - eva)) / 2) - 2 * (target:getMainLvl() - attacker:getMainLvl())) + 20
-        firsthit = hitrate + math.floor((((firstacc - eva)) / 2) - 2 * (target:getMainLvl() - attacker:getMainLvl())) + 20
+        hitdiff = hitrate + math.floor((((acc - eva)) / 2) - 2 * (target:getMainLvl() - attacker:getMainLvl()))
+        firsthit = hitrate + math.floor((((firstacc - eva)) / 2) - 2 * (target:getMainLvl() - attacker:getMainLvl()))
 		slugwinder = hitrate + math.floor(((acc - eva) / 2) - (2 * (target:getMainLvl() - attacker:getMainLvl())))
     end
 
