beb85bc5aa097d1c447003b87827215892e6fe41
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-18 20:38:15 -0400

    Revert "(DNC) Fix Building Flourish message"
    This reverts commit 4c2fcc6bd1c3fe713b3d2b43479d2cf519d58aea.

--- scripts/globals/abilities/building_flourish.lua
+++ scripts/globals/abilities/building_flourish.lua
@@ -41,10 +41,12 @@ function onUseAbility(player, target, ability)
         player:delStatusEffect(tpz.effect.FINISHING_MOVE_3)
         player:addStatusEffect(tpz.effect.BUILDING_FLOURISH, 3, 0, 60, 0, player:getMerit(tpz.merit.BUILDING_FLOURISH_EFFECT))
     elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_4)) then
-        player:delStatusEffect(tpz.effect.FINISHING_MOVE_3)
+        player:delStatusEffect(tpz.effect.FINISHING_MOVE_4)
+        player:addStatusEffect(tpz.effect.FINISHING_MOVE_1, 1, 0, 7200)
         player:addStatusEffect(tpz.effect.BUILDING_FLOURISH, 3, 0, 60, 0, player:getMerit(tpz.merit.BUILDING_FLOURISH_EFFECT))
     elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_5)) then
-        player:delStatusEffect(tpz.effect.FINISHING_MOVE_3)
+        player:delStatusEffect(tpz.effect.FINISHING_MOVE_5)
+        player:addStatusEffect(tpz.effect.FINISHING_MOVE_2, 1, 0, 7200)
         player:addStatusEffect(tpz.effect.BUILDING_FLOURISH, 3, 0, 60, 0, player:getMerit(tpz.merit.BUILDING_FLOURISH_EFFECT))
     end
 
