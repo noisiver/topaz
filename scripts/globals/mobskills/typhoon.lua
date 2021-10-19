7b000ca002b7a4788e9a7c012a5f9fa971197a57
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-19 01:52:59 -0400

    (Sky) Faust tuning

--- scripts/globals/mobskills/typhoon.lua
+++ scripts/globals/mobskills/typhoon.lua
@@ -17,22 +17,13 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local numhits = 3
+    local numhits = 1
     local accmod = 1
     local dmgmod = 1
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT)
-    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT, info.hitslanded)
+    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT, info.hitslanded*math.random(2, 3))
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT)
 	if ((skill:getMsg() ~= tpz.msg.basic.SHADOW_ABSORB) and (dmg > 0)) then   target:tryInterruptSpell(mob, info.hitslanded) end
-
-    if (mob:getName() == "Faust") then
-        if (mob:getLocalVar("Typhoon") == 0) then
-            mob:useMobAbility(539)
-            mob:setLocalVar("Typhoon", 1)
-        else
-            mob:setLocalVar("Typhoon", 0)
-        end
-    end
 
     return dmg
 end
