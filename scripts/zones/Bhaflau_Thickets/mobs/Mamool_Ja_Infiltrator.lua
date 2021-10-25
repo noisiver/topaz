50ee9a2b26bfbfc79424bd08ab16618b570473f6
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-25 19:48:43 -0400

    (ToAU) Fix NIN mamools standing back and raing

--- scripts/zones/Bhaflau_Thickets/mobs/Mamool_Ja_Infiltrator.lua
+++ scripts/zones/Bhaflau_Thickets/mobs/Mamool_Ja_Infiltrator.lua
@@ -5,6 +5,10 @@
 
 local ID = require("scripts/zones/Bhaflau_Thickets/IDs")
 -----------------------------------
+function onMobSpawn(mob)
+	mob:setMobMod(tpz.mobMod.MAGIC_COOL, 45)
+    mob:setMobMod(tpz.mobMod.HP_STANDBACK, -1)
+end
 
 function onMobDeath(mob, player, isKiller)
 	if isKiller and math.random(1,100) <= 24 then 
