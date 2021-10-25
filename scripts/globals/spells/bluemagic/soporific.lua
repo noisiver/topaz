95cd5fffbf54e96d56fe0fb6e2ac6f86d924e1b8
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-24 23:29:12 -0400

    (BLU) Change BLU sleeps to t1 sleep

--- scripts/globals/spells/bluemagic/soporific.lua
+++ scripts/globals/spells/bluemagic/soporific.lua
@@ -44,7 +44,7 @@ function onSpellCast(caster, target, spell)
 	end
 
     if (resist >= 0.5) then -- Do it!
-        if (target:addStatusEffect(typeEffect, 2, 0, duration)) then
+        if (target:addStatusEffect(typeEffect, 1, 0, duration)) then
             spell:setMsg(tpz.msg.basic.MAGIC_ENFEEB)
         else
             spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT)
--- scripts/globals/spells/bluemagic/yawn.lua
+++ scripts/globals/spells/bluemagic/yawn.lua
@@ -45,7 +45,7 @@ function onSpellCast(caster, target, spell)
 
     if (resist >= 0.5) then -- Do it!
         if ((target:isFacing(caster))) then -- TODO: Apparently this check shouldn't exist for enemies using this spell? Need more info.
-            if (target:addStatusEffect(typeEffect, 2, 0, duration)) then
+            if (target:addStatusEffect(typeEffect, 1, 0, duration)) then
                 spell:setMsg(tpz.msg.basic.MAGIC_ENFEEB)
             else
                 spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT)
