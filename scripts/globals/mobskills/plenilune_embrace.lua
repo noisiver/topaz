bc8aaf292ecd8f55cd0ece0851a380ff7e85817e
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-10-29 20:07:17 -0400

    (WOTG) Add Attk And Mattk boost to Plenilune Embrace

--- scripts/globals/mobskills/plenilune_embrace.lua
+++ scripts/globals/mobskills/plenilune_embrace.lua
@@ -20,31 +20,43 @@ function onMobWeaponSkill(target, mob, skill)
    
    local moon = VanadielMoonPhase()
    local moonphase = 0
+   local moonpower = 0
     if moon > 90 then -- Full Moon
         moonphase = math.random(1900, 2300)
+		moonpower = 30
 		mob:eraseStatusEffect()
 		mob:eraseStatusEffect()
 		mob:eraseStatusEffect()
     elseif moon > 75 then
         moonphase = math.random(1200, 1300)
+		moonpower = 25
 		mob:eraseStatusEffect()
 		mob:eraseStatusEffect()
     elseif moon > 60 then
         moonphase = math.random(810, 860)
+		moonpower = 20
 		mob:eraseStatusEffect()
     elseif moon > 40 then
         moonphase = math.random(710, 750)
+		moonpower = 10
     elseif moon > 25 then
         moonphase = math.random(460, 490)
+		moonpower = 7
     elseif moon > 10 then
         moonphase = math.random(250, 290)
+		moonpower = 5
     else
         moonphase = math.random(250, 290)
+		moonpower = 5
     end
 	
 	local heal = moonphase
-	
+	local typeEffect = tpz.effect.ATTACK_BOOST
+    local typeEffectTwo = tpz.effect.MAGIC_ATK_BOOST
+
+	MobBuffMove(mob, typeEffect, moonpower, 0, 120)
+	MobBuffMove(mob, typeEffectTwo, moonpower, 0, 120)
     skill:setMsg(tpz.msg.basic.SELF_HEAL)
 
-    return MobHealMove(mob, heal)
+	return MobHealMove(mob, heal)
 end
