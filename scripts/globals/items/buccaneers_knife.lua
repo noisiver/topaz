89038ac13011ed5eb3fbfde15099dd5c60af9647
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-03 16:34:56 -0400

    (Sky) Buccaneer's Knife fixes

--- scripts/globals/items/buccaneers_knife.lua
+++ scripts/globals/items/buccaneers_knife.lua
@@ -12,15 +12,26 @@ function onAdditionalEffect(player, target, damage)
     if target:getID() == 17502568 and (player:getEquipID(tpz.slot.MAIN) == 17622) then
         local chance = 90
 
-	if math.random(100) <= chance then
+    if (math.random(0, 99) >= chance) then
+        return 0, 0, 0
+    else
+        local dmg = 15
+        local params = {}
+        params.bonusmab = 0
+        params.includemab = false
+        dmg = addBonusesAbility(player, tpz.magic.ele.WATER, target, dmg, params)
+        dmg = dmg * applyResistanceAddEffect(player, target, tpz.magic.ele.WATER, 0)
+        dmg = adjustForTarget(target, dmg, tpz.magic.ele.WATER)
+        dmg = finalMagicNonSpellAdjustments(player, target, tpz.magic.ele.WATER, dmg)
 		target:delStatusEffectSilent(tpz.effect.MAGIC_SHIELD)
-		local finalDMG = math.floor(40, 60)
 			if target:hasStatusEffect(tpz.effect.PHYSICAL_SHIELD) then
 				target:setUnkillable(false)
 			end
-			return tpz.subEffect.WATER_DAMAGE, tpz.msg.basic.ADD_EFFECT_DMG, finalDMG
+		local message = tpz.msg.basic.ADD_EFFECT_DMG
+		if (dmg < 0) then
+			message = tpz.msg.basic.ADD_EFFECT_HEAL
+		end
+			return tpz.subEffect.WATER_DAMAGE, message, dmg
         end
     end
-
-    return 0, 0, 0
 end
