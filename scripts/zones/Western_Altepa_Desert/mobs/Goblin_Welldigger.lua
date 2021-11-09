c0667c5347ee6f70d6ae8823e68372924eb89fd6
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-09 01:52:53 -0500

    (MIsc) Goblin THF TA fixes

--- scripts/zones/Western_Altepa_Desert/mobs/Goblin_Welldigger.lua
+++ scripts/zones/Western_Altepa_Desert/mobs/Goblin_Welldigger.lua
@@ -3,7 +3,13 @@
 --  Mob: Goblin Welldigger
 -----------------------------------
 require("scripts/globals/regimes")
+require("scripts/globals/status")
 -----------------------------------
+
+function onMobSpawn(mob)
+    mob:setMod(tpz.mod.TRIPLE_ATTACK, 5)
+end
+
 
 function onMobDeath(mob, player, isKiller)
     tpz.regime.checkRegime(player, mob, 137, 1, tpz.regime.type.FIELDS)
