d259fc0a7e667e4518481ef040bd38c0102fd763
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-10 23:26:31 -0500

    (DRK) Implement Arcane Crest

--- scripts/globals/effects/arcane_crest.lua
+++ scripts/globals/effects/arcane_crest.lua
@@ -4,10 +4,20 @@
 --     
 -----------------------------------
 function onEffectGain(target, effect)
+	target:addMod(tpz.mod.ACC, -20)
+	target:addMod(tpz.mod.EVA, -20)
+	target:addMod(tpz.mod.MACC, -20)
+	target:addMod(tpz.mod.MEVA, -20)
+	target:addMod(tpz.mod.STORETP, -20)
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
+	target:delMod(tpz.mod.ACC, -20)
+	target:delMod(tpz.mod.EVA, -20)
+	target:delMod(tpz.mod.MACC, -20)
+	target:delMod(tpz.mod.MEVA, -20)
+	target:delMod(tpz.mod.STORETP, -20)
 end
