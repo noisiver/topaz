18f29930b671a74a669429e0620b96597164f230
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-12 14:06:15 -0500

    (DRK) Scarlet Delirium adjustment

--- scripts/globals/effects/footwork.lua
+++ scripts/globals/effects/footwork.lua
@@ -7,8 +7,8 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onEffectGain(target, effect)
-    target:addMod(tpz.mod.ATTP, 10)
 	target:addMod(tpz.mod.MAIN_DMG_RATING, 50)
+    target:addMod(tpz.mod.ATTP, 10)
 	target:addMod(tpz.mod.HASTE_MAGIC, -5000)
     target:addMod(tpz.mod.STORETP, 20)
     target:addMod(tpz.mod.KICK_DMG, effect:getPower())
@@ -18,8 +18,8 @@ function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-    target:delMod(tpz.mod.ATTP, 10)
     target:delMod(tpz.mod.MAIN_DMG_RATING, 50)
+    target:delMod(tpz.mod.ATTP, 10)
 	target:delMod(tpz.mod.HASTE_MAGIC, -5000)
     target:delMod(tpz.mod.STORETP, 20)
     target:delMod(tpz.mod.KICK_DMG, effect:getPower())
--- scripts/globals/effects/scarlet_delirium.lua
+++ scripts/globals/effects/scarlet_delirium.lua
@@ -6,18 +6,28 @@
 require("scripts/globals/status")
 -----------------------------------
 function onEffectGain(target, effect)
+	target:addMod(tpz.mod.MAIN_DMG_RATING, 25)
     target:addMod(tpz.mod.ATTP, 200)
     target:addMod(tpz.mod.MATT, 200)
-    target:addMod(tpz.mod.ACC, -100)
-    target:addMod(tpz.mod.REGEN_DOWN, 50)
+    target:addMod(tpz.mod.HPP, -25)
     target:addMod(tpz.mod.DEFP, -75)
     target:addMod(tpz.mod.UDMGPHYS, 100)
     target:addMod(tpz.mod.UDMGMAGIC, 100)
     target:addMod(tpz.mod.UDMGRANGE, 100)
+    target:addMod(tpz.mod.REGEN_DOWN, 50)
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
+	target:delMod(tpz.mod.MAIN_DMG_RATING, 25)
+    target:delMod(tpz.mod.ATTP, 200)
+    target:delMod(tpz.mod.MATT, 200)
+    target:delMod(tpz.mod.HPP, -25)
+    target:delMod(tpz.mod.DEFP, -75)
+    target:delMod(tpz.mod.UDMGPHYS, 100)
+    target:delMod(tpz.mod.UDMGMAGIC, 100)
+    target:delMod(tpz.mod.UDMGRANGE, 100)
+	target:delMod(tpz.mod.REGEN_DOWN, 50)
 end
