15db1c00dbbd29ace236b48f652ace83353ca5ad
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-13 15:37:07 -0500

    (DRK) Scarlet Delirium Tuning

--- scripts/globals/effects/scarlet_delirium.lua
+++ scripts/globals/effects/scarlet_delirium.lua
@@ -6,14 +6,14 @@
 require("scripts/globals/status")
 -----------------------------------
 function onEffectGain(target, effect)
-	target:addMod(tpz.mod.MAIN_DMG_RATING, 10)
+	target:addMod(tpz.mod.MAIN_DMG_RATING, 25)
     target:addMod(tpz.mod.ATTP, 200)
     target:addMod(tpz.mod.MATT, 200)
-    target:addMod(tpz.mod.HPP, -33)
+    target:addMod(tpz.mod.HPP, -25)
     target:addMod(tpz.mod.DEFP, -75)
-    target:addMod(tpz.mod.UDMGPHYS, 100)
-    target:addMod(tpz.mod.UDMGMAGIC, 100)
-    target:addMod(tpz.mod.UDMGRANGE, 100)
+    target:addMod(tpz.mod.UDMGPHYS, 50)
+    target:addMod(tpz.mod.UDMGMAGIC, 50)
+    target:addMod(tpz.mod.UDMGRANGE, 50)
     target:addMod(tpz.mod.REGEN_DOWN, 50)
 end
 
@@ -21,13 +21,13 @@ function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-	target:delMod(tpz.mod.MAIN_DMG_RATING, 10)
+	target:delMod(tpz.mod.MAIN_DMG_RATING, 25)
     target:delMod(tpz.mod.ATTP, 200)
     target:delMod(tpz.mod.MATT, 200)
-    target:delMod(tpz.mod.HPP, -33)
+    target:delMod(tpz.mod.HPP, -25)
     target:delMod(tpz.mod.DEFP, -75)
-    target:delMod(tpz.mod.UDMGPHYS, 100)
-    target:delMod(tpz.mod.UDMGMAGIC, 100)
-    target:delMod(tpz.mod.UDMGRANGE, 100)
+    target:delMod(tpz.mod.UDMGPHYS, 50)
+    target:delMod(tpz.mod.UDMGMAGIC, 50)
+    target:delMod(tpz.mod.UDMGRANGE, 50)
 	target:delMod(tpz.mod.REGEN_DOWN, 50)
 end
