e241879203226399fc5243a69815dd67b29b724f
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-12 20:10:59 -0500

    (Camps) Jagils Double Attack rate buffed to 25%

--- scripts/zones/Mount_Zhayolm/mobs/Sulphuric_Jagil.lua
+++ scripts/zones/Mount_Zhayolm/mobs/Sulphuric_Jagil.lua
@@ -2,9 +2,10 @@
 -- Area: Mount Zhayolm
 --   NM: Sulphuric_Jagil
 ------------------------------
+require("scripts/globals/status")
 ------------------------------
 function onMobSpawn(mob)
-    mob:setMod(tpz.mod.DOUBLE_ATTACK, 10)
+    mob:setMod(tpz.mod.DOUBLE_ATTACK, 25)
 end
 
 function onMobDeath(mob, player, isKiller)
