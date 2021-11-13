357419785d212e64cae3765f06aa70582a56250a
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-12 20:53:57 -0500

    (Mobs) Add Double Attack to Erucas(Why is it missing???)

--- scripts/zones/Bhaflau_Thickets/mobs/Date_Eruca.lua
+++ scripts/zones/Bhaflau_Thickets/mobs/Date_Eruca.lua
@@ -4,9 +4,11 @@
 -----------------------------------
 mixins = {require("scripts/mixins/families/eruca")}
 local ID = require("scripts/zones/Bhaflau_Thickets/IDs")
+require("scripts/globals/status")
 -----------------------------------
 function onMobSpawn(mob)
 	mob:AnimationSub(0)
+	mob:setMod(tpz.mod.DOUBLE_ATTACK, 25)
 end
 
 function onMobDeath(mob, player, isKiller)
