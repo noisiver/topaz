e44cfc49f728e0533fcddfdcbfccc77c246b1418
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-14 19:39:13 -0500

    (DRK) Make Scarlet Delirium remove regen on activiation

--- scripts/globals/abilities/scarlet_delirium.lua
+++ scripts/globals/abilities/scarlet_delirium.lua
@@ -16,4 +16,5 @@ end
 function onUseAbility(player, target, ability)
     player:addStatusEffect(tpz.effect.SCARLET_DELIRIUM, 8, 1, 30)
     player:addStatusEffect(tpz.effect.CURSE_II, 1, 0, 30)
+    player:delStatusEffectSilent(tpz.effect.REGEN)
 end
