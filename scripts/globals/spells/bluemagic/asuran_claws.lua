f28534db20610c79ea7d8a9f32c1f9b5c4ce401b
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-15 21:57:28 -0500

    Revert "debuggimg"
    This reverts commit f66a1b889a3ae3980f1298b1c244219d43257a9a.

--- scripts/globals/spells/bluemagic/asuran_claws.lua
+++ scripts/globals/spells/bluemagic/asuran_claws.lua
@@ -1,16 +1,16 @@
 -----------------------------------------
--- Spell: Disseverment
--- Delivers a fivefold attack. Additional effect: Poison. Accuracy varies with TP
--- Spell cost: 74 MP
--- Monster Type: Luminians
--- Spell Type: Physical (Piercing)
--- Blue Magic Points: 5
--- Stat Bonus: INT+1, MND-1
--- Level: 72
--- Casting Time: 0.5 seconds
--- Recast Time: 32.75 seconds
--- Skillchain Element(s): Distortion (can open/close Darkness with Gravitation WSs and spells)
--- Combos: Accuracy Bonus
+-- Spell: Asuran Claws
+-- Delivers a sixfold attack. Accuracy varies with TP
+-- Spell cost: 81 MP
+-- Monster Type: Beasts
+-- Spell Type: Physical (Blunt)
+-- Blue Magic Points: 2
+-- Stat Bonus: AGI +3
+-- Level: 70
+-- Casting Time: 3 seconds
+-- Recast Time: 60 seconds
+-- Skillchain Element(s): Fire (Primary) and Lightning (Secondary) - (can open Scission, Detonation, Liquefaction, or Fusion can close Liquefaction, Impaction, or Fusion)
+-- Combos: Counter
 -----------------------------------------
 require("scripts/globals/bluemagic")
 require("scripts/globals/status")
@@ -23,40 +23,37 @@ end
 
 function onSpellCast(caster, target, spell)
     local params = {}
-    params.diff = caster:getStat(tpz.mod.INT) - target:getStat(tpz.mod.INT)
-    params.attribute = tpz.mod.INT
-    params.skillType = tpz.skill.BLUE_MAGIC
-    params.bonus = 0
-    local resist = applyResistance(caster, target, spell, params)
-    local params = {}
     -- This data should match information on http://wiki.ffxiclopedia.org/wiki/Calculating_Blue_Magic_Damage
-    params.tpmod = TPMOD_ACC
-    params.attackType = tpz.attackType.PHYSICAL
-    params.damageType = tpz.damageType.PIERCING
-    params.scattr = SC_DISTORTION
-    params.numhits = 5
-    params.multiplier = 1.5
-    params.tp150 = 1.5
-    params.tp300 = 1.5
-    params.azuretp = 1.5
-    params.duppercap = 100 -- D upper >=69
-    params.str_wsc = 0.2
-    params.dex_wsc = 0.2
-    params.vit_wsc = 0.0
-    params.agi_wsc = 0.0
-    params.int_wsc = 0.0
-    params.mnd_wsc = 0.0
-    params.chr_wsc = 0.0
-    params.AccTPModifier = true
+	params.tpmod = TPMOD_ACC
+	params.attackType = tpz.attackType.PHYSICAL
+	params.damageType = tpz.damageType.SLASHING
+	params.scattr = SC_IMPACTION
+	params.numhits = 6
+	params.multiplier = 1
+	params.tp150 = 1
+	params.tp300 = 1
+	params.azuretp = 1
+	params.duppercap = 80 -- D upper >=69
+	params.str_wsc = 0.0
+	params.dex_wsc = 0.4
+	params.vit_wsc = 0.0
+	params.agi_wsc = 0.0
+	params.int_wsc = 0.0
+	params.mnd_wsc = 0.0
+	params.chr_wsc = 0.0
+	params.AccTPModifier = true
 	params.attkbonus = 0.9
     damage = BluePhysicalSpell(caster, target, spell, params)
+	local lizard = (target:getSystem() == 14)
+	local plantoid = (target:getSystem() == 17)
+	
+	if lizard then
+		damage = damage * (1.25 + caster:getMerit(tpz.merit.MONSTER_CORRELATION)/100 + caster:getMod(tpz.mod.MONSTER_CORRELATION_BONUS)/100)
+	elseif plantoid then
+		damage = damage * 0.75
+	end
     damage = BlueFinalAdjustments(caster, target, spell, damage, params)
-    
-	if (damage > 0 and resist >= 0.5) then
-        local typeEffect = tpz.effect.POISON
-        local power = (caster:getMainLvl()/3) + 5 
-        target:addStatusEffect(typeEffect, power, 3, getBlueEffectDuration(caster, resist, typeEffect, true))
-    end
 
     return damage
 end
+
