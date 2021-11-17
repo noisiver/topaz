9a151053d53a2945d51ce666b880b157688d953b
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-16 23:15:58 -0500

    (WoTG) Oupire fixes

--- scripts/zones/Alzadaal_Undersea_Ruins/mobs/Oupire.lua
+++ scripts/zones/Alzadaal_Undersea_Ruins/mobs/Oupire.lua
@@ -26,6 +26,16 @@ function onMobSpawn(mob)
 	mob:setMod(tpz.mod.GRAVITYRESTRAIT, 100)
 end
 
+function onMobFight(mob, target)
+	local ElementMode = mob:getLocalVar("ElementMode")
+	if ElementMode == 4 then
+		mob:addStatusEffect(tpz.effect.BLAZE_SPIKES, 50, 0, 3600) 
+	elseif ElementMode == 5
+		mob:addStatusEffect(tpz.effect.ICE_SPIKES, 50, 0, 3600) 
+	elseif ElementMode == 6
+		mob:addStatusEffect(tpz.effect.SHOCK_SPIKES, 15, 0, 3600) 
+	end
+end
 
 function onMobWeaponSkill(target, mob, skill)
 	local StoneskinApplied = mob:getLocalVar("StoneskinApplied")
@@ -33,6 +43,8 @@ function onMobWeaponSkill(target, mob, skill)
         mob:setLocalVar("ElementMode", 1) --Earth
 		mob:setSpellList(2005)
 		setResist(mob, 1)
+		DeleteSpikes(mob)
+		mob:castSpell(191) -- Stonega III
 		if StoneskinApplied == 0 then -- Only supposed to get once per fight
 			mob:addStatusEffect(tpz.effect.STONESKIN, 2000, 0, 0) 
 			mob:setLocalVar("StoneskinApplied", 1) 
@@ -41,26 +53,33 @@ function onMobWeaponSkill(target, mob, skill)
         mob:setLocalVar("ElementMode", 2) --Water
 		mob:setSpellList(2006)
 		setResist(mob, 2)
+		DeleteSpikes(mob)
+		mob:castSpell(201) -- Waterga III
 	elseif skill:getID() == 2110 then --Wings of Gehenna
         mob:setLocalVar("ElementMode", 3) --Wind
 		mob:setSpellList(2007)
 		setResist(mob, 0)
+		DeleteSpikes(mob)
+		mob:castSpell(186) -- Aeroga III
 		mob:addStatusEffect(tpz.effect.BLINK, 20, 0, 0) --Blink
     elseif skill:getID() == 2106 then --Bloodrake
         mob:setLocalVar("ElementMode", 4) --Fire
 		mob:setSpellList(2008)
 		setResist(mob, 0)
-		mob:addStatusEffect(tpz.effect.BLAZE_SPIKES, 25, 0, 0) 
+		DeleteSpikes(mob)
+		mob:castSpell(176) -- Firaga III
     elseif skill:getID() == 2111 then --Eternal Damnation
         mob:setLocalVar("ElementMode", 5) --Ice
 		mob:setSpellList(2009)
 		setResist(mob, 5)
-		mob:addStatusEffect(tpz.effect.ICE_SPIKES, 15, 0, 0) 
+		DeleteSpikes(mob)
+		mob:castSpell(181) -- Blizzaga III
     elseif skill:getID() == 2107 then --Decollation
         mob:setLocalVar("ElementMode", 6) --Thunder
 		mob:setSpellList(2010)
 		setResist(mob, 0)
-		mob:addStatusEffect(tpz.effect.SHOCK_SPIKES, 15, 0, 0) 
+		DeleteSpikes(mob)
+		mob:castSpell(196) -- Thundaga III
     end
 end
 
@@ -68,7 +87,7 @@ function onSpellPrecast(mob, spell)
     if (spell:getID() == 252) then -- Stun
         spell:setAoE(tpz.magic.aoe.RADIAL)
         spell:setFlag(tpz.magic.spellFlag.HIT_ALL)
-        spell:setRadius(15)
+        spell:setRadius(10)
     end
 end
 
@@ -107,6 +126,12 @@ local function setResist(mob, element)
 		mob:setMod(tpz.mod.POISONRESTRAIT, 0)
 		mob:setMod(tpz.mod.PARALYZERESTRAIT, 100)
 	end
+end
+
+local function DeleteSpikes(mob)
+	mob:delStatusEffect(tpz.effect.BLAZE_SPIKES)
+	mob:delStatusEffect(tpz.effect.ICE_SPIKES)
+	mob:delStatusEffect(tpz.effect.SHOCK_SPIKES)
 end
 
 function onMobDeath(mob, player, isKiller)
