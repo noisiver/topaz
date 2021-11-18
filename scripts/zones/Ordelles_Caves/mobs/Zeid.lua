766dc9ade711037bedf706808b91c78b1b63fef2
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-18 01:47:06 -0500

    (Mythic) Zeid tuning

--- scripts/zones/Ordelles_Caves/mobs/Zeid.lua
+++ scripts/zones/Ordelles_Caves/mobs/Zeid.lua
@@ -12,7 +12,7 @@ function onMobSpawn(mob)
 	mob:setDamage(120)
     mob:addMod(tpz.mod.ATTP, 50)
     mob:setMod(tpz.mod.REFRESH, 400)
-    mob:setMod(tpz.mod.DOUBLE_ATTACK, 25)
+    mob:setMod(tpz.mod.DOUBLE_ATTACK, 0)
 	mob:setMobMod(tpz.mobMod.SKILL_LIST, 6063)
 	mob:setLocalVar("TwoHourUsed", 0)
 end
@@ -28,14 +28,11 @@ function onMobFight(mob, target)
 	
 	if mob:getHPP() >= 50 then
 		if StunTime == 0 then
-			mob:setLocalVar("StunTime", BattleTime + 30)
+			mob:setLocalVar("StunTime", BattleTime + 45)
 		elseif BattleTime >= StunTime then
 			mob:castSpell(252) -- Stun
-			local zonePlayers = mob:getZone():getPlayers()
-			for _, zonePlayer in pairs(zonePlayers) do
-				target:PrintToPlayer("No, pay closer attention and copy my technique.",0,"Zeid")
-			end
-			mob:setLocalVar("StunTime", BattleTime + 30)
+			target:PrintToPlayer("No, pay closer attention and copy my technique.",0,"Zeid")
+			mob:setLocalVar("StunTime", BattleTime + 45)
 		end
 	end
 	
@@ -45,11 +42,8 @@ function onMobFight(mob, target)
 		elseif BattleTime >= DreadSpikesTime then
 			local typeEffect = tpz.effect.DREAD_SPIKES
 			mob:addStatusEffect(typeEffect, 0, 0, 60, 0, 1000, 1)
-			local zonePlayers = mob:getZone():getPlayers()
-			for _, zonePlayer in pairs(zonePlayers) do
-				target:PrintToPlayer("Give me all you've got!",0,"Zeid")
-			end
-			mob:setLocalVar("DreadSpikesTime", BattleTime + 30)
+			target:PrintToPlayer("Give me all you've got!",0,"Zeid")
+			mob:setLocalVar("DreadSpikesTime", BattleTime + 45)
 		end
 	end
 	
@@ -62,7 +56,7 @@ function onMobFight(mob, target)
 			for _, zonePlayer in pairs(zonePlayers) do
 				target:PrintToPlayer("I feed off your rage!",0,"Zeid")
 			end
-			mob:setLocalVar("DrainTime", BattleTime + 30)
+			mob:setLocalVar("DrainTime", BattleTime + 45)
 		end
 	end
 	
@@ -70,10 +64,7 @@ function onMobFight(mob, target)
 	if mob:getHPP() <= 50 and TwoHourUsed == 0 then
 		mob:useMobAbility(695) -- Blood weapon
 		mob:setMobMod(tpz.mobMod.SKILL_LIST, 6064)
-		local zonePlayers = mob:getZone():getPlayers()
-		for _, zonePlayer in pairs(zonePlayers) do
-			target:PrintToPlayer("I must fight with you longer!",0,"Zeid")
-		end
+		target:PrintToPlayer("I must fight with you longer!",0,"Zeid")
 		mob:setLocalVar("TwoHourUsed", 1)
 	end
 end
