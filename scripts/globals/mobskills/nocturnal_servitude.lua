e914d8e4322aa6259863a895962c47454916a71b
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-20 13:31:53 -0500

    revert nocturnal

--- scripts/globals/mobskills/nocturnal_servitude.lua
+++ scripts/globals/mobskills/nocturnal_servitude.lua
@@ -22,6 +22,25 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
 
-	MobCharmMove(mob, target, 257, 60)
-    return 0
+	local message = tpz.msg.basic.SKILL_MISS
+	local typeEffect = tpz.effect.CHARM_I
+	local power = 0
+
+	if (not target:isPC()) then
+		skill:setMsg(tpz.msg.basic.SKILL_MISS)
+		return typeEffect
+	end
+
+		local msg = MobStatusEffectMove(mob, target, typeEffect, 0, 3, 30)
+		if target:hasStatusEffect(tpz.effect.FEALTY) then
+			skill:setMsg(tpz.msg.basic.SKILL_NO_EFFECT)
+		else
+			if (msg == tpz.msg.basic.SKILL_ENFEEB_IS) then
+			mob:charm(target)
+			target:costume(257) --Red Bat
+		end
+			 skill:setMsg(tpz.msg.basic.SKILL_MISS)
+		end
+
+    return typeEffect
 end
