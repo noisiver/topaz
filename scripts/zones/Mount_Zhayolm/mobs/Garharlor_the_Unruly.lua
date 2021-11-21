9e05cd3a76185d5d3ae86f4c346258307d23c2a1
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-21 13:54:03 -0500

    (ToAU) Troll trio - spawn other 2 trolls once main trolls dead

--- scripts/zones/Mount_Zhayolm/mobs/Garharlor_the_Unruly.lua
+++ scripts/zones/Mount_Zhayolm/mobs/Garharlor_the_Unruly.lua
@@ -2,6 +2,7 @@
 -- Area: Mount Zhayolm
 --   NM: Garharlor_the_Unruly
 -----------------------------------
+local ID = require("scripts/zones/Mount_Zhayolm/IDs")
 mixins = {
 require("scripts/mixins/weapon_break")
 require("scripts/mixins/job_special")
@@ -21,4 +22,6 @@ function onMobDeath(mob)
 	if isKiller and math.random(1,100) <= 15 then 
 		player:addTreasure(5735, mob)--Cotton Coin Purse
 	end
+	    DespawnMob(mob:getID()+1)
+		DespawnMob(mob:getID()+2)
 end
