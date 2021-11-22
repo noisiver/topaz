f2e2ce787780af98f4a25fca90c86f9397f2b320
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-22 01:03:40 -0500

    (COP) 6-4 tuning

--- scripts/globals/mobskills/antimatter.lua
+++ scripts/globals/mobskills/antimatter.lua
@@ -26,7 +26,7 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local dmgmod = 2
+    local dmgmod = 2.5
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.LIGHT, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.LIGHT, MOBPARAM_IGNORE_SHADOWS)
 
