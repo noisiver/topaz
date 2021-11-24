c31e3b3137d2644c69bcdd3a9bec116ea9a5b8a4
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-24 01:40:59 -0500

    Revert "(C++) Add mobs and fellow companions to OnTrigger function"
    This reverts commit 556df6458cd927b08cf4c63a4c1848e7fc0a3c4b.

--- src/map/lua/luautils.cpp
+++ src/map/lua/luautils.cpp
@@ -1495,23 +1495,10 @@ namespace luautils
     *                                                                       *
     ************************************************************************/
 
-        int32 OnTrigger(CCharEntity* PChar, CBaseEntity* PNpc)
+    int32 OnTrigger(CCharEntity* PChar, CBaseEntity* PNpc)
     {
-        const char* script_path = nullptr;
-        switch (PNpc->objtype)
-        {
-            case TYPE_FELLOW:
-                script_path = "scripts/globals/pets/fellow.lua";
-                break;
-            case TYPE_NPC:
-                script_path = "scripts/zones/%s/npcs/%s.lua";
-                break;
-            case TYPE_MOB:
-                script_path = "scripts/zones/%s/mobs/%s.lua";
-                break;
-        }
-
-        lua_prepscript(script_path, PChar->loc.zone->GetName(), PNpc->GetName());
+        TracyZoneScoped;
+        lua_prepscript("scripts/zones/%s/npcs/%s.lua", PChar->loc.zone->GetName(), PNpc->GetName());
 
         PChar->m_event.reset();
         PChar->m_event.Target = PNpc;
