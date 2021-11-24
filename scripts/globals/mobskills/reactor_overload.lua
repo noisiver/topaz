88df5d730ab32ec16195c892d79a37cc5c7832c3
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-24 02:46:59 -0500

    (Mobs) Zdei TP move fixes

--- scripts/globals/mobskills/reactor_overload.lua
+++ scripts/globals/mobskills/reactor_overload.lua
@@ -19,16 +19,15 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-
     local numhits = 1
     local accmod = 1
-    local dmgmod = 2.5
-    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, TP_NO_EFFECT)
-    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.PIERCING, MOBPARAM_3_SHADOW)
+    local dmgmod = 2.5 
+    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT)
+    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, MOBPARAM_3_SHADOW)
     local typeEffect = tpz.effect.SILENCE
 
 
-    target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.PIERCING)
+    target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
 	if ((skill:getMsg() ~= tpz.msg.basic.SHADOW_ABSORB) and (dmg > 0)) then   target:tryInterruptSpell(mob, info.hitslanded) end
     MobPhysicalStatusEffectMove(mob, target, skill, typeEffect, 1, 0, 60)
    return dmg
