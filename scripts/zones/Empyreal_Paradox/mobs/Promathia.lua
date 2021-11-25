218a3abb2abca7614af67550fa8d918a9393e358
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-25 00:03:25 -0500

    (COP) 8-4 fixes

--- scripts/zones/Empyreal_Paradox/mobs/Promathia.lua
+++ scripts/zones/Empyreal_Paradox/mobs/Promathia.lua
@@ -42,8 +42,15 @@ end
 
 function onMobDeath(mob, player, isKiller)
     local battlefield = mob:getBattlefield()
-    player:startEvent(32004)
+    if player then
+        player:startEvent(32004, battlefield:getArea())
+    else
+        for _, member in pairs(battlefield:getPlayers()) do
+            member:startEvent(32004, battlefield:getArea())
+        end
+    end
 end
+
 
 function onEventUpdate(player,csid,option)
 end
