9f3c1bf5b09c90b30d5c9035ea8d92ae32062a62
Shiyo <58101246+ShiyoKozuki@users.noreply.github.com>
2021-11-28 00:51:47 -0500

    (COP) 4-2 tuning

--- scripts/globals/mobskills/typhoon_wing_ouryu_cop.lua
+++ scripts/globals/mobskills/typhoon_wing_ouryu_cop.lua
@@ -27,7 +27,7 @@ function onMobWeaponSkill(target, mob, skill)
 
 
     local dmgmod = 1 -- 430 dmg with shell
-    local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.DARK, dmgmod, TP_NO_EFFECT)
+    local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*1.5, tpz.magic.ele.DARK, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.DARK, MOBPARAM_WIPE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.DARK)
     MobStatusEffectMove(mob, target, typeEffect, 60, 0, 30)
