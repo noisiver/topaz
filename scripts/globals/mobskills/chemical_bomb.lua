c5cc21eb360e50ed3003e4af913c9517f025cf91
Shiyo <strikinginsanity@hotmail.com>
2021-12-20 00:48:45 -0500

    (TP Moves) Misc tuning

--- scripts/globals/mobskills/chemical_bomb.lua
+++ scripts/globals/mobskills/chemical_bomb.lua
@@ -34,6 +34,7 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
+    local accmod = 1
     local dmgmod = 1
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.EARTH, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.EARTH, MOBPARAM_IGNORE_SHADOWS)
