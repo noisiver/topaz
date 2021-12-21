b9c3d21c1cba5f4154a221e7f55007213280f089
Shiyo <strikinginsanity@hotmail.com>
2021-12-20 23:48:30 -0500

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "(C++ & GM Commands) Allow !additem to use names and add separate by ID command"""""""""
    This reverts commit 83bcc085348e12f606d1990656e089624d288759.

--- scripts/commands/additem.lua
+++ scripts/commands/additem.lua
@@ -6,43 +6,31 @@
 cmdprops =
 {
     permission = 1,
-    parameters = "si"
+    parameters = "iiiiiiiiiii"
 }
 
 function error(player, msg)
     player:PrintToPlayer(msg)
-    player:PrintToPlayer("!additem <name> {quantity}")
+    player:PrintToPlayer("!additem <itemId> {quantity} {aug1} {v1} {aug2} {v2} {aug3} {v3} {aug4} {v4} {trial}")
 end
 
-function onTrigger(player, name, quantity)
+function onTrigger(player, itemId, quantity, aug0, aug0val, aug1, aug1val, aug2, aug2val, aug3, aug3val, trialId)
     -- Load needed text ids for players current zone..
     local ID = zones[player:getZoneID()]
-    
+
     -- validate itemId
-    if (name == nil or tostring(name) == nil) then
-        error(player, "Invalid name.")
+    if (itemId == nil or tonumber(itemId) == nil or tonumber(itemId) == 0) then
+        error(player, "Invalid itemId.")
         return
     end
-    
-    local itemId = GetItemIDByName(name)
-    
-    if itemId == 0 or itemId == nil then
-        error(player, "Invalid ID.")
-    end
-    
-    if quantity == nil or quantity == 0 then
-        quantity = 1
-    end
-    
+
     -- Ensure the GM has room to obtain the item...
     if (player:getFreeSlotsCount() == 0) then
         player:messageSpecial( ID.text.ITEM_CANNOT_BE_OBTAINED, itemId )
         return
     end
-    
-    
-    print(string.format("lua:about to ADDITEM with id %i and quantity %i",itemId,quantity))
+
     -- Give the GM the item...
-    player:addItem( itemId, quantity )
+    player:addItem( itemId, quantity, aug0, aug0val, aug1, aug1val, aug2, aug2val, aug3, aug3val, trialId )
     player:messageSpecial( ID.text.ITEM_OBTAINED, itemId )
 end
