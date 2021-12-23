41b282275506434adaa68e648ae3bb0e1711b3d6
Shiyo <strikinginsanity@hotmail.com>
2021-12-23 03:19:53 -0500

    Revert "debugging"
    This reverts commit 659a3dbbaea8285b7d790ad5934bc888190c1878.

--- scripts/globals/monstertpmoves.lua
+++ scripts/globals/monstertpmoves.lua
@@ -881,16 +881,15 @@ function MobCharmMove(mob, target, skill, costume, duration)
 	
 	if resist >= 0.5 and mob:getCharmChance(target, false) > 0 then
 		if target:hasStatusEffect(tpz.effect.FEALTY) then
-		    return skill:setMsg(msg)
+		    return skill:setMsg(tpz.msg.basic.SKILL_NO_EFFECT)
 		else
         	MobStatusEffectMove(mob, target, tpz.effect.CHARM_I, 0, 3, duration * resist)
-            local msg = MobStatusEffectMove(mob, target, tpz.effect.CHARM_I, 0, 3, duration * resist)
 			mob:charm(target)
 			target:costume(costume)
-            return skill:setMsg(msg)
+            return skill:setMsg(tpz.msg.basic.SKILL_ENFEEB_IS)
         end
 	else
-	    return skill:setMsg(msg)
+	    return skill:setMsg(tpz.msg.basic.SKILL_MISS)
 	end
 end
 
