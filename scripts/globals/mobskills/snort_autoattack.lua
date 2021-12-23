e7db8fcf2d8140a54ca24d6e7084fc5ac126d077
Shiyo <strikinginsanity@hotmail.com>
2021-12-22 22:39:10 -0500

    (COP) Geush Urvan fixes

--- scripts/globals/mobskills/snort_autoattack.lua
+++ scripts/globals/mobskills/snort_autoattack.lua
@@ -26,10 +26,10 @@ function onMobWeaponSkill(target, mob, skill)
 
 	local CurrentTP = mob:getLocalVar("TP")
 	local AddTP = CurrentTP + 100
-    if (MobPhysicalHit(skill)) then
-       target:addTP(20)
-       mob:addTP(AddTP)
-    end
+
+    target:addTP(20)
+    mob:addTP(AddTP)
+
 	
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.WIND)
     MobStatusEffectMove(mob, target, typeEffect, 1, 0, 3)
