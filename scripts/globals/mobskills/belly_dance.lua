0f14a4a4f45f9ff195d67de43fc711d2714f0c78
Shiyo <strikinginsanity@hotmail.com>
2021-12-23 23:30:07 -0500

    (TP Moves) Add charm function to belly dance

--- scripts/globals/mobskills/belly_dance.lua
+++ scripts/globals/mobskills/belly_dance.lua
@@ -22,23 +22,9 @@ end
 
 
 function onMobWeaponSkill(target, mob, skill)
-	local message = tpz.msg.basic.SKILL_MISS
-    local typeEffect = tpz.effect.CHARM_I
-    local power = 0
 
-    if (not target:isPC()) then
-        skill:setMsg(tpz.msg.basic.SKILL_MISS)
-        return typeEffect
+    if (target:isFacing(mob)) then
+        MobCharmMove(mob, target, skill, 0, 120)
     end
-
-    local msg = MobGazeMove(mob, target, typeEffect, power, 3, 150)
-    if target:hasStatusEffect(tpz.effect.FEALTY) then
-        skill:setMsg(tpz.msg.basic.SKILL_NO_EFFECT)
-    elseif (msg == tpz.msg.basic.SKILL_ENFEEB_IS) then
-        mob:charm(target)
-    else
-         skill:setMsg(msg)
-    end
-
-    return typeEffect
+    return tpz.effect.CHARM_I
 end
