8b2e838bb6489ce3ee980e0bc5f5edccfe66075b
Shiyo <strikinginsanity@hotmail.com>
2021-12-23 23:45:20 -0500

    (ToAU) Darting Kachaal Ja fixes

--- scripts/zones/Mamook/mobs/Darting_Kachaal_Ja.lua
+++ scripts/zones/Mamook/mobs/Darting_Kachaal_Ja.lua
@@ -12,8 +12,13 @@ function onMobSpawn(mob)
     mob:addMod(tpz.mod.EVA, 30)
     mob:setMod(tpz.mod.REFRESH, 40)
 	mob:SetAutoAttackEnabled(true)
+    mob:SetMagicCastingEnabled(true)
     mob:setMobMod(tpz.mobMod.MAGIC_COOL, 15)
     mob:setMobMod(tpz.mobMod.HP_STANDBACK, 1)
+end
+
+function onMobEngaged(mob)
+    mob:SetMagicCastingEnabled(true)
 end
 
 function onMobFight(mob, target)
@@ -22,18 +27,31 @@ function onMobFight(mob, target)
 	local RunBackTime = mob:getLocalVar("RunAwayTime")
 	local RunAwayPath = mob:getLocalVar("RunBackTime")
 	local BattleTime = mob:getBattleTime()
+
+    local spellList =
+    {
+        [1] = 274, -- sleepga II
+        [2] = 362,-- bindga
+    }
+
+	local Pos = mob:getPos()
+	if Pos.x == -205 and Pos.y == 18 and Pos.z == -367 or Pos.x == 38 and Pos.y == 17 and Pos.z == -391 then
+        mob:SetMagicCastingEnabled(true)
+    end
 	
 	if RunAwayTime == 0 then
 		mob:setLocalVar("RunAwayTime", BattleTime + 45)
 	elseif BattleTime >= RunAwayTime and RunAwayPath == 0 then
-		mob:castSpell(274) -- sleepga II
+        mob:castSpell(spellList[math.random(#spellList)])
+        mob:SetMagicCastingEnabled(false)
 		mob:pathTo(-205, 18, -367)
 		mob:setLocalVar("RunBackTime", BattleTime + 45)
 		mob:setLocalVar("RunAwayPath", 1)
 	end
 	
 	if BattleTime >= RunBackTime and RunAwayPath == 1 then
-		mob:castSpell(362) -- bindga
+        mob:castSpell(spellList[math.random(#spellList)])
+        mob:SetMagicCastingEnabled(false)
 		mob:pathTo(38, 17, -391)
 		mob:setLocalVar("RunAwayTime", BattleTime + 45)
 		mob:setLocalVar("RunAwayPath", 0)
