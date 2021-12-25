e2db4f521c1313d1f120b867a605e90beacea8a4
Shiyo <strikinginsanity@hotmail.com>
2021-12-24 20:50:48 -0500

    (Sea) Baby Xzomits no longer aggressive

--- scripts/zones/AlTaieu/mobs/Omxzomit_Baby.lua
+++ scripts/zones/AlTaieu/mobs/Omxzomit_Baby.lua
@@ -11,6 +11,7 @@ require("scripts/globals/pathfind")
 function onMobSpawn(mob)
     mob:addMod(tpz.mod.MOVE, 45)
     mob:setMobMod(tpz.mobMod.LINK_RADIUS, 30)
+    mob:setAggressive(0)
     onPath(mob)
 end
 
