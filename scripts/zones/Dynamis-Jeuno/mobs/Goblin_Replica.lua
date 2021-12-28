25f4dda8dacf30a840f4d354cc75c5a86b72d31c
Shiyo <strikinginsanity@hotmail.com>
2021-12-28 16:33:25 -0500

    (Temp) Remove after reboot

--- scripts/zones/Dynamis-Jeuno/mobs/Goblin_Replica.lua
+++ scripts/zones/Dynamis-Jeuno/mobs/Goblin_Replica.lua
@@ -9,6 +9,11 @@ function onMobSpawn(mob)
     dynamis.refillStatueOnSpawn(mob)
 end
 
+function onMobFight(mob, target)
+	mob:setDamage(250)
+    mob:setMod(tpz.mod.REFRESH, 300)
+end
+
 function onMobDeath(mob, player, isKiller)
     dynamis.refillStatueOnDeath(mob, player, isKiller)
 end
