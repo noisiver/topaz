4ff7371b460dfba723ace4bfeafdb20b98b8bd84
Shiyo <strikinginsanity@hotmail.com>
2022-01-21 19:13:47 -0500

    (Status Effects) Implement crit hit evasion down

--- scripts/globals/effects/critical_hit_evasion_down.lua
+++ scripts/globals/effects/critical_hit_evasion_down.lua
@@ -5,12 +5,12 @@
 -----------------------------------
 
 function onEffectGain(target, effect)
-    target:addMod(tpz.mod.CRITICAL_HIT_EVASION_DOWN, -effect:getPower())
+    target:addMod(tpz.mod.ENEMYCRITRATE, effect:getPower())
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-    target:delMod(tpz.mod.CRITICAL_HIT_EVASION_DOWN, -effect:getPower())
+    target:delMod(tpz.mod.ENEMYCRITRATE, effect:getPower())
 end
