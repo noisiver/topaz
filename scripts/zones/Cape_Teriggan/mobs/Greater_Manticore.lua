8e59d4abd2d1208e0970adc2a752e99ff2d55dbb
Shiyo <strikinginsanity@hotmail.com>
2022-01-28 00:40:35 -0500

    (debugging) skillchain take

--- scripts/zones/Cape_Teriggan/mobs/Greater_Manticore.lua
+++ scripts/zones/Cape_Teriggan/mobs/Greater_Manticore.lua
@@ -7,6 +7,12 @@ local ID = require("scripts/zones/Cape_Teriggan/IDs")
 require("scripts/globals/regimes")
 require("scripts/globals/mobs")
 
+function onMobFight(mob, target)
+     mob:addListener("SKILLCHAIN_TAKE", "MANTICORE_TAKE_DAMAGE", function(mob)
+        printf("I took a skillchain to the face");
+    end)
+end
+
 function onMobDeath(mob, player, isKiller)
     tpz.regime.checkRegime(player, mob, 108, 2, tpz.regime.type.FIELDS)
 end
