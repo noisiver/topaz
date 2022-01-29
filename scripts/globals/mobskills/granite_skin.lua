114a090b0c354d1105ab06d24ac5670e5ed0b100
Shiyo <strikinginsanity@hotmail.com>
2022-01-29 04:27:42 -0500

    Revert "(debugging) positional pdt"
    This reverts commit ee6c9be11484af3ffcbe0609f3dc695636b68e37.

--- scripts/globals/mobskills/granite_skin.lua
+++ scripts/globals/mobskills/granite_skin.lua
@@ -21,7 +21,7 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local typeEffectOne = tpz.effect.PHYSICAL_SHIELD
-    MobBuffMove(mob, typeEffectOne, 5, 0, 120) -- Frontal PDT
+    MobBuffMove(mob, typeEffectOne, 3, 0, 120) -- Frontal PDT
     local effect1 = mob:getStatusEffect(typeEffectOne)
     effect1:unsetFlag(tpz.effectFlag.DISPELABLE)
 
