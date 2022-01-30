81693a14503caba0326c308797c003e199764f25
Shiyo <strikinginsanity@hotmail.com>
2022-01-30 03:50:29 -0500

    (Debugging) Dark ixion trample effect

---
+++ scripts/globals/mobskills/ixion_trample.lua
@@ -0,0 +1,32 @@
+---------------------------------------------
+-- Trample(Dark Ixion)
+-- Charges forward, dealing high damage to,(400-1000) and lowering the MP (10-30%) of targets hit.
+-- Type: Magical
+-- Range: Melee
+-- Notes: 1 target at > 76%+ HP, 2 at > 26% HP, 3 at < 25% HP.
+---------------------------------------------
+require("scripts/globals/monstertpmoves")
+require("scripts/globals/settings")
+require("scripts/globals/status")
+---------------------------------------------
+
+function onMobSkillCheck(target, mob, skill)
+    return 0
+end
+
+function onMobWeaponSkill(target, mob, skill)
+    local numhits = 1
+    local accmod = 1
+    local dmgmod = 2.5
+    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT)
+    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, info.hitslanded*math.random(1, 4))
+	
+    local mp = (target:getMaxMP() * math.random(1, 3)) / 10
+
+
+    target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
+    MobPhysicalStatusEffectMove(mob, target, skill, typeEffect, 1, 0, 4)
+    target:delMP(mp)
+	if ((skill:getMsg() ~= tpz.msg.basic.SHADOW_ABSORB) and (dmg > 0)) then   target:tryInterruptSpell(mob, info.hitslanded) end
+    return dmg
+end
