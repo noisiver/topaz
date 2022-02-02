dc0899ed48b3494ab4f4acf0e68372b49a4166e4
Shiyo <strikinginsanity@hotmail.com>
2022-02-02 00:04:01 -0500

    (Mythic Weapon Fights) THF fight tuning

--- scripts/globals/mobskills/self-destruct_Goblin_Grenade.lua
+++ scripts/globals/mobskills/self-destruct_Goblin_Grenade.lua
@@ -11,9 +11,10 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local amount = 50
-    local dmg = MobFinalAdjustments(amount, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.FIRE, MOBPARAM_IGNORE_SHADOWS)
+    local dmgmod = 1
+    local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*1, tpz.magic.ele.FIRE, dmgmod, TP_MAB_BONUS, 1)
+    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.FIRE, MOBPARAM_IGNORE_SHADOWS)
+    target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.FIRE)
     mob:setHP(0)
-    target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.FIRE)
     return dmg
 end
