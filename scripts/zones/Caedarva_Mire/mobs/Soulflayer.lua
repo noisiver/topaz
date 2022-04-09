dd0a7e6f4fc6db92ad92184dbf83291809b17b13
Shiyo <strikinginsanity@hotmail.com>
2022-04-08 23:05:11 -0400

    Revert "testing bots revert after"
    This reverts commit 112f2fccc88b427f140127b667a0331fe980ca6b.

--- scripts/zones/Caedarva_Mire/mobs/Soulflayer.lua
+++ scripts/zones/Caedarva_Mire/mobs/Soulflayer.lua
@@ -4,9 +4,6 @@
 -----------------------------------
 mixins = {require("scripts/mixins/families/soulflayer")}
 -----------------------------------
-function onMobWeaponSkillPrepare(mob, target)
-    return  1964
-end
 
 function onMobDeath(mob, player, isKiller)
 	if isKiller and math.random(1,100) <= 24 then 
