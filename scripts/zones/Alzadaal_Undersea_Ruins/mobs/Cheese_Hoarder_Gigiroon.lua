49f16f5fc525f343345e39b7613a5935d6d8ba3f
Shiyo <strikinginsanity@hotmail.com>
2022-04-12 01:20:04 -0400

    (ZNM) bugfix temp

--- scripts/zones/Alzadaal_Undersea_Ruins/mobs/Cheese_Hoarder_Gigiroon.lua
+++ scripts/zones/Alzadaal_Undersea_Ruins/mobs/Cheese_Hoarder_Gigiroon.lua
@@ -135,8 +135,17 @@ function onMobFight(mob, target)
 	end
 end
 
-function onMobDeath(mob)
-    player:addCurrency("zeni_point", 500)
+function onMobDeath(mob, killer)
+    player:addCurrency("zeni_point", 1000)
+	if isKiller  then 
+		player:addTreasure(5735, mob)--Cotton Coin Purse
+	end
+	if isKiller and math.random(1,100) <= 24 then 
+		player:addTreasure(5735, mob)--Cotton Coin Purse
+	end
+	if isKiller and math.random(1,100) <= 15 then 
+		player:addTreasure(5735, mob)--Cotton Coin Purse
+	end
 	if isKiller  then 
 		player:addTreasure(5735, mob)--Cotton Coin Purse
 	end
