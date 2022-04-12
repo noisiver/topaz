444ea3d5c71abeba54df2a1fc153fc236dd438cf
Shiyo <strikinginsanity@hotmail.com>
2022-04-12 03:13:15 -0400

    Revert "Fishing GP fix?"
    This reverts commit 0a49f9cf69505fdfc792210fc050ae61d2d2a873.

--- scripts/zones/Port_Windurst/npcs/Fennella.lua
+++ scripts/zones/Port_Windurst/npcs/Fennella.lua
@@ -32,32 +32,32 @@ local keyitems = {
 }
 
 local items = {
-    [-1] = {
+    [0] = {
         id = 17002, -- Robber's Rig
         rank = 3,
         cost = 1500
     },
-    [0] = {
+    [1] = {
         id = 15452, -- Fisherman's Belt
         rank = 4,
         cost = 10000
     },
-    [1] = {
+    [2] = {
         id = 14195, -- Pair of Waders
         rank = 5,
         cost = 70000
     },
-    [2] = {
+    [3] = {
         id = 14400, -- Fisherman's Apron
         rank = 7,
         cost = 100000
     },
-    [3] = {
+    [4] = {
         id = 191, -- Fishing hole map
         rank = 9,
         cost = 150000
     },
-    [4] = {
+    [5] = {
         id = 340, -- Fisherman's Signboard
         rank = 9,
         cost = 200000
