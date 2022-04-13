99ca593779b19700427501c03dffb7ee0bd42a4f
Shiyo <strikinginsanity@hotmail.com>
2022-04-13 05:24:09 -0400

    Banish spdef down vs undead adjustments
    The def down can now be resisted.
    Lower than half resist = doesn't land.
    Duration reduced by resist rate(up to 1/2 duration)
    
    spdef values corrected to be more retail accurate.
    WHM relic glvoes and Fenian ring now scales the spdef down.

--- scripts/globals/spells/banish_iii.lua
+++ scripts/globals/spells/banish_iii.lua
@@ -30,12 +30,24 @@ function onSpellCast(caster, target, spell)
     params.hasMultipleTargetReduction = false
     params.resistBonus = 1.0
     dmg = doDivineBanishNuke(caster, target, spell, params)
+    local resist = applyResistance(caster, target, spell, params)
+    local spdefdown = 95
+	-- Add Enhances potency of "Banish" vs. undead gear
+	local hands = caster:getEquipID(tpz.slot.HANDS)
+	if hands == 15104 or hands == 14911 then -- Cleric's mitts and Cleric's Mitts +1
+		spdefdown = spdefdown + 1 
+	end
+	local Lring = caster:getEquipID(tpz.slot.RING1)
+    local Rring = caster:getEquipID(tpz.slot.RING2)
+	if Lring == 15831 or Rring == 15831 then -- Fenian Ring
+		spdefdown = spdefdown + 1 
+	end 
     
-    -- special defense down, 50% I, 70% II, 97% III
-    if target:isUndead() and target:getMod(tpz.mod.SPDEF_DOWN) == 0 then
-        local duration = 45 + caster:getMerit(tpz.merit.BANISH_EFFECT)
+    -- special defense down, 43% I, 73% II, 95% III
+    if target:isUndead() and target:getMod(tpz.mod.SPDEF_DOWN) == 0 and resist >= 0.5 then
+        local duration = (45 + caster:getMerit(tpz.merit.BANISH_EFFECT)) * resist
         target:queue(0, function(target)
-            target:addMod(tpz.mod.SPDEF_DOWN,97)
+            target:addMod(tpz.mod.SPDEF_DOWN,spdefdown)
         end)
         target:queue(duration*1000, function(target)
             target:setMod(tpz.mod.SPDEF_DOWN,0)
