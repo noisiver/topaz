d4ce2eba7159d1fcd978611a3c3768f49c021a2f
Shiyo <strikinginsanity@hotmail.com>
2022-04-14 19:08:59 -0400

    missing fish I forgot

--- sql/fishing_fish.sql
+++ sql/fishing_fish.sql
@@ -96,7 +96,7 @@ INSERT INTO `fishing_fish` VALUES (4579,'Elshimo Newt',0,60,16,8,11,1,1,0,0,255,
 INSERT INTO `fishing_fish` VALUES (4454,'Emperor Fish',0,91,36,4,13,64,179,1,0,255,255,0,0,0,0,1,200,0,'',0);
 INSERT INTO `fishing_fish` VALUES (4501,'Fat Greedie',0,23,18,10,8,1,1,0,1,255,255,0,0,0,0,1,600,0,'',0);
 INSERT INTO `fishing_fish` VALUES (12316,'Fish Scale Shield',0,7,15,13,2,1,1,0,0,255,255,0,0,0,1,1,500,0,'',0);
-INSERT INTO `fishing_fish` VALUES (4289,'Forest Carp',0,20,21,9,11,1,1,0,0,255,255,0,0,0,0,1,0,0,'',0);
+INSERT INTO `fishing_fish` VALUES (4289,'Forest Carp',0,20,21,9,11,1,1,0,0,255,255,0,0,0,0,1,900,0,'',0);
 INSERT INTO `fishing_fish` VALUES (5472,'Garpike',0,83,22,11,7,1,1,0,0,255,255,0,0,0,0,1,650,0,'',0);
 INSERT INTO `fishing_fish` VALUES (4477,'Gavial Fish',0,81,30,14,15,40,133,1,0,255,255,0,0,0,0,1,550,0,'',0);
 INSERT INTO `fishing_fish` VALUES (5471,'Gerrothorax',90,100,38,7,8,210,250,1,0,255,255,0,1,0,0,1,500,0,'',0);
