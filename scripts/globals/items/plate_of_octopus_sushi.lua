fced5d9c91b5b995b6f307f17cdb8c2f285cda15
Shiyo <strikinginsanity@hotmail.com>
2022-04-14 02:41:39 -0400

    (Food) Buff Octopus / Shrimp sushi
    Octopus Sushi:
    -- Health 30
    -- +3 MAB
    -- Mind -1
    -- Accuracy % 15
    -- Ranged ACC % 15
    -- Sleep Resist 1
    
    Shrimp Sushi:
    -- Health 30
    -- +10 Evasion
    -- Mind -1
    -- Accuracy % 15
    -- Ranged ACC % 15
    -- Sleep Resist 1

--- scripts/globals/items/plate_of_octopus_sushi.lua
+++ scripts/globals/items/plate_of_octopus_sushi.lua
@@ -3,10 +3,12 @@
 -- Item: plate_of_octopus_sushi
 -- Food Effect: 30Min, All Races
 -----------------------------------------
--- Strength 1
--- Accuracy % 14 (cap 68)
--- Ranged Accuracy % 14 (cap 68)
--- Resist Sleep +1
+-- Health 30
+-- +3 MAB
+-- Mind -1
+-- Accuracy % 15
+-- Ranged ACC % 15
+-- Sleep Resist 1
 -----------------------------------------
 require("scripts/globals/status")
 require("scripts/globals/msg")
@@ -25,19 +27,23 @@ function onItemUse(target)
 end
 
 function onEffectGain(target, effect)
-    target:addMod(tpz.mod.STR, 1)
-    target:addMod(tpz.mod.FOOD_ACCP, 14)
-    target:addMod(tpz.mod.FOOD_ACC_CAP, 68)
-    target:addMod(tpz.mod.FOOD_RACCP, 14)
-    target:addMod(tpz.mod.FOOD_RACC_CAP, 68)
+    target:addMod(tpz.mod.HP, 30)
+    target:addMod(tpz.mod.MATT, 3)
+    target:addMod(tpz.mod.MND, -1)
+    target:addMod(tpz.mod.FOOD_ACCP, 15)
+    target:addMod(tpz.mod.FOOD_ACC_CAP, 72)
+    target:addMod(tpz.mod.FOOD_RACCP, 15)
+    target:addMod(tpz.mod.FOOD_RACC_CAP, 72)
     target:addMod(tpz.mod.SLEEPRESTRAIT, 1)
 end
 
 function onEffectLose(target, effect)
-    target:delMod(tpz.mod.STR, 1)
-    target:delMod(tpz.mod.FOOD_ACCP, 14)
-    target:delMod(tpz.mod.FOOD_ACC_CAP, 68)
-    target:delMod(tpz.mod.FOOD_RACCP, 14)
-    target:delMod(tpz.mod.FOOD_RACC_CAP, 68)
+    target:delMod(tpz.mod.HP, 30)
+    target:delMod(tpz.mod.MATT, 3)
+    target:delMod(tpz.mod.MND, -1)
+    target:delMod(tpz.mod.FOOD_ACCP, 15)
+    target:delMod(tpz.mod.FOOD_ACC_CAP, 72)
+    target:delMod(tpz.mod.FOOD_RACCP, 15)
+    target:delMod(tpz.mod.FOOD_RACC_CAP, 72)
     target:delMod(tpz.mod.SLEEPRESTRAIT, 1)
 end
