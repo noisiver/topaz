19eceafe22d4c18241b023204fad542922649fa7
Shiyo <strikinginsanity@hotmail.com>
2022-04-17 02:04:55 -0400

    testing charm

--- scripts/zones/Mamook/mobs/Poroggo.lua
+++ scripts/zones/Mamook/mobs/Poroggo.lua
@@ -5,6 +5,10 @@
 mixins = {require("scripts/mixins/families/poroggo")}
 -----------------------------------
 
+function onMobWeaponSkillPrepare(mob, target)
+    return 1957
+end
+
 function onMobDeath(mob, player, isKiller)
 	if isKiller and math.random(1,100) <= 24 then 
 		player:addTreasure(5735, mob)--Cotton Coin Purse
