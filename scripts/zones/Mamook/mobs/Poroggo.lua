79938a2716bb32ecdc22b245c3a0a5761a13e794
Shiyo <strikinginsanity@hotmail.com>
2022-04-17 02:07:29 -0400

    Revert "testing charm"
    This reverts commit 19eceafe22d4c18241b023204fad542922649fa7.

--- scripts/zones/Mamook/mobs/Poroggo.lua
+++ scripts/zones/Mamook/mobs/Poroggo.lua
@@ -5,10 +5,6 @@
 mixins = {require("scripts/mixins/families/poroggo")}
 -----------------------------------
 
-function onMobWeaponSkillPrepare(mob, target)
-    return 1957
-end
-
 function onMobDeath(mob, player, isKiller)
 	if isKiller and math.random(1,100) <= 24 then 
 		player:addTreasure(5735, mob)--Cotton Coin Purse
