f17d2bf5bc1dcde14b861308e6f83cd6fc475339
Shiyo <strikinginsanity@hotmail.com>
2022-04-21 03:37:20 -0400

    Missing fixes

--- scripts/zones/Ship_bound_for_Selbina_Pirates/mobs/Ship_Wight.lua
+++ scripts/zones/Ship_bound_for_Selbina_Pirates/mobs/Ship_Wight.lua
@@ -11,5 +11,5 @@ function onMobDeath(mob, player, isKiller)
 end
 
 function onMobDespawn(mob)
-    tpz.mob.phOnDespawn(mob, ID.mob.BLACKBEARD_PH, 100, 3600) -- 1 hour
+    tpz.mob.phOnDespawn(mob, ID.mob.BLACKBEARD_PH, 20, 3600) -- 1 hour
 end
