ee1ee049b3f2387fc348e4e87c8a90bb3185f3bf
Shiyo <strikinginsanity@hotmail.com>
2022-04-22 17:57:01 -0400

    (MNK) Revert Counterstance back to era
    -50% defense, no +enmity

--- scripts/globals/effects/counterstance.lua
+++ scripts/globals/effects/counterstance.lua
@@ -8,7 +8,6 @@ require("scripts/globals/status")
 function onEffectGain(target, effect)
     local power = effect:getPower()
     target:addMod(tpz.mod.COUNTER, effect:getPower())
-    target:addMod(tpz.mod.ENMITY, effect:getPower())
     target:addMod(tpz.mod.ATTP, -power)
     target:addMod(tpz.mod.RATTP, -power)
 end
@@ -19,7 +18,6 @@ end
 function onEffectLose(target, effect)
     local power = effect:getPower()
     target:delMod(tpz.mod.COUNTER, effect:getPower())
-    target:delMod(tpz.mod.ENMITY, effect:getPower())
     target:delMod(tpz.mod.ATTP, -power)
     target:delMod(tpz.mod.RATTP, -power)
 end
