bd16d53aa023804409339e255b335e13224c6bd2
Shiyo <strikinginsanity@hotmail.com>
2022-04-22 22:08:06 -0400

    (BLU) Saline Coat adjusted
    Now
    
    +20 MDB.
     3m duration
     3m CD

--- scripts/globals/spells/bluemagic/saline_coat.lua
+++ scripts/globals/spells/bluemagic/saline_coat.lua
@@ -25,8 +25,8 @@ end
 
 function onSpellCast(caster, target, spell)
     local typeEffect = tpz.effect.MAGIC_DEF_BOOST
-    local power = 100
-    local duration = 30
+    local power = 20
+    local duration = 180
 
     if (caster:hasStatusEffect(tpz.effect.DIFFUSION)) then
         local diffMerit = caster:getMerit(tpz.merit.DIFFUSION)
