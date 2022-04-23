1aea33d32bc7c18118a5f5f6a16a8a9202664d4b
Shiyo <strikinginsanity@hotmail.com>
2022-04-23 00:55:08 -0400

    Body slam adjustments
    Should remove 2-3 shadows not 1

--- scripts/globals/mobskills/pl_body_slam.lua
+++ scripts/globals/mobskills/pl_body_slam.lua
@@ -22,9 +22,9 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local numhits = 1
     local accmod = 1
-    local dmgmod = 1
+    local dmgmod = 3
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_DMG_VARIES, 1, 2, 3)
-    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT, info.hitslanded)
+    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT, info.hitslanded*math.random(2, 3))
 
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT)
     return dmg
