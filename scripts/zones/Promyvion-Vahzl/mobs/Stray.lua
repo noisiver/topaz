39d60140bcdd564047ea416a1df6c98517eaf912
Shiyo <strikinginsanity@hotmail.com>
2022-04-27 19:35:48 -0400

    Promy Adjustments
    Strays / Memory Receptacles no longer give exp. gil, or drops

--- scripts/zones/Promyvion-Vahzl/mobs/Stray.lua
+++ scripts/zones/Promyvion-Vahzl/mobs/Stray.lua
@@ -5,12 +5,25 @@
 require("scripts/globals/promyvion")
 -----------------------------------
 function onMobRoam(mob)
+    mob:setMobMod(tpz.mobMod.EXP_BONUS, -100)
+    mob:setMobMod(tpz.mobMod.GIL_MAX, 0)
+    mob:setMobMod(tpz.mobMod.GIL_MIN, 0)
+    mob:setMobMod(tpz.mobMod.GIL_BONUS, 0)
+    mob:setMobMod(tpz.mobMod.NO_DROPS, 1)
     tpz.promyvion.strayOnRoam(mob)
 end
 
 function onMobSpawn(mob)
+    mob:setMobMod(tpz.mobMod.EXP_BONUS, -100)
+    mob:setMobMod(tpz.mobMod.GIL_MAX, 0)
+    mob:setMobMod(tpz.mobMod.GIL_MIN, 0)
+    mob:setMobMod(tpz.mobMod.GIL_BONUS, 0)
+    mob:setMobMod(tpz.mobMod.NO_DROPS, 1)
     tpz.promyvion.strayOnSpawn(mob)
 	tpz.promyvion.setEmptyModel(mob)
+end
+
+function onMobFight(mob, target)
 end
 
 function onMobDeath(mob, player, isKiller)
