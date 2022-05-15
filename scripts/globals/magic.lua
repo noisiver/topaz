dcbad20d64eb132fd886e043ee49cad60f485f76
Shiyo <strikinginsanity@hotmail.com>
2022-05-15 06:38:17 -0400

    Remove magic acc print

--- scripts/globals/magic.lua
+++ scripts/globals/magic.lua
@@ -742,7 +742,7 @@ function calculateMagicHitRate(magicacc, magiceva, percentBonus, casterLvl, targ
     
     magicacc = magicacc + (casterLvl - targetLvl)*4
     local dMAcc = magicacc - magiceva
-    print(string.format("magicacc = %u, magiceva = %u",magicacc,magiceva))
+    --print(string.format("magicacc = %u, magiceva = %u",magicacc,magiceva))
     if dMAcc < 0 then -- when penalty, half effective
         p = 50 + math.floor(dMAcc/2)
     else
