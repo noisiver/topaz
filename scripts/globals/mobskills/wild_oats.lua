4b28a6e757c8f5f92247343db5d13a76cbaf2f53
Shiyo <strikinginsanity@hotmail.com>
2022-05-16 04:42:03 -0400

    Wild oats message fix

--- scripts/globals/mobskills/wild_oats.lua
+++ scripts/globals/mobskills/wild_oats.lua
@@ -27,7 +27,7 @@ function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.VIT_DOWN
     local power = (target:getStat(tpz.mod.VIT) * 0.2) +5
 
-    skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, power, 3, 300))
+    MobStatusEffectMove(mob, target, typeEffect, power, 3, 300)
 
-    return tpz.effect.VIT_DOWN
+    return dmg
 end
