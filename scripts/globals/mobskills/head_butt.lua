5d17f4318f4fa72c4ba0da67183d34a33eeed9da
Shiyo <strikinginsanity@hotmail.com>
2022-05-19 20:29:21 -0400

    TP Moves G-H

--- scripts/globals/mobskills/head_butt.lua
+++ scripts/globals/mobskills/head_butt.lua
@@ -21,15 +21,39 @@ function onMobWeaponSkill(target, mob, skill)
     local dmgmod = 2
     if mob:getFamily() == 178 then -- Mandragora, monks so -50% damage and need doubled dmgmod
         dmgmod = 3
+        local params_phys = {}
+        params_phys.multiplier = dmgmod
+        params_phys.tp150 = 1
+        params_phys.tp300 = 1
+        params_phys.str_wsc = 0.2
+        params_phys.dex_wsc = 0.0
+        params_phys.vit_wsc = 0.2
+        params_phys.agi_wsc = 0.0
+        params_phys.int_wsc = 0.0
+        params_phys.mnd_wsc = 0.0
+        params_phys.chr_wsc = 0.0
     end
-    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_DMG_VARIES, 1, 2, 3)
+
+    if mob:isInDynamis() or mob:isNM() then  
+        dmgmod = 3
+        local params_phys = {}
+        params_phys.multiplier = dmgmod
+        params_phys.tp150 = 1
+        params_phys.tp300 = 1
+        params_phys.str_wsc = 0.2
+        params_phys.dex_wsc = 0.0
+        params_phys.vit_wsc = 0.2
+        params_phys.agi_wsc = 0.0
+        params_phys.int_wsc = 0.0
+        params_phys.mnd_wsc = 0.0
+        params_phys.chr_wsc = 0.0
+    end
+
+    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_DMG_VARIES, params_phys, 2, 3)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT, info.hitslanded)
 
     local typeEffect = tpz.effect.STUN
 
-    if mob:isInDynamis() or mob:isNM() then  
-        dmgmod = 3
-    end
 
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT)
     MobPhysicalStatusEffectMove(mob, target, skill, typeEffect, 1, 0, 4)
--- scripts/globals/mobskills/head_butt_turtle.lua
+++ scripts/globals/mobskills/head_butt_turtle.lua
@@ -18,7 +18,18 @@ function onMobWeaponSkill(target, mob, skill)
     local numhits = 1
     local accmod = 1
     local dmgmod = 2
-    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT)
+    local params_phys = {}
+    params_phys.multiplier = dmgmod
+    params_phys.tp150 = 1
+    params_phys.tp300 = 1
+    params_phys.str_wsc = 0.2
+    params_phys.dex_wsc = 0.0
+    params_phys.vit_wsc = 0.2
+    params_phys.agi_wsc = 0.0
+    params_phys.int_wsc = 0.0
+    params_phys.mnd_wsc = 0.0
+    params_phys.chr_wsc = 0.0
+    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT, params_phys)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT, info.hitslanded)
 
     -- Large Knockdown ----------------------------
