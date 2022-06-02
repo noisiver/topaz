605ae3738fa47b4c9db1874b61090bde1d286060
Shiyo <strikinginsanity@hotmail.com>
2022-06-02 07:34:37 -0400

    Implement "The Prankster" quest
    Caedarva Mire map quest

--- scripts/zones/Aht_Urhgan_Whitegate/npcs/Ahaadah.lua
+++ scripts/zones/Aht_Urhgan_Whitegate/npcs/Ahaadah.lua
@@ -8,11 +8,25 @@ function onTrade(player, npc, trade)
 end
 
 function onTrigger(player, npc)
-    player:startEvent(870)
+    if (player:getQuestStatus(AHT_URHGAN, tpz.quest.id.ahtUrhgan.THE_PRANKSTER) == QUEST_AVAILABLE) then
+        player:startEvent(1)
+    elseif (player:getCharVar("ThePrankster") == 1) then
+        player:startEvent(15)
+    elseif (player:getCharVar("ThePrankster") == 2) then
+        player:startEvent(16)
+    elseif (player:getCharVar("ThePrankster") == 4) then
+        player:startEvent(17)
+    else
+        player:startEvent(870)
+    end
 end
 
 function onEventUpdate(player, csid, option)
 end
 
 function onEventFinish(player, csid, option)
+    if (csid == 1) then
+        player:addQuest(AHT_URHGAN, tpz.quest.id.ahtUrhgan.THE_PRANKSTER)
+        player:setCharVar("ThePrankster", 1)
+    end
 end
---
+++ scripts/zones/Bhaflau_Thickets/mobs/Plague_Chigoe.lua
@@ -0,0 +1,44 @@
+-----------------------------------
+-- Area: Bhaflau Thickets
+--  MOB: Plague Chigoe
+-----------------------------------
+require("scripts/globals/utils")
+require("scripts/globals/mobs")
+require("scripts/globals/status")
+-----------------------------------
+
+function onMobInitialize(mob)
+    mob:setMobMod(tpz.mobMod.IDLE_DESPAWN, 180)
+end
+
+function onMobSpawn(mob)
+	mob:setDamage(25)
+    mob:setMod(tpz.mod.ATTP, 50)
+    mob:setMod(tpz.mod.DEFP, 25)
+    mob:setMod(tpz.mod.TRIPLE_ATTACK, 75)
+    mob:setMobMod(tpz.mobMod.ADD_EFFECT, 1)
+    mob:setMobMod(tpz.mobMod.HP_STANDBACK, -1)
+    mob:setMobMod(tpz.mobMod.EXP_BONUS, -100)
+    mob:setMobMod(tpz.mobMod.NO_DROPS, 1)
+    mob:setMobMod(tpz.mobMod.GIL_MIN, 0)
+    mob:setMobMod(tpz.mobMod.GIL_MAX, 0) 
+    mob:setMobMod(tpz.mobMod.GIL_BONUS, 0)
+    mob:hideName(false)
+    mob:untargetable(false)
+end
+
+function onMobDeath(mob, player, isKiller)
+    if (player:getCharVar("ThePrankster") == 3) then
+        player:setCharVar("ThePrankster", 4)
+    end
+end
+
+function onAdditionalEffect(mob, target, damage)
+        local amount = 0
+        -- Drains 1% MP per hit
+        amount = target:getMaxMP() * 0.01
+        if amount > target:getMP() then
+            amount = target:getMP()
+        end
+    return tpz.mob.onAddEffect(mob, target, damage, tpz.mob.ae.MP_DRAIN, {chance = 100, power = amount})
+end
