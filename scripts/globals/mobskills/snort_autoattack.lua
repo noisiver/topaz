24692342f21490741cc822d07e84a53624889472
Shiyo <strikinginsanity@hotmail.com>
2022-06-04 08:51:20 -0400

    Geush Urvan adjustments
    Model Size increased to biggest.
    Auto-attack damage re-adjusted back to what it was at before the mob magic move formula revamp.

--- scripts/globals/mobskills/snort_autoattack.lua
+++ scripts/globals/mobskills/snort_autoattack.lua
@@ -19,7 +19,7 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local accmod = 1
-    local dmgmod = 2.7
+    local dmgmod = 9
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.WIND, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.WIND, MOBPARAM_IGNORE_SHADOWS)
 
