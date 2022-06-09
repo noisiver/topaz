106cf23a90a6569dc9bc50401d0ce602e4d97dd4
Shiyo <strikinginsanity@hotmail.com>
2022-06-08 20:42:20 -0400

    TP move Tuning

--- scripts/globals/mobskills/charged_whisker.lua
+++ scripts/globals/mobskills/charged_whisker.lua
@@ -12,7 +12,7 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local dmgmod = 2
+    local dmgmod = 4.5
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.THUNDER, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.LIGHTNING, MOBPARAM_WIPE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.LIGHTNING)
