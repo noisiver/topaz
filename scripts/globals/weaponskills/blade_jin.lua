14d1aa8709de8ce9b7ff3c2c5efe23f55a2b1455
Shiyo <strikinginsanity@hotmail.com>
2022-06-17 03:58:29 -0400

    Ninja DPS adjustments
    Daken reduced from 15/20%(level 30 and 70) to 7/10%
    
    Blade: Jin no longer fTP transfers and changed to 1.5 fTP, 4.5 total. Down from 5.32 total.

--- scripts/globals/weaponskills/blade_jin.lua
+++ scripts/globals/weaponskills/blade_jin.lua
@@ -20,13 +20,12 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
 
     local params = {}
     params.numHits = 3
-    params.ftp100 = 1.33 params.ftp200 = 1.33 params.ftp300 = 1.33
+    params.ftp100 = 1.5 params.ftp200 = 1.5 params.ftp300 = 1.5
     params.str_wsc = 0.3 params.dex_wsc = 0.3 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.15 params.crit200 = 0.3 params.crit300 = 0.5
     params.canCrit = true
     params.acc100 = 0.0 params.acc200= 0.0 params.acc300= 0.0
     params.atk100 = 1; params.atk200 = 1; params.atk300 = 1
-    params.multiHitfTP = true
 
     if (USE_ADOULIN_WEAPON_SKILL_CHANGES == true) then
         params.ftp100 = 1.375 params.ftp200 = 1.375 params.ftp300 = 1.375
