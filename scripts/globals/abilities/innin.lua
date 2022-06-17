a28fe3f057f319b8aa24fa2d4e9066d828d654d4
Shiyo <strikinginsanity@hotmail.com>
2022-06-17 02:16:57 -0400

    Ninja DPS adjustments
    Blade Jin changed to 30% STR 30% DEX WSC(Was 50% DEX)
    Innin changed to +15% Critical Hit Rate, +15 Acc, -15 Enmity (Was +30% crit +30 acc -20 enmity)

--- scripts/globals/abilities/innin.lua
+++ scripts/globals/abilities/innin.lua
@@ -1,6 +1,7 @@
 -----------------------------------
 -- Ability: Innin
 -- Reduces enmity and impairs evasion. Grants a bonus to accuracy, critical hit rate, and ninjutsu damage when striking target from behind.
+-- +15% Critical Hit Rate, +15 Acc, -15 Enmity
 -- Obtained: Ninja Level 40
 -- Recast Time: 3:00
 -- Duration: 5:00
@@ -17,5 +18,5 @@ function onUseAbility(player, target, ability)
     target:delStatusEffectSilent(tpz.effect.INNIN)
     target:delStatusEffectSilent(tpz.effect.YONIN)
    -- target:addStatusEffect(tpz.effect.INNIN, 30, 15, 300, 0, 20)
-    target:addStatusEffect(tpz.effect.INNIN, math.floor(player:getMainLvl() / 4) + 2, 15, 300, 0, math.floor(player:getMainLvl() / 3) + 5) -- 30(get power), 15(MAB), 300(duration), 0, 20(get sub power)
+    target:addStatusEffect(tpz.effect.INNIN, math.floor(player:getMainLvl() / 5), 15, 300, 0, math.floor(player:getMainLvl() / 5)) -- 30(get power), 15(MAB), 300(duration), 0, 20(get sub power)
 end
