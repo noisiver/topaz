0a49e3b910f8b83b47c52f03aba118580d00e1e4
Shiyo <strikinginsanity@hotmail.com>
2022-06-17 11:40:39 -0400

    Remove OnSpikessDamage
    No mobs spikes uses this function anymore. Now uses C++ damage formula for spikes.

--- scripts/zones/Beaucedine_Glacier/mobs/Humbaba.lua
+++ scripts/zones/Beaucedine_Glacier/mobs/Humbaba.lua
@@ -18,29 +18,6 @@ function onMobSpawn(mob)
     mob:setMod(tpz.mod.DOUBLE_ATTACK, 25)
 end
 
-function onSpikesDamage(mob, target, damage)
-    local INT_diff = mob:getStat(tpz.mod.INT) - target:getStat(tpz.mod.INT)
-
-    if INT_diff > 20 then
-        INT_diff = 20 + (INT_diff - 20) * 0.5 -- INT above 20 is half as effective.
-    end
-
-    local dmg = (damage + INT_diff) * 0.5 -- INT adjustment and base damage averaged together.
-    local params = {}
-    params.bonusmab = 0
-    params.includemab = false
-    dmg = addBonusesAbility(mob, tpz.magic.ele.ICE, target, dmg, params)
-    dmg = dmg * applyResistanceAddEffect(mob, target, tpz.magic.ele.ICE, 0)
-    dmg = adjustForTarget(target, dmg, tpz.magic.ele.ICE)
-    dmg = finalMagicNonSpellAdjustments(mob, target, tpz.magic.ele.ICE, dmg)
-
-    if dmg < 0 then
-        dmg = 0
-    end
-
-    return tpz.subEffect.ICE_SPIKES, tpz.msg.basic.SPIKES_EFFECT_DMG, dmg
-end
-
 function onMobDeath(mob, player, isKiller)
     tpz.hunts.checkHunt(mob, player, 314)
 end
--- scripts/zones/RoMaeve/mobs/Martinet.lua
+++ scripts/zones/RoMaeve/mobs/Martinet.lua
@@ -14,30 +14,6 @@ function onMobInitialize(mob)
     mob:getStatusEffect(tpz.effect.SHOCK_SPIKES):setFlag(tpz.effectFlag.DEATH)
 end
 
-function onSpikesDamage(mob, target, damage)
-    local INT_diff = mob:getStat(tpz.mod.INT) - target:getStat(tpz.mod.INT)
-
-    if INT_diff > 20 then
-        INT_diff = 20 + (INT_diff - 20) * 0.5 -- INT above 20 is half as effective.
-    end
-
-    local dmg = (damage + INT_diff) * 0.5 -- INT adjustment and base damage averaged together.
-    local params = {}
-    params.bonusmab = 0
-    params.includemab = false
-    dmg = addBonusesAbility(mob, tpz.magic.ele.THUNDER, target, dmg, params)
-    dmg = dmg * applyResistanceAddEffect(mob, target, tpz.magic.ele.THUNDER, 0)
-    dmg = adjustForTarget(target, dmg, tpz.magic.ele.THUNDER)
-    dmg = finalMagicNonSpellAdjustments(mob, target, tpz.magic.ele.THUNDER, dmg)
-
-    if dmg < 0 then
-        dmg = 0
-    end
-
-    return tpz.subEffect.SHOCK_SPIKES, tpz.msg.basic.SPIKES_EFFECT_DMG, dmg
-
-end
-
 function onMobDeath(mob, player, isKiller)
     tpz.hunts.checkHunt(mob, player, 329)
 end
