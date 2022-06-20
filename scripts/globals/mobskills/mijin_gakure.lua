23b288f2a9c2b3fb3bbefdad196a076472854226
Shiyo <strikinginsanity@hotmail.com>
2022-06-20 14:48:42 -0400

    Mijin Gakure adjustments
    Updated dmg to the new formula, copying bomb toss suicides damage.

--- scripts/globals/mobskills/mijin_gakure.lua
+++ scripts/globals/mobskills/mijin_gakure.lua
@@ -9,27 +9,26 @@ function onMobSkillCheck(target, mob, skill)
     return 0
 end
 
+
 function onMobWeaponSkill(target, mob, skill)
-    local dmgmod = 1,5
-    local hpmod = skill:getMobHPP() / 100
-    local basePower = (mob:getFamily() == 335) and 4 or 6 -- Maat has a weaker (4) Mijin than usual (6)
-    local power = hpmod * 1.3 + basePower -- changed from *10
-    local baseDmg = mob:getWeaponDmg() * power
-    local info = MobMagicalMove(mob, target, skill, baseDmg, tpz.magic.ele.NONE, dmgmod, TP_MAB_BONUS, 1)
+    local dmgmod = 12
+    local BOMB_TOSS_HPP = skill:getMobHPP() / 100
+    dmgmod = math.floor(dmgmod * BOMB_TOSS_HPP)
+    if dmgmod < 1 then dmgmod = 1 end
+
+    local job = mob:getMainJob()
+    local power = 12
+
+    local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*power*BOMB_TOSS_HPP, tpz.magic.ele.NONE, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.ELEMENTAL, MOBPARAM_IGNORE_SHADOWS)
-	
+
     if target:hasStatusEffect(tpz.effect.FEALTY) then
         dmg = math.floor(dmg / 16)
     end
 
     if dmg > 1250 then dmg = 1250 end -- Don't do silly damage if the mob has high HP
 
-
---    if mob:isInDynamis() then -- dynamis mobs will kill themselves, other mobs might not
---        mob:setHP(0)
---    end
-
+    mob:setHP(0)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.ELEMENTAL)
-
     return dmg
 end
