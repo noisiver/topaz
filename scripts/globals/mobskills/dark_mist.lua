032a21010d22b6c7e0521a78e7f32b8fda99bfe2
Shiyo <strikinginsanity@hotmail.com>
2022-06-20 03:34:57 -0400

    Misc Magical TP move tuning

--- scripts/globals/mobskills/dark_mist.lua
+++ scripts/globals/mobskills/dark_mist.lua
@@ -21,14 +21,13 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local numhits = 1
-    local dmgmod = 1.5
+    local dmgmod = 5
     local typeEffect = tpz.effect.WEIGHT
 
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.DARK, dmgmod, TP_MAB_BONUS)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.DARK, MOBPARAM_WIPE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.DARK)
-    MobStatusEffectMove(mob, target, typeEffect, 50, 0, 300)
+    MobStatusEffectMove(mob, target, typeEffect, 25, 0, 300)
 
     return dmg
 end
