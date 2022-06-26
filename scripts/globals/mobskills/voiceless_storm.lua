d3027040332f24ca6a02a9a75a5b40cb7d9c2c00
Shiyo <strikinginsanity@hotmail.com>
2022-06-26 08:21:11 -0400

    Phuabo TP move tuning
    Reduced the fTP of their magical TP moves

--- scripts/globals/mobskills/voiceless_storm.lua
+++ scripts/globals/mobskills/voiceless_storm.lua
@@ -1,6 +1,7 @@
 ---------------------------------------------
 --  Voiceless Storm
---  Description: AOE Damage, Silence, strips Utsusemi (MOBPARAM_WIPE_SHADOWS)
+--  Description: AOE Damage, Silence.
+--  Wipes shadows
 ---------------------------------------------
 require("scripts/globals/monstertpmoves")
 require("scripts/globals/settings")
@@ -13,7 +14,7 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.SILENCE
-    local dmgmod = 2.5
+    local dmgmod = 1
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg() * 6, tpz.magic.ele.WATER, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.WATER, MOBPARAM_WIPE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.WATER)
