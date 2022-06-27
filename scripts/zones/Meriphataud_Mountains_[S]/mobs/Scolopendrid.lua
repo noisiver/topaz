cb504408fd21576cc065cbe45f5f5e5d4c1e02af
Shiyo <strikinginsanity@hotmail.com>
2022-06-27 19:15:40 -0400

    Implement "Gold Scorpion" mixin

--- scripts/zones/Meriphataud_Mountains_[S]/mobs/Scolopendrid.lua
+++ scripts/zones/Meriphataud_Mountains_[S]/mobs/Scolopendrid.lua
@@ -1,7 +1,6 @@
 -----------------------------------
 -- Area: Meriphataud Mountains [S]
---  Mob: Mountain Scolopendrid
--- Note: PH for Centipedal Centruroides
+--  Mob: Scolopendrid
 -----------------------------------
 local ID = require("scripts/zones/Meriphataud_Mountains_[S]/IDs")
 require("scripts/globals/mobs")
@@ -12,5 +11,4 @@ function onMobDeath(mob, player, isKiller)
 end
 
 function onMobDespawn(mob)
-    tpz.mob.phOnDespawn(mob, ID.mob.CENTIPEDAL_CENTRUROIDES_PH, 20, 7200) -- 2 hours
 end
