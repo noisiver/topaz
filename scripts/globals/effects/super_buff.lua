fd3dd59ed8172c4cc05e1a45e083aa5bdeb2de17
Shiyo <strikinginsanity@hotmail.com>
2022-06-27 14:29:14 -0400

    Nidhogg adjustments
    Will no longer use TP or gain random incorrect stats when using his 2 hour "cloud" ability

--- scripts/globals/effects/super_buff.lua
+++ scripts/globals/effects/super_buff.lua
@@ -8,12 +8,6 @@ require("scripts/globals/status")
 
 function onEffectGain(target, effect)
     local power = effect:getPower()
-    target:addMod(tpz.mod.ATTP, power)
-    target:addMod(tpz.mod.DEFP, power)
-    target:addMod(tpz.mod.MATT, power)
-    target:addMod(tpz.mod.MEVA, power)
-    -- The following only applies to Nidhogg.  If this buff is to be used anywhere else, a check on mob name (NOT ID) would be a good choice
-    target:AnimationSub(2)
 end
 
 function onEffectTick(target, effect)
@@ -21,9 +15,4 @@ end
 
 function onEffectLose(target, effect)
     local power = effect:getPower()
-    target:delMod(tpz.mod.ATTP, power)
-    target:delMod(tpz.mod.DEFP, power)
-    target:delMod(tpz.mod.MATT, power)
-    target:delMod(tpz.mod.MEVA, power)
-    target:AnimationSub(0)
 end
