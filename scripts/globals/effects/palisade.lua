b2ddda763e487bce5743914d651bd2b5edab2478
Shiyo <strikinginsanity@hotmail.com>
2022-06-28 20:20:48 -0400

    Palisade revamp
    Palisade now causes all enemies around you unable to move, while also causing you unable to move.

--- scripts/globals/effects/palisade.lua
+++ scripts/globals/effects/palisade.lua
@@ -2,10 +2,21 @@
 --
 --     tpz.effect.PALISADE
 --     +100% defense for 1 minute
+--  Removes movement from user and all enemy targets in range
 -----------------------------------
 function onEffectGain(target, effect)
 	target:addMod(tpz.mod.DEFP, 100)
     target:addMod(tpz.mod.ENMITY_LOSS_REDUCTION, 100)
+    effect:setPower(target:speed())
+    target:speed(0)
+    local entities = target:getNearbyMobs(12)
+    local mobNearby = false;
+    for i, entity in pairs(entities) do
+        if entity:getAggressive() == 1 and target:getID() ~= entity:getID() and entity:isSpawned() then
+            mobNearby = true;
+            entity:setMobMod(tpz.mobMod.NO_MOVE, 1)
+		end
+	end
 end
 
 function onEffectTick(target, effect)
@@ -14,4 +25,13 @@ end
 function onEffectLose(target, effect)
     target:delMod(tpz.mod.DEFP, 100)
     target:delMod(tpz.mod.ENMITY_LOSS_REDUCTION, 100)
+    target:speed(effect:getPower())
+    local entities = target:getNearbyMobs(12)
+    local mobNearby = false;
+    for i, entity in pairs(entities) do
+        if entity:getAggressive() == 1 and target:getID() ~= entity:getID() and entity:isSpawned() then
+            mobNearby = true;
+            entity:setMobMod(tpz.mobMod.NO_MOVE, 0)
+		end
+	end
 end
