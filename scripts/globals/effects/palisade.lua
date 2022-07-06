daa2ee87a3f5e139164c8738d858f2b35f1bbab0
Shiyo <strikinginsanity@hotmail.com>
2022-07-06 18:33:39 -0400

    Palisade / Bind bugfix
    Palisade and bind will no longer make a character unable to move if both debuffs are applied to them.

--- scripts/globals/effects/palisade.lua
+++ scripts/globals/effects/palisade.lua
@@ -4,11 +4,15 @@
 --     +100% defense for 1 minute
 --  Removes movement from user and all enemy targets in range
 -----------------------------------
+require("scripts/globals/status")
+-----------------------------------
 function onEffectGain(target, effect)
 	target:addMod(tpz.mod.DEFP, 100)
     target:addMod(tpz.mod.ENMITY_LOSS_REDUCTION, 100)
-    effect:setPower(target:speed())
-    target:speed(0)
+    if not target:hasStatusEffect(tpz.effect.BIND) then
+        effect:setPower(target:speed())
+        target:speed(0)
+    end
     local entities = target:getNearbyMobs(12)
     local mobNearby = false;
     for i, entity in pairs(entities) do
@@ -25,7 +29,9 @@ end
 function onEffectLose(target, effect)
     target:delMod(tpz.mod.DEFP, 100)
     target:delMod(tpz.mod.ENMITY_LOSS_REDUCTION, 100)
-    target:speed(effect:getPower())
+    if not target:hasStatusEffect(tpz.effect.BIND) then
+        target:speed(effect:getPower())
+    end
     local entities = target:getNearbyMobs(12)
     local mobNearby = false;
     for i, entity in pairs(entities) do
