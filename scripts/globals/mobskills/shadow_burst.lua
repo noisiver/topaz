2b94154cff228ee91d6cb2eaa6f9ca195bcb6d1e
Shiyo <strikinginsanity@hotmail.com>
2022-07-09 17:03:16 -0400

    "Big" Magical TP moves adjustments
    Updated to new formula by increasing fTPs to 9(was 7)

--- scripts/globals/mobskills/shadow_burst.lua
+++ scripts/globals/mobskills/shadow_burst.lua
@@ -21,7 +21,7 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
 local typeEffect = tpz.effect.CURSE_I
-    local dmgmod = 7
+    local dmgmod = 9
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg() * 3, tpz.magic.ele.DARK, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.DARK, MOBPARAM_WIPE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.DARK)
