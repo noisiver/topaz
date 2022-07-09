29b8857cd6d9d72ea433efa035a41bb13e4969f8
Shiyo <strikinginsanity@hotmail.com>
2022-07-09 17:04:23 -0400

    TP move bugfix
    Now properly track number of hits for determinig interrupts

--- scripts/globals/mobskills/oisoya.lua
+++ scripts/globals/mobskills/oisoya.lua
@@ -36,6 +36,6 @@ function onMobWeaponSkill(target, mob, skill)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.RANGED, tpz.damageType.BLUNT, MOBPARAM_IGNORE_SHADOWS)
 
     target:takeDamage(dmg, mob, tpz.attackType.RANGED, tpz.damageType.BLUNT)
-	if ((skill:getMsg() ~= tpz.msg.basic.SHADOW_ABSORB) and (dmg > 0)) then   target:tryInterruptSpell(mob, 1) end
+	if ((skill:getMsg() ~= tpz.msg.basic.SHADOW_ABSORB) and (dmg > 0)) then   target:tryInterruptSpell(mob, info.hitslanded) end
     return dmg
 end
