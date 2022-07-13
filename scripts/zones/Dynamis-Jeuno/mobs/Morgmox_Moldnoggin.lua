33e13b6bc811349bc9c117d8582f1f5653d2d8a4
Shiyo <strikinginsanity@hotmail.com>
2022-07-13 17:49:11 -0400

    Dynamis bugfix
    Ensure pet is always spawned

--- scripts/zones/Dynamis-Jeuno/mobs/Morgmox_Moldnoggin.lua
+++ scripts/zones/Dynamis-Jeuno/mobs/Morgmox_Moldnoggin.lua
@@ -8,6 +8,9 @@ mixins =
     require("scripts/mixins/job_special")
 }
 -----------------------------------
+function onMobEngaged(mob, target)
+	mob:spawnPet()
+end
 
 function onMobDeath(mob, player, isKiller)
 end
