8d18373035e6f1f88b7d1202322869e8562a10d4
Shiyo <strikinginsanity@hotmail.com>
2022-07-21 20:12:36 -0400

    Euvhi mixin adjustments part2
    Will now randomly open/close while roaming.
    
    Now uses TP moves based on if it's open or closed.
    
    Corrected damage type and potency of some of their TP moves
    
    Now gain increased attack speed when bloomed, and increased defense when a bud.

--- scripts/mixins/families/euvhi.lua
+++ scripts/mixins/families/euvhi.lua
@@ -5,54 +5,64 @@ g_mixins.families = g_mixins.families or {}
 
 function close_form(mob)
     mob:setMobMod(tpz.mobMod.LINK_RADIUS, 11)
-    mob:setMod(tpz.mod.DMG, -25) 
-    mob:setMobMod(tpz.mobMod.NO_ROAM, 0)
+    mob:setMod(tpz.mod.DEFP, 100) 
+    mob:setMod(tpz.mod.DMG, -25)
+    mob:setMod(tpz.mod.HASTE_GEAR, 0)
+    mob:setMod(tpz.mod.HASTE_ABILITY, 0)
+    mob:setMobMod(tpz.mobMod.NO_MOVE, 0)
     mob:addMobMod(tpz.mobMod.ROAM_DISTANCE, 5)
     mob:addMobMod(tpz.mobMod.ROAM_COOL, 30)
-    mob:setLocalVar("damageThreshold", 0)
     mob:AnimationSub(4)
 end
 
 function open_form(mob)
-    mob:delMobMod(tpz.mobMod.ROAM_DISTANCE, 5)
-    mob:delMobMod(tpz.mobMod.ROAM_COOL, 30)
-    mob:setMobMod(tpz.mobMod.NO_ROAM, 1)
+    mob:setMod(tpz.mod.DMG, 12.5)
+    mob:setMod(tpz.mod.HASTE_GEAR, 2500)
+    mob:setMod(tpz.mod.HASTE_ABILITY, 2500) 
+    mob:setMod(tpz.mod.DEFP, 0)
+    mob:setMobMod(tpz.mobMod.NO_MOVE, 1)
     mob:setMobMod(tpz.mobMod.LINK_RADIUS, 0)
     mob:setMobMod(tpz.mobMod.SOUND_RANGE, 11)
-    mob:setMod(tpz.mod.DMG, 12.5) 
-    mob:setLocalVar("[euvhi]closeMouthHP", mob:getHP() - math.ceil(mob:getMaxHP() / 3))
     mob:AnimationSub(6)
 end
 
 g_mixins.families.euvhi = function(mob)
-
     mob:addListener("SPAWN", "EUVHI_SPAWN", function(mob)
-        mob:AnimationSub(4)
+        mob:setLocalVar("changeTime", os.time() + math.random(5, 10))
+    end)
+
+    mob:addListener("ROAM_TICK", "EUVHI_ROAM", function(mob)
+        local changeTime = mob:getLocalVar("changeTime")
+        -- Open if time expired
+        if os.time() >= changeTime then
+            if mob:AnimationSub() == 4 then
+                open_form(mob)
+                mob:setLocalVar("changeTime", os.time() + math.random(60, 90))
+            elseif mob:AnimationSub() == 6 then
+                close_form(mob) 
+                mob:setLocalVar("changeTime", os.time() + math.random(60, 90))
+            end
+        end
     end)
 
     mob:addListener("ENGAGE", "EUVHI_ENGAGE", function(mob, target)
-    mob:setLocalVar("changeTime", os.time() + math.random(50, 80))
-        if mob:AnimationSub() == 6 then
-            mob:AnimationSub(4)
-        end
+        mob:setMobMod(tpz.mobMod.NO_MOVE, 0)
     end)
 
     mob:addListener("DISENGAGE", "EUVHI_DISENGAGE", function(mob)
-        if mob:AnimationSub() == 4 then
-            mob:AnimationSub(6)
-        end
     end)
 
 
     mob:addListener("COMBAT_TICK", "EUVHI_CTICK", function(mob)
         local changeTime = mob:getLocalVar("changeTime")
-        -- Open if time expired, does not go back to closed unless it is crit hit 
-        if os.time() >= changeTime and mob:getCurrentAction() == tpz.act.ATTACK then
+        -- Open if time expired
+        if os.time() >= changeTime then
             if mob:AnimationSub() == 4 then
                 open_form(mob)
-            elseif mob:AnimationSub() == 6 and mob:getHP() <  mob:getLocalVar("[euvhi]closeMouthHP") then
+                mob:setLocalVar("changeTime", os.time() + math.random(30, 40))
+            elseif mob:AnimationSub() == 6 then
                 close_form(mob) 
-                mob:setLocalVar("changeTime", os.time() + math.random(50, 80))
+                mob:setLocalVar("changeTime", os.time() + math.random(30, 40))
             end
         end
     end)
