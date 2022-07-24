4f83652c3b3647afc734751ff39eb081cc145e81
Shiyo <strikinginsanity@hotmail.com>
2022-07-24 18:57:34 -0400

    TP move adjustments
    Increased the duration of Sound Vacuum to 5m

--- scripts/globals/mobskills/sound_vacuum.lua
+++ scripts/globals/mobskills/sound_vacuum.lua
@@ -19,7 +19,7 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.SILENCE
 
-    skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, 1, 0, 45))
+    skill:setMsg(MobStatusEffectMove(mob, target, typeEffect, 1, 0, 300))
 
     return typeEffect
 end
