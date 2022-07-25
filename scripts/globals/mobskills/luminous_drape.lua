2537599ba01bad10f84ece744efed69b3b0d13c1
Shiyo <strikinginsanity@hotmail.com>
2022-07-25 19:49:27 -0400

    TP move adjustments
    Reduced Luminous Drape duration to 60s(was 120s)

--- scripts/globals/mobskills/luminous_drape.lua
+++ scripts/globals/mobskills/luminous_drape.lua
@@ -19,7 +19,7 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
 
-    MobCharmMove(mob, target, skill, 0, 120)
+    MobCharmMove(mob, target, skill, 0, 60)
 
     return tpz.effect.CHARM_I
 end
