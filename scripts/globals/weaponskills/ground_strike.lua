0448917660abae375c8a47cd0e677b44c2982b6c
Shiyo <strikinginsanity@hotmail.com>
2022-07-29 19:27:37 -0400

    WS update again
    Swapped  reduced dmg at 1k, greatly increased at 2k+ with:
    
    Spinning Slash > Sickle Moon
    Impulse Drive > Wheeling Thrust
    Spiral
    Spiral Hell > Cross Reaper

--- scripts/globals/weaponskills/ground_strike.lua
+++ scripts/globals/weaponskills/ground_strike.lua
@@ -15,7 +15,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
 
     local params = {}
     params.numHits = 1
-    params.ftp100 = 1.5 params.ftp200 = 4.0 params.ftp300 = 4.5
+    params.ftp100 = 2.5 params.ftp200 = 2.7 params.ftp300 = 3.0
     params.str_wsc = 0.5 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.5 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
