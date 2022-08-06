12659a38291afe6c20f003d38d33e4ae4ceb37d9
Shiyo <strikinginsanity@hotmail.com>
2022-08-06 10:56:41 -0400

    TP move tuning
    Reduced the fTP of Hex Palm to 5(was 7)

--- scripts/globals/mobskills/hex_palm.lua
+++ scripts/globals/mobskills/hex_palm.lua
@@ -21,7 +21,7 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local dmgmod = 7
+    local dmgmod = 5
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*1.5, tpz.magic.ele.DARK, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.DARK, MOBPARAM_WIPE_SHADOWS)
 
