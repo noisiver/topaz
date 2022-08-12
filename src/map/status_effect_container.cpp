8d301c59206610d137b6e4719f44b5c2549419fa
Shiyo <strikinginsanity@hotmail.com>
2022-08-12 19:47:21 -0400

    StatusEffect expiring msgs
    Sneak Attack / Trick Attack / Third Eye / Hasso / Seigan  will no longer be displayed in the log when expiring.

--- src/map/status_effect_container.cpp
+++ src/map/status_effect_container.cpp
@@ -1589,8 +1589,9 @@ void CStatusEffectContainer::CheckEffectsExpiry(time_point tick)
             {
                 RemoveStatusEffect(PStatusEffect, true);
             }
-            // Two hours fading shouldn't display in log
-            else if (PStatusEffect->GetStatusID() > 43 && PStatusEffect->GetStatusID() < 56)
+            // Status effects that shouldn't display in log(two hours, SA/TA etc)
+            else if (PStatusEffect->GetStatusID() > 43 && PStatusEffect->GetStatusID() < 56 && PStatusEffect->GetStatusID() != 87 &&
+                     PStatusEffect->GetStatusID() != 67 && PStatusEffect->GetStatusID() != 353 && PStatusEffect->GetStatusID() != 354)
             {
                 RemoveStatusEffect(PStatusEffect, true);
             }
