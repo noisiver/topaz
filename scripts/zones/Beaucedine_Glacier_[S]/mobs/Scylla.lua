cacf4d9c01877d4bd1bbd840fa01d57448650846
Shiyo <strikinginsanity@hotmail.com>
2022-08-17 20:07:35 -0400

    Scylla adjustments
    Fixed mechanics to work properly if targetting multiple players.

--- scripts/zones/Beaucedine_Glacier_[S]/mobs/Scylla.lua
+++ scripts/zones/Beaucedine_Glacier_[S]/mobs/Scylla.lua
@@ -19,6 +19,8 @@ function onMobSpawn(mob)
     mob:addMod(tpz.mod.ACC, 25)
 	mob:setMod(tpz.mod.VIT, 69)
     mob:setMod(tpz.mod.REFRESH, 400)
+    mob:setMod(tpz.mod.REGAIN, 50)
+    mob:setMod(tpz.mod.HUMANOID_KILLER, 25)
 	mob:setMod(tpz.mod.SLEEPRESTRAIT, 100)
 	mob:setMod(tpz.mod.LULLABYRESTRAIT, 100)
 	mob:setMod(tpz.mod.BINDRESTRAIT, 100)
@@ -26,13 +28,91 @@ function onMobSpawn(mob)
 	mob:AnimationSub(0)
 end
 
-function onMobWeaponSkill(target, mob, skill)
-	local AquaCannonCounter = mob:getLocalVar("AquaCannonCounter")
+function onMobFight(mob, target)
+	local AquaCannonTime = mob:getLocalVar("AquaCannonTime")
 	local AquaCannonMax = mob:getLocalVar("AquaCannonMax")
-	local IceGuillotineCounter = mob:getLocalVar("IceGuillotineCounter")
 	local IceGuillotineMax = mob:getLocalVar("IceGuillotineMax")
+	local IceGuillotineTime = mob:getLocalVar("IceGuillotineTime")
+    -- Use Aqua Cannon 4/6/8/10 times in a row after using Hydro Wave
+    if (AquaCannonTime == 1) then
+	    if (AquaCannonMax == 10) then
+		    mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+	    elseif (AquaCannonMax == 8) then
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+	    elseif (AquaCannonMax == 6) then
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+	    elseif (AquaCannonMax == 4) then
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+            mob:useMobAbility(2441) -- Aqua Cannon
+	    end
+        mob:setLocalVar("AquaCannonTime", 0)
+    end
+    -- Use Ice Guillotine 4/6/8/10 times in a row after using Frozen mst
+    if (IceGuillotineTime == 1) then
+	    if (IceGuillotineMax == 10) then
+		    mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+	    elseif (IceGuillotineMax == 8) then
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+	    elseif (IceGuillotineMax == 6) then
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+	    elseif (IceGuillotineMax == 4) then
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+            mob:useMobAbility(2440) -- Ice Guillotine
+	    end
+        mob:setLocalVar("IceGuillotineTime", 0)
+    end
+end
+
+function onMobWeaponSkill(target, mob, skill)
 	local Roll = math.random()
     if skill:getID() == 2439 then -- Hydro Wave
+        mob:addStatusEffectEx(tpz.effect.COLURE_ACTIVE, tpz.effect.COLURE_ACTIVE, 13, 3, 300, tpz.effect.MUTE, 1, tpz.auraTarget.ENEMIES, tpz.effectFlag.AURA)
 		if Roll < 0.2 then
 			AquaCannonMax = 10
 		elseif Roll < 0.5 then
@@ -43,21 +123,11 @@ function onMobWeaponSkill(target, mob, skill)
 			AquaCannonMax = 4
 		end
 		mob:setLocalVar("AquaCannonMax", AquaCannonMax)
+        mob:setLocalVar("AquaCannonTime", 1)
 	end
 	
-	if AquaCannonMax > 0 then
-		if AquaCannonCounter > AquaCannonMax then
-			mob:setLocalVar("AquaCannonCounter", 0)
-			mob:setLocalVar("AquaCannonMax", 0)
-			mob:delStatusEffectEx(tpz.effect.COLURE_ACTIVE, tpz.effect.COLURE_ACTIVE, 13, 3, 60, tpz.effect.SILENCE, 1, tpz.auraTarget.ENEMIES, tpz.effectFlag.AURA)
-		else
-			mob:useMobAbility(2441) -- Aqua Cannon
-			AquaCannonCounter = AquaCannonCounter + 1
-			mob:setLocalVar("AquaCannonCounter", AquaCannonCounter)
-		end
-	end
-
     if skill:getID() == 2438 then -- Frozen Mist
+        mob:addStatusEffectEx(tpz.effect.COLURE_ACTIVE, tpz.effect.COLURE_ACTIVE, 13, 3, 300, tpz.effect.GEO_PARALYSIS, 50, tpz.auraTarget.ENEMIES, tpz.effectFlag.AURA)
 		if Roll < 0.2 then
 			IceGuillotineMax = 10
 		elseif Roll < 0.5 then
@@ -68,18 +138,7 @@ function onMobWeaponSkill(target, mob, skill)
 			IceGuillotineMax = 4
 		end
 		mob:setLocalVar("IceGuillotineMax", IceGuillotineMax)
-	end
-
-	  if IceGuillotineMax > 0 then
-		if IceGuillotineCounter > IceGuillotineMax then
-			mob:setLocalVar("IceGuillotineCounter", 0)
-			mob:setLocalVar("IceGuillotineMax", 0)
-			mob:delStatusEffectEx(tpz.effect.COLURE_ACTIVE, tpz.effect.COLURE_ACTIVE, 13, 3, 60, tpz.effect.PARALYSIS, 1, tpz.auraTarget.ENEMIES, tpz.effectFlag.AURA)
-		else
-			mob:useMobAbility(2440) -- Ice Guillotine
-			IceGuillotineCounter = IceGuillotineCounter + 1
-			mob:setLocalVar("IceGuillotineCounter", IceGuillotineCounter)
-		end
+        mob:setLocalVar("IceGuillotineTime", 1)
 	end
 end
 
