39ec9ee30b45a483dbdf070608b13876a0ba565d
Shiyo <strikinginsanity@hotmail.com>
2022-09-12 22:03:04 -0400

    GM command update
    Use spell can now specify target or self.

--- scripts/commands/usespell.lua
+++ scripts/commands/usespell.lua
@@ -1,12 +1,12 @@
 ---------------------------------------------------------------------------------------------------
 -- func: useSpell
--- desc: Tells the target mob to cast specificed spell.
+-- desc: Tells the target mob to cast specificed spell on target or self.
 ---------------------------------------------------------------------------------------------------
 
 cmdprops =
 {
     permission = 1,
-    parameters = "s"
+    parameters = "ss"
 }
 
 function error(player, msg)
@@ -14,13 +14,17 @@ function error(player, msg)
     player:PrintToPlayer("!nocast")
 end
 
-function onTrigger(player, spell)
+function onTrigger(player, spell, self)
     
     local targ = player:getCursorTarget()
     
     if targ == nil or targ:isMob() == false then
         error(player, "you must select a target monster with the cursor first")
     else
-        targ:castSpell(spell)
+        if self == nil then
+            targ:castSpell(spell)
+        else
+            targ:castSpell(spell, targ)
+        end
     end
 end
