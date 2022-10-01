04faafce11edce27306584622b90c77d2cc55acf
Shiyo <strikinginsanity@hotmail.com>
2022-09-30 23:25:39 -0400

    SMN Update part 2
    New trait, Blood Boon.
    
    Occasionally cuts down MP cost of Blood Pact abilities
    
    20% proc at level 30, 22% at level 45, 24% at 60, and 25% at 75.
    
    New Ability: Mana Cede
    
    Level50, 3 minute cooldown.
    
    Transfers some of your HP to your currently summoned pet.
    
    Amount transferred is 25% of current HP + (summoning magic skill past current level cap / 2)

--- scripts/globals/abilities/mana_cede.lua
+++ scripts/globals/abilities/mana_cede.lua
@@ -1,17 +1,47 @@
 -----------------------------------
 -- Ability: Mana Cede
--- Description: Channels your MP into TP for avatars and elementals.
--- Obtained: SMN Level 87
--- Recast Time: 00:05:00
+-- Description: Transfers some of your HP to your currently summoned pet.
+-- Obtained: SMN Level 50
+-- Recast Time: 00:03:00
 -----------------------------------
 require("scripts/globals/settings")
 require("scripts/globals/status")
+require("scripts/globals/summon")
 -----------------------------------
 
 function onAbilityCheck(player, target, ability)
-    return 0, 0
+    local playerHPP = player:getHPP()
+    local healPower = math.floor(player:getMainLvl() / 3)
+    -- Can only be used if the player has more than 25% of their max HP
+    if playerHPP > healPower then
+        return 0, 0
+    else
+        return tpz.msg.basic.CANNOT_PERFORM_ACTION, 0
+    end 
 end
 
 function onUseAbility(player, target, ability)
-    target:addStatusEffect(tpz.effect.MANA_CEDE, 15, 1, 1)
+    local pet = player:getPet()
+    local playerMaxHP = player:getMaxHP()
+    local playerHP = player:getHP()
+    local petHP = pet:getHP()
+    local lvl = player:getMainLvl()
+    local skill = getSummoningSkillOverCap(pet)
+
+    -- Amount healed is FLOORED((player level / 3) + (summoning magic skill over cap / 2))
+    local basePower = math.floor(lvl / 3)
+    local playerHPReduction = playerMaxHP * (basePower / 100)
+    local playerHPFinal = playerHP - playerHPReduction
+
+    -- Set player to 1 HP instaed of killing them if their HP is less than te cost of Mana Cede(25% of their max HP)
+    if playerHPFinal < 0 then
+        playerHPFinal = 1
+    end
+
+    local power = basePower + (skill / 2)
+    local finalPower = playerHP * (power / 100)
+    local petHPFinal = petHP + finalPower
+
+    player:setHP(playerHPFinal)
+    pet:setHP(petHPFinal)
 end
