39f49e18f6e45b03865ae979dfc017246e7afd63
Shiyo <strikinginsanity@hotmail.com>
2022-10-29 19:28:55 -0400

    Re / RaiseAll GM command updates
    Now only adds the GMRaise variable if target is dead.

--- scripts/commands/re.lua
+++ scripts/commands/re.lua
@@ -13,8 +13,10 @@ cmdprops =
 function onTrigger(player)
     local zonePlayers = player:getZone():getPlayers()
     for _, zonePlayer in pairs(zonePlayers) do
-        zonePlayer:setLocalVar("GMRaise", 1)
-        zonePlayer:sendRaise(3)
+        if zonePlayer:getHP() == 0 then
+            zonePlayer:setLocalVar("GMRaise", 1)
+            zonePlayer:sendRaise(3)
+        end
         zonePlayer:delStatusEffect(1)
         zonePlayer:resetRecasts()
         zonePlayer:PrintToPlayer( string.format( "Sucessfully raised and reset everyone's JA's") )
