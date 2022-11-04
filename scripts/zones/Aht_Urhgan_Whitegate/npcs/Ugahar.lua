471e9566f68ba002b73e52802a85ce76bb1f2c5c
Shiyo <strikinginsanity@hotmail.com>
2022-11-04 19:30:47 -0400

    ToAU augments update
    No longer stuns, was causing bugs.

--- scripts/zones/Aht_Urhgan_Whitegate/npcs/Ugahar.lua
+++ scripts/zones/Aht_Urhgan_Whitegate/npcs/Ugahar.lua
@@ -3,33 +3,29 @@
 --  NPC: Ugahar
 -- Standard Info NPC
 -- ToAU Augmenting NPC
--- !pos 79 -0 54 50
+-- !pos 87 -0 45 50
 -----------------------------------
 
 function onTrade(player, npc, trade)
 end
 
 function onTrigger(player, npc)
-    if not player:hasStatusEffect(tpz.effect.STUN) then
-        player:addStatusEffect(tpz.effect.STUN, 1, 0, 300)
-        player:PrintToPlayer("Let's see here, the weapons that can be imbued are...",0,"Ugahar")
-        player:timer(3000, function(player)
-            player:PrintToPlayer("Weapons - Coming Soon",0xF,"Ugahar")
-        end)
-        player:timer(6000, function(player)
-            player:PrintToPlayer("Let's see here, the armor that can be imbued are...",0,"Ugahar")
-        end)
-        player:timer(9000, function(player)
-            player:PrintToPlayer("Jaridah Peti Set, Siphahi Jawshan Set, Tabin Set, Marid Mittens, Marid Leggings, Marid Belt.",0xF,"Ugahar")
-        end)
-        player:timer(12000, function(player)
-            player:PrintToPlayer("Lastly, the pet items that can be imbued are...",0,"Ugahar")
-        end)
-        player:timer(15000, function(player)
-            player:delStatusEffectSilent(tpz.effect.STUN)
-            player:PrintToPlayer("Moonwatch Wand, Spellcaster's Ecu, Mantra Coin, Silken Coat Set, Star Pendant, Moon Earring, Demon's Ring, Brocade Obi, Green Cape.",0xF,"Ugahar")
-        end)
-    end
+    player:PrintToPlayer("Let's see here, the weapons that can be imbued are...",0,"Ugahar")
+    player:timer(3000, function(player)
+        player:PrintToPlayer("Weapons - Coming Soon",0xF,"Ugahar")
+    end)
+    player:timer(6000, function(player)
+        player:PrintToPlayer("Let's see here, the armor that can be imbued are...",0,"Ugahar")
+    end)
+    player:timer(9000, function(player)
+        player:PrintToPlayer("Jaridah Peti Set, Siphahi Jawshan Set, Tabin Set, Marid Mittens, Marid Leggings, Marid Belt.",0xF,"Ugahar")
+    end)
+    player:timer(12000, function(player)
+        player:PrintToPlayer("Lastly, the pet items that can be imbued are...",0,"Ugahar")
+    end)
+    player:timer(15000, function(player)
+        player:PrintToPlayer("Moonwatch Wand, Spellcaster's Ecu, Mantra Coin, Silken Coat Set, Star Pendant, Moon Earring, Demon's Ring, Brocade Obi, Green Cape.",0xF,"Ugahar")
+    end)
     --player:startEvent(253)
 end
 
