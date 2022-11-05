3b7fda5ab29c533b3b963ec9278ae43c0801eaa5
Shiyo <strikinginsanity@hotmail.com>
2022-11-05 19:49:16 -0400

    Euvhi update
    Fixed a bug where Euvhi did not properly aggro while in "Open Bloom Flower" form

--- scripts/mixins/families/euvhi.lua
+++ scripts/mixins/families/euvhi.lua
@@ -4,13 +4,14 @@ g_mixins = g_mixins or {}
 g_mixins.families = g_mixins.families or {}
 
 function close_form(mob)
-    mob:setMobMod(tpz.mobMod.LINK_RADIUS, 11)
     mob:setMod(tpz.mod.DEFP, 100) 
     mob:setMod(tpz.mod.DMG, -25)
     mob:setDelay(4000)
     mob:setMobMod(tpz.mobMod.NO_MOVE, 0)
+    mob:setMobMod(tpz.mobMod.LINK_RADIUS, 11)
     mob:addMobMod(tpz.mobMod.ROAM_DISTANCE, 5)
     mob:addMobMod(tpz.mobMod.ROAM_COOL, 30)
+    mob:setAggressive(0)
     mob:AnimationSub(4)
 end
 
@@ -21,6 +22,7 @@ function open_form(mob)
     mob:setMobMod(tpz.mobMod.NO_MOVE, 1)
     mob:setMobMod(tpz.mobMod.LINK_RADIUS, 0)
     mob:setMobMod(tpz.mobMod.SOUND_RANGE, 11)
+    mob:setAggressive(1)
     mob:AnimationSub(6)
 end
 
@@ -52,7 +54,7 @@ g_mixins.families.euvhi = function(mob)
 
 
     mob:addListener("COMBAT_TICK", "EUVHI_CTICK", function(mob)
-        -- Ensure the Euvhi won't stop moving mid fight'
+        -- Ensure the Euvhi won't stop moving mid fight
         mob:setMobMod(tpz.mobMod.NO_MOVE, 0)
         local changeTime = mob:getLocalVar("changeTime")
         -- Open if time expired
