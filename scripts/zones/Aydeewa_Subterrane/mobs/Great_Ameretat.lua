5b6b031ddb317be6181f84ff0da0cc6984dfd46a
Shiyo <strikinginsanity@hotmail.com>
2022-11-10 21:38:03 -0500

    Missing amertat file

--- scripts/zones/Aydeewa_Subterrane/mobs/Great_Ameretat.lua
+++ scripts/zones/Aydeewa_Subterrane/mobs/Great_Ameretat.lua
@@ -10,7 +10,7 @@ function onMobInitialize(mob)
 end
 
 function onAdditionalEffect(mob, target, damage)
-    return tpz.mob.onAddEffect(mob, target, damage, tpz.mob.ae.HP_DRAIN, {chance = 25, power = math.random(100, 200)})
+    return tpz.mob.onAddEffect(mob, target, damage, tpz.mob.ae.HP_DRAIN, {chance = 100, power = math.random(100, 200)})
 end
 
 function onMobDeath(mob, player, isKiller)
