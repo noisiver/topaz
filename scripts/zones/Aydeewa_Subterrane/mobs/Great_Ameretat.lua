5dcf4ba066eab0440ae46207cf26605f648186bc
Shiyo <strikinginsanity@hotmail.com>
2022-11-10 21:37:46 -0500

    Ameretat update
    Strong regen when near it's spawn
    Absorbs HP as an additional effect of normal attacks when out of range (100%, absorbs about 10 times)
    It will not move at night ( it will not sleep and deactivate like Elka or Karakul , if you approach without sneaking , it will attack and chase you)
    Vampiric route added to special skills
    
    Thanks to JP wiki.

--- scripts/zones/Aydeewa_Subterrane/mobs/Great_Ameretat.lua
+++ scripts/zones/Aydeewa_Subterrane/mobs/Great_Ameretat.lua
@@ -7,11 +7,10 @@ require("scripts/globals/status")
 mixins = {require("scripts/mixins/families/ameretat")}
 -----------------------------------
 function onMobInitialize(mob)
-    mob:setMobMod(tpz.mobMod.ADD_EFFECT, 1)
 end
 
 function onAdditionalEffect(mob, target, damage)
-    return tpz.mob.onAddEffect(mob, target, damage, tpz.mob.ae.HP_DRAIN, {chance = 25, power = math.random(50, 75)})
+    return tpz.mob.onAddEffect(mob, target, damage, tpz.mob.ae.HP_DRAIN, {chance = 25, power = math.random(100, 200)})
 end
 
 function onMobDeath(mob, player, isKiller)
