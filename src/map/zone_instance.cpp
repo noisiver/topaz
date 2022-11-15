b2ddec85f24a8a2b3e534be9c6cf96d4893a0a05
Shiyo <strikinginsanity@hotmail.com>
2022-11-15 18:48:32 -0500

    Revert "Load instance weather"
    This reverts commit 23f057be3d0ffdf4ede19160c36adc6904f8e900.

--- src/map/zone_instance.cpp
+++ src/map/zone_instance.cpp
@@ -36,7 +36,6 @@ along with this program.  If not, see http://www.gnu.org/licenses/
 CZoneInstance::CZoneInstance(ZONEID ZoneID, REGIONTYPE RegionID, CONTINENTTYPE ContinentID)
     : CZone(ZoneID, RegionID, ContinentID)
 {
-    CZone::LoadZoneWeather();
 }
 
 CZoneInstance::~CZoneInstance()
