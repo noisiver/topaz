e5072d16a0847ee97731b0d10b65707a4773f977
Shiyo <strikinginsanity@hotmail.com>
2022-11-20 12:12:15 -0500

    Magical drain TP move bugfix

--- scripts/globals/mobskills/nosferatus_kiss.lua
+++ scripts/globals/mobskills/nosferatus_kiss.lua
@@ -15,8 +15,9 @@ function onMobWeaponSkill(target, mob, skill)
     local dmgmod = 1
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.DARK, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.DARK, MOBPARAM_IGNORE_SHADOWS)
-	MobPhysicalDrainMove(mob, target, skill, MOBDRAIN_MP, dmg)
-    MobPhysicalDrainMove(mob, target, skill, MOBDRAIN_TP, dmg)
-	skill:setMsg(MobPhysicalDrainMove(mob, target, skill, MOBDRAIN_HP, dmg))
+	MobDrainMove(mob, target, MOBDRAIN_MP, dmg, tpz.attackType.MAGICAL, tpz.damageType.DARK)
+    MobDrainMove(mob, target, MOBDRAIN_TP, dmg, tpz.attackType.MAGICAL, tpz.damageType.DARK)
+	skill:setMsg(MobDrainMove(mob, target, MOBDRAIN_HP, dmg, tpz.attackType.MAGICAL, tpz.damageType.DARK))
+
     return dmg
 end
--- scripts/globals/mobskills/psychomancy.lua
+++ scripts/globals/mobskills/psychomancy.lua
@@ -17,11 +17,11 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local dmgmod = 1
+    local dmgmod = 3
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.DARK, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.DARK, MOBPARAM_IGNORE_SHADOWS)
 
-    skill:setMsg(MobPhysicalDrainMove(mob, target, skill, MOBDRAIN_MP, dmg))
+    skill:setMsg(MobDrainMove(mob, target, MOBDRAIN_HP, dmg, tpz.attackType.MAGICAL, tpz.damageType.DARK))
 
     return dmg
 end
