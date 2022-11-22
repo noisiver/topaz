1eccc06c143f10b9c88e73c9df3e256ffe080dbd
Shiyo <strikinginsanity@hotmail.com>
2022-11-21 20:05:54 -0500

    Implement Angra Mainyu

---
+++ scripts/zones/Dynamis-Beaucedine/mobs/Fire_Pukis.lua
@@ -0,0 +1,19 @@
+-----------------------------------
+-- Area: Dynamis - Beaucedine
+--  Mob: Fire Pukis (Angra Mainyuu Dragon)
+-----------------------------------
+require("scripts/globals/dynamis")
+require("scripts/globals/status")
+require("scripts/globals/mobs")
+-----------------------------------
+function onMobSpawn(mob)
+    mob:setDamage(25)
+    mob:setBehaviour(bit.bor(mob:getBehaviour(), tpz.behavior.NO_TURN))
+end
+
+function onMobWeaponSkillPrepare(mob, target)
+    return 642 -- Only uses Flame Breath
+end
+
+function onMobDeath(mob, player, isKiller)
+end
