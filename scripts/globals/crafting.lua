1c20d0d071dfac1142dfba76634208b2e6be384c
Shiyo <strikinginsanity@hotmail.com>
2022-11-26 16:11:47 -0500

    debug print

--- scripts/globals/crafting.lua
+++ scripts/globals/crafting.lua
@@ -294,7 +294,8 @@ end
 function unionRepresentativeTrade(player, npc, trade, csid, guildID)
     local gpItem, remainingPoints = player:getCurrentGPItem(guildID)
     local text = zones[player:getZoneID()].text
-
+    print(gpItem)
+    print(remainingPoints)
     if (player:getCharVar('[GUILD]currentGuild') - 1 == guildID) then
         if remainingPoints == 0 then
             player:messageText(npc, text.NO_MORE_GP_ELIGIBLE)
