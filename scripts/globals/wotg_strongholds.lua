3fbd1355f438d24140f16b70fb96de86c9580ffb
Shiyo <strikinginsanity@hotmail.com>
2022-11-29 22:18:21 -0500

    (Strongholds) Utils file.

---
+++ scripts/globals/wotg_strongholds.lua
@@ -0,0 +1,20 @@
+-----------------------------------
+--
+--  WotG Strongholds utilities
+--
+-----------------------------------
+require("scripts/globals/settings")
+require("scripts/globals/msg")
+require("scripts/globals/status")
+require("scripts/globals/zone")
+require("scripts/globals/items")
+-----------------------------------
+
+tpz = tpz or {}
+tpz.wotg_strongholds = tpz.wotg_strongholds or {}
+
+tpz.wotg_strongholds.TrashMods = function(mob)
+end
+
+tpz.wotg_strongholds.NMMods = function(mob)
+end
