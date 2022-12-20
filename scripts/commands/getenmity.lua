ae2346b1242e3aecfc12b6d809ee487b9433a1a2
Shiyo <strikinginsanity@hotmail.com>
2022-12-20 17:19:35 -0500

    GM command

--- scripts/commands/getenmity.lua
+++ scripts/commands/getenmity.lua
@@ -1,4 +1,4 @@
----------------------------------------------------------------------------------------------------
+ ---------------------------------------------------------------------------------------------------
 -- func: getenmity
 -- desc: prints the target mob's current CE and VE towards you
 ---------------------------------------------------------------------------------------------------
@@ -6,7 +6,7 @@ require("scripts/globals/status")
 
 cmdprops =
 {
-    permission = 1,
+    permission = 2,
     parameters = ""
 }
 
@@ -19,10 +19,9 @@ function onTrigger(player)
     
     local targ = player:getCursorTarget()
     
-    if targ == nil or targ:isMob() == false then
+    if targ == nil or targ:isMob() == false or player:hasStatusEffect(tpz.effect.CHARM_I) or player:hasStatusEffect(tpz.effect.CHARM_II) then
         error(player, "you must select a target monster with the cursor first")
     end
     
     player:PrintToPlayer(string.format("your enmity against %s is ... CE = %u ... VE = %u", targ:getName(), targ:getCE(player), targ:getVE(player)))
-    
-end
+ end
