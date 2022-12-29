c115f059f4694afed11264840c6c9b79dbcbe0e0
Shiyo <strikinginsanity@hotmail.com>
2022-12-28 21:01:16 -0500

    Implement Beadeaux [S} Quadav Charm gate

---
+++ scripts/zones/Beadeaux_[S]/npcs/_2k1.lua
@@ -0,0 +1,28 @@
+-----------------------------------
+-- Area: Beadeaux_[S]
+-----------------------------------
+local ID = require("scripts/zones/Beadeaux_[S]/IDs")
+require("scripts/globals/npc_util")
+require("scripts/globals/items")
+-----------------------------------
+-- !gotoid 17154804
+-----------------------------------
+function onTrigger(player, npc)
+    if player:getXPos() < 40 then
+        player:messageSpecial(ID.text.EMPTY_SOCKET)
+    else
+        npc:openDoor()
+    end
+end
+
+function onTrade(player, npc, trade)
+    if npcUtil.tradeHasExactly(trade, tpz.items.QUADAV_CHARM, true) then
+        npc:openDoor()
+    end
+end
+
+function onEventUpdate(player, csid, option)
+end
+
+function onEventFinish(player, eventid, result, door)
+end
