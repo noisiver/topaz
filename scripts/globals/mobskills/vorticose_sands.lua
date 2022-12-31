1b861df6d1d7978f606faa6bb09c6028eed6a075
Shiyo <strikinginsanity@hotmail.com>
2022-12-31 10:21:22 -0500

    (Strongholds) Beastmen king tuning
    Now immune to stun.
    Buffed fTP of super strong TP moves

--- scripts/globals/mobskills/vorticose_sands.lua
+++ scripts/globals/mobskills/vorticose_sands.lua
@@ -32,7 +32,7 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.SILENCE
 
-    local dmgmod = 2
+    local dmgmod = 7
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*6, tpz.magic.ele.WIND, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.WIND, MOBPARAM_WIPE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.WIND)
