f8740a3ccce9109dac06e9039b012eb8351dfcda
Shiyo <strikinginsanity@hotmail.com>
2023-01-01 10:09:00 -0500

    Additional effects bugfix
    A few weapons now properly get reduced duration on their add effect based on resist

--- scripts/globals/items/ruler.lua
+++ scripts/globals/items/ruler.lua
@@ -11,12 +11,12 @@ require("scripts/globals/msg")
 function onAdditionalEffect(player, target, damage)
     local chance = 10
     local resist = getAdditionalEffectStatusResist(player, target, tpz.effect.FLASH, tpz.magic.ele.LIGHT, 0)
-    local duration = math.floor(60 * resist)
+    local duration = math.floor(12 * resist)
    
     if math.random(0, 99) >= chance or resist < 0.5 then
         return 0, 0, 0
     else
-        target:addStatusEffect(tpz.effect.FLASH, 100, 0, 12)
+        target:addStatusEffect(tpz.effect.FLASH, 100, 3, duration)
         return tpz.subEffect.LIGHT_DAMAGE, tpz.msg.basic.ADD_EFFECT_STATUS, tpz.effect.FLASH
     end
 end
