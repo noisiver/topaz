8b3364b9c775dd0003a5377b209b2e84d2e2f761
Shiyo <strikinginsanity@hotmail.com>
2022-12-31 19:25:38 -0500

    Sickle Moon TP scaling
    No longer scales fTP with TP

--- scripts/globals/weaponskills/sickle_moon.lua
+++ scripts/globals/weaponskills/sickle_moon.lua
@@ -15,7 +15,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
 
     local params = {}
     params.numHits = 2
-    params.ftp100 = 2 params.ftp200 = 2.1 params.ftp300 = 2.2
+    params.ftp100 = 2 params.ftp200 = 2 params.ftp300 = 2
     params.str_wsc = 0.5 params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.15 params.crit200 = 0.3 params.crit300 = 0.5
     params.canCrit = true
