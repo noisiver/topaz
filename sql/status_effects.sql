3cc1875c75d22a45ece8cec71bc31a03824cc7a1
Shiyo <strikinginsanity@hotmail.com>
2023-01-02 19:33:18 -0500

    (SAM)Inifnite aura bugfix

--- sql/status_effects.sql
+++ sql/status_effects.sql
@@ -642,7 +642,7 @@ INSERT INTO `status_effects` VALUES (608,'negate_virus',289,0,0,0,0,0,0,0,0);
 INSERT INTO `status_effects` VALUES (609,'negate_curse',289,0,0,0,0,0,0,0,0);
 INSERT INTO `status_effects` VALUES (610,'negate_charm',289,0,0,0,0,0,0,0,0);
 INSERT INTO `status_effects` VALUES (611,'magic_evasion_boost_ii',32,0,0,0,0,0,0,0,0);
-INSERT INTO `status_effects` VALUES (612,'colure_active',9437440,0,0,3,0,0,0,0,0);
+INSERT INTO `status_effects` VALUES (612,'colure_active',14287136,0,0,3,0,0,0,0,0);
 INSERT INTO `status_effects` VALUES (623,'rampart',5243168,0,0,0,0,0,0,0,0);
 INSERT INTO `status_effects` VALUES (768,'abyssea_str',256,0,0,0,0,0,1,0,0);
 INSERT INTO `status_effects` VALUES (769,'abyssea_dex',256,0,0,0,0,0,5,0,0);
