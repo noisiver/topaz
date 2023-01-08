8404a216935216fa29fa4c0e881aa0326822292d
Shiyo <strikinginsanity@hotmail.com>
2023-01-08 14:59:35 -0500

    (ENM) Sheep in antlion
    Adds no longer drop loot

--- scripts/zones/Boneyard_Gully/mobs/Swift_Hunter.lua
+++ scripts/zones/Boneyard_Gully/mobs/Swift_Hunter.lua
@@ -10,6 +10,7 @@ function onMobSpawn(mob)
     -- Aggros via ambush, not superlinking
     mob:setMobMod(tpz.mobMod.SUPERLINK, 0)
     mob:setMobMod(tpz.mobMod.NO_MOVE, 1)
+    mob:setMobMod(tpz.mobMod.NO_DROPS, 1)
 end
 
 function onMobEngaged(mob, target)
