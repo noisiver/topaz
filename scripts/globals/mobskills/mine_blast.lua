acb7f7f6edbcb0b71a8b34b24cf50b29cd5d2aa5
Shiyo <strikinginsanity@hotmail.com>
2023-01-11 13:26:47 -0500

    Mine Blast adjustments

--- scripts/globals/mobskills/mine_blast.lua
+++ scripts/globals/mobskills/mine_blast.lua
@@ -13,7 +13,7 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local dmgmod = 2
-    local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.FIRE, dmgmod, TP_MAB_BONUS, 1)
+    local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.FIRE, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.FIRE, MOBPARAM_IGNORE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.FIRE)
     mob:setHP(0)
