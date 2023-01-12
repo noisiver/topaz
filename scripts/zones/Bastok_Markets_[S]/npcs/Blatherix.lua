328b5630914f6e29eca1ac519ebd7f64b269e67c
Shiyo <strikinginsanity@hotmail.com>
2023-01-11 22:11:46 -0500

    (Strongholds) Custom RMEA

--- scripts/zones/Bastok_Markets_[S]/npcs/Blatherix.lua
+++ scripts/zones/Bastok_Markets_[S]/npcs/Blatherix.lua
@@ -1,10 +1,13 @@
 -----------------------------------
 -- Area: Bastok Markets (S) (F-8)
 --  NPC: Blatherix
---  involved in Zilartian weapons
+--  Involved in Zilartian weapons
+-- !gotoid 17134141
 -----------------------------------
 require("scripts/globals/items")
 require("scripts/globals/npc_util")
+require("scripts/globals/shop")
+
 local ID = require("scripts/zones/Bastok_Markets_[S]/IDs")
 -----------------------------------
 
@@ -27,7 +30,28 @@ function onTrade(player, npc, trade)
 end
 
 function onTrigger(player, npc)
-    player:startEvent(29)
+    local stock =
+    {
+        21509, 999999999, 3,    -- Premium Mogti
+        20571, 999999999, 3,    -- Infiltrator
+        20665, 999999999, 3,    -- Kam'lanaut's Sword
+        21682, 999999999, 3,    -- Lament
+        21745, 999999999, 3,    -- Dullahan Axe
+        21759, 999999999, 3,    -- Autarch's Axe
+        21820, 999999999, 3,    -- Lost Sickle
+        21862, 999999999, 3,    -- Mizukage Naginata
+        21923, 999999999, 3,    -- Debahocho
+        21977, 999999999, 3,    -- Mutsunokami
+        21086, 999999999, 3,    -- Heartstopper
+        22070, 999999999, 3,    -- Ranine Staff
+        22112, 999999999, 3,    -- Mizukage-no-Yumi
+        21272, 999999999, 3,    -- Troll Gun
+        -- 22070, 999999999, 3,    -- Shield NYI?
+        -- 22070, 999999999, 3,    -- Instrument NYI?
+    }
+
+    player:PrintToPlayer("Not much business in weapons anymore. But I like it that way.",0,"Blatherix")
+    tpz.shop.nation(player, stock, tpz.nation.BASTOK)
 end
 
 function onEventUpdate(player, csid, option)
