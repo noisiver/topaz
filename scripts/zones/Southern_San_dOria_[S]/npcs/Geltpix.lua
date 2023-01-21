cf54e015eef9be3c5a596199cbce893d9909b60a
Shiyo <strikinginsanity@hotmail.com>
2023-01-21 16:57:09 -0500

    Add Antiacids to WotG goblin merchants in towns

--- scripts/zones/Southern_San_dOria_[S]/npcs/Geltpix.lua
+++ scripts/zones/Southern_San_dOria_[S]/npcs/Geltpix.lua
@@ -3,12 +3,25 @@
 --  NPC: Geltpix
 -- !pos 154 -2 103 80
 -----------------------------------
+local ID = require("scripts/zones/Southern_San_dOria_[S]/IDs")
+require("scripts/globals/shop")
+-----------------------------------
 
 function onTrade(player, npc, trade)
 end
 
 function onTrigger(player, npc)
+    local stock =
+    {
+        4116,  4500,    -- Hi-Potion
+        4132, 28000,    -- Hi-Ether
+        605,    200,    -- Pickaxe
+        1020,   300     -- Sickle
+        4153,  1000,    -- Antacid
+    }
+
     player:showText(npc, 7043) -- Don't hurt poor Geltpix! Geltpix's just a merchant from Boodlix's Emporium in Jeuno. Kingdom vendors don't like gil, but Boodlix knows true value of new money.
+    tpz.shop.general(player, stock)
 end
 
 function onEventUpdate(player, csid, option)
