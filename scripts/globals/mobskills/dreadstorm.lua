3005a0f042633cfec541da7f1be561a28a7bfd05
Shiyo <strikinginsanity@hotmail.com>
2023-01-22 18:56:40 -0500

    (ZNM) Tuning

--- scripts/globals/mobskills/dreadstorm.lua
+++ scripts/globals/mobskills/dreadstorm.lua
@@ -26,13 +26,10 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.TERROR
-    local duration = 10
-
-    skill:setMsg(MobGazeMove(mob, target, typeEffect, 1, 0, duration))
-
     local dmgmod = 1.5
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg() * 3, tpz.magic.ele.DARK, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.DARK, MOBPARAM_WIPE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.DARK)
+    MobStatusEffectMove(mob, target, typeEffect, power, 0, 5)
     return dmg
 end
