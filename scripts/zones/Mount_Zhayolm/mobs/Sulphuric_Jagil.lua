19e141aefc51983c79e1d767472e5d23353b33c5
Shiyo <strikinginsanity@hotmail.com>
2023-01-22 10:21:19 -0500

    (ToAU) Add more Sikclemoon Jagils

--- scripts/zones/Mount_Zhayolm/mobs/Sulphuric_Jagil.lua
+++ scripts/zones/Mount_Zhayolm/mobs/Sulphuric_Jagil.lua
@@ -3,6 +3,8 @@
 --   NM: Sulphuric_Jagil
 ------------------------------
 require("scripts/globals/status")
+require("scripts/globals/keyitems")
+local ID = require("scripts/zones/Mount_Zhayolm/IDs")
 ------------------------------
 function onMobSpawn(mob)
     mob:setMod(tpz.mod.DOUBLE_ATTACK, 25)
