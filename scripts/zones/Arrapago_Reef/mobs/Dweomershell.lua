f8cf4d2d60fd6093de538d8f389e4a363cfb18f1
Shiyo <strikinginsanity@hotmail.com>
2023-01-22 11:24:36 -0500

    (TOAU) Temp fix Dweomershell drop table

--- scripts/zones/Arrapago_Reef/mobs/Dweomershell.lua
+++ scripts/zones/Arrapago_Reef/mobs/Dweomershell.lua
@@ -6,6 +6,11 @@ require("scripts/globals/status")
 require("scripts/globals/keyitems")
 local ID = require("scripts/zones/Arrapago_Reef/IDs")
 ------------------------------
+
+function onMobFight(mob, target)
+    mob:setDropID(2107)
+end
+
 function onMobDeath(mob, player, isKiller)
     if math.random(1,100) <= 5 then
 	    local zonePlayers = mob:getZone():getPlayers()
