3af7fcdedbb326ced634aaaa52b4c7fe5ef9b48a
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-05-05 12:00:50 -0400

    (Duo Rare Drops TEST) Prints

--- src/map/entities/mobentity.cpp
+++ src/map/entities/mobentity.cpp
@@ -1176,27 +1176,27 @@ void CMobEntity::DropItems(CCharEntity* PChar)
         if (GetMLevel() >= 11 && GetMLevel() <= 20) // 11-20 bracket cloth
         {
             AddItemToPool(834, ++dropCount); // Saruta Cotton
-            //printf("11-20\n");
+            printf("11-20  Lv:%u\n", GetMLevel());
         }
         else if (GetMLevel() >= 21 && GetMLevel() <= 30) // 21-30 bracket cloth
         {
             AddItemToPool(835, ++dropCount); // Flax Flower
-            //printf("21-30\n");
+            printf("21-30  Lv:%u\n", GetMLevel());
         }
         else if (GetMLevel() >= 31 && GetMLevel() <= 40) // 31-40 bracket cloth
         {
             AddItemToPool(832, ++dropCount); // Sheep Wool
-            //printf("31-40\n");
+            printf("31-40  Lv:%u\n", GetMLevel());
         }
         else if (GetMLevel() >= 41 && GetMLevel() <= 60) // 41-60 bracket cloth
         {
             AddItemToPool(839, ++dropCount); // Crawler Cocoon
-            //printf("41-60\n");
+            printf("41-60  Lv:%u\n", GetMLevel());
         }
         else if (GetMLevel() >= 61) // 61+ bracket cloth
         {
             AddItemToPool(838, ++dropCount); // Spider Web
-            //printf("61+\n");
+            printf("61+  Lv:%u\n", GetMLevel());
         }
     }
     // Roll for random rare items
