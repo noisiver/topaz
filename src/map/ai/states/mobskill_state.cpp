96c1ce92ba71d2cf54d690fb73d58f33a64065fe
Shiyo <strikinginsanity@hotmail.com>
2023-05-06 00:09:58 -0400

    Mob WS no TP deplete bugfix

--- src/map/ai/states/mobskill_state.cpp
+++ src/map/ai/states/mobskill_state.cpp
@@ -97,7 +97,7 @@ void CMobSkillState::SpendCost()
         {
             tp = m_PEntity->health.tp;
 
-            if (m_PEntity->getMod(Mod::WS_NO_DEPLETE) < tpzrand::GetRandomNumber(100))
+            if (m_PEntity->getMod(Mod::WS_NO_DEPLETE) <= tpzrand::GetRandomNumber(100))
             {
                 m_PEntity->health.tp = 0;
             }
