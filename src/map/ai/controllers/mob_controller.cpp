b47661ea5f2d7ea425176694302884bf2f29af11
Shiyo <strikinginsanity@hotmail.com>
2023-05-11 16:35:41 -0400

    (Pets TEST) Mobs linking to charmed pets bugfix

--- src/map/ai/controllers/mob_controller.cpp
+++ src/map/ai/controllers/mob_controller.cpp
@@ -150,7 +150,7 @@ bool CMobController::CheckDetection(CBattleEntity* PTarget)
 void CMobController::TryLink()
 {
     TracyZoneScoped;
-    if (PTarget == nullptr || PTarget->objtype == TYPE_PET) // Mobs shouldn't link to pets
+    if (PTarget == nullptr || PTarget->objtype == TYPE_PET || PTarget->isCharmed) // Mobs shouldn't link to pets
     {
         return;
     }
