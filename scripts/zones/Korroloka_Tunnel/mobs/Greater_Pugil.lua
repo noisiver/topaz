b9826c18ead0b42a51ea2adbcf1a393f70a8d424
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-05-12 10:18:43 -0400

    (Spawns) Korroloka Tunnel Pugils

--- scripts/zones/Korroloka_Tunnel/mobs/Greater_Pugil.lua
+++ scripts/zones/Korroloka_Tunnel/mobs/Greater_Pugil.lua
@@ -3,7 +3,11 @@
 --  Mob: Greater Pugil
 -----------------------------------
 require("scripts/globals/regimes")
+require("scripts/globals/status")
 -----------------------------------
+function onMobSpawn(mob)
+    mob:setMobMod(tpz.mobMod.NO_ROAM, 1)
+end
 
 function onMobDeath(mob, player, isKiller)
     tpz.regime.checkRegime(player, mob, 731, 2, tpz.regime.type.GROUNDS)
