aaac49902072201b719dd12b1ac1c2d2fc40d822
Shiyo <strikinginsanity@hotmail.com>
2023-05-14 23:27:52 -0400

    (Synthesis) Revert to retail accurate HQ rates

--- src/map/utils/synthutils.cpp
+++ src/map/utils/synthutils.cpp
@@ -324,8 +324,8 @@ uint8 calcSynthResult(CCharEntity* PChar)
         {
             case 4:  chance = 0.5;      break; // 1 in 2
             case 3:  chance = 0.25;     break; // 1 in 4
-            case 2:  chance = 0.10;     break; // 1 in 10
-            case 1:  chance = 0.0625;   break; // 1 in 16
+            case 2:  chance = 0.0625;   break; // 1 in 16
+            case 1:  chance = 0.015625; break; // 1 in 64
             default: chance = 0.000;    break;
         }
 
