0588fb62299a6f03f8890e32d240f0d8985218e1
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-05-16 12:13:30 -0400

    (Skills TEST) Cap skills at level 99 mob

--- src/map/utils/battleutils.cpp
+++ src/map/utils/battleutils.cpp
@@ -305,8 +305,8 @@ namespace battleutils
     }
 
     /************************************************************************
-    *   Return Max Skill by SkillType, JobType, and level                   *
-    ************************************************************************/
+     *   Return Max Skill by SkillType, JobType, and level                   *
+     ************************************************************************/
     // 1 = A+
     // 2 = A-
     // 3 = B+
@@ -321,12 +321,31 @@ namespace battleutils
     // 12 = G
     uint16 GetMaxSkill(SKILLTYPE SkillID, JOBTYPE JobID, uint8 level)
     {
-        return g_SkillTable[level][g_SkillRanks[SkillID][JobID]];
-    }
+        // The skill_caps table is 0-indexed, so our maximum level should one lower
+        // than the size of the array.
+        auto maxLevel = static_cast<uint8>(g_SkillTable.size() - 1);
 
+        // TODO: Research on mobs level 99+ is still on-going. This line can be removed once the correct formula/skilltype have been established.
+        // max indexed value and level is capped at 99 as stated above for skill_caps table
+        if (level > 99)
+        {
+            level = 99;
+        }
+
+        if (level > maxLevel)
+        {
+            ShowDebug("battleutils::GetMaxSkill() received level value greater than array size! (Received: %d, Clamped to: %d)", level, maxLevel);
+        }
+        return g_SkillTable[std::clamp<uint8>(level, 0, maxLevel)][g_SkillRanks[SkillID][JobID]];
+    }
     uint16 GetMaxSkill(uint8 rank, uint8 level)
     {
-        return g_SkillTable[level][rank];
+        auto maxLevel = static_cast<uint8>(g_SkillTable.size() - 1);
+        if (level > maxLevel)
+        {
+            ShowDebug("battleutils::GetMaxSkill() received level value greater than array size! (Received: %d, Clamped to: %d)", level, maxLevel);
+        }
+        return g_SkillTable[std::clamp<uint8>(level, 0, maxLevel)][rank];
     }
 
     bool isValidSelfTargetWeaponskill(int wsid) {
