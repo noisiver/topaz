7f2acf7c393ec6e5c41545de9c318c135bd43a5d
Shiyo <strikinginsanity@hotmail.com>
2023-05-18 20:44:38 -0400

    (LB) PUP part1

---
+++ scripts/zones/Navukgo_Execution_Chamber/bcnms/achieving_true_power.lua
@@ -0,0 +1,41 @@
+-----------------------------------
+-- Area: Navukgo Execution Chamber
+-- BCNM: PUP G5 Achieving True Power
+-----------------------------------
+require("scripts/globals/battlefield")
+require("scripts/globals/keyitems")
+local ID = require("scripts/zones/Navukgo_Execution_Chamber/IDs")
+----------------------------------------
+
+function onBattlefieldTick(battlefield, tick)
+    tpz.battlefield.onBattlefieldTick(battlefield, tick)
+end
+
+function onBattlefieldRegister(player, battlefield)
+end
+
+function onBattlefieldEnter(player, battlefield)
+end
+
+function onBattlefieldEnter(player, battlefield)
+end
+
+function onBattlefieldLeave(player, battlefield, leavecode)
+    if leavecode == tpz.battlefield.leaveCode.WON then
+        local name, clearTime, partySize = battlefield:getRecord()
+        player:startEvent(32001, battlefield:getArea(), clearTime, partySize, battlefield:getTimeInside(), 1, battlefield:getLocalVar("[cs]bit"), 0)
+    elseif leavecode == tpz.battlefield.leaveCode.LOST then
+        player:startEvent(32002)
+    end
+end
+
+function onEventUpdate(player, csid, option)
+end
+
+function onEventFinish(player, csid, option)
+    if csid == 32001 and player:getQuestStatus(BASTOK, tpz.quest.id.ACHIEVING_TRUE_POWER) == QUEST_ACCEPTED then
+        npcUtil.completeQuest(player, BASTOK, ACHIEVING_TRUE_POWER, {item=4181, title=tpz.title.MASTER_OF_MANIPULATION})
+        player:levelCap(75)
+        player:messageSpecial(ID.text.LEVEL_LIMIT_75)
+    end
+end
---
+++ scripts/zones/Navukgo_Execution_Chamber/mobs/Shamarhaan.lua
@@ -0,0 +1,72 @@
+-----------------------------------
+-- Area: Navukgo Execution Chamber
+-- MOB: Shamarhaan
+-----------------------------------
+local ID = require("scripts/zones/Navukgo_Execution_Chamber/IDs")
+mixins = {require("scripts/mixins/job_special")}
+-----------------------------------
+
+function onMobInitialize(mob)
+    mob:addListener("WEAPONSKILL_TAKE", "SHAMARHAAN_WEAPONSKILL_TAKE", function(target, attacker, skillId, tp, action)
+        target:messageText(target, ID.text.SHAMARHAAN_UNDERESTIMATED)
+    end)
+
+    mob:addListener("WEAPONSKILL_USE", "SHAMARHAAN_WEAPONSKILL_USE", function(mob, target, wsid, tp, action)
+        mob:messageText(mob, ID.text.SHAMARHAAN_TRICKS)
+    end)
+end
+
+function onMobSpawn(mob)
+    dsp.mix.jobSpecial.config(mob, {
+        specials =
+        {
+            {
+                id = dsp.jsa.OVERDRIVE_SHAMARHAAN, hpp = 90,
+                begCode = function(mob)
+                    mob:messageText(mob, ID.text.SHAMARHAAN_FULL_STEAM)
+                end,
+            },
+        },
+    })
+end
+
+function onMobRoam(mob)
+    if mob:getLocalVar("engaged") == 0 then
+        for _, player in pairs(mob:getBattlefield():getPlayers()) do
+            if player:checkDistance(mob) < 15 then
+                mob:messageText(mob, ID.text.SHAMARHAAN_LET_US_BEGIN)
+                mob:setLocalVar("engaged", player:getID())
+            end
+        end
+    end
+end
+
+function onMobEngaged(mob, target)
+    if mob:getLocalVar("engaged") == 0 then
+        mob:messageText(mob, ID.text.SHAMARHAAN_LET_US_BEGIN)
+        mob:setLocalVar("engaged", target:getID())
+    end
+end
+
+function onMobDisengaged(mob)
+    engagedID = mob:getLocalVar("engaged")
+    if engagedID ~= 0 then
+        player = GetPlayerByID(engagedID)
+        if player:getHP() == 0 then
+            mob:showText(mob, ID.text.SHAMARHAAN_NOT_READY)
+        end
+    end
+end
+
+function onMobFight(mob,target)
+    -- TODO: Shamarhaan regularly (roughly once a minute) does Maneuvers which appear to slightly change Valkeng's behavior
+
+    if mob:getHPP() < 20 then
+        mob:showText(mob, ID.text.SHAMARHAAN_MAGNIFICENT)
+        mob:getBattlefield():win()
+    end
+end
+
+function onMobDeath(mob, player, isKiller)
+    mob:messageText(mob, ID.text.SHAMARHAAN_MAGNIFICENT)
+end
