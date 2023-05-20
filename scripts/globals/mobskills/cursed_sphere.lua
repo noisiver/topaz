1a7b24b805818d4471c6fc30e0be06c342f86a52
Shiyo <strikinginsanity@hotmail.com>
2023-05-20 13:54:45 -0400

    (TP Moves) Tuning

--- scripts/globals/mobskills/cursed_sphere.lua
+++ scripts/globals/mobskills/cursed_sphere.lua
@@ -15,7 +15,7 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local dmgmod = 1
+    local dmgmod = 3
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.DARK, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.DARK, MOBPARAM_IGNORE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.DARK)
