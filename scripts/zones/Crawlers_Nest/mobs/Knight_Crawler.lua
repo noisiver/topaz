06295a746582696e5c0938baed10e524f2c13215
Shiyo <strikinginsanity@hotmail.com>
2023-05-21 16:00:05 -0400

    (Spawns) Crawler's Nest Knight Crawler

--- scripts/zones/Crawlers_Nest/mobs/Knight_Crawler.lua
+++ scripts/zones/Crawlers_Nest/mobs/Knight_Crawler.lua
@@ -6,5 +6,9 @@ require("scripts/globals/regimes")
 -----------------------------------
 
 function onMobDeath(mob, player, isKiller)
+    -- Temp bandaid
+    if mob:getID() == 17584322 then
+        mob:setSpawn(-335,-1,223)
+    end
     tpz.regime.checkRegime(player, mob, 692, 2, tpz.regime.type.GROUNDS)
 end
