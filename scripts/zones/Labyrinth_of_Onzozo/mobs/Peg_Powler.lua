cc8472e817689ea0086ce6fd095775788c04f086
Shiyo <strikinginsanity@hotmail.com>
2023-05-21 01:00:32 -0400

    (Gil) NM drop adjustments

--- scripts/zones/Labyrinth_of_Onzozo/mobs/Peg_Powler.lua
+++ scripts/zones/Labyrinth_of_Onzozo/mobs/Peg_Powler.lua
@@ -12,7 +12,7 @@ function onMobSpawn(mob)
     mob:addMod(tpz.mod.ATTP, 20)
     mob:setMod(tpz.mod.ACC, 50) 
     mob:addMod(tpz.mod.EVA, 50)
-    mob:setMobMod(tpz.mobMod.GIL_MIN, 9000)
+    mob:setMobMod(tpz.mobMod.GIL_MIN, 3000)
 end
 
 function onMobDeath(mob, player, isKiller)
