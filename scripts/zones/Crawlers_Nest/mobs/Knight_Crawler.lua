ec5c2a7f9d6cc600543cd2af7bb0d6020a1ea4b5
Shiyo <strikinginsanity@hotmail.com>
2023-05-21 16:53:45 -0400

    (Drops) Some chest keys

--- scripts/zones/Crawlers_Nest/mobs/Knight_Crawler.lua
+++ scripts/zones/Crawlers_Nest/mobs/Knight_Crawler.lua
@@ -4,23 +4,6 @@
 -----------------------------------
 require("scripts/globals/regimes")
 -----------------------------------
-function onMobSpawn(mob)
-    -- Temp bandaid
-    if mob:getID() == 17584322 then
-        mob:setSpawn(-335,-1,223)
-    end
-end
-function onMobFight(mob, target)
-    -- Temp bandaid
-    if mob:getID() == 17584322 then
-        mob:setSpawn(-335,-1,223)
-    end
-end
-
 function onMobDeath(mob, player, isKiller)
-    -- Temp bandaid
-    if mob:getID() == 17584322 then
-        mob:setSpawn(-335,-1,223)
-    end
     tpz.regime.checkRegime(player, mob, 692, 2, tpz.regime.type.GROUNDS)
 end
