29446d3f78a648f92b4d90869eff543d81d8d16f
Shiyo <strikinginsanity@hotmail.com>
2023-05-28 17:43:53 -0400

    Revert "Bandaid"
    This reverts commit 6a913b18f473c54da93a5819fa7dfc5ab2b7136a.

--- scripts/zones/Crawlers_Nest/mobs/Knight_Crawler.lua
+++ scripts/zones/Crawlers_Nest/mobs/Knight_Crawler.lua
@@ -4,21 +4,6 @@
 -----------------------------------
 require("scripts/globals/regimes")
 -----------------------------------
-
-function onMobSpawn(mob)
-    -- Temp bandaid
-    if mob:getID() == 17584322 then
-        mob:setSpawn(-335,-1,-262)
-    end
-end
-
-function onMobRoam(mob)
-    -- Temp bandaid
-    if mob:getID() == 17584322 then
-        mob:setSpawn(-335,-1,-262)
-    end
-end
-
 function onMobDeath(mob, player, isKiller)
     tpz.regime.checkRegime(player, mob, 692, 2, tpz.regime.type.GROUNDS)
 end
