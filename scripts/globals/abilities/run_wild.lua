265d354a7f87ee6dbb8404c23c2ee28fce24bc96
Shiyo <strikinginsanity@hotmail.com>
2023-06-01 19:52:44 -0400

    (BST) Run WIld active pet check

--- scripts/globals/abilities/run_wild.lua
+++ scripts/globals/abilities/run_wild.lua
@@ -6,13 +6,22 @@
 -- Recast Time: 00:30
 -- Duration: 5:00
 -----------------------------------
-
+require("scripts/globals/pets")
 require("scripts/globals/settings")
 require("scripts/globals/status")
+require("scripts/globals/pets")
+require("scripts/globals/msg")
 -----------------------------------
 
 function onAbilityCheck(player, target, ability)
-    return 0, 0
+    local pet = player:getPet()
+    if not pet then
+        return tpz.msg.basic.REQUIRES_A_PET, 0
+    elseif not player:isJugPet() and pet:getObjType() ~= tpz.objType.MOB then
+        return tpz.msg.basic.NO_EFFECT_ON_PET, 0
+    else
+        return 0, 0
+    end
 end
 
 function onUseAbility(player, target, ability)
