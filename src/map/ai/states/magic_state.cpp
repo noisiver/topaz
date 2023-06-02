272902531b298f3045d7028a41af5fb442768069
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-06-02 11:06:05 -0400

    (TEST PUP Spellcasting) Should be interrupted on movement like players

--- src/map/ai/states/magic_state.cpp
+++ src/map/ai/states/magic_state.cpp
@@ -104,8 +104,7 @@ bool CMagicState::Update(time_point tick)
 
         action_t action;
 
-        if (!PTarget || m_errorMsg || (HasMoved() && (m_PEntity->objtype != TYPE_PET ||
-            static_cast<CPetEntity*>(m_PEntity)->getPetType() != PETTYPE_AUTOMATON)) || !CanCastSpell(PTarget))
+        if (!PTarget || m_errorMsg || (HasMoved()) || !CanCastSpell(PTarget))
         {
             m_interrupted = true;
         }
