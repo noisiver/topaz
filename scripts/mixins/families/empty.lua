b4d8ca7eee9c8a2dd0e3a5ae826800919247281e
Shiyo <strikinginsanity@hotmail.com>
2023-06-02 21:52:19 -0400

    Comments

--- scripts/mixins/families/empty.lua
+++ scripts/mixins/families/empty.lua
@@ -52,53 +52,53 @@ g_mixins.families.empty = function(mob)
         printf("Drop Rate: %s", dropRate)
         if element == 8  then
 		if math.random(1,100) <= dropRate then 
-            killer:addTreasure(1613, mob) -- Malevolent Memory (darkness skillchain)
+            killer:addTreasure(1613, mob) -- Malevolent Memory (darkness)
 		end
         elseif element == 6 then
 			if math.random(1,100) <= dropRate then 
 				killer:addTreasure(1611, mob) -- Somber Memory (water)
 			end
             if math.random(1,100) <= dropRate then
-                killer:addTreasure(1613) -- Malevolent Memory (darkness skillchain)
+                killer:addTreasure(1613) -- Malevolent Memory (darkness)
             end
         elseif element == 5 then
 			if math.random(1,100) <= dropRate then 
 				killer:addTreasure(1610, mob) -- Startling Memory (lightning)
 			end
             if math.random(1,100) <= dropRate then 
-                killer:addTreasure(1612, mob) -- Radiant Memory (light skillchain)
+                killer:addTreasure(1612, mob) -- Radiant Memory (light)
             end
         elseif element == 4  then
 			if math.random(1,100) <= dropRate then 
 				killer:addTreasure(1609, mob) -- Profane Memory (earth)
 			end
 			if math.random(1,100) <= dropRate then 
-				killer:addTreasure(1613, mob) -- Malevolent Memory (darkness skillchain)
+				killer:addTreasure(1613, mob) -- Malevolent Memory (darkness)
 			end
         elseif element == 7 then
 			if math.random(1,100) <= dropRate then 
-				killer:addTreasure(1612, mob) -- Radiant Memory (light skillchain)
+				killer:addTreasure(1612, mob) -- Radiant Memory (light)
 			end
         elseif element == 1  then
 			if math.random(1,100) <= dropRate then 
 				killer:addTreasure(1606, mob) -- Burning Memory (fire)
 			end
 			if math.random(1,100) <= dropRate then 
-				killer:addTreasure(1612, mob) -- Radiant Memory (light skillchain)
+				killer:addTreasure(1612, mob) -- Radiant Memory (light)
 			end
         elseif element == 2 then
 			if math.random(1,100) <= dropRate then 
 				killer:addTreasure(1607, mob) -- Bitter Memory (ice)
 			end
 			if math.random(1,100) <= dropRate then 
-				killer:addTreasure(1613, mob) -- Malevolent Memory (darkness skillchain)
+				killer:addTreasure(1613, mob) -- Malevolent Memory (darkness)
 			end
         elseif element == 3 then
 			if math.random(1,100) <= dropRate then 
 				killer:addTreasure(1608, mob) -- Fleeting Memory (wind)
 			end
 			if math.random(1,100) <= dropRate then 
-				killer:addTreasure(1612, mob) -- Radiant Memory (light skillchain)
+				killer:addTreasure(1612, mob) -- Radiant Memory (light)
 			end
         end
         if math.random(1,100) <= utils.getDropRate(mob, 10) then
