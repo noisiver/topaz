2d316dfc7dd7c28cb4da5a6f6d8d1343cb844778
Shiyo <strikinginsanity@hotmail.com>
2023-06-03 20:07:38 -0400

    (Quests) Chasing Dreams - fixes

--- scripts/zones/Korroloka_Tunnel/npcs/_4t2.lua
+++ scripts/zones/Korroloka_Tunnel/npcs/_4t2.lua
@@ -15,10 +15,9 @@ function onTrigger(player, npc)
 
      if (player:getCharVar("ChasingDreams") == 7) then
          player:messageSpecial(ID.text.CAREFULLY_DRAW_WATER, tpz.ki.FLASK_OF_CLAM_WATER)
-         player:messageSpecial(ID.text.FLASK_FULL) 
+         player:messageSpecial(ID.text.FLASK_FULL, tpz.ki.FLASK_OF_CLAM_WATER)) 
          player:delKeyItem(tpz.ki.WASHUS_FLASK)
          npcUtil.giveKeyItem(player, tpz.ki.FLASK_OF_CLAM_WATER)
-         player:messageSpecial(ID.text.KEYITEM_OBTAINED, tpz.ki.FLASK_OF_CLAM_WATER) 
          player:setCharVar("ChasingDreams", 8)
      else
         player:messageSpecial(ID.text.CLAM_EMPTY) 
