c4c32a19315b01d2aab184006c079a99d8f71508
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-06-05 11:52:33 -0400

    (TEST TP Moves) Missing files

--- scripts/globals/mobskills/poison_breath_crawler.lua
+++ scripts/globals/mobskills/poison_breath_crawler.lua
@@ -1,14 +1,14 @@
 ---------------------------------------------
---  Poison Breath (Dragon)
+--  Poison Breath (Crawler)
 --
---  Description: Deals Water breath damage to enemies within a fan-shaped area originating from the caster.
---  Type: Magical (Water)
+--  Description: Deals water damage to enemies within a fan-shaped area originating from the caster. Additional effect: Poison.
+--  Type: Magical Water (Element)
+--
+--
 ---------------------------------------------
-
 require("scripts/globals/settings")
 require("scripts/globals/status")
 require("scripts/globals/monstertpmoves")
-
 ---------------------------------------------
 
 function onMobSkillCheck(target, mob, skill)
@@ -16,15 +16,9 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-
-    local dmgmod = MobHPBasedMove(mob, target, 0.10, 1, tpz.magic.ele.WATER, 900)
-    if mob:getPool() == 9056 then -- Vishap(Assault - Lebros Cavern)
-        dmgmod = MobHPBasedMove(mob, target, 0.10, 1, tpz.magic.ele.WATER, 400)
-    end
-
     local typeEffect = tpz.effect.POISON
-    local power = 50
-
+    local power = 1
+    local dmgmod = MobHPBasedMove(mob, target, 0.103, 1, tpz.magic.ele.WATER, 405)
     local dmg = MobFinalAdjustments(dmgmod, mob, skill, target, tpz.attackType.BREATH, tpz.damageType.WATER, MOBPARAM_IGNORE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.BREATH, tpz.damageType.WATER)
     MobStatusEffectMove(mob, target, typeEffect, power, 3, 90)
--- scripts/globals/mobskills/poison_breath_hound.lua
+++ scripts/globals/mobskills/poison_breath_hound.lua
@@ -1,5 +1,5 @@
 ---------------------------------------------
---  Poison Breath
+--  Poison Breath (Hound)
 --
 --  Description: Deals water damage to enemies within a fan-shaped area originating from the caster. Additional effect: Poison.
 --  Type: Magical Water (Element)
@@ -18,10 +18,7 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = tpz.effect.POISON
     local power = 1
-
-
     local dmgmod = MobHPBasedMove(mob, target, 0.0625, 1, tpz.magic.ele.WATER, 400)
-
     local dmg = MobFinalAdjustments(dmgmod, mob, skill, target, tpz.attackType.BREATH, tpz.damageType.WATER, MOBPARAM_IGNORE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.BREATH, tpz.damageType.WATER)
     MobStatusEffectMove(mob, target, typeEffect, power, 3, 90)
