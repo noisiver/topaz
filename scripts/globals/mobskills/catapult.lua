155647883659f8b0dca97c3348f82c673d2cb4e6
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-06-12 10:52:42 -0400

    (TP Moves) Catapult enmity reset bugfix
    Should only reset enmity if Agrios NM

--- scripts/globals/mobskills/catapult.lua
+++ scripts/globals/mobskills/catapult.lua
@@ -44,8 +44,10 @@ function onMobWeaponSkill(target, mob, skill)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.RANGED, tpz.damageType.RANGED, info.hitslanded)
     target:takeDamage(dmg, mob, tpz.attackType.RANGED, tpz.damageType.RANGED)
     local agrios = mob:getPool() == 64
-    if (MobPhysicalHit(mob, skill)) then
-        mob:resetEnmity(target)
+    if agrios then
+        if (MobPhysicalHit(mob, skill)) then
+            mob:resetEnmity(target)
+        end
     end
 	if ((skill:getMsg() ~= tpz.msg.basic.SHADOW_ABSORB) and (dmg > 0)) then   target:tryInterruptSpell(mob, info.hitslanded) end
     return dmg
