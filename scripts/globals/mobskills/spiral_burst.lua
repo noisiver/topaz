8ddd90a17334d068cf05575a317c6ec90a94e068
Shiyo <strikinginsanity@hotmail.com>
2023-06-16 15:40:52 -0400

    Spiral burst bugfix

--- scripts/globals/mobskills/spiral_burst.lua
+++ scripts/globals/mobskills/spiral_burst.lua
@@ -27,7 +27,7 @@ end
 function onMobWeaponSkill(target, mob, skill)
     local dmgmod = 2.5
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.FIRE, dmgmod, TP_NO_EFFECT)
-    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, MOBSKILL_MAGICAL, MOBPARAM_FIRE, MOBPARAM_WIPE_SHADOWS)
+    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGIC, tpz.damageType.FIRE, MOBPARAM_WIPE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.FIRE)
     utils.FullSelfEraseNa(mob)
     return dmg
