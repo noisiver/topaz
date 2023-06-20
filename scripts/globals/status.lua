cb9c442ef662af6819084f51388a8ade01d54288
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-06-20 10:56:31 -0400

    Formatting

--- scripts/globals/status.lua
+++ scripts/globals/status.lua
@@ -125,7 +125,10 @@ tpz.subEffect =
     BLINDNESS           = 12,  -- 1-00110      25
     SILENCE             = 13,
     PETRIFY             = 14,
+    PETRIFICATION       = 14,
     PLAGUE              = 15,
+    BANE                = 15,
+    ADDLE               = 15,
     STUN                = 16,
     CURSE               = 17,
     DEFENSE_DOWN        = 18,  -- 1-01001      37
@@ -177,26 +180,26 @@ tpz.subEffect =
 
 tpz.skillchainEle =
 {
-    NONE = 0, -- Lv0 None
+    NONE            = 0, -- Lv0 None
 
-    TRANSFIXION = 1, -- Lv1 Light
-    COMPRESSION = 2, -- Lv1 Dark
-    LIQUEFACTION = 3, -- Lv1 Fire
-    SCISSION = 4, -- Lv1 Earth
-    REVERBERATION = 5, -- Lv1 Water
-    DETONATION = 6, -- Lv1 Wind
-    INDURATION = 7, -- Lv1 Ice
-    IMPACTION = 8, -- Lv1 Thunder
+    TRANSFIXION     = 1, -- Lv1 Light
+    COMPRESSION     = 2, -- Lv1 Dark
+    LIQUEFACTION    = 3, -- Lv1 Fire
+    SCISSION        = 4, -- Lv1 Earth
+    REVERBERATION   = 5, -- Lv1 Water
+    DETONATION      = 6, -- Lv1 Wind
+    INDURATION      = 7, -- Lv1 Ice
+    IMPACTION       = 8, -- Lv1 Thunder
 
-    GRAVITATION = 9, -- Lv2 Dark & Earth
-    DISTORTION = 10, -- Lv2 Water & Ice
-    FUSION = 11, -- Lv2 Fire & Light
-    FRAGMENTATION = 12, -- Lv2 Wind & Thunder
+    GRAVITATION     = 9, -- Lv2 Dark & Earth
+    DISTORTION      = 10, -- Lv2 Water & Ice
+    FUSION          = 11, -- Lv2 Fire & Light
+    FRAGMENTATION   = 12, -- Lv2 Wind & Thunder
 
-    LIGHT = 13, -- Lv3 Fire, Light, Wind, Thunder
-    DARKNESS = 14, -- Lv3 Dark, Earth, Water, Ice
-    LIGHT_II = 15, -- Lv4 Light
-    DARKNESS_II = 16, -- Lv4 Darkness
+    LIGHT           = 13, -- Lv3 Fire, Light, Wind, Thunder
+    DARKNESS        = 14, -- Lv3 Dark, Earth, Water, Ice
+    LIGHT_II        = 15, -- Lv4 Light
+    DARKNESS_II     = 16, -- Lv4 Darkness
 }
 ------------------------------------
 -- These codes represent the actual status effects.
