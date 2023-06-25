eeda66653679c2eebb205a91b2c415bb781822af
Shiyo <strikinginsanity@hotmail.com>
2023-06-25 19:08:03 -0400

    (Items Addeffect) Holy Lance(+1)

---
+++ scripts/globals/items/holy_lance.lua
@@ -0,0 +1,27 @@
+-----------------------------------------
+-- ID: 16860
+-- Item: Holy Lance
+-- Additional Effect: Light Damage
+-----------------------------------------
+require("scripts/globals/status")
+require("scripts/globals/magic")
+require("scripts/globals/msg")
+-----------------------------------
+
+function onAdditionalEffect(player, target, damage)
+    local chance = 10
+    local damage = 35
+    local dmg = doAdditionalEffectDamage(player, target, chance, damage, nil, false, 0, tpz.magic.ele.LIGHT, 0)
+
+    if dmg == 0 then
+        return 0, 0, 0
+    end
+
+    local message = tpz.msg.basic.ADD_EFFECT_DMG
+    if (dmg < 0) then
+        message = tpz.msg.basic.ADD_EFFECT_HEAL
+        dmg = target:addHP(-dmg)
+    end
+
+    return tpz.subEffect.LIGHT_DAMAGE, message, dmg
+end
