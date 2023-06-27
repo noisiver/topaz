2f8b131433fc3d0bb117aff06a2296c7b2e9d967
Shiyo <strikinginsanity@hotmail.com>
2023-06-27 17:20:47 -0400

    Debug

--- scripts/globals/weaponskills.lua
+++ scripts/globals/weaponskills.lua
@@ -1716,6 +1716,8 @@ end
 function GetMaxWeaponPdif(attacker)
     local weaponStyle = utils.getWeaponStyle(attacker)
     local weaponType = utils.GetWeaponType(attacker)
+    printf("weapon style %s", weaponStyle)
+    printf("weaponTyper %s", weaponType)
 
     if (weaponStyle == 'H2H') or (weaponType == 'GREAT KATANA') then
         return 2.1
