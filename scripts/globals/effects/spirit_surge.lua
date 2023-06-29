0e51f5c78642ba5ed19ec1cf6e7d35f535ca0a9d
Shiyo <strikinginsanity@hotmail.com>
2023-06-29 17:45:58 -0400

    fixes

--- scripts/globals/effects/spirit_surge.lua
+++ scripts/globals/effects/spirit_surge.lua
@@ -18,8 +18,8 @@ function onEffectGain(target, effect)
         target:addMod(tpz.mod.CRITHITRATE, effect:getPower())
         target:addMod(tpz.mod.CRIT_DMG_INCREASE, effect:getPower())
         target:addMod(tpz.mod.ALL_WSDMG_ALL_HITS, effect:getPower())
+        target:addMod(tpz.mod.WYVERN_BREATH, 100)
         target:addPetMod(tpz.mod.ATTP, 45)
-        target:addPetMod(tpz.mod.WYVERN_BREATH, 100)
     end
 end
 
@@ -39,7 +39,7 @@ function onEffectLose(target, effect)
         target:delMod(tpz.mod.CRITHITRATE, effect:getPower())
         target:delMod(tpz.mod.CRIT_DMG_INCREASE, effect:getPower())
         target:delMod(tpz.mod.ALL_WSDMG_ALL_HITS, effect:getPower())
+        target:delMod(tpz.mod.WYVERN_BREATH, 100)
         target:delPetMod(tpz.mod.ATTP, 45)
-        target:delPetMod(tpz.mod.WYVERN_BREATH, 100)
     end
 end
