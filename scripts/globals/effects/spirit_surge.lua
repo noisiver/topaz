ced39dddba756d361d7723a7f71aae4a605c3cb3
Shiyo <strikinginsanity@hotmail.com>
2023-06-29 17:40:11 -0400

    (DRG) Spirit Surge wyvern breath adjustment
    Should now properly buff wyvern breath using tpz.mod.WYVERN_BREATH

--- scripts/globals/effects/spirit_surge.lua
+++ scripts/globals/effects/spirit_surge.lua
@@ -19,7 +19,7 @@ function onEffectGain(target, effect)
         target:addMod(tpz.mod.CRIT_DMG_INCREASE, effect:getPower())
         target:addMod(tpz.mod.ALL_WSDMG_ALL_HITS, effect:getPower())
         target:addPetMod(tpz.mod.ATTP, 45)
-        target:addPetMod(tpz.mod.MATT, 45)
+        target:addPetMod(tpz.mod.WYVERN_BREATH, 100)
     end
 end
 
@@ -40,6 +40,6 @@ function onEffectLose(target, effect)
         target:delMod(tpz.mod.CRIT_DMG_INCREASE, effect:getPower())
         target:delMod(tpz.mod.ALL_WSDMG_ALL_HITS, effect:getPower())
         target:delPetMod(tpz.mod.ATTP, 45)
-        target:delPetMod(tpz.mod.MATT, 45)
+        target:delPetMod(tpz.mod.WYVERN_BREATH, 100)
     end
 end
