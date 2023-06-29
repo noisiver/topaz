553ec7c26c2c38c024056499c8ca32e776c35079
Shiyo <strikinginsanity@hotmail.com>
2023-06-29 19:43:38 -0400

    Fixes

--- scripts/globals/mobskills/optic_induration.lua
+++ scripts/globals/mobskills/optic_induration.lua
@@ -42,6 +42,6 @@ function onMobWeaponSkill(target, mob, skill)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING, info.hitslanded)
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.SLASHING)
 	if ((skill:getMsg() ~= tpz.msg.basic.SHADOW_ABSORB) and (dmg > 0)) then   target:tryInterruptSpell(mob, info.hitslanded) end
-    MobPhysicalStatusEffectMoveSub(mob, target, typeEffect, 1, 0, 30, 0, 0, 0)
+    MobPhysicalStatusEffectMoveSub(mob, target, skill, typeEffect, 1, 0, 30, 0, 0, 0)
     return dmg
 end
--- scripts/globals/mobskills/rock_smash.lua
+++ scripts/globals/mobskills/rock_smash.lua
@@ -41,7 +41,7 @@ function onMobWeaponSkill(target, mob, skill)
     local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT, params_phys)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT, info.hitslanded)
     target:takeDamage(dmg, mob, tpz.attackType.PHYSICAL, tpz.damageType.BLUNT)
-    MobPhysicalStatusEffectMoveSub(mob, target, typeEffect, 1, 0, 15, 0, 0, 0)
+    MobPhysicalStatusEffectMoveSub(mob, target, skill, typeEffect, 1, 0, 15, 0, 0, 0)
 	if ((skill:getMsg() ~= tpz.msg.basic.SHADOW_ABSORB) and (dmg > 0)) then   target:tryInterruptSpell(mob, info.hitslanded) end
     return dmg
 end
