4a82385ac12aee60b64b7851de142ec6f8da768c
Shiyo <strikinginsanity@hotmail.com>
2023-06-29 21:34:40 -0400

    (DRG) Spirit Surge breath bonus tuning

--- scripts/globals/effects/spirit_surge.lua
+++ scripts/globals/effects/spirit_surge.lua
@@ -18,7 +18,7 @@ function onEffectGain(target, effect)
         target:addMod(tpz.mod.CRITHITRATE, effect:getPower())
         target:addMod(tpz.mod.CRIT_DMG_INCREASE, effect:getPower())
         target:addMod(tpz.mod.ALL_WSDMG_ALL_HITS, effect:getPower())
-        target:addMod(tpz.mod.WYVERN_BREATH, 100)
+        target:addMod(tpz.mod.WYVERN_BREATH, 25)
         target:addPetMod(tpz.mod.ATTP, 45)
     end
 end
@@ -39,7 +39,7 @@ function onEffectLose(target, effect)
         target:delMod(tpz.mod.CRITHITRATE, effect:getPower())
         target:delMod(tpz.mod.CRIT_DMG_INCREASE, effect:getPower())
         target:delMod(tpz.mod.ALL_WSDMG_ALL_HITS, effect:getPower())
-        target:delMod(tpz.mod.WYVERN_BREATH, 100)
+        target:delMod(tpz.mod.WYVERN_BREATH, 25)
         target:delPetMod(tpz.mod.ATTP, 45)
     end
 end
