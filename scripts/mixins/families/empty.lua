8e73099615dafa50c1f57a3f281cf2cad8bb663b
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-07-26 10:20:58 -0400

    (TEST utils.getDropRate)
    Now math.randoms the correct amount (10000)

--- scripts/mixins/families/empty.lua
+++ scripts/mixins/families/empty.lua
@@ -14,57 +14,57 @@ g_mixins.families.empty = function(mob)
 
         -- printf("Drop Rate: %s", dropRate)
         if element == 8  then
-		if math.random(1,100) <= utils.getDropRate(mob, 2400) then 
+		if math.random(1,10000) <= utils.getDropRate(mob, 2400) then 
             killer:addTreasure(1613, mob) -- Malevolent Memory (darkness)
 		end
         elseif element == 6 then
-			if math.random(1,100) <= utils.getDropRate(mob, 2400) then 
+			if math.random(1,10000) <= utils.getDropRate(mob, 2400) then 
 				killer:addTreasure(1611, mob) -- Somber Memory (water)
 			end
-            if math.random(1,100) <= utils.getDropRate(mob, 2400) then
+            if math.random(1,10000) <= utils.getDropRate(mob, 2400) then
                 killer:addTreasure(1613) -- Malevolent Memory (darkness)
             end
         elseif element == 5 then
-			if math.random(1,100) <= utils.getDropRate(mob, 2400) then 
+			if math.random(1,10000) <= utils.getDropRate(mob, 2400) then 
 				killer:addTreasure(1610, mob) -- Startling Memory (lightning)
 			end
-            if math.random(1,100) <= utils.getDropRate(mob, 2400) then 
+            if math.random(1,10000) <= utils.getDropRate(mob, 2400) then 
                 killer:addTreasure(1612, mob) -- Radiant Memory (light)
             end
         elseif element == 4  then
-			if math.random(1,100) <= utils.getDropRate(mob, 2400) then 
+			if math.random(1,10000) <= utils.getDropRate(mob, 2400) then 
 				killer:addTreasure(1609, mob) -- Profane Memory (earth)
 			end
-			if math.random(1,100) <= utils.getDropRate(mob, 2400) then 
+			if math.random(1,10000) <= utils.getDropRate(mob, 2400) then 
 				killer:addTreasure(1613, mob) -- Malevolent Memory (darkness)
 			end
         elseif element == 7 then
-			if math.random(1,100) <= utils.getDropRate(mob, 2400) then 
+			if math.random(1,10000) <= utils.getDropRate(mob, 2400) then 
 				killer:addTreasure(1612, mob) -- Radiant Memory (light)
 			end
         elseif element == 1  then
-			if math.random(1,100) <= utils.getDropRate(mob, 2400) then 
+			if math.random(1,10000) <= utils.getDropRate(mob, 2400) then 
 				killer:addTreasure(1606, mob) -- Burning Memory (fire)
 			end
-			if math.random(1,100) <= utils.getDropRate(mob, 2400) then 
+			if math.random(1,10000) <= utils.getDropRate(mob, 2400) then 
 				killer:addTreasure(1612, mob) -- Radiant Memory (light)
 			end
         elseif element == 2 then
-			if math.random(1,100) <= utils.getDropRate(mob, 2400) then 
+			if math.random(1,10000) <= utils.getDropRate(mob, 2400) then 
 				killer:addTreasure(1607, mob) -- Bitter Memory (ice)
 			end
-			if math.random(1,100) <= utils.getDropRate(mob, 2400) then 
+			if math.random(1,10000) <= utils.getDropRate(mob, 2400) then 
 				killer:addTreasure(1613, mob) -- Malevolent Memory (darkness)
 			end
         elseif element == 3 then
-			if math.random(1,100) <= utils.getDropRate(mob, 2400) then 
+			if math.random(1,10000) <= utils.getDropRate(mob, 2400) then 
 				killer:addTreasure(1608, mob) -- Fleeting Memory (wind)
 			end
-			if math.random(1,100) <= utils.getDropRate(mob, 2400) then 
+			if math.random(1,10000) <= utils.getDropRate(mob, 2400) then 
 				killer:addTreasure(1612, mob) -- Radiant Memory (light)
 			end
         end
-        if math.random(1,100) <= utils.getDropRate(mob, 1500) then
+        if math.random(1,10000) <= utils.getDropRate(mob, 1500) then
             killer:addTreasure(math.random(1687, 1689), mob) -- Recolection of x(Anima items)
         end
     end)
