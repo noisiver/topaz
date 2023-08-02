516850957ffc7d0af72a3406f9dc5006365ec7eb
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-08-02 10:27:01 -0400

    (TEST Mob Pets) Stats / spell list
    Mob pets were overwriting their stat/spell calculations from petutils and getting the wrong spell list and stats. That should be corrected now.

--- src/map/entities/mobentity.cpp
+++ src/map/entities/mobentity.cpp
@@ -748,8 +748,12 @@ void CMobEntity::Spawn()
     SetMLevel(level);
     SetSLevel(level);//calculated in function
 
-    mobutils::CalculateMobStats(this, true);
-    mobutils::GetAvailableSpells(this);
+    // Don't calculate spells/stats again if a monster pet, they've already been calculated in petutils.cpp
+    if (objtype != TYPE_PET)
+    {
+        mobutils::CalculateMobStats(this, true);
+        mobutils::GetAvailableSpells(this);
+    }
 
     // spawn somewhere around my point
     loc.p = m_SpawnPoint;
