21f8ef4c52b3252de32d1dff034804b78a53b247
Shiyo <strikinginsanity@hotmail.com>
2023-08-10 16:56:09 -0400

    Revert "(TEST Pets Despawn)"
    This reverts commit 9bb53f7e72d440f0295f156fa259781973145e85.

--- src/map/entities/petentity.cpp
+++ src/map/entities/petentity.cpp
@@ -202,7 +202,7 @@ void CPetEntity::FadeOut()
 void CPetEntity::Die()
 {
     PAI->ClearStateStack();
-    PAI->Internal_Die(2500ms);
+    PAI->Internal_Die(0s);
     m_unkillable = false;
     
     if ((PAI != nullptr) && (PAI->GetController() != nullptr))
