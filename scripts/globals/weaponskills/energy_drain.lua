fe6d6b81b1602995f8f86226e4713335cbe63d31
Shiyo <strikinginsanity@hotmail.com>
2023-08-11 16:44:07 -0400

    (Debug) Off

--- scripts/globals/weaponskills/energy_drain.lua
+++ scripts/globals/weaponskills/energy_drain.lua
@@ -11,7 +11,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local fTPAnchors = { 1.25, 2.5, 4.125 }
 
     local startingAnchor = math.floor(tp / 1000)
-    player:PrintToPlayer(string.format( "startingAnchor %i.", startingAnchor))
+    
     local multiplier = 0
 
     if tp >= 3000 then
@@ -23,19 +23,13 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
         -- TP = 1250; multiplier = 1.25 + ( (2.5 - 1.25) / 1000 * (1250 - (1000 * 1))
         --            multiplier = 1.25 + (1.25 / 1000) * 250)
         --            multiplier = 1.25 + 0.3125 = 1.5625
-        player:PrintToPlayer(string.format( "basefTP %i.", basefTP))
-        player:PrintToPlayer(string.format( "nextfTP %i.", nextfTP))
-        player:PrintToPlayer(string.format( "multPerTP %i.", multPerTP))
         multiplier = basefTP + multPerTP
-        player:PrintToPlayer(string.format( "multiplier %i.", multiplier))
     end
 
     local skill = player:getSkillLevel(tpz.skill.DAGGER)
     local wsc   = player:getStat(tpz.mod.MND) * 1.0
-    player:PrintToPlayer(string.format( "skill %i.", skill))
-    player:PrintToPlayer(string.format( "wsc %i.", wsc))
     local mpRestored = math.floor((math.floor(skill * 0.11) + wsc) * multiplier)
-    player:PrintToPlayer(string.format( "mpRestored %i.", mpRestored))
+
     if target:isUndead() then
         mpRestored = 0
     else
