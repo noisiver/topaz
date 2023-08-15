a9b408e7fac09ddffefd4174bb986035c08d9b14
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-08-15 11:07:48 -0400

    (WHM) Mystic Boon update
    Now applies Stoneskin instead of Regen to nearby allies.
    Formula is 200 + MND

--- scripts/globals/weaponskills/mystic_boon.lua
+++ scripts/globals/weaponskills/mystic_boon.lua
@@ -42,7 +42,8 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
 
     local party = player:getParty()
     local healAmount = math.floor(damage / 2)
-    local regenAmount = math.floor(player:getMainLvl() / 4)
+    local MND = player:getStat(tpz.mod.MND)
+    local stoneskinAmount = 250 + MND
 
     if party ~= nil then
         for _,member in ipairs(party) do
@@ -52,8 +53,8 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
             member:addHP(healAmount)
             player:updateEnmityFromCure(member, healAmount)
             member:removeAllNegativeEffects()
-            if not member:hasStatusEffect(tpz.effect.REGEN) then
-                member:addStatusEffect(tpz.effect.REGEN, regenAmount, 3, 30)
+            if not member:hasStatusEffect(tpz.effect.STONESKIN) then
+                member:addStatusEffect(tpz.effect.STONESKIN, stoneskinAmount, 3, 30)
             end
             if not member:hasStatusEffect(tpz.effect.tpz.effect.MAGIC_DEF_BOOST) then
                 member:addStatusEffect(tpz.effect.tpz.effect.MAGIC_DEF_BOOST, 25, 0, 60)
