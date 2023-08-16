1b2bb96769732d06b73b6b6fc5e041f49305fce3
Shiyo <strikinginsanity@hotmail.com>
2023-08-16 00:10:57 -0400

    (Debug)

--- scripts/zones/Cape_Teriggan/mobs/Greater_Manticore.lua
+++ scripts/zones/Cape_Teriggan/mobs/Greater_Manticore.lua
@@ -31,19 +31,19 @@ function onMobFight(mob, target)
             if mob:getCurrentAction() ~= tpz.action.MOBABILITY_START and mob:getCurrentAction() ~= tpz.action.MOBABILITY_USING and
                 mob:actionQueueEmpty() and (tp < 1000) then
                 if (hpp <= 69) and (prisonUses == 0) then
-                    prisonTarget = enmity.entity:getID()
+                    prisonTarget = enmity.entity:getID() -- TODO: Randomize target somehow...
                     mob:setLocalVar("prisonUses", 1)
                     ForceDrawIn(mob, prisonTarget)
                     GetPlayerByID(prisonTarget):addStatusEffect(tpz.effect.BIND, 1, 0, 10)
                     mob:useMobAbility(486, GetPlayerByID(prisonTarget)) -- Whip Tongue
                 elseif (hpp <= 39) and (prisonUses == 1) then
-                    prisonTarget = enmity[math.random(#enmityList)]:getID()
+                    prisonTarget = enmity.entity[math.random(#enmityList)] -- Not a valid target
                     mob:setLocalVar("prisonUses", 2)
                     ForceDrawIn(mob, prisonTarget)
                     GetPlayerByID(prisonTarget):addStatusEffect(tpz.effect.BIND, 1, 0, 10)
                     mob:useMobAbility(486, GetPlayerByID(prisonTarget)) -- Whip Tongue
                 elseif (hpp <= 19) and (prisonUses == 2) then
-                    prisonTarget = enmity.entity[math.random(#enmityList)]
+                    prisonTarget = enmity.entity[math.random(#enmityList)]-- Not a valid target
                     mob:setLocalVar("prisonUses", 3)
                     ForceDrawIn(mob, prisonTarget)
                     GetPlayerByID(prisonTarget):addStatusEffect(tpz.effect.BIND, 1, 0, 10)
