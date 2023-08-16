4db1cebe8bb2de04cdcfc9f986c80994c059044c
Shiyo <strikinginsanity@hotmail.com>
2023-08-16 00:01:51 -0400

    (debugging)

--- scripts/zones/Cape_Teriggan/mobs/Greater_Manticore.lua
+++ scripts/zones/Cape_Teriggan/mobs/Greater_Manticore.lua
@@ -29,21 +29,21 @@ function onMobFight(mob, target)
     for _, enmity in ipairs(enmityList) do
         if enmityList and #enmityList > 0 then
             if mob:getCurrentAction() ~= tpz.action.MOBABILITY_START and mob:getCurrentAction() ~= tpz.action.MOBABILITY_USING and
-                mob:actionQueueEmpty() and (tp < 1000) then 
+                mob:actionQueueEmpty() and (tp < 1000) then
                 if (hpp <= 69) and (prisonUses == 0) then
-                    prisonTarget = enmityList.entity[math.random(#enmityList)]
+                    prisonTarget = enmity.entity[math.random(#enmityList)]
                     mob:setLocalVar("prisonUses", 1)
                     ForceDrawIn(mob, prisonTarget)
                     GetPlayerByID(prisonTarget):addStatusEffect(tpz.effect.BIND, 1, 0, 10)
                     mob:useMobAbility(486, GetPlayerByID(prisonTarget)) -- Whip Tongue
                 elseif (hpp <= 39) and (prisonUses == 1) then
-                    prisonTarget = enmityList.entity[math.random(#enmityList)]
+                    prisonTarget = enmity.entity[math.random(#enmityList)]
                     mob:setLocalVar("prisonUses", 2)
                     ForceDrawIn(mob, prisonTarget)
                     GetPlayerByID(prisonTarget):addStatusEffect(tpz.effect.BIND, 1, 0, 10)
                     mob:useMobAbility(486, GetPlayerByID(prisonTarget)) -- Whip Tongue
                 elseif (hpp <= 19) and (prisonUses == 2) then
-                    prisonTarget = enmityList.entity[math.random(#enmityList)]
+                    prisonTarget = enmity.entity[math.random(#enmityList)]
                     mob:setLocalVar("prisonUses", 3)
                     ForceDrawIn(mob, prisonTarget)
                     GetPlayerByID(prisonTarget):addStatusEffect(tpz.effect.BIND, 1, 0, 10)
