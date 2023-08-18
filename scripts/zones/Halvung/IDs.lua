ef3d1d5c2daaef0f0d7686bbf9a82a7590163d8d
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-08-18 13:07:05 -0400

    (Zonemsgs) Halvung

--- scripts/zones/Halvung/IDs.lua
+++ scripts/zones/Halvung/IDs.lua
@@ -21,21 +21,15 @@ zones[tpz.zone.HALVUNG] =
         GURFURLUR_DOOR_KEYS     = 7901, -- The <key> shatters... The <key> shatters... The <key> shatters...
         MINING_IS_POSSIBLE_HERE = 7923, -- Mining is possible here if you have <item>.
         BLUE_FLAMES             = 7962, -- You can see blue flames flickering from a hole in the ground here...
-        OPERATING_LEVER_START   = 7894, -- <player> begins pushing on the lever!
-        OPERATING_LEVER_FAIL    = 7897, -- <player> releases the lever.
-        OPERATING_LEVER_SUCCESS = 8040, -- You lift the lever with all your might!
-		
-        -- NOTHING_HAPPENS         = 119, -- Nothing happens...
-        -- ITEM_CANNOT_BE_OBTAINED = 6382, -- You cannot obtain the <item>. Come back after sorting your inventory.
-        -- ITEM_OBTAINED           = 6388, -- Obtained: <item>.
-        -- GIL_OBTAINED            = 6389, -- Obtained <number> gil.
-        -- KEYITEM_OBTAINED        = 6391, -- Obtained key item: <keyitem>.
-        -- CARRIED_OVER_POINTS     = 6999, -- You have carried over <number> login point[/s].
-        -- LOGIN_CAMPAIGN_UNDERWAY = 7000, -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!<space>
-        -- LOGIN_NUMBER            = 7001, -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
-        -- MINING_IS_POSSIBLE_HERE = 7924, -- Mining is possible here if you have <item>.
-        -- BLUE_FLAMES             = 7963, -- You can see blue flames flickering from a hole in the ground here...
-        -- COMMON_SENSE_SURVIVAL   = 8102, -- It appears that you have arrived at a new survival guide provided by the Adventurers' Mutual Aid Network. Common sense dictates that you should now be able to teleport here from similar tomes throughout the world.
+        OPERATING_LEVER_START   = 7895, -- <player> begins pushing on the lever!
+        OPERATING_LEVER_FAIL    = 7898, -- <player> releases the lever.
+        OPERATING_LEVER_SUCCESS = 8041, -- You lift the lever with all your might!
+		CARRIED_OVER_POINTS     = 6999, -- You have carried over <number> login point[/s].
+        LOGIN_CAMPAIGN_UNDERWAY = 7000, -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!<space>
+        LOGIN_NUMBER            = 7001, -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
+        MINING_IS_POSSIBLE_HERE = 7924, -- Mining is possible here if you have <item>.
+        BLUE_FLAMES             = 7963, -- You can see blue flames flickering from a hole in the ground here...
+        COMMON_SENSE_SURVIVAL   = 8102, -- It appears that you have arrived at a new survival guide provided by the Adventurers' Mutual Aid Network. Common sense dictates that you should now be able to teleport here from similar tomes throughout the world.
     },
     mob =
     {
