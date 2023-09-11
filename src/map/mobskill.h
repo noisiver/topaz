a619d6f94ce346ead472610b6032c7b2a2a57ee2
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-09-11 12:42:46 -0400

    (TEST Auto-attack TP Moves)
    use SKILLFLAG_REPLACE_ATTACK for "auto-attack" TP moves.

--- src/map/mobskill.h
+++ src/map/mobskill.h
@@ -28,13 +28,13 @@
 enum SKILLFLAG
 {
     SKILLFLAG_NONE           = 0x000,
-    SKILLFLAG_JOB_ABILITY    = 0x001,
-    SKILLFLAG_TWO_HOUR       = 0x002,
+    SKILLFLAG_JOB_ABILITY    = 0x001, // 1
+    SKILLFLAG_TWO_HOUR       = 0x002, // 2
     // Special skill (ranged attack / call beast)
-    SKILLFLAG_SPECIAL        = 0x004,
-    SKILLFLAG_HIT_ALL        = 0x008,
-    SKILLFLAG_REPLACE_ATTACK = 0x010,
-    SKILLFLAG_DRAW_IN        = 0x020
+    SKILLFLAG_SPECIAL        = 0x004, // 4
+    SKILLFLAG_HIT_ALL        = 0x008, // 8
+    SKILLFLAG_REPLACE_ATTACK = 0x010, // 16
+    SKILLFLAG_DRAW_IN        = 0x020 // 32
 };
 
 #define MAX_MOBSKILL_ID	4262
