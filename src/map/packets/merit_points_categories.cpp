0fafdc3c7f25ec29cb801f00803f365aa02ccb1f
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-09-22 11:15:52 -0400

    (TEST Merits) Geo / Rune fencer merits
    Might break merits

--- src/map/packets/merit_points_categories.cpp
+++ src/map/packets/merit_points_categories.cpp
@@ -65,7 +65,7 @@ struct
 CMeritPointsCategoriesPacket::CMeritPointsCategoriesPacket(CCharEntity* PChar)
 {
     this->type = 0x8C;
-	this->size = 0x80;
+	this->size = 0x100;
 
     ref<uint8>(0x04) = MAX_MERITS_IN_PACKET;
 
@@ -75,7 +75,7 @@ CMeritPointsCategoriesPacket::CMeritPointsCategoriesPacket(CCharEntity* PChar)
 
         PChar->pushPacket(new CBasicPacket(*this));
     }
-    MeritPointsCategoriesPacket(PChar, (uint8)(5 * MAX_MERITS_IN_PACKET));
+    MeritPointsCategoriesPacket(PChar, static_cast<uint8>(5 * MAX_MERITS_IN_PACKET));
 }
 
 /************************************************************************
@@ -87,7 +87,7 @@ CMeritPointsCategoriesPacket::CMeritPointsCategoriesPacket(CCharEntity* PChar)
 CMeritPointsCategoriesPacket::CMeritPointsCategoriesPacket(CCharEntity* PChar, MERIT_TYPE merit)
 {
     this->type = 0x8C;
-	this->size = 0x08;
+    this->size = 0x10;
 
     ref<uint8>(0x04) = 1;
     ref<uint32>(0x08) = PChar->PMeritPoints->GetMerit(merit)->data;
