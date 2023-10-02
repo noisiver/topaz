88202935f49436a1aa1d00aeb5642e9ab7970523
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-10-02 14:43:42 -0400

    fixes

--- scripts/globals/chocobo_digging.lua
+++ scripts/globals/chocobo_digging.lua
@@ -1022,15 +1022,21 @@ tpz.chocoboDig.start = function(player, precheck)
     end
 end
 
-local itemList =
-{
-    { body =  { tpz.items.PURPLE_RACING_SILKS } , material = { tpz.items.VOMP_CARROT }, reward = { tpz.items.GREEN_RACING_SILKS } },
-    { body =  { tpz.items.GREEN_RACING_SILKS } , material = { tpz.items.BUNCH_OF_SHARUG_GREENS }, reward = { tpz.items.BLUE_RACING_SILKS } },
-    { body =  { tpz.items.BLUE_RACING_SILKS } , material = { tpz.items.BALL_OF_VEGETABLE_PASTE }, reward = { tpz.items.RED_RACING_SILKS } },
-    { body =  { tpz.items.RED_RACING_SILKS } , material = { tpz.items.CLUMP_OF_TOKOPEKKO_WILDGRASS }, reward = { tpz.items.WHITE_RACING_SILKS } },
-    { body =  { tpz.items.WHITE_RACING_SILKS } , material = { tpz.items.GREGARIOUS_WORM }, reward = { tpz.items.BLACK_RACING_SILKS } },
-}
+tpz.chocoboDig.onTriggerNPC(player, npc)
+    local npcName = npc:getName()
 
+    for _, items in pairs(itemList) do
+        for v = tpz.inv.INVENTORY, tpz.inv.WARDROBE8 do -- inventory locations enums
+            if (player:hasItem(itemList.body[1], v)) then
+                local currentBody = itemList.body[1]:getName()
+                local newBody = itemList.reward[1]:getName()
+                local requiredMat = itemList.material[1]:getName()
+                player:PrintToPlayer("I will upgrade your " .. currentBody .. " to " .. newBody .. " if you provide me with " .. requiredMat .. " x100",0, npcName)
+                break
+            end
+        end
+    end
+end
 
 tpz.chocoboDig.onTradeNPC(player, npc, trade)
     local npcName = npc:getName()
