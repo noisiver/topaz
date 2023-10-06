2fa56fb5206dc7d052c15d6d082d5777aa308e06
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-10-06 14:26:21 -0400

    (JP)

--- scripts/globals/effects/camouflage.lua
+++ scripts/globals/effects/camouflage.lua
@@ -7,12 +7,18 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onEffectGain(target, effect)
+    local jpValue = target:getJobPointLevel(tpz.jp.CAMOUFLAGE_EFFECT)
+
     target:addMod(tpz.mod.ENMITY, -25)
+    target:addMod(tpz.mod.CRITHITRATE, jpValue)
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
+    local jpValue = target:getJobPointLevel(tpz.jp.CAMOUFLAGE_EFFECT)
+
     target:delMod(tpz.mod.ENMITY, -25)
+    target:delMod(tpz.mod.CRITHITRATE, jpValue)
 end
