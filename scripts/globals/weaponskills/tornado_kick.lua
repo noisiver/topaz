eaa312532a9ee4925a2b45626695d1506b8d9529
Shiyo <strikinginsanity@hotmail.com>
2023-10-10 20:46:58 -0400

    (WS) Tornado Kick
    Can now crit.
    fTP lowered to 1.0/1.0/1.0
    Was 1.5/1.7/2.0

--- scripts/globals/weaponskills/tornado_kick.lua
+++ scripts/globals/weaponskills/tornado_kick.lua
@@ -16,10 +16,10 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
 
     local params = {}
     params.numHits = 3
-    params.ftp100 = 1.5 params.ftp200 = 1.7 params.ftp300 = 2.0
+    params.ftp100 = 1.0 params.ftp200 = 1.0 params.ftp300 = 1.0
     params.str_wsc = 0.4 params.dex_wsc = 0.0 params.vit_wsc = 0.4 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
-    params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
-    params.canCrit = false
+    params.crit100 = 0.15 params.crit200 = 0.3 params.crit300 = 0.5
+    params.canCrit = true
     params.acc100 = 0.0 params.acc200= 0.0 params.acc300= 0.0
     params.atk100 = 1; params.atk200 = 1; params.atk300 = 1
     params.kick = true -- https://www.bluegartr.com/threads/112776-Dev-Tracker-Findings-Posts-%28NO-DISCUSSION%29?p=6712150&viewfull=1#post6712150
