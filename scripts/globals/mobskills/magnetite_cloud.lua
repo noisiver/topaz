c3e884b65719476a09eef1f4570e7c23f8ea48fe
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-10-18 12:36:04 -0400

    (TP Moves) Shadow logic

--- scripts/globals/mobskills/magnetite_cloud.lua
+++ scripts/globals/mobskills/magnetite_cloud.lua
@@ -18,7 +18,7 @@ function onMobWeaponSkill(target, mob, skill)
 
     local dmgmod = MobHPBasedMove(mob, target, 0.15, 1, tpz.magic.ele.EARTH, 400)
 
-    local dmg = MobFinalAdjustments(dmgmod, mob, skill, target, tpz.attackType.BREATH, tpz.damageType.EARTH, MOBPARAM_WIPE_SHADOWS)
+    local dmg = MobFinalAdjustments(dmgmod, mob, skill, target, tpz.attackType.BREATH, tpz.damageType.EARTH, MOBPARAM_IGNORE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.BREATH, tpz.damageType.EARTH)
     MobStatusEffectMove(mob, target, typeEffect, 75, 0, 300)
     return dmg
