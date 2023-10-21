72cbf8ca6e0d51a2494ccd080f40ad284333ae91
Shiyo <strikinginsanity@hotmail.com>
2023-10-21 18:47:34 -0400

    (DUO) CoP Diabolos tuning

--- sql/mob_groups.sql
+++ sql/mob_groups.sql
@@ -433,7 +433,7 @@ INSERT INTO `mob_groups` VALUES (67,2043,9,'Ice_Elemental',300,4,1347,0,0,84,86,
 -- The_Shrouded_Maw (Zone 10)
 -- ------------------------------------------------------------
 
-INSERT INTO `mob_groups` VALUES (1,1027,10,'Diabolos',0,128,0,6000,5000,54,55,0);
+INSERT INTO `mob_groups` VALUES (1,1027,10,'Diabolos',0,128,0,4500,5000,54,55,0);
 INSERT INTO `mob_groups` VALUES (2,5156,10,'Diremite',10,128,0,0,0,43,43,0);
 INSERT INTO `mob_groups` VALUES (3,3104,10,'Pasuk',0,128,0,6000,0,48,48,0);
 INSERT INTO `mob_groups` VALUES (4,5186,10,'Diabolos_Prime',0,128,3198,10000,0,82,82,0);
