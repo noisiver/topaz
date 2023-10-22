b57889996e5b79d12b41b3fbc2fd268143eae0a5
Shiyo <strikinginsanity@hotmail.com>
2023-10-22 17:27:02 -0400

    (COP) Vahzl fixes

--- scripts/zones/Spire_of_Vahzl/mobs/Agonizer.lua
+++ scripts/zones/Spire_of_Vahzl/mobs/Agonizer.lua
@@ -17,7 +17,7 @@ function onMobSpawn(mob)
     mob:setMobMod(tpz.mobMod.NO_DROPS, 1)
     mob:setMobMod(tpz.mobMod.EXP_BONUS, -100)
     mob:setMobMod(tpz.mobMod.GIL_MAX, -1)
-    mob:setMobMod(tpz.mobMod.ECOSYSTEMLINK, 0)
+    mob:setMobMod(tpz.mobMod.NO_LINK, 1)
 end
 
 function onMobEngaged(mob, target)
