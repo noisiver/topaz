fa992b52c2f8f57086e81954732be1beb6f0e3ba
Shiyo <strikinginsanity@hotmail.com>
2023-10-22 17:21:34 -0400

    (COP) Fixes

--- scripts/zones/Spire_of_Vahzl/mobs/Procreator.lua
+++ scripts/zones/Spire_of_Vahzl/mobs/Procreator.lua
@@ -18,6 +18,7 @@ function onMobSpawn(mob)
     mob:setMobMod(tpz.mobMod.NO_DROPS, 1)
     mob:setMobMod(tpz.mobMod.EXP_BONUS, -100)
     mob:setMobMod(tpz.mobMod.GIL_MAX, -1)
+    mob:setMobMod(tpz.mobMod.ECOSYSTEMLINK, 0)
 end
 
 function onMobEngaged(mob, target)
