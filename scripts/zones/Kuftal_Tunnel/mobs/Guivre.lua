b057fc79e17af2a598ff5d22542dfd025b62d65f
Shiyo <strikinginsanity@hotmail.com>
2023-10-28 18:12:00 -0400

    (Pathfinding) Guivre pathing fix

--- scripts/zones/Kuftal_Tunnel/mobs/Guivre.lua
+++ scripts/zones/Kuftal_Tunnel/mobs/Guivre.lua
@@ -5,185 +5,7 @@
 require("scripts/globals/pathfind")
 require("scripts/globals/status")
 
-local pathStart =
-{
-    { x = 102.00, y = -0.19, z = 3.00 }
-}
 
-local pathBranch1 =
-{
-    { x = 102.00, y = -0.19, z = 3.00 },
-    { x = 109.49, y = 0.59, z = -0.07 },
-    { x = 124.47, y = 0.00, z = -42.08 },
-    { x = 91.93, y = -5.15, z = -58.24 },
-    { x = 66.46, y = -8.81, z = -62.91 },
-    { x = 60.86, y = -9.02, z = -73.80 },
-    { x = 57.09, y = -9.37, z = -96.88 },
-    { x = 38.39, y = -8.62, z = -98.74 },
-    { x = 15.73, y = -1.02, z = -94.88 },
-    { x = -0.65, y = 0.00, z = -84.13 },
-}
-
-local pathBranch2 =
-{
-    { x = -40.24, y = 0.00, z = -38.35 },
-    { x = -44.36, y = 0.68, z = 13.98 },
-}
-
-local pathBranch3 =
-{
-    { x = -31.28, y = -0.23, z = 48.64 },
-    { x = -0.52, y = 0.00, z = 82.02 },
-}
-
-local pathBranch4 =
-{
-    { x = 3.06, y = 0.15, z = 125.56 },
-    { x = 40.12, y = 0.000, z = 158.89 },
-    { x = 60.35, y = -4.24, z = 143.23 },
-    { x = 79.86, y = -8.75, z = 139.89 },
-    { x = 85.86, y = -8.82, z = 138.11 },
-    { x = 91.87, y = -8.83, z = 142.52 },
-    { x = 101.60, y = -8.71, z = 143.17 },
-}
-
-local pathBranch5 =
-{
-    { x = 100.62, y = -8.75, z = 78.86 },
-    { x = 108.29, y = -4.60, z = 54.26 },
-    { x = 118.98, y = 0.00, z = 38.12 },
-    { x = 102.00, y = -0.19, z = 3.00 },
-}
-
-local pathFind =
-{
-    ['pathFind1'] = function(mob, reversePath)
-        local pathNodes = {}
-        if reversePath == 0 or reversePath == 1 then
-            mob:setLocalVar('mobPath', 2)
-            local reverseCheck = math.random(0, 2)
-            if reverseCheck == 0 then
-                mob:setLocalVar('reversePath', 0)
-                pathNodes = pathBranch1
-            else
-                mob:setLocalVar('reversePath', 1)
-                pathNodes = pathBranch5
-            end
-        end
-
-        return pathNodes
-    end,
-
-    ['pathFind2'] = function(mob, reversePath)
-        local pathNodes = pathBranch4
-        mob:setLocalVar('mobPath', 3)
-        if reversePath == 0 then
-            pathNodes = pathBranch2
-        end
-
-        return pathNodes
-    end,
-
-    ['pathFind3'] = function(mob, reversePath)
-        local pathNodes = {}
-        mob:setLocalVar('mobPath', 4)
-        if reversePath == 0 or reversePath == 1 then
-            pathNodes = pathBranch3
-        end
-
-        return pathNodes
-    end,
-
-    ['pathFind4'] = function(mob, reversePath)
-        local pathNodes = pathBranch4
-        if reversePath == 0 then
-            mob:setLocalVar('mobPath', 5)
-        else
-            local reverseCheck = math.random(0, 2)
-            if reverseCheck == 0 then
-                mob:setLocalVar('mobPath', 4)
-                mob:setLocalVar('reversePath', 0)
-                pathNodes = pathBranch3
-            else
-                mob:setLocalVar('mobPath', 5)
-                mob:setLocalVar('reversePath', 1)
-                pathNodes = pathBranch2
-            end
-        end
-
-        return pathNodes
-    end,
-
-    ['pathFind5'] = function(mob, reversePath)
-        local pathNodes = pathBranch1
-        mob:setLocalVar('mobPath', 6)
-
-        if reversePath == 0 then
-            local reverseCheck = math.random(0, 2)
-            if reverseCheck == 0 then
-                mob:setLocalVar('mobPath', 6)
-                mob:setLocalVar('reversePath', 0)
-                pathNodes = pathBranch5
-            else
-                mob:setLocalVar('mobPath', 3)
-                mob:setLocalVar('reversePath', 1)
-                pathNodes = pathBranch4
-            end
-        end
-
-        return pathNodes
-    end,
-
-    ['pathFind6'] = function(mob, reversePath)
-        local pathNodes = {}
-        if reversePath == 0 or reversePath == 1 then
-            mob:setLocalVar('mobPath', 1)
-            pathNodes = pathStart
-        end
-
-        return pathNodes
-    end,
-}
-
-function onPath(mob)
-   if not mob:isFollowingPath() then
-        if mob:getLocalVar('isPaused') ~= 0 then
-            local currentPath = 'pathFind' .. mob:getLocalVar('mobPath')
-            local reversePath = mob:getLocalVar('reversePath')
-
-            mob:setLocalVar('isPaused', 0)
-            mob:clearPath()
-
-            local pathNodes = pathFind[currentPath](mob, reversePath)
-
-            local newReverse = mob:getLocalVar('reversePath')
-            if newReverse == 0 then
-                mob:pathThrough(pathNodes, tpz.path.flag.COORDS)
-            else
-                mob:pathThrough(pathNodes, bit.bor(tpz.path.flag.COORDS, tpz.path.flag.REVERSE))
-            end
-        else
-            -- Guivre is paused, he will wait and rotate
-            -- a random amount of times before resuming his path
-            mob:clearPath()
-            local x = mob:getXPos()
-            local y = mob:getYPos()
-            local z = mob:getZPos()
-            local rotations = {}
-            local count = math.random(2, 6)
-            for i = 0, count do
-                local wait = math.random(1500, 3000)
-                rotations[i + 1] =
-                {
-                    x = x, y = y, z = z, rotation = math.random(0, 255), wait = wait
-                }
-            end
-
-            mob:pathThrough(rotations, tpz.path.flag.COORDS)
-            mob:setLocalVar('isPaused', 1)
-        end
-    end
-end
 
 function onMobSpawn(mob)
     mob:setDamage(125)
@@ -195,13 +17,40 @@ function onMobSpawn(mob)
     mob:setMobMod(tpz.mobMod.GIL_MIN, 18000)
     mob:setMobMod(tpz.mobMod.GIL_MAX, 18000) 
     mob:setMobMod(tpz.mobMod.GIL_BONUS, 0)
-    mob:setLocalVar('isPaused', 0)
-    mob:setLocalVar('mobPath', 1)
-    mob:pathThrough(pathStart, tpz.path.flag.COORDS)
-    onPath(mob)
+    mob:pathTo(101, 0, -4, tpz.path.flag.NONE)
+    mob:setLocalVar("path", 1)
 end
 
 function onMobRoam(mob)
+    local Pos = mob:getPos()
+    local path = mob:getLocalVar("path")
+
+    if not mob:isFollowingPath() then
+        if (path == 1) then
+            mob:setLocalVar("path", 2)
+            mob:pathTo(71, -8, -56, tpz.path.flag.NONE)
+        end
+
+        if (path == 2) then
+            mob:setLocalVar("path", 3)
+            mob:pathTo(40, -8, -98, tpz.path.flag.NONE)
+        end
+
+        if (path == 3) then
+            mob:setLocalVar("path", 4)
+            mob:pathTo(-45, 0, 11, tpz.path.flag.NONE)
+        end
+
+        if (path == 4) then
+            mob:setLocalVar("path", 5)
+            mob:pathTo(6, 0, 72, tpz.path.flag.NONE)
+        end
+
+        if (path == 5) then
+            mob:setLocalVar("path", 1)  
+            mob:pathTo(101, 0, -4, tpz.path.flag.NONE)
+        end
+    end
 end
 
 function onMobDeath(mob, player, isKiller, noKiller)
