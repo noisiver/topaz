ca750c995e48cb81958c4cdc121f23eea1b4fe04
Shiyo <strikinginsanity@hotmail.com>
2023-11-04 12:24:48 -0400

    (WS) Expiacion tuning
    Reduced ftp scaling to 2..0/2.2/2.5 (was 2.5/2.7/3.0)

--- scripts/globals/weaponskills/expiacion.lua
+++ scripts/globals/weaponskills/expiacion.lua
@@ -20,7 +20,7 @@ require("scripts/globals/weaponskills")
 function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local params = {}
     params.numHits = 2
-    params.ftp100 = 2.5 params.ftp200 = 2.7 params.ftp300 = 3.0
+    params.ftp100 = 2.0 params.ftp200 = 2.2 params.ftp300 = 2.5
     params.str_wsc = 0.4 params.dex_wsc = 0.4 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0
     params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
