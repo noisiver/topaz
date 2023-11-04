c1a21417f956468304f4011f74c3dc3fc77a499c
Shiyo <strikinginsanity@hotmail.com>
2023-11-04 15:31:22 -0400

    (WS Tuning
    Expiacion fTP no longer scales with TP.
    Always 2.0, attack scales, +50/75/100%

--- scripts/globals/weaponskills/expiacion.lua
+++ scripts/globals/weaponskills/expiacion.lua
@@ -20,13 +20,13 @@ require("scripts/globals/weaponskills")
 function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local params = {}
     params.numHits = 2
-    params.ftp100 = 2.0 params.ftp200 = 2.2 params.ftp300 = 2.5
+    params.ftp100 = 2.0 params.ftp200 = 2.0 params.ftp300 = 2.0
     params.str_wsc = 0.4 params.dex_wsc = 0.4 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0
     params.mnd_wsc = 0.0 params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
     params.acc100 = 0.0 params.acc200 = 0.0 params.acc300 = 0.0
-    params.atk100 = 1.5; params.atk200 = 1.5; params.atk300 = 1.5
+    params.atk100 = 1.5; params.atk200 = 1.75; params.atk300 = 2.0
 
     if USE_ADOULIN_WEAPON_SKILL_CHANGES then
         params.ftp100 = 3.75 params.ftp200 = 10.25 params.ftp300 = 12.5
