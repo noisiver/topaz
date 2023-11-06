80eee366470d7341c37b0c916d6db1156718b509
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-11-06 10:25:55 -0500

    (Curio Moogle) Lockpicking tools added to ammo

--- scripts/globals/shop.lua
+++ scripts/globals/shop.lua
@@ -307,6 +307,9 @@ tpz.shop =
                 6302,     700,      tpz.ki.RHAPSODY_IN_AZURE,   -- Fuma Shuriken Pouch
                 6303,    1000,      tpz.ki.RHAPSODY_IN_MAUVE,   -- Iga Shuriken Pouch
                 5870,    4752,      tpz.ki.RHAPSODY_IN_WHITE,   -- Trump Card Case
+                1022,    3643,      tpz.ki.RHAPSODY_IN_AZURE,   -- Thief's Tools
+                1023,    5520,      tpz.ki.RHAPSODY_IN_EMERALD, -- Living Key
+                1115,    6574,      tpz.ki.RHAPSODY_IN_MAUVE,   -- Skeleton Key
         },
         [curio.ninjutsuTools] =
         {
