0e027e6a5615c127b38cf7113b1336ffc7ed6815
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-11-08 10:35:45 -0500

    (Mobs) Linking to pets bugfix
    Checks TARGET PMaster isn't nil not it's own

--- src/map/ai/controllers/mob_controller.cpp
+++ src/map/ai/controllers/mob_controller.cpp
@@ -185,7 +185,7 @@ void CMobController::TryLink()
     }
 
     // Mobs shouldn't link to pets without master being engaged
-    if (PTarget->objtype == TYPE_PET && this->PTarget->PMaster != nullptr && !PTarget->PMaster->PAI->IsEngaged())
+    if (PTarget->objtype == TYPE_PET && PTarget->PMaster != nullptr && !PTarget->PMaster->PAI->IsEngaged())
     {
         // Make sure the mob isn't supposed to super link
         if (PMob->getMobMod(MOBMOD_SUPERLINK) == 0)
@@ -195,7 +195,7 @@ void CMobController::TryLink()
     }
 
     // Mobs shouldn't link to charmed pets
-    if (PTarget->objtype == TYPE_MOB && PTarget->isCharmed && this->PTarget->PMaster != nullptr && !PTarget->PMaster->PAI->IsEngaged())
+    if (PTarget->objtype == TYPE_MOB && PTarget->isCharmed && PTarget->PMaster != nullptr && !PTarget->PMaster->PAI->IsEngaged())
     {
         // Make sure the mob isn't supposed to super link
         if (PMob->getMobMod(MOBMOD_SUPERLINK) == 0)
