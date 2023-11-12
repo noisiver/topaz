a3c065cc04349f7987a46849644d2a6b6d6601d1
Shiyo <strikinginsanity@hotmail.com>
2023-11-12 13:47:10 -0500

    Typo

--- scripts/globals/abilities/steal.lua
+++ scripts/globals/abilities/steal.lua
@@ -114,7 +114,7 @@ function onUseAbility(player, target, ability, action)
         end
 
         -- No valid effects on target to steal
-        if (effect == tpz.effect.NONE)
+        if (effect == tpz.effect.NONE) then
             ability:setMsg(tpz.msg.basic.STEAL_FAIL) -- Failed to steal
             action:animation(target:getID(), 182)
         end
