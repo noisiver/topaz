b6c05d438f417817aed99a5ee1a9a3da1adb8a05
Shiyo <strikinginsanity@hotmail.com>
2023-11-12 14:23:14 -0500

    (Mobs) Tuning

--- scripts/globals/mobskills/flying_hip_press.lua
+++ scripts/globals/mobskills/flying_hip_press.lua
@@ -20,6 +20,10 @@ end
 
 function onMobWeaponSkill(target, mob, skill)
     local dmgmod = 2.0
+    local isMatman = mob:getPool() == 562
+    if isMatman then
+        dmgmod = 7.0
+    end
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, tpz.magic.ele.WIND, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.WIND, MOBPARAM_WIPE_SHADOWS)
     target:takeDamage(dmg, mob, tpz.attackType.MAGICAL, tpz.damageType.WIND)
