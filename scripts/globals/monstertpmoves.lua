3964132588150873050e7e8370ca0ffbaef19816
Shiyo <strikinginsanity@hotmail.com>
2023-11-12 23:21:46 -0500

    (TP Moves) isH2h check
    Added is H2H to MNK / PUP job check as well

--- scripts/globals/monstertpmoves.lua
+++ scripts/globals/monstertpmoves.lua
@@ -682,11 +682,9 @@ function MobFinalAdjustments(dmg, mob, skill, target, attackType, damageType, sh
 
     -- MNK and PUP mobs have a -50% end multiplier for wep damage and need to do 2x for physical moves to do proper damage
     local isH2H = mob:getWeaponSkillType(tpz.slot.MAIN) == tpz.skill.HAND_TO_HAND
-    if mob:getMainJob() == tpz.job.MNK or mob:getMainJob() == tpz.job.PUP then
-        if isH2H then
-            if (attackType == tpz.attackType.PHYSICAL) or (attackType == tpz.attackType.RANGED) then
-                dmg = dmg * 2
-            end
+    if mob:getMainJob() == tpz.job.MNK or mob:getMainJob() == tpz.job.PUP or isH2H then
+        if (attackType == tpz.attackType.PHYSICAL) or (attackType == tpz.attackType.RANGED) then
+            dmg = dmg * 2
         end
     end
 
