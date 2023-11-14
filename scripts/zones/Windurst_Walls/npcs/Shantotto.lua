32701ba571b8203fff0ef6131414ce4c646a4c02
Shiyo <strikinginsanity@hotmail.com>
2023-11-13 22:04:38 -0500

    (Quests) Remove day wait off curses coiled again quests

--- scripts/zones/Windurst_Walls/npcs/Shantotto.lua
+++ scripts/zones/Windurst_Walls/npcs/Shantotto.lua
@@ -122,13 +122,13 @@ function onTrigger(player, npc)
     elseif (foiledAgain == QUEST_COMPLETED and CFA2 == QUEST_AVAILABLE and CFAtimer == 0) then
         if player:getCharVar("DayWait[CFA1]") == gameDay then
             player:startEvent(174)
-         else
+        else
             player:startEvent(179)
         end
 
         -- Curses, Foiled...Again!?
     elseif (foiledAgain == QUEST_COMPLETED and CFA2 == QUEST_AVAILABLE and player:getFameLevel(WINDURST) >= 2 and
-        player:getMainLvl() >= 5 and CFAtimer == 1) then
+        player:getMainLvl() >= 5 and CFAtimer == 0) then
         player:startEvent(180, 0, 0, 0, 0, 928, 880, 17316, 940) -- Quest Start
     elseif (CFA2 == QUEST_ACCEPTED) then
         player:startEvent(181, 0, 0, 0, 0, 0, 0, 17316, 940) -- Reminder dialog
