a77557dae7ca00ce454eed0a704729f5a7bfefbb
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-11-14 10:00:30 -0500

    SPONTANEITY recast fix

--- scripts/globals/effects/spontaneity.lua
+++ scripts/globals/effects/spontaneity.lua
@@ -8,7 +8,7 @@ require("scripts/globals/status")
 
 function onEffectGain(target, effect)
     target:addMod(tpz.mod.UFASTCAST, 150)
-    effect:setFlag(tpz.effectFlag.MAGIC_BEGIN)
+    effect:setFlag(tpz.effectFlag.MAGIC_END)
 end
 
 function onEffectTick(target, effect)
