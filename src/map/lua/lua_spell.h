7a839be8dd4bda4e73e24354312aabdf9127f966
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-11-15 10:48:25 -0500

    (Lua) getSpellFamily() binding

--- src/map/lua/lua_spell.h
+++ src/map/lua/lua_spell.h
@@ -67,6 +67,7 @@ public:
     int32 getSkillType(lua_State*);
     int32 setSkillType(lua_State*);
     int32 getSpellGroup(lua_State*);
+    int32 getSpellFamily(lua_State*);
     int32 getFlag(lua_State*);
     int32 getRequirements(lua_State*);
     int32 setRequirements(lua_State*);
