40ba1950ae766df40abc4a33b7316f0d71d6cc35
ShiyoKozuki <strikinginsanity@hotmail.com>
2023-11-17 12:08:02 -0500

    (Mobs) Single Gear shouldnt have mixin

--- scripts/zones/Alzadaal_Undersea_Ruins/mobs/Archaic_Gear.lua
+++ scripts/zones/Alzadaal_Undersea_Ruins/mobs/Archaic_Gear.lua
@@ -3,7 +3,6 @@
 --  Mob: Archaic Gear
 -- !pos -19 -4 -153
 -----------------------------------
-mixins = {require("scripts/mixins/families/gears")}
 -----------------------------------
 function onMobDeath(mob, player, isKiller, noKiller)
 end
