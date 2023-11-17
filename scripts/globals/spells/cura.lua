8129025dbcfebc0257f73953de73c572fae33555
Shiyo <strikinginsanity@hotmail.com>
2023-11-17 18:55:52 -0500

    (WHM) Add bonus to Cura if Sacrosanctity is active

--- scripts/globals/spells/cura.lua
+++ scripts/globals/spells/cura.lua
@@ -95,12 +95,17 @@ function onSpellCast(caster, target, spell)
 
         --printf("BEFORE AFFLATUS MISERY BONUS: %d", basecure)
 
-        basecure = basecure + (misery * 2)
+        basecure = basecure + misery
 
         --printf("AFTER AFFLATUS MISERY BONUS: %d", basecure)
 
         --Afflatus Misery Mod Gets Used Up
         caster:setMod(tpz.mod.AFFLATUS_MISERY, 0)
+    end
+
+    -- Triple the healing if Sacrosanctity is active
+    if caster:hasStatusEffect(tpz.effect.SACROSANCTITY) then
+        basecure = basecure * 3
     end
 
     final = getCureFinal(caster, spell, basecure, minCure, false)
