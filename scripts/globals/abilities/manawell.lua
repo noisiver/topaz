ff413e466ae7676b6310cca5d3b73930052e0b77
Shiyo <strikinginsanity@hotmail.com>
2023-11-17 21:04:00 -0500

    (BLM) Manawell gravity undispellable

--- scripts/globals/abilities/manawell.lua
+++ scripts/globals/abilities/manawell.lua
@@ -17,4 +17,6 @@ function onUseAbility(player, target, ability)
     player:addStatusEffect(tpz.effect.WEIGHT, 95, 0, 30)
     player:addStatusEffect(tpz.effect.MUTE, 1, 0, 30)
     player:addStatusEffect(tpz.effect.MANAWELL, 1, 0, 30)
+    local effect = player:getStatusEffect(tpz.effect.WEIGHT)
+    effect:unsetFlag(tpz.effectFlag.WALTZABLE)
 end
