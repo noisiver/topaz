b069ec01a9d9990067c818b5f4c81a20a8dbebb6
Shiyo <strikinginsanity@hotmail.com>
2023-11-26 21:00:37 -0500

    (NMs) Hakutaku spawnable with all eyes in 1 trade

--- scripts/zones/Den_of_Rancor/npcs/qm1.lua
+++ scripts/zones/Den_of_Rancor/npcs/qm1.lua
@@ -8,7 +8,10 @@ require("scripts/globals/npc_util")
 -----------------------------------
 
 function onTrade(player, npc, trade)
+    -- Eye Cluster or all eyes
     if npcUtil.tradeHas(trade, 1298) and npcUtil.popFromQM(player, npc, ID.mob.HAKUTAKU) then
+        player:confirmTrade()
+    elseif npcUtil.tradeHas(trade, { 1288, 1289, 1290, 1291, 1292 }) and npcUtil.popFromQM(player, npc, ID.mob.HAKUTAKU) then
         player:confirmTrade()
     end
 end
