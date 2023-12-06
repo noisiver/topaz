ad3c537b8d24d0b66550c1779fd22f41afe61143
Shiyo <strikinginsanity@hotmail.com>
2023-12-06 15:50:42 -0500

    Revert "(ISNM) Clone immunity fix"
    This reverts commit 05a00a8bd6d6366aa2410bf28af16ab754053bde.

--- scripts/zones/Jade_Sepulcher/mobs/Phantom_Puk_Clone.lua
+++ scripts/zones/Jade_Sepulcher/mobs/Phantom_Puk_Clone.lua
@@ -12,9 +12,6 @@ function onMobSpawn(mob)
     mob:setMobMod(tpz.mobMod.EXP_BONUS, -100)
     mob:setMobMod(tpz.mobMod.GIL_MAX, -1)
     mob:setMobMod(tpz.mobMod.NO_DROPS, 1)
-    mob:delImmunity(tpz.immunity.SLEEP)
-    mob:delImmunity(tpz.immunity.LIGHTSLEEP)
-    mob:delImmunity(tpz.immunity.DARKSLEEP)
 end
 
 function onMobFight(mob, target)
